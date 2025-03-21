d6e3503d0ae10b24cd4ff34381ddc506ffe14e9b
dandiAW <dandiferrari75@gmail.com>
2025-03-21 13:30:46 +0800

    fix outbox

--- resources/js/Components/DataDisplay/Dashboard/Widget/UserSubscribe.vue
+++ resources/js/Components/DataDisplay/Dashboard/Widget/UserSubscribe.vue
@@ -131,9 +131,9 @@ const saveChanges = () => {
 	router.post(route(routeToSubmit.name, routeToSubmit.parameters), payload, {
 		preserveScroll: true,
 		onSuccess: (page) => {
-			if (page.props.widget) {
-				widgetItems.value = page.props.widget
-			}
+
+			widgetItems.value = page.props.showcase.outbox_subscribe.data
+			
 			notify({
 				title: trans("Succes"),
 				text: trans("Successfully attach"),
