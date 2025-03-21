6bf16827669d67efd4e15633518d2b12bbfc0631
Ganes556 <gustiganes@gmail.com>
2025-03-21 11:53:30 +0800

    fix problem validation

--- app/Actions/Comms/OutboxHasSubscribers/StoreManyOutboxHasSubscriber.php
+++ app/Actions/Comms/OutboxHasSubscribers/StoreManyOutboxHasSubscriber.php
@@ -23,6 +23,8 @@ class StoreManyOutboxHasSubscriber extends OrgAction
 {
     use WithNoStrictRules;
 
+    protected Outbox $outbox;
+
     public function handle(Outbox $outbox, array $modelData): Collection
     {
         $externalEmails = $modelData['external_emails'] ?? [];
@@ -56,6 +58,9 @@ class StoreManyOutboxHasSubscriber extends OrgAction
                 'exists:users,id',
                 function ($attribute, $value, $fail) {
                     $existingSubscribers = \DB::table('outbox_has_subscribers')
+                        ->when('organisation_id', $this->organisationId, function ($query) {
+                            return $query->where('organisation_id', $this->organisationId)->where('outbox_id', $this->outbox->id);
+                        })
                         ->whereIn('user_id', $value)
                         ->pluck('user_id')
                         ->toArray();
@@ -71,6 +76,9 @@ class StoreManyOutboxHasSubscriber extends OrgAction
                 'array',
                 function ($attribute, $value, $fail) {
                     $existingEmails = \DB::table('outbox_has_subscribers')
+                        ->when('organisation_id', $this->organisationId, function ($query) {
+                            return $query->where('organisation_id', $this->organisationId)->where('outbox_id', $this->outbox->id);
+                        })
                         ->whereIn('external_email', $value)
                         ->pluck('external_email')
                         ->toArray();
@@ -87,6 +95,7 @@ class StoreManyOutboxHasSubscriber extends OrgAction
 
     public function inFulfilment(Fulfilment $fulfilment, Outbox $outbox, ActionRequest $request)
     {
+        $this->outbox = $outbox;
         $this->initialisationFromFulfilment($fulfilment, $request);
 
         $this->handle($outbox, $this->validatedData);
