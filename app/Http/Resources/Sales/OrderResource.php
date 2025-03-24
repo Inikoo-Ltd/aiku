35b7cb822bb608fe857b65461ad212e7d0ed754f
KirinZero0 <aryapermana02@gmail.com>
2025-03-24 15:31:21 +0800

    Payment Showcase

--- app/Http/Resources/Sales/OrderResource.php
+++ app/Http/Resources/Sales/OrderResource.php
@@ -21,6 +21,7 @@ class OrderResource extends JsonResource
         return [
             'id'            => $order->id,
             'reference'     => $order->reference,
+            'slug'          => $order->slug,
             'state'         => $order->state->value,
             'state_label'   => $order->state->labels()[$order->state->value],
             'state_icon'    => $order->state->stateIcon()[$order->state->value],
