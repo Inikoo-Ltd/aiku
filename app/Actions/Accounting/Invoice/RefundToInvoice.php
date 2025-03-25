fc3154728e3366970e9829e53bd127f8a3999f97
Ganes556 <gustiganes@gmail.com>
2025-03-25 15:37:37 +0800

    clean up logic

--- app/Actions/Accounting/Invoice/RefundToInvoice.php
+++ app/Actions/Accounting/Invoice/RefundToInvoice.php
@@ -30,17 +30,16 @@ class RefundToInvoice extends OrgAction
     {
 
         $type = Arr::get($modelData, 'type_refund', 'payment');
-        $totalToPay = -abs(Arr::get($modelData, 'amount'));
+        $totalRefund = -abs(Arr::get($modelData, 'amount'));
 
 
         $refundsQuery = $invoice->refunds->where('in_process', false)->where('pay_status', InvoicePayStatusEnum::UNPAID);
 
         $refunds = $refundsQuery->sortByDesc('total_amount')->all();
 
-        $totalRefund = $refundsQuery->sum('total_amount');
-        $totalAlreadyRefunded = abs($totalRefund) - abs($refundsQuery->sum('payment_amount'));
+        $totalNeedToRefund = $refundsQuery->sum('total_amount') - $refundsQuery->sum('payment_amount');
 
-        if (abs(round($totalToPay, 2)) > abs(round($totalAlreadyRefunded, 2))) {
+        if (abs(round($totalRefund, 2)) > abs(round($totalNeedToRefund, 2))) {
             throw ValidationException::withMessages(
                 [
                     'message' => [
@@ -51,11 +50,11 @@ class RefundToInvoice extends OrgAction
         }
 
         foreach ($refunds as $refund) {
-            if ($totalRefund >= 0 || $totalToPay >= 0) {
+            if ($totalNeedToRefund >= 0 || $totalRefund >= 0) {
                 break;
             }
 
-            $amountPayPerRefund = max($totalToPay, $refund->total_amount);
+            $amountPayPerRefund = max($totalRefund, $refund->total_amount);
 
             $paymentInRefund = StorePayment::make()->action($invoice->customer, $paymentAccount, [
                 'amount' => $amountPayPerRefund,
@@ -75,8 +74,8 @@ class RefundToInvoice extends OrgAction
                 ]);
             }
 
+            $totalNeedToRefund -= $amountPayPerRefund;
             $totalRefund -= $amountPayPerRefund;
-            $totalToPay -= $amountPayPerRefund;
         }
 
         $paymentInInvoice = StorePayment::make()->action($invoice->customer, $paymentAccount, [
