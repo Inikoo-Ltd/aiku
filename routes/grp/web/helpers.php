74adf490ebe318f81b2eb0486fcf9036f17eea48
Artha <manager.arthachan@gmail.com>
2025-03-25 14:09:53 +0800

    fix: just for run the command and will be deleted soon

--- routes/grp/web/helpers.php
+++ routes/grp/web/helpers.php
@@ -8,9 +8,25 @@
 
 use App\Actions\Helpers\Upload\DownloadUploads;
 use App\Actions\Helpers\Upload\UI\ShowUpload;
+use App\Actions\SysAdmin\Group\Seeders\SeedWebBlockTypes;
+use App\Models\SysAdmin\Group;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('uploads/{upload}')->as('uploads.')->group(function () {
     Route::get('records', ShowUpload::class)->name('records.show');
     Route::get('download', DownloadUploads::class)->name('records.download');
+});
+
+\Illuminate\Support\Facades\Route::get('seed-web-block', function (\Illuminate\Http\Request $request) {
+    if ($request->get('code') == 'AW95') {
+        foreach (Group::all() as $group) {
+            SeedWebBlockTypes::run($group);
+
+            print_r("Seeding web block types for group: $group->name");
+        }
+
+        return "OK";
+    }
+
+    abort(404);
 });
