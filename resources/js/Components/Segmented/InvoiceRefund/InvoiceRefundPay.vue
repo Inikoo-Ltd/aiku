3092b9a68f6b8dc9bcf5cff4824f4fc9dac109a4
YudhistiraA <aryarajasa0@gmail.com>
2025-03-26 13:50:45 +0800

    fix refund

--- resources/js/Components/Segmented/InvoiceRefund/InvoiceRefundPay.vue
+++ resources/js/Components/Segmented/InvoiceRefund/InvoiceRefundPay.vue
@@ -296,9 +296,10 @@ const listPaymentRefund = computed(() => [
     {
         label: trans("Refund money to payment method of the invoice"),
         value: 'invoice_payment_method',
-        disable: Number(props.invoice_pay.total_need_to_refund_in_payment_method) == 0
+        disable: Number(props.invoice_pay.total_need_to_refund_in_payment_method) <= 0
     }
 ]);
+
 
 </script>
 
