1e059e09ac3d9e74f2a946a6c8cbd20731fbe2e3
YudhistiraA <aryarajasa0@gmail.com>
2025-03-28 10:25:51 +0800

    data structure seeder

--- app/Actions/Web/Webpage/UI/EditWebpage.php
+++ app/Actions/Web/Webpage/UI/EditWebpage.php
@@ -99,7 +99,7 @@ class EditWebpage extends OrgAction
                                     'value'     => $webpage->title,
                                     'required'  => true,
                                 ],    
-                                'url' => [
+                                /* 'url' => [
                                     'type'      => 'inputWithAddOn',
                                     'label'     => __('URL'),
                                     'label_no_capitalize' => true,
@@ -108,42 +108,28 @@ class EditWebpage extends OrgAction
                                     ],
                                     'value'     => $webpage->url,
                                     'required'  => true,
-                                ],    
+                                ],     */
                             ]
                         ],
                         [
-                            'label'  => __('Meta'),
+                            'label'  => __('SEO (Settings)'),
                             'icon'   => 'fab fa-google',
                             'fields' => [
-                                /* 'google_search' => [
+                                'google_search' => [
                                     'type'     => 'googleSearch',
                                     'domain'    => $webpage->website->domain . '/',
                                     'value'    => [
                                         'image'         => [    // TODO
                                             'original'  => 'https://socialsharepreview.com/api/image-proxy?url=https%3A%2F%2Fwww.zelolab.com%2Fwp-content%2Fuploads%2F2022%2F12%2Fhow-to-create-and-set-up-a-social-share-preview-image-on-your-website.jpg',
                                         ],
-                                        'seotitle'       => Arr::get($webpage->data, 'seotitle')       ?? '',
-                                        'seodescription' => Arr::get($webpage->data, 'seodescription') ?? '',
-                                        'seourl'         => Arr::get($webpage->data, 'seourl')         ?? '',
-                                        'redirecturl'    => implode(",", $redirectUrlArr) ?? '',
+                                        'seo_title'       => Arr::get($webpage->seo_data, 'seo_title')       ?? '',
+                                        'seo_description' => Arr::get($webpage->seo_data, 'seo_description') ?? '',
+                                        'url'             =>  $webpage->url,
+                                        'is_use_canonical_url' => $webpage->is_use_canonical_url,
+                                        'canonical_url'     => $webpage->canonical_url,
                                     ],
                                     'noTitle'  => true,
-                                ] */
-                                'meta_title' => [
-                                        'type'     => 'input',
-                                        'label'    => __('Meta title'),
-                                        'value'    => Arr::get($webpage->seo_data, 'meta_title')
-                                ],
-                                'meta_description' => [
-                                        'type'     => 'textarea',
-                                        'label'    => __('Meta description'),
-                                        'value'    => Arr::get($webpage->seo_data, 'meta_description')
-                                ],
-                                /* 'meta_keywords' => [
-                                        'type'     => 'textarea',
-                                        'label'    => __('Meta keywords'),
-                                        'value'    => Arr::get($webpage->seo_data, 'meta_keywords')
-                                ], */
+                                ]
                             ],  
                         ],
                         [
--- resources/js/Components/Forms/Fields/GoogleSearch.vue
+++ resources/js/Components/Forms/Fields/GoogleSearch.vue
@@ -34,12 +34,13 @@ const onFileSelect = (event: Event) => {
     reader.readAsDataURL(file);
 }
 
+console.log(props.form[props.fieldName])
 // console.log('======', props.form[props.fieldName])
 
 // const dummyValue = reactive({
 //     seourl: 'https://www.nenecourtemporium.co.uk/collections/frenchic-wall-paint',
-//     seotitle: 'Frenchic Wall paint - NEXT WORKING DAY Delivery',
-//     seodescription: 'Frenchic Wall Paint - Transform your space with the eco-friendly, luxurious choice for discerning decorators. Our high-quality, low-VOC paint provides a durable, elegannt finish, perfect for any room'
+//     seo_title: 'Frenchic Wall paint - NEXT WORKING DAY Delivery',
+//     seo_description: 'Frenchic Wall Paint - Transform your space with the eco-friendly, luxurious choice for discerning decorators. Our high-quality, low-VOC paint provides a durable, elegannt finish, perfect for any room'
 // })
 
 const selectedPreview = ref('facebook')
@@ -89,20 +90,20 @@ const selectedPreview = ref('facebook')
                 
                 <div class="px-4 pt-2 pb-3">
                     <div class="text-gray-500 mt-1 font-light text-xs uppercase">{{ fieldData?.domain || 'https://example.com' }}</div>
-                    <div class="text-gray-900 font-semibold text-lg leading-5">{{ form[fieldName].seotitle }}</div>
+                    <div class="text-gray-900 font-semibold text-lg leading-5">{{ form[fieldName].seo_title }}</div>
                 </div>
             </div>
 
             <!-- Preview: Google -->
             <div v-if="selectedPreview === 'google'" class="h-fit mx-auto mb-4 bg-white rounded-lg border border-gray-200 p-4">
                 <div class="text-blue-600 hover:text-blue-700 font-medium text-2xl text-ellipsis overflow-hidden">
-                    {{ form[fieldName].seotitle || 'Wall paint - Bight, Shine, Protection'}}
+                    {{ form[fieldName].seo_title || 'Wall paint - Bight, Shine, Protection'}}
                 </div>
                 <div class="text-green-700">
-                    {{ fieldData?.domain || 'https://example.com' }}{{ form[fieldName].seourl || '' }}
+                    {{ fieldData?.domain || 'https://example.com' }}{{ form[fieldName].url || '' }}
                 </div>
                 <div class="text-gray-600 break-words">
-                    {{ form[fieldName].seodescription || 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum inventore vitae quas accusamus, numquam quo aut. Vero modi nihil provident!' }}
+                    {{ form[fieldName].seo_description || 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum inventore vitae quas accusamus, numquam quo aut. Vero modi nihil provident!' }}
                 </div>
             </div>
 
@@ -113,7 +114,7 @@ const selectedPreview = ref('facebook')
                 </div>
 
                 <div class="px-4 pt-2 pb-3">
-                    <div class="text-gray-900 font-semibold text-lg leading-6 text-ellipsis overflow-hidden">{{ form[fieldName].seotitle }}</div>
+                    <div class="text-gray-900 font-semibold text-lg leading-6 text-ellipsis overflow-hidden">{{ form[fieldName].seo_title }}</div>
                     <div class="text-gray-500 mt-1 font-light text-xs tracking-wider">{{ fieldData?.domain || 'https://example.com' }}</div>
                 </div>
             </div>
@@ -125,7 +126,7 @@ const selectedPreview = ref('facebook')
                 </div>
 
                 <div class="absolute left-5 bottom-2 w-[93%]">
-                    <p class="bg-gray-900/40 text-white w-fit max-w-full px-1 rounded text-sm truncate">{{ form[fieldName].seotitle }}</p>
+                    <p class="bg-gray-900/40 text-white w-fit max-w-full px-1 rounded text-sm truncate">{{ form[fieldName].seo_title }}</p>
                 </div>
             </div>
         </div>
@@ -133,7 +134,7 @@ const selectedPreview = ref('facebook')
         <!-- Section: Field -->
         <div class="space-y-4 pt-4 border-t border-dashed border-gray-300">
             <div class="">
-                <label for="seotitle" class="text-gray-600 font-semibold cursor-pointer">Image</label>
+                <label for="seo_title" class="text-gray-600 font-semibold cursor-pointer">Image</label>
                 <div class="aspect-[1.91/1] max-h-56 max-w-96 mx-auto group relative rounded-md overflow-hidden border border-dashed border-gray-300">
                     <Image :src="form[fieldName].image" imageCover />
                     <div class="opacity-0 group-hover:opacity-100 absolute inset-0 hover:bg-gray-900/50 flex items-center justify-center">
@@ -143,36 +144,43 @@ const selectedPreview = ref('facebook')
             </div>
 
             <div>
-                <label for="seotitle" class="text-gray-600 font-semibold cursor-pointer">Title</label>
-                <PureInput v-model="form[fieldName].seotitle" inputName="seotitle" :maxLength="70" placeholder="Wall paint - Bight, Shine, Protection" />
-                <div class="mt-1 text-gray-500 italic tabular-nums">{{ form[fieldName].seotitle?.length || 0 }} of 70 characters used</div>
+                <label for="seo_title" class="text-gray-600 font-semibold cursor-pointer">Title</label>
+                <PureInput v-model="form[fieldName].seo_title" inputName="seo_title" :maxLength="70" placeholder="Wall paint - Bight, Shine, Protection" />
+                <div class="mt-1 text-gray-500 italic tabular-nums">{{ form[fieldName].seo_title?.length || 0 }} of 70 characters used</div>
             </div>
 
             <div>
-                <label for="seodescription" class="text-gray-600 font-semibold cursor-pointer">SEO Description</label>
-                <PureTextarea v-model="form[fieldName].seodescription" inputName="seodescription" :rows="6" maxLength="320" placeholder="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum inventore vitae quas accusamus, numquam quo aut. Vero modi nihil provident!" />
-                <div class="mt-1 text-gray-500 italic tabular-nums">{{ form[fieldName].seodescription?.length || 0 }} of 320 characters used</div>
+                <label for="seo_description" class="text-gray-600 font-semibold cursor-pointer">SEO Description</label>
+                <PureTextarea v-model="form[fieldName].seo_description" inputName="seo_description" :rows="6" maxLength="320" placeholder="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum inventore vitae quas accusamus, numquam quo aut. Vero modi nihil provident!" />
+                <div class="mt-1 text-gray-500 italic tabular-nums">{{ form[fieldName].seo_description?.length || 0 }} of 320 characters used</div>
             </div>
 
-            <div>
-                <label for="seoUrl" class="text-gray-600 font-semibold cursor-pointer">Chanonical URL</label>
-                <PureInput v-model="form[fieldName].seourl" inputName="seoUrl" placeholder="profile">
+           <div class="mt-4">
+            <label for="url" class="text-gray-600 font-semibold cursor-pointer">URL</label>
+            <PureInput v-model="form[fieldName].url" inputName="seoUrl" placeholder="profile">
+                <template #prefix>
+                    <div class="pl-3 -mr-2 whitespace-nowrap text-gray-400">
+                        {{ fieldData?.domain || 'https://example.com' }}
+                    </div>
+                </template>
+            </PureInput>
+            
+            <div class="mt-3 flex items-center">
+                <input type="checkbox" v-model="form[fieldName].is_use_canonical_url" id="canonical" class="mr-2">
+                <label for="canonical" class="text-gray-600 cursor-pointer">Use Canonical URL</label>
+            </div>
+            
+            <div v-if="form[fieldName].is_use_canonical_url" class="mt-3">
+                <label for="canonical_url" class="text-gray-600 font-semibold cursor-pointer">Canonical URL</label>
+                <PureInput v-model="form[fieldName].canonical_url" inputName="canonical_url" placeholder="" >
                     <template #prefix>
-                        <div class="pl-3 -mr-2 whitespace-nowrap text-gray-400">
-                            {{ fieldData?.domain || 'https://example.com' }}
-                        </div>
-                    </template>
+                    <div class="pl-3 -mr-2 whitespace-nowrap text-gray-400">
+                        {{ fieldData?.domain || 'https://example.com' }}
+                    </div>
+                </template>
                 </PureInput>
             </div>
-
-            <div>
-                <label for="redirecturl" class="text-gray-600 font-semibold cursor-pointer">
-                    <span>Redirect URL</span>
-                    <FontAwesomeIcon v-tooltip="trans('Separate each URL with comma')" icon='fal fa-info-circle' class='ml-1 text-gray-400' fixed-width aria-hidden='true' />
-                </label>
-                <PureTextarea v-model="form[fieldName].redirecturl" inputName="redirecturl" :rows="6" maxLength="320" placeholder="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum inventore vitae quas accusamus, numquam quo aut. Vero modi nihil provident!" />
-                <div class="mt-1 text-gray-500 italic tabular-nums">{{ form[fieldName].redirecturl?.split(',').map((item: string) => item.trim()).filter((item: string) => item !== '').length }} URL will be redirected</div>
-            </div>
+        </div>
         </div>
     </div>
 </template>
