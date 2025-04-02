5d0ce39c09579fb68d136e372a101fa8d938be54
dandiAW <dandiferrari75@gmail.com>
2025-04-02 15:00:14 +0800

    add page head

--- resources/js/Pages/Grp/Org/Fulfilment/FulfilmentCustomerPlatforms.vue
+++ resources/js/Pages/Grp/Org/Fulfilment/FulfilmentCustomerPlatforms.vue
@@ -1,11 +1,24 @@
 
 <script setup lang="ts">
+import PageHeading from "@/Components/Headings/PageHeading.vue";
 import TableFulfilmentCustomerPlatforms from "@/Components/Tables/Grp/Org/Fulfilment/TableFulfilmentCustomerPlatforms.vue"
+import { Head } from "@inertiajs/vue3";
+import { PageHeading as PageHeadingTS } from '@/types/PageHeading'
+import { capitalize } from "@/Composables/capitalize"
+
 const props = defineProps<{
     data:{}
+    title: string
+    pageHead: PageHeadingTS
+    tabs: {
+        current: string
+        navigation: {}
+    }
 }>()
 </script>
 
 <template>
+     <Head :title="capitalize(title)" />
+    <PageHeading :data="pageHead" />
    <TableFulfilmentCustomerPlatforms :data="data" />
 </template>
