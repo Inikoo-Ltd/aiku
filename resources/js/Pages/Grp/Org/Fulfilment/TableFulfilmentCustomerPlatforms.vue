72bf656d47e77bc513245f3f8a66ed7353cf5790
dandiAW <dandiferrari75@gmail.com>
2025-04-02 14:35:10 +0800

    create file fullfilment customer platforms and table fullfilment customer platform

---
+++ resources/js/Pages/Grp/Org/Fulfilment/TableFulfilmentCustomerPlatforms.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Table from '@/Components/Table/Table.vue'
+
+defineProps<{
+    data: object
+}>()
+</script>
+
+<template>
+     <Table :resource="data" >
+        
+    </Table>
+</template>
