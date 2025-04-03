ab97ebbf0dd8ec89fd1599eef3253ad0ce996483
dandiAW <dandiferrari75@gmail.com>
2025-04-03 10:33:50 +0800

    add subnav and tabs in show fulfilment customer platform

--- resources/js/Pages/Grp/Org/Fulfilment/FulfilmentCustomerPlatform.vue
+++ resources/js/Pages/Grp/Org/Fulfilment/FulfilmentCustomerPlatform.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
+import PageHeading from '@/Components/Headings/PageHeading.vue';
+import Tabs from '@/Components/Navigation/Tabs.vue';
+import { useTabChange } from '@/Composables/tab-change';
+import { PageHeading as PageHeadingTS } from '@/types/PageHeading'
+import { ref } from 'vue';
+
+const props = defineProps<{
+    title: string
+    pageHead: PageHeadingTS
+    tabs: {
+        current: string
+        navigation: {}
+    }
+}>()
+
+let currentTab = ref(props.tabs.current)
+const handleTabUpdate = (tabSlug: string) => useTabChange(tabSlug, currentTab)
 
 </script>
 
 <template>
-	xx'as
+	<PageHeading :data="pageHead" />
+    <Tabs :current="currentTab" :navigation="tabs['navigation']" @update:tab="handleTabUpdate" />
 </template>
