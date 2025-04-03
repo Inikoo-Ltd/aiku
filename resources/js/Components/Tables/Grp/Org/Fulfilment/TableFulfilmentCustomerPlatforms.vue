4481c21c6b44fbba01faf97bb6e86550e57d4ee9
dandiAW <dandiferrari75@gmail.com>
2025-04-03 10:21:47 +0800

    create file show fulfilmentCustomerPlatform and add route to show

--- resources/js/Components/Tables/Grp/Org/Fulfilment/TableFulfilmentCustomerPlatforms.vue
+++ resources/js/Components/Tables/Grp/Org/Fulfilment/TableFulfilmentCustomerPlatforms.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import Table from '@/Components/Table/Table.vue'
+import { Link } from '@inertiajs/vue3';
 
 defineProps<{
     data: object
 }>()
+
+function customerRoute(customer: any) {
+   
+}
 </script>
 
 <template>
      <Table :resource="data" >
-        
+        <template #cell(code)="{ item: customer }">
+            <Link :href="customerRoute(customer)" class="primaryLink">
+                {{ customer["code"] }}
+            </Link>
+        </template>
     </Table>
 </template>
