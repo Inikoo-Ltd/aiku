30acb831427cdc0d5b433eb999550793a1e3dad1
Raul Perusquia <raul@inikoo.com>
2025-04-03 10:38:37 +0800

    action to calculate shipping amount

--- app/Actions/Ordering/Order/GetShippingZone.php
+++ app/Actions/Ordering/Order/GetShippingZone.php
@@ -9,9 +9,9 @@
 namespace App\Actions\Ordering\Order;
 
 use App\Enums\Ordering\ShippingZoneSchema\ShippingZoneSchemaStateEnum;
+use App\Models\Helpers\Address;
 use App\Models\Ordering\Order;
 use App\Models\Ordering\ShippingZone;
-use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -36,24 +36,8 @@ class GetShippingZone
             $territories = $shippingZone->territories;
 
             foreach ($territories as $territory) {
-                if ($order->deliveryAddress->country_code == Arr::get($territory, 'country_code')) {
-                    if (Arr::has($territory, 'included_postal_codes')) {
-                        if (preg_match(
-                            Arr::get($territory, 'included_postal_codes'),
-                            $order->deliveryAddress->postal_code,
-                        )) {
-                            return $shippingZone;
-                        }
-                    } elseif (Arr::has($territory, 'excluded_postal_codes')) {
-                        if (!preg_match(
-                            Arr::get($territory, 'excluded_postal_codes'),
-                            $order->deliveryAddress->postal_code,
-                        )) {
-                            return $shippingZone;
-                        }
-                    } else {
-                        return $shippingZone;
-                    }
+                if ($this->getShippingZoneFromTerritory($shippingZone, $order->deliveryAddress, $territory)) {
+                    return $shippingZone;
                 }
             }
         }
@@ -61,6 +45,37 @@ class GetShippingZone
 
         return null;
     }
+
+
+    private function getShippingZoneFromTerritory(ShippingZone $shippingZone, Address $address, array $territory): ?ShippingZone
+    {
+        if ($address->country_code == Arr::get($territory, 'country_code')) {
+            if (Arr::has($territory, 'included_postal_codes')) {
+                if ($this->postCodeMatch($address->postal_code, $territory['included_postal_codes'])) {
+                    return $shippingZone;
+                }
+            } elseif (Arr::has($territory, 'excluded_postal_codes')) {
+                if ($this->postCodeMatch($address->postal_code, $territory['excluded_postal_codes'])) {
+                    return $shippingZone;
+                }
+            } else {
+                return $shippingZone;
+            }
+        }
+
+        return null;
+    }
+
+
+    private function postCodeMatch(string $postalCode, string $pattern): bool
+    {
+        if (preg_match($pattern, $postalCode)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
     private function getShippingZoneSchema(Order $order, $isDiscounted)
     {
@@ -76,43 +91,42 @@ class GetShippingZone
     }
 
 
-    public string $commandSignature = 'order:get-shipping-zone {order? : The ID of the order}';
-
-
-    public function commandProcess(Command $command, Order $order): void
-    {
-        $shippingZone = $this->handle($order);
-
-        if ($shippingZone) {
-            $command->info('Shipping zone found: '.$shippingZone->name.' ('.$shippingZone->slug.') ['.$shippingZone->id.']');
-        } else {
-            $command->info('No shipping zone found for the order.');
-        }
-    }
-
-    public function asCommand(Command $command): int
-    {
-        if ($command->argument('order')) {
-            $orderId = $command->argument('order');
-            $order   = Order::findOrFail($orderId);
-            $this->commandProcess($command, $order);
-        } else {
-            $count = 0;
-            $command->info('Processing all orders in chunks of 1000...');
-
-            Order::chunk(1000, function ($orders) use ($command, &$count) {
-                foreach ($orders as $order) {
-                    $this->commandProcess($command, $order);
-                    $count++;
-                }
-                $command->info("Processed $count orders so far...");
-            });
-
-            $command->info("Completed processing all $count orders.");
-        }
-
-
-        return 0;
-    }
+    //    public string $commandSignature = 'order:get-shipping-zone {order? : The ID of the order}';
+    //
+    //    public function commandProcess(Command $command, Order $order): void
+    //    {
+    //        $shippingZone = $this->handle($order);
+    //
+    //        if ($shippingZone) {
+    //            $command->info('Shipping zone found: '.$shippingZone->name.' ('.$shippingZone->slug.') ['.$shippingZone->id.']');
+    //        } else {
+    //            $command->info('No shipping zone found for the order.');
+    //        }
+    //    }
+    //
+    //    public function asCommand(Command $command): int
+    //    {
+    //        if ($command->argument('order')) {
+    //            $orderId = $command->argument('order');
+    //            $order   = Order::findOrFail($orderId);
+    //            $this->commandProcess($command, $order);
+    //        } else {
+    //            $count = 0;
+    //            $command->info('Processing all orders in chunks of 1000...');
+    //
+    //            Order::chunk(1000, function ($orders) use ($command, &$count) {
+    //                foreach ($orders as $order) {
+    //                    $this->commandProcess($command, $order);
+    //                    $count++;
+    //                }
+    //                $command->info("Processed $count orders so far...");
+    //            });
+    //
+    //            $command->info("Completed processing all $count orders.");
+    //        }
+    //
+    //
+    //        return 0;
+    //    }
 
 }
