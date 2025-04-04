f40492609ef602c1a13a05a6f5577e6cdf2122dd
Ganes556 <gustiganes@gmail.com>
2025-04-04 16:13:31 +0800

    change rule

--- app/Actions/Helpers/AI/AskBotVision.php
+++ app/Actions/Helpers/AI/AskBotVision.php
@@ -56,10 +56,8 @@ class AskBotVision extends OrgAction
 
     public function asController(ActionRequest $request)
     {
-        $image = $request->file('image');
-        $urlOrBase64Image = '';
-        if ($image && $image->isValid()) {
-            $urlOrBase64Image = base64_encode(file_get_contents($image->getRealPath()));
+        if (!$request->input('url')) {
+            $urlOrBase64Image = $request->input('image_base64');
         } else {
             $urlOrBase64Image = $request->input('url');
         }
@@ -72,16 +70,47 @@ class AskBotVision extends OrgAction
     {
         return [
             'url'   => ['required_without:image', 'url'],
-            'image' => ['required_without:url', 'file', 'mimes:jpeg,png,jpg', 'max:50000'],
+            'image_base64' => ['required_without:url', 'string'],
             'prompt' => ['required', 'in:default,alt'],
         ];
     }
 
+    // public function afterValidator($validator): void
+    // {
+    //     if(!$this->get("url")) {
+    //         $imageBase64 = $this->get('image_base64');
+    //         if (preg_match('/^data:image\/(\w+);base64,/', $imageBase64, $type)) {
+    //             $imageBase64 = substr($imageBase64, strpos($imageBase64, ',') + 1);
+    //             $imageBase64 = base64_decode($imageBase64);
+    //             if ($imageBase64 === false) {
+    //                 $this->error(0->add('Invalid base64 string');
+    //             }
+    //         } else {
+    //             $this->error('Invalid base64 string');
+    //         }
+
+    //     }
+
+    // }
 
 
-    public $commandSignature = 'ask:bot {q}';
 
-    public function asCommand($command): void
-    {
-    }
+    // public $commandSignature = 'ask:botvision {url} {--prompt=default}';
+
+    // public function asCommand($command): void
+    // {
+    //     $urlOrBase64Image = $command->argument('url');
+    //     $prompt = $command->option('prompt');
+
+    //     if ($prompt == 'alt') {
+    //         $this->promptAltImageWithLLPhant();
+    //     }
+
+    //     if (config('askbot-laravel.ai_provider') == 'openai') {
+    //         $this->handle($urlOrBase64Image, $prompt);
+    //     } else {
+    //         $this->error('AI provider not supported');
+    //     }
+
+    // }
 }
