e8cfa1851181d1fd2a9c3c63bc22122c37eb9195
KirinZero0 <aryapermana02@gmail.com>
2025-04-07 11:59:51 +0800

    Cancel order

--- routes/grp/web/models/ordering/order.php
+++ routes/grp/web/models/ordering/order.php
@@ -28,6 +28,7 @@ use App\Actions\Helpers\Media\DetachAttachmentFromModel;
 use App\Actions\Ordering\Order\PayOrder;
 use App\Actions\Ordering\Order\SendOrderToWarehouse;
 use App\Actions\Ordering\Order\UpdateOrder;
+use App\Actions\Ordering\Order\UpdateOrderStateToCancelled;
 use App\Actions\Ordering\Order\UpdateOrderStateToSubmitted;
 use App\Actions\Ordering\Order\UpdateStateToCreatingOrder;
 use App\Actions\Ordering\Order\UpdateStateToDispatchedOrder;
@@ -58,6 +59,7 @@ Route::name('order.')->prefix('order/{order:id}')->group(function () {
     Route::name('state.')->prefix('state')->group(function () {
         Route::patch('creating', UpdateStateToCreatingOrder::class)->name('creating');
         Route::patch('submitted', UpdateOrderStateToSubmitted::class)->name('submitted');
+        Route::patch('cancelled', UpdateOrderStateToCancelled::class)->name('cancelled');
         Route::patch('in-warehouse', SendOrderToWarehouse::class)->name('in-warehouse');
         Route::patch('handling', UpdateStateToHandlingOrder::class)->name('handling');
         Route::patch('packed', UpdateStateToPackedOrder::class)->name('packed');
