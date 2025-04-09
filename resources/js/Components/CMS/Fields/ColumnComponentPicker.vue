ccc9a7c5a4e5b2b4e0c5f25d5aab3f7b28751a71
YudhistiraA <aryarajasa0@gmail.com>
2025-04-09 09:31:36 +0800

    Column bluprint setup

--- resources/js/Components/CMS/Fields/ColumnComponentPicker.vue
+++ resources/js/Components/CMS/Fields/ColumnComponentPicker.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, provide } from 'vue'
 import { trans } from "laravel-vue-i18n"
 import BlockList from '@/Components/CMS/Webpage/BlockList.vue'
 import Modal from "@/Components/Utils/Modal.vue"
 import Button from '@/Components/Elements/Buttons/Button.vue'
 import { notify } from '@kyvg/vue3-notification'
 import { router } from '@inertiajs/vue3'
+import SideEditor from '@/Components/Workshop/SideEditor/SideEditor.vue'
 import Image from '@/Components/Image.vue'
 import axios from 'axios'
+import { routeType } from "@/types/route";
+import { getBlueprint } from '@/Composables/getBlueprintWorkshop'
 
+const props = defineProps<{ uploadRoutes: routeType }>();
 const model = defineModel()
 const emit = defineEmits(['update:modelValue'])
 
@@ -33,34 +37,51 @@ const getWebBlockTypes = async () => {
 	}
 }
 
+
+const onSaveWorkshopFromId = (blockId: number, from?: string) => {
+  emit("update:modelValue", model.value);
+};
+
+provide("onSaveWorkshopFromId", onSaveWorkshopFromId);
+
+
 onMounted(() => {
 	getWebBlockTypes()
 })
 </script>
 
 <template>
-	<div class="group hover:bg-gray-100 relative border border-gray-400 border-dashed overflow-hidden rounded-md text-center cursor-pointer" @click="modelModalBlocklist = true">
+	<div class="group hover:bg-gray-100 relative border border-gray-400 border-dashed overflow-hidden rounded-md text-center cursor-pointer"
+		@click="modelModalBlocklist = true">
+		<div v-if="!model" class="text-sm">
+			<div class="py-3">
+				<p>{{ trans("Pick Block") }}</p>
+			</div>
+		</div>
 
-        <div v-if="!model" class="text-sm">
-            <div class="py-3">
-                <p>{{ trans("Pick Block") }}</p>
-            </div>
-        </div>
+		<div v-else class="h-32 relative flex justify-center items-center">
+			<Image :src="model?.screenshot" class="w-auto h-fit" />
+			<div
+				class="absolute hover:bg-black/50 z-10 inset-0 flex items-center justify-center text-white text-sm opacity-0 group-hover:opacity-100">
+				{{ trans("Pick Block") }}
+			</div>
+			
+		</div>
+	</div>
 
-        <div v-else class="h-32 relative flex justify-center items-center">
-            <Image :src="model?.screenshot" class="w-auto h-fit" />
-            <div class="absolute hover:bg-black/50 z-10 inset-0 flex items-center justify-center text-white text-sm opacity-0 group-hover:opacity-100">
-                {{ trans("Pick Block") }}
-            </div>
-        </div>
-    </div>
-		<Modal :isOpen="modelModalBlocklist" @onClose="modelModalBlocklist = false">
-			<BlockList
-				:onPickBlock="onPickBlock"
-				:webBlockTypes="webBlockTypes"
-				scope="webpage"
-			/>
-		</Modal>
+
+	<div v-if="model" class="w-full mt-2">
+		<SideEditor 
+			v-model="model.data.fieldValue" 
+			:blueprint="getBlueprint(model.code)" 
+			@update:modelValue="(e) =>{emit('update:modelValue', e)}"
+			:uploadImageRoute="uploadRoutes" 
+		/>
+	</div>
+		
+	<Modal :isOpen="modelModalBlocklist" @onClose="modelModalBlocklist = false">
+		<BlockList :onPickBlock="onPickBlock" :webBlockTypes="webBlockTypes" scope="webpage" />
+	</Modal>
 </template>
 
 <style scoped>
