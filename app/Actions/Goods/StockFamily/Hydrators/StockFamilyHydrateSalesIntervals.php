13504e73b47977445d10bb5de2a71b2718d41625
Raul Perusquia <raul@inikoo.com>
2025-04-11 14:32:36 +0800

    feat: basic StockFamilyHydrateSalesIntervals

--- app/Actions/Goods/StockFamily/Hydrators/StockFamilyHydrateSalesIntervals.php
+++ app/Actions/Goods/StockFamily/Hydrators/StockFamilyHydrateSalesIntervals.php
@@ -20,9 +20,9 @@ class StockFamilyHydrateSalesIntervals implements ShouldBeUnique
     use AsAction;
     use WithIntervalsAggregators;
 
-    public string $jobQueue = 'urgent';
+    public string $jobQueue = 'sales';
 
-    public function getJobUniqueId(StockFamily $stockFamily, ?array $intervals = null, ?array $doPreviousPeriods = null): string
+    public function getJobUniqueId(StockFamily $stockFamily, ?array $intervals = null, ?array $doPreviousPeriods = null, ?DeliveryNoteItemSalesTypeEnum $onlyProcessSalesType = null): string
     {
         $uniqueId = $stockFamily->id;
         if ($intervals !== null) {
@@ -32,24 +32,56 @@ class StockFamilyHydrateSalesIntervals implements ShouldBeUnique
             $uniqueId .= '-'.implode('-', $doPreviousPeriods);
         }
 
+        if ($onlyProcessSalesType !== null) {
+            $uniqueId .= '-'.$onlyProcessSalesType->value;
+        }
+
         return $uniqueId;
     }
 
-    public function handle(StockFamily $stockFamily, ?array $intervals = null, ?array $doPreviousPeriods = null): void
+    public function handle(StockFamily $stockFamily, ?array $intervals = null, ?array $doPreviousPeriods = null, ?DeliveryNoteItemSalesTypeEnum $onlyProcessSalesType = null): void
     {
-
         $stats = [];
 
-        $queryBase = DeliveryNoteItem::where('sales_type', DeliveryNoteItemSalesTypeEnum::B2B)->where('stock_family_id', $stockFamily->id)->selectRaw('sum(grp_revenue_amount) as sum_aggregate');
+        $queryBase = DeliveryNoteItem::where('sales_type', '!=', DeliveryNoteItemSalesTypeEnum::NA)->where('stock_family_id', $stockFamily->id)->selectRaw('sum(grp_revenue_amount) as sum_aggregate');
         $stats     = $this->getIntervalsData(
             stats: $stats,
             queryBase: $queryBase,
-            statField: 'revenue_b2b_grp_currency_',
+            statField: 'revenue_grp_currency_',
             intervals: $intervals,
             doPreviousPeriods: $doPreviousPeriods
         );
 
+        if ($onlyProcessSalesType) {
+            $salesTypes = [$onlyProcessSalesType];
+        } else {
+            $salesTypes = DeliveryNoteItemSalesTypeEnum::cases();
+        }
+
+        foreach ($salesTypes as $salesType) {
+            if ($salesType == DeliveryNoteItemSalesTypeEnum::NA) {
+                continue;
+            }
+            $stats = array_merge(
+                $stats,
+                $this->perSalesType($stockFamily, $stats, $salesType, $intervals, $doPreviousPeriods)
+            );
+        }
+
         $stockFamily->salesIntervals()->update($stats);
+    }
+
+    public function perSalesType(StockFamily $stockFamily, array $stats, DeliveryNoteItemSalesTypeEnum $salesType, ?array $intervals, ?array $doPreviousPeriods): array
+    {
+        $queryBase = DeliveryNoteItem::where('sales_type', DeliveryNoteItemSalesTypeEnum::B2B)->where('stock_family_id', $stockFamily->id)->selectRaw('sum(grp_revenue_amount) as sum_aggregate');
+
+        return $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'revenue_'.$salesType->value.'_grp_currency_',
+            intervals: $intervals,
+            doPreviousPeriods: $doPreviousPeriods
+        );
     }
 
 }
