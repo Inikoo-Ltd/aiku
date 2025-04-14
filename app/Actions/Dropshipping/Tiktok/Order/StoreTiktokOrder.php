4002de2bfd71c9fd4d01e76ce751af3b8edb0e1a
Raul Perusquia <raul@inikoo.com>
2025-04-14 17:45:47 +0800

    clean up

--- app/Actions/Dropshipping/Tiktok/Order/StoreTiktokOrder.php
+++ app/Actions/Dropshipping/Tiktok/Order/StoreTiktokOrder.php
@@ -16,7 +16,6 @@ use App\Actions\Retina\Dropshipping\Client\StoreRetinaClientFromPlatform;
 use App\Actions\Retina\Dropshipping\Client\Traits\WithGeneratedTiktokAddress;
 use App\Actions\RetinaAction;
 use App\Actions\Traits\WithActionUpdate;
-use App\Enums\Dropshipping\ShopifyFulfilmentReasonEnum;
 use App\Enums\Dropshipping\ChannelFulfilmentStateEnum;
 use App\Enums\Fulfilment\PalletReturn\PalletReturnTypeEnum;
 use App\Enums\Ordering\Order\OrderStateEnum;
@@ -35,10 +34,10 @@ class StoreTiktokOrder extends RetinaAction
     use WithActionUpdate;
     use WithGeneratedTiktokAddress;
 
-    public function handle(TiktokUser $tiktokUser, array $attributes)
+    public function handle(TiktokUser $tiktokUser, array $attributes): void
     {
         $tiktokOrders = $attributes;
-        $address = Arr::get($tiktokOrders, 'recipient_address');
+        $address      = Arr::get($tiktokOrders, 'recipient_address');
         data_set($address, 'email', Arr::get($tiktokOrders, 'buyer_email'));
 
         $orderState = match (Arr::get($tiktokOrders, 'status')) {
@@ -49,7 +48,7 @@ class StoreTiktokOrder extends RetinaAction
 
         /** @var CustomerClient $customerClient */
         $customerClient = $tiktokUser->customer->clients()->where('email', Arr::get($attributes, 'buyer_email'))->first();
-        $address = $this->getAddressAttributes($address);
+        $address        = $this->getAddressAttributes($address);
 
         $customerClient = StoreRetinaClientFromPlatform::run($tiktokUser, $address, [
             'id' => Arr::get($tiktokOrders, 'user_id')
@@ -59,71 +58,78 @@ class StoreTiktokOrder extends RetinaAction
         data_set($tiktokOrders, 'state', $orderState);
 
         if ($tiktokUser->customer->is_fulfilment) {
-            $palletReturn = StorePalletReturn::make()->actionWithDropshipping($tiktokUser->customer->fulfilmentCustomer, [
-                'type' => PalletReturnTypeEnum::DROPSHIPPING,
-                'platform_id' => $tiktokUser->customer->platforms()->where('type', PlatformTypeEnum::TIKTOK->value)->first()->id
-            ]);
-
-            $storedItems = [];
-            $allComplete = true;
-            $someComplete = false;
-            foreach (Arr::get($attributes, 'line_items') as $lineItem) {
-                $tiktokUserHasProduct = TiktokUserHasProduct::where('tiktok_user_id', $tiktokUser->id)
-                    ->where('tiktok_product_id', $lineItem['product_id'])
-                    ->first();
-
-                if (!$tiktokUserHasProduct) {
-                    continue;
-                }
-
-                $storedItems[$tiktokUserHasProduct->portfolio->item_id] = [
-                    'quantity' => Arr::get($lineItem, 'quantity', 1)
-                ];
-
-                $itemQuantity = (int) $tiktokUserHasProduct->portfolio->item->total_quantity;
-                $requiredQuantity = Arr::get($lineItem, 'quantity', 1);
-
-                if ($itemQuantity >= $requiredQuantity) {
-                    $someComplete = true;
-                } else {
-                    $allComplete = false;
-                }
-            }
-
-            if (blank($storedItems)) {
-                return false;
-            }
-
-            $reasons = [
-                'no_fulfilment_reason' => ShopifyFulfilmentReasonEnum::INVENTORY_OUT_OF_STOCK,
-                'no_fulfilment_reason_notes' => ShopifyFulfilmentReasonEnum::INVENTORY_OUT_OF_STOCK->notes()[ShopifyFulfilmentReasonEnum::INVENTORY_OUT_OF_STOCK->value],
-            ];
-
-            if ($allComplete) {
-                $status = ChannelFulfilmentStateEnum::OPEN;
-                $reasons = [];
-            } elseif ($someComplete) {
-                $status = ChannelFulfilmentStateEnum::HOLD;
-            } else {
-                $status = ChannelFulfilmentStateEnum::INCOMPLETE;
-            }
-
-            StoreStoredItemsToReturn::make()->action($palletReturn, [
-                'stored_items' => $storedItems
-            ]);
-
-            $tiktokUser->orders()->create([
-                'orderable_type' => $palletReturn->getMorphClass(),
-                'orderable_id' => $palletReturn->id,
-                'state' => $status,
-                'tiktok_order_id' => Arr::get($attributes, 'id'),
-                'customer_client_id' => $customerClient->id
-            ]);
+            $this->processFulfilment($tiktokUser, $customerClient, $attributes);
         } else {
-            $order = StoreOrder::make()->action();
-            foreach (Arr::get($attributes, 'line_items') as $lineItem) {
-                StoreTransaction::run($order, []);
-            }
+            $this->processDropshippingShop($attributes);
         }
     }
+
+    //todo: complete this method
+    protected function processDropshippingShop($attributes)
+    {
+        $order = StoreOrder::make()->action();
+        foreach (Arr::get($attributes, 'line_items') as $lineItem) {
+            StoreTransaction::run($order, []);
+        }
+    }
+
+    protected function processFulfilment(TiktokUser $tiktokUser, CustomerClient $customerClient, array $attributes)
+    {
+        $palletReturn = StorePalletReturn::make()->actionWithDropshipping($tiktokUser->customer->fulfilmentCustomer, [
+            'type'        => PalletReturnTypeEnum::DROPSHIPPING,
+            'platform_id' => $tiktokUser->customer->platforms()->where('type', PlatformTypeEnum::TIKTOK->value)->first()->id
+        ]);
+
+        $storedItems  = [];
+        $allComplete  = true;
+        $someComplete = false;
+        foreach (Arr::get($attributes, 'line_items') as $lineItem) {
+            $tiktokUserHasProduct = TiktokUserHasProduct::where('tiktok_user_id', $tiktokUser->id)
+                ->where('tiktok_product_id', $lineItem['product_id'])
+                ->first();
+
+            if (!$tiktokUserHasProduct) {
+                continue;
+            }
+
+            $storedItems[$tiktokUserHasProduct->portfolio->item_id] = [
+                'quantity' => Arr::get($lineItem, 'quantity', 1)
+            ];
+
+            $itemQuantity     = (int)$tiktokUserHasProduct->portfolio->item->total_quantity;
+            $requiredQuantity = Arr::get($lineItem, 'quantity', 1);
+
+            if ($itemQuantity >= $requiredQuantity) {
+                $someComplete = true;
+            } else {
+                $allComplete = false;
+            }
+        }
+
+        if (blank($storedItems)) {
+            return false;
+        }
+
+
+        if ($allComplete) {
+            $status = ChannelFulfilmentStateEnum::OPEN;
+        } elseif ($someComplete) {
+            $status = ChannelFulfilmentStateEnum::HOLD;
+        } else {
+            $status = ChannelFulfilmentStateEnum::INCOMPLETE;
+        }
+
+        StoreStoredItemsToReturn::make()->action($palletReturn, [
+            'stored_items' => $storedItems
+        ]);
+
+        $tiktokUser->orders()->create([
+            'orderable_type'     => $palletReturn->getMorphClass(),
+            'orderable_id'       => $palletReturn->id,
+            'state'              => $status,
+            'tiktok_order_id'    => Arr::get($attributes, 'id'),
+            'customer_client_id' => $customerClient->id
+        ]);
+    }
+
 }
