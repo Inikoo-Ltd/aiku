8cf62324e70ab25b79ecf19a6c971741f65dca02
YudhistiraA <aryarajasa0@gmail.com>
2025-04-16 16:23:01 +0800

    departemant master

--- resources/js/Pages/Grp/Goods/DepartementMasterBlueprint.vue
+++ resources/js/Pages/Grp/Goods/DepartementMasterBlueprint.vue
@@ -1,82 +1,162 @@
 <script setup lang="ts">
-import { ref, inject } from 'vue'
+import { ref } from 'vue'
 import Button from '@/Components/Elements/Buttons/Button.vue'
-import Dialog from 'primevue/dialog'
-import SwiperImage from '@/Components/Elements/SwiperImage.vue'
 import Modal from '@/Components/Utils/Modal.vue'
 import PageHeading from '@/Components/Headings/PageHeading.vue'
-import { faPlug, faPlus, faPlusCircle } from '@far'
+import { faChevronCircleLeft, faChevronCircleRight, faImage, faEye, faEyeSlash } from '@far'
 import GalleryManagement from '@/Components/Utils/GalleryManagement/GalleryManagement.vue'
+import DepartementRender from '@/Components/CMS/Webpage/Department1/DepartementRender.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import PureInput from '@/Components/Pure/PureInput.vue'
+import { useConfirm } from "primevue/useconfirm";
 
 const props = defineProps<{
-    pageHead : {}
-    data: {
-        departement: {
-            data: {
-                code: string
-                name: string
-                description: string
-                images: Array<string>
-            }
-        }
-
-    }
+    pageHead: {},
+    department: {},
+    families: { data: Array<{ id: number, name: string }> },
+    web_block_types: Array<string>
 }>()
 
-console.log(props)
-
-const visible = ref(false)
+const department = ref(props.department.data)
+const usedTemplate = ref(props.web_block_types.data[0])
 const isModalGallery = ref(false)
+const selectedItems = ref([])
+console.log(props)
+const confirm = useConfirm();
+const handleImageUpload = (e: Event) => {
+    const target = e.target as HTMLInputElement
+    const file = target.files?.[0]
+    if (file) {
+        console.log('Uploaded:', file.name)
+    }
+}
 
-console.log(props.data)
-const images = [
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-    {image : 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png'},
-]
+const familiesOption = ref(
+  props.families.data.map((item) => ({
+    ...item,  // Spread the original item properties
+    show: true,  // Add the show property to each item
+  }))
+)
+
+const goToPrev = () => {
+    console.log('Previous clicked')
+}
+
+const goToNext = () => {
+    console.log('Next clicked')
+}
+
+const components: Record<string, Component> = {
+    'department-1': DepartementRender,
+}
+
+const getComponent = (componentName: string) => {
+    return components[componentName]
+}
+
+const confirmDelete = (event: MouseEvent, item: { id: number; name: string; show: boolean }) => {
+  confirm.require({
+    target: event.currentTarget as HTMLElement,
+    message: `Are you sure you want to hide "${item.name}" from view?`,
+    rejectProps: {
+      label: 'Cancel',
+      severity: 'secondary',
+      outlined: true,
+    },
+    acceptProps: {
+      label: 'Yes, hide it',
+      severity: 'danger',
+    },
+    accept: () => {
+      item.show = false; // Toggle visibility
+    },
+  });
+};
 
 </script>
 
+
 <template>
-   <PageHeading :data="pageHead">
+    <PageHeading :data="pageHead">
+      <template #button-save>
+        <Button type="save" />
+      </template>
     </PageHeading>
-    <div class="p-6">
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <!-- Left: Product Form -->
-            <div class="bg-white rounded-xl shadow-md  pt-0 space-y-6 w-full h-full overflow-hidden">
-                <div class="relative">
-                    <Button type="primary" label="Upload" :icon="faPlusCircle" :size="'xs'"
-                        @click="isModalGallery = true"
-                        class="absolute top-2 right-2 z-10 bg-white text-sm border rounded px-3 py-1 cursor-pointer shadow" />
-                    <div class="flex justify-center items-start">
-                        <SwiperImage :images="images" />
-                    </div>
-                </div>
-
-                <div class="p-4">
-                    <h2 class="text-lg font-semibold text-gray-900">departement name</h2>
-                    <p class="mt-2 text-sm text-gray-600">Lorem Ipsum is simply dummy text of the
-                         printing and typesetting industry. Lorem Ipsum has been the industry's 
-                         standard dummy text ever since the 1500s, when an unknown printer took a 
-                         galley of type and scrambled it to make a type specimen book. It has survived 
-                         not only five centuries, but also the leap into electronic typesetting, remaining 
-                         essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                         sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-                         software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-
+  
+    <div class="grid grid-cols-1 lg:grid-cols-[30%_1fr] gap-6 px-4 pb-8 m-5">
+      <!-- Sidebar -->
+      <div class="bg-white p-6 rounded-2xl shadow-md border border-gray-200">
+        <!-- Navigation & Preview -->
+        <div class="flex items-center justify-between mb-6">
+          <button @click="goToPrev" aria-label="Previous">
+            <FontAwesomeIcon :icon="faChevronCircleLeft" class="text-xl text-gray-600 hover:text-primary" />
+          </button>
+          <div class="flex-1 mx-4">
+            <component :is="getComponent(usedTemplate.code)" :data="department" />
+          </div>
+          <button @click="goToNext" aria-label="Next">
+            <FontAwesomeIcon :icon="faChevronCircleRight" class="text-xl text-gray-600 hover:text-primary" />
+          </button>
         </div>
+  
+        <!-- Form -->
+        <div class="border-t pt-4 space-y-4">
+          <div>
+            <label class="block text-sm font-medium text-gray-700 mb-1">Name</label>
+            <PureInput v-model="department.name" type="text" placeholder="Enter name" />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-700 mb-1">Image</label>
+            <Button label="Upload Image" :type="'tertiary'" :icon="faImage" @click="isModalGallery = true" />
+          </div>
+        </div>
+      </div>
+  
+      <!-- Families List -->
+      <div class="bg-white p-6 rounded-2xl shadow-md border border-gray-200">
+        <h3 class="text-xl font-semibold mb-4 border-b">Families List</h3>
+        <ul class="divide-y divide-gray-100">
+            <li
+  v-for="(item, index) in familiesOption"
+  :key="item.slug"
+  class="flex items-center justify-between py-4 hover:bg-gray-50 px-2 rounded-lg transition"
+>
+  <div class="flex items-center gap-4">
+    <div class="w-12 h-12 bg-gray-100 flex items-center justify-center rounded-lg overflow-hidden">
+      <img
+        v-if="item.image"
+        :src="item.image"
+        alt="Item Image"
+        class="w-full h-full object-cover"
+      />
+      <FontAwesomeIcon v-else :icon="faImage" class="text-gray-400 text-xl" />
     </div>
+    <div>
+      <div class="font-medium text-gray-800">{{ item.name }}</div>
+      <div class="text-sm text-gray-500">{{ item.code }}</div>
+    </div>
+  </div>
 
+  <!-- Action -->
+  <div
+    class="text-gray-500 hover:text-primary cursor-pointer transition"
+    @click="(e) => confirmDelete(e, item)"
+    title="Toggle visibility"
+  >
+    <FontAwesomeIcon :icon="item.show ? faEye : faEyeSlash" />
+  </div>
+</li>
 
-
-
+        </ul>
+      </div>
+    </div>
+  
+    <!-- Gallery Modal -->
     <Modal :isOpen="isModalGallery" @onClose="() => (isModalGallery = false)" width="w-3/4">
-        <GalleryManagement :uploadRoute="{ name: '', parameters: '' }" :closePopup="() => (isModalGallery = false)" />
+      <GalleryManagement
+        :uploadRoute="{ name: '', parameters: '' }"
+        :closePopup="() => (isModalGallery = false)"
+      />
     </Modal>
-</template>
+  </template>
+  
