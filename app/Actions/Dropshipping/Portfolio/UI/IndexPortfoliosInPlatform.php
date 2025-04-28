4d7001e5b6cb2c64256f10d9295451219ea5eebf
Artha <manager.arthachan@gmail.com>
2025-04-28 14:29:34 +0800

    fix: index portfolio platforms

--- app/Actions/Dropshipping/Portfolio/UI/IndexPortfoliosInPlatform.php
+++ app/Actions/Dropshipping/Portfolio/UI/IndexPortfoliosInPlatform.php
@@ -108,8 +108,8 @@ class IndexPortfoliosInPlatform extends OrgAction
             $table
                 ->withModelOperations($modelOperations)
                 ->withGlobalSearch()
-                ->column(key: 'product_code', label: __('product'), canBeHidden: false, searchable: true)
-                ->column(key: 'product_name', label: __('product name'), canBeHidden: false, searchable: true)
+                ->column(key: 'item_code', label: __('product'), canBeHidden: false, searchable: true)
+                ->column(key: 'item_name', label: __('product name'), canBeHidden: false, searchable: true)
                 ->column(key: 'reference', label: __('customer reference'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'type', label: __('type'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'created_at', label: __('created at'), canBeHidden: false, sortable: true, searchable: true);
