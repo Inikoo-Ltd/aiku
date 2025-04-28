bed0245da1019a8f8b5b9169f3563c45bfcd215f
KirinZero0 <aryapermana02@gmail.com>
2025-04-28 15:40:13 +0800

    polls

---
+++ app/Http/Resources/CRM/PollOptionsResource.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 28-04-2025-15h-35m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Http\Resources\CRM;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PollOptionsResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        /** @var PollOption $pollOption */
+                $pollOption = $this;
+        return [
+            'id'                 => $pollOption->id,
+            'slug'               => $pollOption->slug,
+            'value'              => $pollOption->value,
+            'label'              => $pollOption->label
+        ];
+    }
+}
