29a832b5ea105d7bdb1553b0addc596b75489693
Ganes556 <gustiganes@gmail.com>
2025-04-30 10:57:08 +0800

    add migration

--- database/migrations/2025_04_30_023436_add_amount_basket_to_stats_table.php
+++ database/migrations/2025_04_30_023436_add_amount_basket_to_stats_table.php
@@ -12,15 +12,19 @@ return new class () extends Migration {
      */
     public function up()
     {
-        Schema::table('customer_stats', function (Blueprint $table) {
+        Schema::table('customers', function (Blueprint $table) {
             $table->decimal('amount_in_basket', 16)->default(0);
             $table->decimal('amount_in_basket_org_currency', 16)->default(0);
             $table->decimal('amount_in_basket_grp_currency', 16)->default(0);
+            $table->unsignedInteger('current_order_in_basket_id')->nullable()->index();
+            $table->foreign('current_order_in_basket_id')->references('id')->on('orders');
         });
-        Schema::table('customer_client_stats', function (Blueprint $table) {
+        Schema::table('customer_clients', function (Blueprint $table) {
             $table->decimal('amount_in_basket', 16)->default(0);
             $table->decimal('amount_in_basket_org_currency', 16)->default(0);
             $table->decimal('amount_in_basket_grp_currency', 16)->default(0);
+            $table->unsignedInteger('current_order_in_basket_id')->nullable()->index();
+            $table->foreign('current_order_in_basket_id')->references('id')->on('orders');
         });
     }
 
@@ -31,11 +35,15 @@ return new class () extends Migration {
      */
     public function down()
     {
-        Schema::table('customer_stats', function (Blueprint $table) {
+        Schema::table('customers', function (Blueprint $table) {
             $table->dropColumn(['amount_in_basket', 'amount_in_basket_org_currency', 'amount_in_basket_grp_currency']);
+            $table->dropForeign(['current_order_in_basket_id']);
+            $table->dropColumn('current_order_in_basket_id');
         });
-        Schema::table('customer_client_stats', function (Blueprint $table) {
+        Schema::table('customer_clients', function (Blueprint $table) {
             $table->dropColumn(['amount_in_basket', 'amount_in_basket_org_currency', 'amount_in_basket_grp_currency']);
+            $table->dropForeign(['current_order_in_basket_id']);
+            $table->dropColumn('current_order_in_basket_id');
         });
     }
 };
