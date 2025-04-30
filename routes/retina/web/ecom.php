7f4cfae1787c2a4ec35ce14af094470cd53be5bf
Ganes556 <gustiganes@gmail.com>
2025-04-30 16:20:32 +0800

    #1655 fix data and change route name

--- routes/retina/web/ecom.php
+++ routes/retina/web/ecom.php
@@ -14,8 +14,8 @@ use App\Actions\Retina\Ecom\Checkout\UI\ShowRetinaEcomCheckout;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('basket')->as('basket.')->group(function () {
-    Route::get('/', ShowRetinaEcomBasket::class)->name('index');
+    Route::get('/', ShowRetinaEcomBasket::class)->name('show');
 });
 Route::prefix('checkout')->as('checkout.')->group(function () {
-    Route::get('/', ShowRetinaEcomCheckout::class)->name('index');
+    Route::get('/', ShowRetinaEcomCheckout::class)->name('show');
 });
