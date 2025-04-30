ad0e3d4ccd67d9a94ec28fbad1c29049f9499515
Ganes556 <gustiganes@gmail.com>
2025-04-30 12:03:38 +0800

    #1654 hydrate foundation

---
+++ app/Actions/CRM/Customer/Hydrators/CustomerClientHydrateBasket.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 30-04-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\CRM\Customer\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Models\Dropshipping\CustomerClient;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\DB;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CustomerClientHydrateBasket implements ShouldBeUnique
+{
+    use AsAction;
+    use WithEnumStats;
+
+    public function getJobUniqueId(CustomerClient $customerClient): string
+    {
+        return $customerClient->id;
+    }
+
+    public function handle(CustomerClient $customerClient): void
+    {
+        $stats = [
+            'amount_in_basket' => DB::table('orders')
+                ->where('customer_client_id', $customerClient->id)
+                ->where('state', OrderStateEnum::CREATING->value)
+                ->sum('total_amount'),
+            'current_order_in_basket_id' => DB::table('orders')
+                ->where('customer_client_id', $customerClient->id)
+                ->where('state', OrderStateEnum::CREATING->value)
+                ->orderBy('date', 'desc')
+                ->value('id'),
+        ];
+
+        $customerClient->update($stats);
+    }
+
+}
---
+++ app/Actions/CRM/Customer/Hydrators/CustomerHydrateBasket.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 30-04-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\CRM\Customer\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Models\CRM\Customer;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\DB;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CustomerHydrateBasket implements ShouldBeUnique
+{
+    use AsAction;
+    use WithEnumStats;
+
+    public function getJobUniqueId(Customer $customer): string
+    {
+        return $customer->id;
+    }
+
+    public function handle(Customer $customer): void
+    {
+        $stats = [
+            'amount_in_basket' => DB::table('orders')
+                ->where('customer_id', $customer->id)
+                ->where('state', OrderStateEnum::CREATING->value)
+                ->sum('total_amount'),
+            'current_order_in_basket_id' => DB::table('orders')
+                ->where('customer_id', $customer->id)
+                ->where('state', OrderStateEnum::CREATING->value)
+                ->orderBy('date', 'desc')
+                ->value('id'),
+        ];
+
+        $customer->update($stats);
+    }
+
+}
