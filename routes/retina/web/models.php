5591a80cdb3d758fa4f7548c5c794702dbb292ce
Ganes556 <gustiganes@gmail.com>
2025-04-30 16:36:15 +0800

    #1655 update & delete order basket

--- routes/retina/web/models.php
+++ routes/retina/web/models.php
@@ -11,6 +11,8 @@ use App\Actions\Dropshipping\Shopify\Product\GetApiProductsFromShopify;
 use App\Actions\Dropshipping\Tiktok\Product\GetProductsFromTiktokApi;
 use App\Actions\Dropshipping\Tiktok\Product\StoreProductToTiktok;
 use App\Actions\Dropshipping\Tiktok\User\DeleteTiktokUser;
+use App\Actions\Ordering\Transaction\DeleteTransaction;
+use App\Actions\Ordering\Transaction\UpdateTransaction;
 use App\Actions\Retina\CRM\DeleteRetinaCustomerDeliveryAddress;
 use App\Actions\Retina\CRM\StoreRetinaCustomerClient;
 use App\Actions\Retina\CRM\UpdateRetinaCustomerAddress;
@@ -171,3 +173,9 @@ Route::name('web-users.')->prefix('web-users')->group(function () {
 });
 
 Route::get('attachment/{media:ulid}', DownloadRetinaAttachment::class)->name('attachment.download');
+
+
+Route::name('transaction.')->prefix('transaction')->group(function () {
+    Route::delete('{transaction:id}', DeleteTransaction::class)->name('delete')->withoutScopedBindings();
+    Route::patch('{transaction:id}', UpdateTransaction::class)->name('update')->withoutScopedBindings();
+});
