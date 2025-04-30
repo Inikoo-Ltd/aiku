b6b83d8578e86c93c7fd551076ed7dccb3bab1da
Ganes556 <gustiganes@gmail.com>
2025-04-30 14:47:13 +0800

    #1655 change route

--- routes/retina/web/ecom.php
+++ routes/retina/web/ecom.php
@@ -13,9 +13,9 @@ use App\Actions\Retina\Ecom\Basket\UI\IndexRetinaEcomBaskets;
 use App\Actions\Retina\Ecom\Checkout\UI\IndexRetinaEcomCheckouts;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('baskets')->as('baskets.')->group(function () {
+Route::prefix('basket')->as('basket.')->group(function () {
     Route::get('/', IndexRetinaEcomBaskets::class)->name('index');
 });
-Route::prefix('checkouts')->as('checkouts.')->group(function () {
+Route::prefix('checkout')->as('checkout.')->group(function () {
     Route::get('/', IndexRetinaEcomCheckouts::class)->name('index');
 });
