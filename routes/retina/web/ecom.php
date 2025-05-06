cc07fe57a6faa72e3536a04f229c7e0bf0946b4d
Ganes556 <gustiganes@gmail.com>
2025-05-06 14:09:28 +0800

    favourites index in retina ecom

--- routes/retina/web/ecom.php
+++ routes/retina/web/ecom.php
@@ -11,6 +11,7 @@
 
 use App\Actions\Retina\Ecom\Basket\UI\ShowRetinaEcomBasket;
 use App\Actions\Retina\Ecom\Checkout\UI\ShowRetinaEcomCheckout;
+use App\Actions\Retina\Ecom\Favourite\UI\IndexRetinaEcomFavourites;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('basket')->as('basket.')->group(function () {
@@ -18,4 +19,8 @@ Route::prefix('basket')->as('basket.')->group(function () {
 });
 Route::prefix('checkout')->as('checkout.')->group(function () {
     Route::get('/', ShowRetinaEcomCheckout::class)->name('show');
+});
+
+Route::prefix('favourites')->as('favourites.')->group(function () {
+    Route::get('/', IndexRetinaEcomFavourites::class)->name('index');
 });
