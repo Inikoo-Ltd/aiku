16cbc49fe800d7d950910e394b489cf33513be21
YudhistiraA <aryarajasa0@gmail.com>
2025-05-06 16:56:16 +0800

    Departement & family BluePrint

--- resources/js/Components/CMS/Webpage/Family1/FamilyIris1.vue
+++ resources/js/Components/CMS/Webpage/Family1/FamilyIris1.vue
@@ -6,6 +6,7 @@ import { faChevronCircleLeft, faChevronCircleRight, faImage } from '@far'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import Image from '@/Components/Image.vue'
+import Family1Render from './Family1Render.vue'
 
 // Register all necessary icons
 library.add(faCube, faLink, faStar, faCircle, faChevronCircleLeft, faChevronCircleRight)
@@ -15,8 +16,8 @@ const props = defineProps<{
     family: {
       name: string
       description: string
-      images: { source: string }[] 
-    }[] 
+      images: { source: string }[]
+    }[]
   }
   webpageData?: any
   blockData?: Object
@@ -42,43 +43,8 @@ function prevImage(index: number) {
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-3">
-    <div
-      v-for="(product, index) in visibleFamilies"
-      :key="index"
-      class="bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-xl"
-    >
-      <div class="relative w-full mb-3">
-        <!-- Display image or fallback icon -->
-        <Image
-          v-if="product.images && product.images.length > 0"
-          :src="product.images[imageIndexes[index]].source"
-          alt="Product image"
-          class="w-full h-48 object-cover"
-        />
-        <div v-else class="flex justify-center items-center bg-gray-100 w-full h-48">
-          <FontAwesomeIcon :icon="faImage" class="w-8 h-8 text-gray-400" />
-        </div>
-        <!-- Navigation buttons -->
-        <button
-          @click="prevImage(index)"
-          class="absolute top-1/2 left-2 transform -translate-y-1/2"
-        >
-          <FontAwesomeIcon :icon="faChevronCircleLeft" />
-        </button>
-        <button
-          @click="nextImage(index)"
-          class="absolute top-1/2 right-2 transform -translate-y-1/2"
-        >
-          <FontAwesomeIcon :icon="faChevronCircleRight" />
-        </button>
-      </div>
-
-      <div class="px-4 py-2">
-        <!-- Product name -->
-        <div class="text-lg font-semibold text-gray-800">{{ product.name }}</div>
-        <!-- Product description -->
-        <p class="mt-2 text-sm text-gray-600 truncate">{{ product.description || 'No description available' }}</p>
-      </div>
+    <div v-for="(family, index) in visibleFamilies" :key="index">
+      <Family1Render :data="family" />
     </div>
   </div>
 </template>
