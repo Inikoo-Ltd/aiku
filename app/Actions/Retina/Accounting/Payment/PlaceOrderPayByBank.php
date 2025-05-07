7112d9b74a32cc91dae86d63ddb42afe2c4ec16a
KirinZero0 <aryapermana02@gmail.com>
2025-05-07 15:58:04 +0800

    place order by bank (empty)

---
+++ app/Actions/Retina/Accounting/Payment/PlaceOrderPayByBank.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 07-05-2025-15h-53m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Retina\Accounting\Payment;
+
+use App\Actions\OrgAction;
+use App\Actions\RetinaAction;
+use Lorisleiva\Actions\ActionRequest;
+
+class PlaceOrderPayByBank extends RetinaAction
+{
+    public function handle()
+    {
+        return dd('hello');
+    }
+
+    public function asController(ActionRequest $request): void
+    {
+        $this->initialisation($request);
+        $this->handle();
+    }
+
+}
