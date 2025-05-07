f3d67a6eb593ef4a0aa614f887d9240939f21d9b
Artha <manager.arthachan@gmail.com>
2025-05-07 15:41:18 +0800

    fix: error

--- routes/retina/web/top_up.php
+++ routes/retina/web/top_up.php
@@ -7,15 +7,14 @@
  */
 
 use App\Actions\Accounting\PaymentGateway\Paypal\Orders\CapturePaymentOrderPaypal;
-use App\Actions\Retina\UI\Topup\CreateRetinaTopUp;
-use App\Actions\Retina\UI\Topup\IndexRetinaTopUp;
-use App\Actions\Retina\UI\Topup\ShowRetinaTopUpCheckout;
-use App\Actions\Retina\UI\Topup\ShowRetinaTopUpDashboard;
+use App\Actions\Retina\UI\TopUp\IndexRetinaTopUp;
+use App\Actions\Retina\UI\TopUp\ShowRetinaTopUpCheckout;
+use App\Actions\Retina\UI\TopUp\ShowRetinaTopUpDashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', IndexRetinaTopUp::class)->name('index');
 Route::get('/dashboard', ShowRetinaTopUpDashboard::class)->name('dashboard');
-Route::get('/create', CreateRetinaTopUp::class)->name('create');
+//Route::get('/create', CreateRetinaTopUp::class)->name('create');
 Route::get('/checkout', ShowRetinaTopUpCheckout::class)->name('checkout');
 
 Route::get('paypal-payment-capture/{payment:id}', CapturePaymentOrderPaypal::class)->name('paypal.capture_payment');
