128595fdf4f6c4c4cfd2f9d4f1555bab6d7f6ef3
Raul Perusquia <raul@inikoo.com>
2025-05-07 11:58:59 +0800

    master related migrations

--- app/Models/Catalogue/Asset.php
+++ app/Models/Catalogue/Asset.php
@@ -60,6 +60,7 @@ use Spatie\Sluggable\SlugOptions;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property bool $follow_master
  * @property ProductUnitRelationshipType $unit_relationship_type
  * @property-read \Illuminate\Database\Eloquent\Collection<int, Barcode> $barcode
  * @property-read Charge|null $charge
