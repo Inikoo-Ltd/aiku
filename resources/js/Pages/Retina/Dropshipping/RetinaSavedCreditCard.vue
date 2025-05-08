e103ee997c79286ec28a5ec188b4fcfd245cff2b
aqordeon <aqordivika@yahoo.co.id>
2025-05-08 16:02:19 +0800

    feat: Retina: RetinaSavedCreditCard

---
+++ resources/js/Pages/Retina/Dropshipping/RetinaSavedCreditCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+    data: { }
+}>()
+</script>
+
+<template>
+    <div class="p-8 text-xl font-bold w-full text-center">
+        Hello, world!
+        <pre>{{ data }}</pre>
+    </div>
+</template>
