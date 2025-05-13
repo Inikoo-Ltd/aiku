2a06f359b7f5afa5ca99576b8a781757b74e6789
KirinZero0 <aryapermana02@gmail.com>
2025-05-13 10:12:21 +0800

    Api Store order

--- routes/retina/api/retina_api.php
+++ routes/retina/api/retina_api.php
@@ -7,6 +7,7 @@
  */
 
 use App\Actions\Retina\Api\GetProfile;
+use App\Actions\Retina\Api\Order\StoreApiOrder;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
@@ -15,4 +16,8 @@ Route::get('/ping', function () {
 
 Route::middleware(['auth:sanctum', 'ability:retina'])->group(function () {
     Route::get('/profile', GetProfile::class)->name('profile');
+
+    Route::prefix('order')->as('order.')->group(function () {
+        Route::post('{customer:id}/store', StoreApiOrder::class)->name('store');
+    });
 });
