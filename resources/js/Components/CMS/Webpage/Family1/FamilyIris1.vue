eef1360b1132682ffb918a10a99fb491444fd09d
YudhistiraA <aryarajasa0@gmail.com>
2025-05-15 08:17:46 +0800

    family & Departement

--- resources/js/Components/CMS/Webpage/Family1/FamilyIris1.vue
+++ resources/js/Components/CMS/Webpage/Family1/FamilyIris1.vue
@@ -5,6 +5,7 @@ import { faStar, faCircle } from "@fas"
 import { faChevronCircleLeft, faChevronCircleRight } from '@far'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import Family1Render from './Family1Render.vue'
+import EmptyState from '@/Components/Utils/EmptyState.vue'
 library.add(faCube, faLink, faStar, faCircle, faChevronCircleLeft, faChevronCircleRight)
 
 const props = defineProps<{
@@ -20,16 +21,17 @@ const props = defineProps<{
 }>()
 
 
-const maxDisplay = 10
-const visibleFamilies = ref(props.fieldValue.family.slice(0, maxDisplay))
+/* const maxDisplay = 10
+const visibleFamilies = ref(props.fieldValue.family.slice(0, maxDisplay)) */
 
-
+console.log(props)
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-3">
-    <div v-for="(family, index) in visibleFamilies" :key="index">
+  <div v-if="props.fieldValue.family && props.fieldValue.family.length" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-3">
+    <div v-for="(family, index) in props.fieldValue.family" :key="index">
       <Family1Render :data="family" />
     </div>
   </div>
+ <EmptyState v-else :data="{title:'Empty Families'}" />
 </template>
