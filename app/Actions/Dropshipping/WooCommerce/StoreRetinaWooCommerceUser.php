52db4463820b69b37c4a2c258f1db8b0fa124c54
Artha <manager.arthachan@gmail.com>
2025-05-16 14:22:35 +0800

    fix: auth callback woo

--- app/Actions/Dropshipping/WooCommerce/StoreRetinaWooCommerceUser.php
+++ app/Actions/Dropshipping/WooCommerce/StoreRetinaWooCommerceUser.php
@@ -8,9 +8,12 @@
 
 namespace App\Actions\Dropshipping\WooCommerce;
 
+use App\Actions\CRM\Customer\AttachCustomerToPlatform;
 use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Ordering\Platform\PlatformTypeEnum;
 use App\Models\CRM\Customer;
+use App\Models\Dropshipping\Platform;
 use Illuminate\Support\Arr;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
@@ -26,10 +29,12 @@ class StoreRetinaWooCommerceUser extends OrgAction
         data_set($modelData, 'group_id', $customer->group_id);
         data_set($modelData, 'organisation_id', $customer->organisation_id);
         data_set($modelData, 'name', Arr::get($modelData, 'name'));
-        data_set($modelData, 'settings.credentials.consumer_key', Arr::get($modelData, 'consumer_key'));
-        data_set($modelData, 'settings.credentials.consumer_secret', Arr::get($modelData, 'consumer_secret'));
-        data_set($modelData, 'settings.credentials.store_url', Arr::get($modelData, 'store_url'));
+        data_set($modelData, 'settings.credentials.consumer_key', Arr::pull($modelData, 'consumer_key'));
+        data_set($modelData, 'settings.credentials.consumer_secret', Arr::pull($modelData, 'consumer_secret'));
+        data_set($modelData, 'settings.credentials.store_url', Arr::pull($modelData, 'store_url'));
 
         $customer->wooCommerceUser()->create($modelData);
+
+        AttachCustomerToPlatform::make()->action($customer, Platform::where('type', PlatformTypeEnum::WOOCOMMERCE->value)->first(), []);
     }
 }
