0900880aeb510430a80fc73ab267b463b03c92ba
Raul Perusquia <raul@inikoo.com>
2025-05-20 13:53:58 +0800

    Remove unused IndexPortfolios action and related routes
    The IndexPortfolios action and its associated routes have been removed as they were no longer needed. This cleanup includes deleting the controller, route definitions, and references in customers.php. Changes simplify handling customer portfolios and ensure unused code is eliminated.

--- app/Actions/Dropshipping/Portfolio/UI/IndexPortfoliosInCustomerSalesChannels.php
+++ app/Actions/Dropshipping/Portfolio/UI/IndexPortfoliosInCustomerSalesChannels.php
@@ -13,6 +13,7 @@ use App\Actions\Dropshipping\CustomerSalesChannel\UI\ShowCustomerSalesChannel;
 use App\Actions\Dropshipping\CustomerSalesChannel\UI\WithCustomerSalesChannelSubNavigation;
 use App\Actions\OrgAction;
 use App\Actions\Traits\Authorisations\WithCRMAuthorisation;
+use App\Enums\Ordering\Platform\PlatformTypeEnum;
 use App\Http\Resources\CRM\PortfoliosResource;
 use App\InertiaTable\InertiaTable;
 use App\Models\Catalogue\Shop;
@@ -102,6 +103,9 @@ class IndexPortfoliosInCustomerSalesChannels extends OrgAction
                     'icon'          => $icon,
                     'subNavigation' => $subNavigation,
                 ],
+
+
+                'show_add_products_modal'=> $this->customerSalesChannel->platform->type == PlatformTypeEnum::MANUAL,
                 'data'        => PortfoliosResource::collection($portfolios),
                 'customer'      => $this->customerSalesChannel->customer,
             ]
