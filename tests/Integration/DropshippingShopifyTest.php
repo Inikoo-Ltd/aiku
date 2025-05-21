99063bf3d18bf52ece9846380fa0126102e712e1
Raul Perusquia <raul@inikoo.com>
2025-05-21 13:12:43 +0800

    fix DS shopify  tests

--- tests/Integration/DropshippingShopifyTest.php
+++ tests/Integration/DropshippingShopifyTest.php
@@ -15,8 +15,6 @@ use App\Actions\Dropshipping\Portfolio\StorePortfolio;
 use App\Enums\Ordering\Platform\PlatformTypeEnum;
 use App\Models\Dropshipping\CustomerClient;
 use App\Models\Dropshipping\CustomerSalesChannel;
-use App\Models\Dropshipping\Platform;
-use App\Models\Dropshipping\PlatformStats;
 use App\Models\Dropshipping\Portfolio;
 
 uses()->group('integration');
@@ -30,31 +28,45 @@ beforeEach(function () {
     \Tests\Helpers\setupDropshippingTest($this);
 });
 
-test('test platform were seeded ', function () {
-    expect($this->group->platforms()->count())->toBe(4);
-    $platform = Platform::first();
-    expect($platform)->toBeInstanceOf(Platform::class)
-        ->and($platform->stats)->toBeInstanceOf(PlatformStats::class);
 
-    $this->artisan('group:seed-platforms')->assertExitCode(0);
-    expect($this->group->platforms()->count())->toBe(4);
+test('create shopify channel', function () {
+    $platform = $this->group->platforms()->where('type', PlatformTypeEnum::SHOPIFY)->first();
+
+
+    expect($this->customer->customerSalesChannels()->count())->toBe(0);
+    $customerSalesChannel = StoreCustomerSalesChannel::make()->action(
+        $this->customer,
+        $platform,
+        [
+            'reference' => 'test_shopify_reference'
+        ]
+    );
+
+
+    $customer = $customerSalesChannel->customer;
+    expect($customer->customerSalesChannels()->first())->toBeInstanceOf(CustomerSalesChannel::class);
+
+
+    return $customerSalesChannel;
 });
 
-test('create customer client', function () {
-    $customerClient = StoreCustomerClient::make()->action($this->customer, CustomerClient::factory()->definition());
+
+test('create customer client', function (CustomerSalesChannel $customerSalesChannel) {
+    $customerClient = StoreCustomerClient::make()->action($customerSalesChannel, CustomerClient::factory()->definition());
     expect($customerClient)->toBeInstanceOf(CustomerClient::class);
 
     return $customerClient;
-});
+})->depends('create shopify channel');
 
 test('update customer client', function ($customerClient) {
     $customerClient = UpdateCustomerClient::make()->action($customerClient, ['reference' => '001']);
     expect($customerClient->reference)->toBe('001');
+    return $customerClient;
 })->depends('create customer client');
 
-test('add product to customer portfolio', function () {
+test('add product to customer portfolio', function (CustomerClient $customerClient) {
     $dropshippingCustomerPortfolio = StorePortfolio::make()->action(
-        $this->customer,
+        $customerClient->salesChannel,
         $this->product,
         [
         ]
@@ -62,29 +74,5 @@ test('add product to customer portfolio', function () {
     expect($dropshippingCustomerPortfolio)->toBeInstanceOf(Portfolio::class);
 
     return $dropshippingCustomerPortfolio;
-});
+})->depends('update customer client');
 
-test('associate customer shopify to customer', function () {
-    $platform = $this->group->platforms()->where('type', PlatformTypeEnum::SHOPIFY)->first();
-
-
-    expect($this->customer->platforms->count())->toBe(0);
-    $customer = StoreCustomerSalesChannel::make()->action(
-        $this->customer,
-        $platform,
-        [
-            'reference' => 'test_shopify_reference' // todo add shopify id?? to .env.test
-        ]
-    );
-
-
-    $customer->refresh();
-
-
-    expect($customer->customerSalesChannels()->first())->toBeInstanceOf(CustomerSalesChannel::class);
-
-
-
-
-    return $customer;
-});
