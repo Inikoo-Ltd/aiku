3b2bff3e512cac5f29781a925912c632b9f1754f
KirinZero0 <aryapermana02@gmail.com>
2025-05-21 15:28:58 +0800

    fix show product ds

--- routes/retina/web/dropshipping/dropshipping_app.php
+++ routes/retina/web/dropshipping/dropshipping_app.php
@@ -98,6 +98,7 @@ Route::prefix('channels/{customerSalesChannel}')->as('customer_sales_channels.')
 
     Route::prefix('portfolios')->as('portfolios.')->group(function () {
         Route::get('my-portfolio', IndexRetinaPortfolios::class)->name('index');
+        Route::get('my-portfolio/{product}', [ShowRetinaProduct::class, 'inPlatform'])->name('show');
         Route::get('filtered-products', IndexRetinaFilteredProducts::class)->name('filtered_products.index');
         Route::get('products', [IndexRetinaProductsInDropshipping::class, 'inPlatform'])->name('products.index');
     });
