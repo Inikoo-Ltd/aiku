35a83a2e49fd75ac3aa92b6eae8ad24694b121bf
KirinZero0 <aryapermana02@gmail.com>
2025-05-22 10:29:02 +0800

    scopedBinding

--- routes/grp/web/models/ordering/order.php
+++ routes/grp/web/models/ordering/order.php
@@ -82,7 +82,7 @@ Route::name('delivery-note.')->prefix('delivery-note/{deliveryNote:id}')->group(
     Route::patch('update', UpdateDeliveryNote::class)->name('update');
     Route::patch('employee-pick', PickDeliveryNoteAsEmployee::class)->name('employee.pick');
     Route::name('state.')->prefix('state')->group(function () {
-        Route::patch('in-queue/{employee:id}', UpdateDeliveryNoteStateToInQueue::class)->name('in-queue');
+        Route::patch('in-queue/{employee:id}', UpdateDeliveryNoteStateToInQueue::class)->name('in-queue')->withoutScopedBindings();
         Route::patch('picker-assigned', UpdateDeliveryNoteStateToPickerAssigned::class)->name('picker-assigned');
         Route::patch('picking', UpdateDeliveryNoteStateToPicking::class)->name('picking');
         Route::patch('picked', UpdateDeliveryNoteStateToPicked::class)->name('picked');
