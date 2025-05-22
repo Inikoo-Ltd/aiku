4059da4ff1a20c98204ae51df4561e046fad9120
Ganes556 <gustiganes@gmail.com>
2025-05-22 13:23:53 +0800

    add migration for brands and ethical attribute in products

---
+++ database/migrations/2025_05_22_051655_create_brands_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique()->collation('und_ns');
+
+            $table->unsignedInteger('image_id')->nullable();
+            $table->foreign('image_id')->references('id')->on('media')->onDelete('cascade');
+            $table->string('name')->collation('und_ns')->index();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
