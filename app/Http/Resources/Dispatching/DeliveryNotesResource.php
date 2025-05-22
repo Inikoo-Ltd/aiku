cf3536f6cfb801a32612514c04c4a6704bdce8c0
KirinZero0 <aryapermana02@gmail.com>
2025-05-22 11:37:35 +0800

    Effective weight missing index DN

--- app/Http/Resources/Dispatching/DeliveryNotesResource.php
+++ app/Http/Resources/Dispatching/DeliveryNotesResource.php
@@ -48,6 +48,7 @@ class DeliveryNotesResource extends JsonResource
             'type'              => $this->type,
             'weight'            => $this->weight,
             'estimated_weight'  => $this->estimated_weight,
+            'effective_weight'  => $this->effective_weight,
             'created_at'        => $this->created_at,
             'updated_at'        => $this->updated_at,
             'shop_slug'         => $this->shop_slug,
