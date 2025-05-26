aba725d8c2110b4d6a1fab120582e2df1723678e
aqordeon <aqordivika@yahoo.co.id>
2025-05-26 14:42:30 +0800

    fix: Delivery Note: not picked quantity

--- app/Http/Resources/Dispatching/DeliveryNoteItemsResource.php
+++ app/Http/Resources/Dispatching/DeliveryNoteItemsResource.php
@@ -43,8 +43,8 @@ class DeliveryNoteItemsResource extends JsonResource
             'org_stock_code'      => $this->org_stock_code,
             'org_stock_name'      => $this->org_stock_name,
             'locations'           => $orgStock->locationOrgstocks ? LocationOrgStocksResource::collection($orgStock->locationOrgstocks) : [],
-            'pickings'            => $deliveryNoteItem->pickings 
-                ? $deliveryNoteItem->pickings->keyBy(fn($item) => $item->location_id ?? $item->location->id)
+            'pickings'            => $deliveryNoteItem->pickings
+                ? $deliveryNoteItem->pickings->keyBy(fn($item) => $item->location_id ?? $item->location?->id)
                     ->map(fn($item) => new PickingsResource($item))
                 : [],
             'packings'            => $deliveryNoteItem->packings ? PackingsResource::collection($deliveryNoteItem->packings) : [],
