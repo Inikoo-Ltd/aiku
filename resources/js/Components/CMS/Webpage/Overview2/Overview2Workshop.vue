5f89509113bdc871ee5162eb1ec59ee7c451db1c
YudhistiraA <aryarajasa0@gmail.com>
2025-05-26 11:29:37 +0800

    fix Cta simple

--- resources/js/Components/CMS/Webpage/Overview2/Overview2Workshop.vue
+++ resources/js/Components/CMS/Webpage/Overview2/Overview2Workshop.vue
@@ -35,28 +35,42 @@ function onSave() {
 }
 </script>
 
+
 <template>
-	<div class="relative" :style="getStyles(modelValue.container.properties,screenType)">
-		<!-- Image Section -->
-		<div class="relative h-auto overflow-hidden md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
-			<div	@click="() => sendMessageToParent('activeChildBlock', Blueprint?.blueprint?.[0]?.key?.join('-'))">
-				<img v-if="!modelValue?.image?.source" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" :alt="modelValue?.image?.alt" class="h-full w-full object-cover" />
-				<Image 
-					v-else
-					:src="modelValue?.image?.source" 
-					:imageCover=true 
-					:alt="modelValue?.image?.alt"
-					:imgAttributes="modelValue?.image?.attributes"
-					:style="getStyles(modelValue?.image?.properties)"
-				/>
-			</div>
+	<!-- Wrapper -->
+	<div
+		class="flex flex-col md:flex-row w-full rounded-lg overflow-hidden"
+		:style="getStyles(modelValue.container.properties, screenType)"
+	>
+		<!-- Section 1: Image -->
+		<div
+			class="w-full h-64 sm:h-72 md:h-auto md:w-1/3 lg:w-1/2 bg-cover bg-center bg-no-repeat"
+			@click="() => sendMessageToParent('activeChildBlock', Blueprint?.blueprint?.[0]?.key?.join('-'))"
+		>
+			<img
+				v-if="!modelValue?.image?.source"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+				:alt="modelValue?.image?.alt"
+				class="h-full w-full object-cover"
+			/>
+			<Image
+				v-else
+				:src="modelValue?.image?.source"
+				:imageCover
+				:alt="modelValue?.image?.alt"
+				:imgAttributes="modelValue?.image?.attributes"
+				:style="getStyles(modelValue?.image?.properties)"
+			/>
 		</div>
 
-		<!-- Details Section -->
-		<div class="py-16 sm:py- lg:px-8">
-			<div class="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
-				<Editor v-model="modelValue.text" @update:modelValue="() => emits('autoSave')"/>	
-			</div>
+		<!-- Section 2: Content -->
+		<div
+			class="flex items-center justify-center w-full md:w-2/3 lg:w-1/2 bg-white bg-opacity-90 backdrop-blur px-6 py-12 sm:px-12 lg:px-20"
+		>
+			<Editor
+				v-model="modelValue.text"
+				@update:modelValue="() => emits('autoSave')"
+			/>
 		</div>
 	</div>
 </template>
