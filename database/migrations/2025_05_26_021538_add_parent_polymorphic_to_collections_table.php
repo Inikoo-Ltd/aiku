dec2d7da576a9c28d4db41e8f783ec6f6009635f
KirinZero0 <aryapermana02@gmail.com>
2025-05-26 10:17:18 +0800

    parent type and id in collections

---
+++ database/migrations/2025_05_26_021538_add_parent_polymorphic_to_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->string('parent_type')->index();
+            $table->unsignedInteger('parent_id')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->dropIndex(['parent_type']);
+            $table->dropIndex(['parent_id']);
+            $table->dropColumn(['parent_type', 'parent_id']);
+        });
+    }
+};
