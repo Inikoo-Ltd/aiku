e9114fc5eacc75480d80fca826acce8ad66e4bd2
Ganes556 <gustiganes@gmail.com>
2025-06-04 11:21:08 +0800

    action: template dpd

--- config/app.php
+++ config/app.php
@@ -273,6 +273,7 @@ return [
 
         'shipper_itd_token' => env('ITD_TOKEN'),
         'shipper_apc_token' => env('APC_TOKEN'),
+        'shipper_dpd_gb_token' => env('DPD_GB_TOKEN'),
 
 
         'local_share_url' => env('SANDBOX_SHARE_URL'),
