6dfa1301a6e0b663388fec1c9e43ec8691ac56f6
Raul Perusquia <raul@inikoo.com>
2025-06-09 10:28:41 +0800

    #1535 SEO 1 - Create a Sitemap and Update robots.txt (start)

---
+++ app/Actions/Web/Website/SaveWebsiteSitemap.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 09 Jun 2025 10:18:53 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Web\Website;
+
+class SaveWebsiteSitemap
+{
+}
