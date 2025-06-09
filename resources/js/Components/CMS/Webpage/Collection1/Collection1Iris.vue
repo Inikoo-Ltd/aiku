19725735b0aeb03def3398b96a5b6e66338d330a
YudhistiraA <aryarajasa0@gmail.com>
2025-06-09 15:03:09 +0800

    Create Collections json

---
+++ resources/js/Components/CMS/Webpage/Collection1/Collection1Iris.vue
@@ -0,0 +1,30 @@
+<!--
+  - Author: Raul Perusquia <raul@inikoo.com>
+  - Created: Wed, 07 Jun 2023 02:45:27 Malaysia Time, Kuala Lumpur, Malaysia
+  - Copyright (c) 2023, Raul A Perusquia Flores
+  -->
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core"
+import Image from "@/Components/Image.vue"
+import { getStyles } from "@/Composables/styles"
+import { FieldValue } from "@/types/webpageTypes"
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faCube, faLink, faImage } from "@fal"
+
+library.add(faCube, faLink, faImage)
+
+const props = defineProps<{
+    fieldValue: FieldValue
+    webpageData?: any
+    blockData?: Object
+    screenType: 'mobile' | 'tablet' | 'desktop'
+}>()
+
+
+</script>
+
+<template>
+    <div>Collections1Iris</div>
+</template>
