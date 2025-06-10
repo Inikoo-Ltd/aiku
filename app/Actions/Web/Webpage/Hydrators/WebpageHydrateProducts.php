94341d0b0d9a4a132e6efcf4a1caaec41d28abd9
KirinZero0 <aryapermana02@gmail.com>
2025-06-10 13:23:03 +0800

    Hydrator Webpage has Products

---
+++ app/Actions/Web/Webpage/Hydrators/WebpageHydrateProducts.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 10-06-2025-13h-17m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Web\Webpage\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Models\Web\Webpage;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class WebpageHydrateProducts
+{
+    use AsAction;
+    use WithEnumStats;
+
+    private Webpage $webpage;
+
+    public function __construct(Webpage $webpage)
+    {
+        $this->webpage = $webpage;
+    }
+
+    public function getJobMiddleware(): array
+    {
+        return [(new WithoutOverlapping($this->webpage->id))->dontRelease()];
+    }
+
+    public function handle(Webpage $webpage): void
+    {
+        $stats = [
+            'number_products' => $webpage->webpageHasProducts()->count(),
+        ];
+
+        $webpage->stats()->update($stats);
+    }
+
+
+}
