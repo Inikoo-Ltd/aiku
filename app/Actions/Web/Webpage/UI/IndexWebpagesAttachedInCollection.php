25427620ef84e47bab533cf4052d25c623d8fb6a
KirinZero0 <aryapermana02@gmail.com>
2025-06-10 11:00:44 +0800

    attached webpages data in collection

---
+++ app/Actions/Web/Webpage/UI/IndexWebpagesAttachedInCollection.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 10-06-2025-10h-55m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Web\Webpage\UI;
+
+use App\Actions\OrgAction;
+use App\Http\Resources\Web\WebpagesResource;
+use App\InertiaTable\InertiaTable;
+use App\Models\Catalogue\Collection;
+use App\Models\Web\Webpage;
+use App\Services\QueryBuilder;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class IndexWebpagesAttachedInCollection extends OrgAction
+{
+    public function handle(Collection $collection, $prefix = null, $bucket = null): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->whereStartWith('webpages.code', $value)
+                    ->orWhereStartWith('webpages.url', $value);
+            });
+        });
+
+        if ($prefix) {
+            InertiaTable::updateQueryBuilderParameters($prefix);
+        }
+
+        $queryBuilder = QueryBuilder::for(Webpage::class);
+        $queryBuilder->leftjoin('webpage_has_collections', 'webpages.id', '=', 'webpage_has_collections.webpage_id')
+            ->where('webpage_has_collections.collection_id', $collection->id);
+
+        if (isset(request()->query()['json']) && request()->query()['json'] === 'true' || (function_exists('request') && request() && request()->expectsJson())) {
+            $queryBuilder->orderByRaw("CASE 
+            WHEN webpages.sub_type = 'storefront' THEN 1
+            WHEN webpages.sub_type = 'department' THEN 2
+            WHEN webpages.sub_type = 'sub_department' THEN 3
+            WHEN webpages.sub_type = 'family' THEN 4
+            WHEN webpages.sub_type = 'catalogue' THEN 5
+            WHEN webpages.sub_type = 'product' THEN 6
+            ELSE 7
+            END, webpages.sub_type ASC");
+        }
+
+        $queryBuilder->leftJoin('organisations', 'webpages.organisation_id', '=', 'organisations.id');
+        $queryBuilder->leftJoin('shops', 'webpages.shop_id', '=', 'shops.id');
+        $queryBuilder->leftJoin('websites', 'webpages.website_id', '=', 'websites.id');
+
+        return $queryBuilder
+            ->defaultSort('webpages.level')
+            ->select([
+                'webpages.code',
+                'webpages.id',
+                'webpages.type',
+                'webpages.slug',
+                'webpages.level',
+                'webpages.sub_type',
+                'webpages.url',
+                'organisations.slug as organisation_slug',
+                'shops.slug as shop_slug',
+                'shops.name as shop_name',
+                'organisations.name as organisation_name',
+                'websites.domain as website_url',
+                'websites.slug as website_slug'
+            ])
+            ->allowedSorts(['code', 'type', 'level', 'url'])
+            ->allowedFilters([$globalSearch])
+            ->withPaginator($prefix, tableName: request()->route()->getName())
+            ->withQueryString();
+    }
+
+    public function jsonResponse(LengthAwarePaginator $webpages): AnonymousResourceCollection
+    {
+        return WebpagesResource::collection($webpages);
+    }
+}
