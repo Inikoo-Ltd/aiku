0bd26f4a001a9b9a49557131f14ffc64bec4ee50
Ganes556 <gustiganes@gmail.com>
2025-06-10 10:19:25 +0800

    shipment gls

--- app/Actions/Dispatching/Shipment/StoreShipment.php
+++ app/Actions/Dispatching/Shipment/StoreShipment.php
@@ -9,6 +9,7 @@
 namespace App\Actions\Dispatching\Shipment;
 
 use App\Actions\Dispatching\Shipment\ApiCalls\CallApiApcGbShipping;
+use App\Actions\Dispatching\Shipment\ApiCalls\CallApiGlsSKShipping;
 use App\Actions\Dispatching\Shipment\ApiCalls\CallApiItdShipping;
 use App\Actions\Dispatching\Shipment\ApiCalls\DpdGbCallShipperApi;
 use App\Actions\Dispatching\Shipment\ApiCalls\DpdSkCallShipperApi;
@@ -48,6 +49,7 @@ class StoreShipment extends OrgAction
         if ($shipper->api_shipper) {
             $shipmentData = match ($shipper->api_shipper) {
                 'apc-gb' => CallApiApcGbShipping::run($parent, $shipper),
+                'gls-sk' => CallApiGlsSKShipping::run($parent, $shipper),
                 'dpd-gb' => DpdGbCallShipperApi::run($parent, $shipper),
                 'dpd-sk' => DpdSkCallShipperApi::run($parent, $shipper),
                 'pst-mn' => PostmenCallShipperApi::run($parent, $shipper),
