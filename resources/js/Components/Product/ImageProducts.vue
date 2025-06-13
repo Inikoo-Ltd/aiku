68c8a5f670395ff2a77962ed902e094eb6dd24c3
YudhistiraA <aryarajasa0@gmail.com>
2025-06-13 17:13:30 +0800

    iris Product page

--- resources/js/Components/Product/ImageProducts.vue
+++ resources/js/Components/Product/ImageProducts.vue
@@ -4,26 +4,35 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/thumbs'
-
 import { Navigation, Autoplay, Thumbs } from 'swiper/modules'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faChevronCircleLeft, faChevronCircleRight } from '@fal' // pastikan path ini benar
-
+import { faChevronCircleLeft, faChevronCircleRight, faTimesCircle } from '@fal'
 import { ulid } from 'ulid'
 import Image from '@/Components/Image.vue'
 
 const props = defineProps<{
-  images: string[]
+  images: { source: string; thumbnail: string }[]
 }>()
 
 const keySwiperMain = ref(ulid())
 const keySwiperThumb = ref(ulid())
 
 const thumbsSwiper = ref(null)
-
 const prevEl = ref<HTMLElement | null>(null)
 const nextEl = ref<HTMLElement | null>(null)
 const navigation = ref({ prevEl: null, nextEl: null })
+
+const showModal = ref(false)
+const selectedIndex = ref(0)
+
+function openImageModal(index: number) {
+  selectedIndex.value = index
+  showModal.value = true
+}
+
+function closeImageModal() {
+  showModal.value = false
+}
 
 onMounted(async () => {
   await nextTick()
@@ -33,58 +42,130 @@ onMounted(async () => {
   }
 })
 </script>
+
 <template>
   <div class="w-full flex flex-col items-center relative">
-    <!-- Main Swiper with Navigation -->
-    <div class="relative w-full">
-      <Swiper :key="keySwiperMain" :slides-per-view="1" :loop="true" :autoplay="false" :navigation="navigation"
-        :modules="[Navigation, Autoplay, Thumbs]" :thumbs="{ swiper: thumbsSwiper }"
-        class="aspect-square w-full rounded-lg mb-4">
-        <SwiperSlide v-for="(image, index) in images" :key="index" class="flex justify-center items-center">
-          <div class="bg-gray-100 w-full aspect-square flex items-center justify-center overflow-hidden rounded-lg">
-            <Image :src="image.source" :alt="`Image ${index + 1}`" class="w-full h-full object-cover" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-
-      <!-- Navigation Buttons -->
-      <div ref="prevEl"
-        class="absolute left-4 top-1/2 -translate-y-1/2 z-30 text-3xl cursor-pointer text-gray-700 select-none">
+    <!-- Shared Navigation Buttons -->
+    <div class="absolute inset-0 pointer-events-none z-50">
+      <div
+        ref="prevEl"
+        class="absolute left-4 top-1/2 -translate-y-1/2 text-3xl text-white cursor-pointer pointer-events-auto"
+      >
         <FontAwesomeIcon :icon="faChevronCircleLeft" />
       </div>
-      <div ref="nextEl"
-        class="absolute right-4 top-1/2 -translate-y-1/2 z-30 text-3xl cursor-pointer text-gray-700 select-none">
+      <div
+        ref="nextEl"
+        class="absolute right-4 top-1/2 -translate-y-1/2 text-3xl text-white cursor-pointer pointer-events-auto"
+      >
         <FontAwesomeIcon :icon="faChevronCircleRight" />
       </div>
     </div>
 
+    <!-- Main Swiper -->
+    <div class="relative w-full">
+      <Swiper
+        :key="keySwiperMain"
+        :slides-per-view="1"
+        :loop="true"
+        :autoplay="false"
+        :navigation="navigation"
+        :modules="[Navigation, Autoplay, Thumbs]"
+        :thumbs="{ swiper: thumbsSwiper }"
+        class="aspect-square w-full rounded-lg mb-4"
+      >
+        <SwiperSlide
+          v-for="(image, index) in props.images"
+          :key="index"
+          class="flex justify-center items-center"
+        >
+          <div
+            class="bg-gray-100 w-full aspect-square flex items-center justify-center overflow-hidden rounded-lg cursor-pointer"
+            @click="openImageModal(index)"
+          >
+            <Image
+              :src="image.source"
+              :alt="`Image ${index + 1}`"
+              class="w-full h-full object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+
     <!-- Thumbnail Swiper -->
-    <Swiper :key="keySwiperThumb" :slides-per-view="auto" :space-between="8" watch-slides-progress :modules="[Thumbs]"
-      @swiper="(swiper) => (thumbsSwiper = swiper)" :style="{ width: 'fit-content', marginLeft: '0px' }">
-      <SwiperSlide v-for="(image, index) in images" :key="index"
-        class="cursor-pointer rounded overflow-hidden border border-gray-300 !w-[60px]">
+    <Swiper
+      :key="keySwiperThumb"
+      :slides-per-view="'auto'"
+      :space-between="8"
+      watch-slides-progress
+      :modules="[Thumbs]"
+      @swiper="(swiper) => (thumbsSwiper = swiper)"
+      :style="{ width: 'fit-content', marginLeft: '0px' }"
+    >
+      <SwiperSlide
+        v-for="(image, index) in props.images"
+        :key="index"
+        class="cursor-pointer rounded overflow-hidden border border-gray-300 !w-[60px]"
+      >
         <slot name="image-thumbnail" :image="image">
           <div class="aspect-square w-full">
-            <Image :src="image.thumbnail" :alt="`Thumbnail ${index + 1}`" class="w-full h-full object-cover" />
+            <Image
+              :src="image.thumbnail"
+              :alt="`Thumbnail ${index + 1}`"
+              class="w-full h-full object-cover"
+            />
           </div>
         </slot>
-
       </SwiperSlide>
     </Swiper>
 
+    <!-- Modal Swiper -->
+    <div
+      v-if="showModal"
+      class="fixed inset-0 bg-black/90 z-50 flex items-center justify-center"
+      @click.self="closeImageModal"
+    >
+      <div class="relative w-full max-w-5xl p-4">
+        <!-- Close Button -->
+        <button
+          class="absolute top-4 right-4 text-white text-3xl z-50"
+          @click="closeImageModal"
+        >
+          <FontAwesomeIcon :icon="faTimesCircle" />
+        </button>
 
+        <!-- Swiper in Modal -->
+        <Swiper
+          :initial-slide="selectedIndex"
+          :slides-per-view="1"
+          :loop="true"
+          :navigation="navigation"
+          :modules="[Navigation]"
+          class="w-full"
+        >
+          <SwiperSlide
+            v-for="(image, index) in props.images"
+            :key="index"
+            class="flex items-center justify-center"
+          >
+            <Image
+              :src="image.source"
+              :alt="`Zoomed Image ${index + 1}`"
+              class="w-full max-h-[80vh] object-contain rounded-lg shadow-lg"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
   </div>
 </template>
-
-
 
 <style scoped lang="scss">
 .swiper {
   touch-action: pan-y;
 }
 
-/* Sesuaikan posisi tombol navigasi supaya di luar gambar */
-.absolute {
-  user-select: none;
+button {
+  outline: none;
 }
 </style>
