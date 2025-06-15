5d394a2f31fcd7bc01ee9da9133bb659c37f2c9c
Raul Perusquia <raul@inikoo.com>
2025-06-15 11:11:19 +0800

    fix tests; refactor: Handle null-safe country code in `AddressResource` and adjust expected currency count in `SysAdminTest`.

--- app/Http/Resources/Helpers/AddressResource.php
+++ app/Http/Resources/Helpers/AddressResource.php
@@ -37,7 +37,7 @@ class AddressResource extends JsonResource
 
         $adr = new Adr();
         $adr = $adr
-            ->withCountryCode($address->country_code)
+            ->withCountryCode($address->country_code??'')
             ->withAdministrativeArea($address->administrative_area ?? '')
             ->withDependentLocality($address->dependent_locality ?? '')
             ->withLocality($address->locality ?? '')
