8b87534ffd16f8c7326e160a88b3ccf3dd081134
Ganes556 <gustiganes@gmail.com>
2025-06-16 14:58:01 +0800

    register route

--- routes/retina/web/auth.php
+++ routes/retina/web/auth.php
@@ -18,6 +18,7 @@ use App\Actions\CRM\WebUser\Retina\UI\ShowRetinaResetWebUserPassword;
 use App\Actions\CRM\WebUser\Retina\UI\ShowRetinaResetWebUserPasswordError;
 use App\Actions\CRM\WebUser\Retina\UpdateRetinaWebUserPassword;
 use App\Actions\Retina\SysAdmin\FinishPreRegisterRetinaCustomer;
+use App\Actions\Retina\SysAdmin\PreRegisterRetina;
 use App\Actions\Retina\SysAdmin\PreRegisterRetinaCustomer;
 use App\Actions\Retina\SysAdmin\RegisterRetinaFulfilmentCustomer;
 use App\Actions\Retina\UI\Auth\SendRetinaResetPasswordEmail;
@@ -32,6 +33,7 @@ Route::middleware('guest:retina')->group(function () {
 
     Route::post('{shop:id}/register-pre-customer', PreRegisterRetinaCustomer::class)->name('register_pre_customer.store');
     Route::post('{shop:id}/login-google', GoogleLoginRetina::class)->name('login_google');
+    Route::post('{shop:id}/register-google', PreRegisterRetina::class)->name('register_pre_customer_google.store');
 
 
     Route::get('register', ShowRetinaRegister::class)->name('register');
