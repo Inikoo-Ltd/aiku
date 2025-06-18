db7c646e2cbda0dcaec938ab7c9e30219cd91a88
Ganes556 <gustiganes@gmail.com>
2025-06-18 11:24:43 +0800

    pass image tag

--- app/Http/Resources/Web/WebBlockProductResource.php
+++ app/Http/Resources/Web/WebBlockProductResource.php
@@ -9,6 +9,7 @@
 namespace App\Http\Resources\Web;
 
 use App\Http\Resources\Accounting\OrgPaymentProvidersResource;
+use App\Http\Resources\Catalogue\TagResource;
 use App\Http\Resources\HasSelfCall;
 use App\Models\Catalogue\Product;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -78,7 +79,7 @@ class WebBlockProductResource extends JsonResource
             'updated_at'      => $product->updated_at,
             'images'          => ImageResource::collection($product->images)->toArray($request),
             'service_providers' => OrgPaymentProvidersResource::collection($resultSp)->toArray($request),
-            'tags' => $product->tradeUnitTagsViaTradeUnits(),
+            'tags' => TagResource::collections($product->tradeUnitTagsViaTradeUnits())->toArray($request),
             'return_policy' => Arr::get($product->webpage->website->settings, 'return_policy', ''),
         ];
     }
