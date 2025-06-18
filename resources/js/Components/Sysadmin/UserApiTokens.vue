3c5fa85fa2ea763d4917eb23f5e88e354e08c8ca
aqordeon <aqordivika@yahoo.co.id>
2025-06-18 16:35:52 +0800

    feat: User: generate API token

---
+++ resources/js/Components/Sysadmin/UserApiTokens.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useFormatTime } from '@/Composables/useFormatTime'
+import Table from '../Table/Table.vue'
+
+const props = defineProps<{
+    data: {}
+    tab: string
+}>()
+</script>
+
+<template>
+    <div>
+        <Table :resource="data" :name="tab">
+            <template #cell(created_at)="{ item }">
+                {{ useFormatTime(item.created_at) }}
+            </template>
+        </Table>
+    </div>
+</template>
--- resources/js/Pages/Grp/SysAdmin/User.vue
+++ resources/js/Pages/Grp/SysAdmin/User.vue
@@ -5,7 +5,7 @@
   -->
 
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3'
+import { Head, router } from '@inertiajs/vue3'
 
 import PageHeading from '@/Components/Headings/PageHeading.vue'
 import { computed, defineAsyncComponent, ref } from "vue"
@@ -14,7 +14,7 @@ import ModelDetails from "@/Components/ModelDetails.vue"
 import TableUserRequestLogs from "@/Components/Tables/Grp/SysAdmin/TableUserRequestLogs.vue"
 import TableHistories from "@/Components/Tables/Grp/Helpers/TableHistories.vue"
 import Tabs from "@/Components/Navigation/Tabs.vue"
-import {faIdCard, faUser, faClock, faDatabase, faEnvelope, faHexagon, faFile, faShieldCheck, faUserTag, faKey} from '@fal'
+import { faIdCard, faUser, faClock, faDatabase, faEnvelope, faHexagon, faFile, faShieldCheck, faUserTag, faKey } from '@fal'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { capitalize } from "@/Composables/capitalize"
 import { faRoad } from "@fas"
@@ -24,6 +24,15 @@ import UserRoles from '@/Components/Sysadmin/UserRoles.vue'
 import { PageHeading as PageHeadingTypes } from '@/types/PageHeading'
 import { Tabs as TSTabs } from '@/types/Tabs'
 import type { Component } from 'vue'
+import UserApiTokens from '@/Components/Sysadmin/UserApiTokens.vue'
+import Button from '@/Components/Elements/Buttons/Button.vue'
+import Modal from '@/Components/Utils/Modal.vue'
+import { routeType } from '@/types/route'
+import { notify } from '@kyvg/vue3-notification'
+import { trans } from 'laravel-vue-i18n'
+import axios from 'axios'
+import { useCopyText } from '@/Composables/useCopyText'
+import PureInput from '@/Components/Pure/PureInput.vue'
 library.add(faIdCard, faUser, faClock, faDatabase, faEnvelope, faHexagon, faFile, faRoad, faShieldCheck, faUserTag, faKey)
 
 const ModelChangelog = defineAsyncComponent(() => import('@/Components/ModelChangelog.vue'))
@@ -38,9 +47,13 @@ const props = defineProps<{
     permissions?: {}
     roles?: {}
     api_tokens?: {}
+    apiRoutes: {
+        createToken: routeType
+        deleteToken: routeType
+    }
 }>()
 
-
+const isModalApiToken = ref(false)
 
 const currentTab = ref(props.tabs.current)
 const handleTabUpdate = (tabSlug: string) => useTabChange(tabSlug, currentTab)
@@ -48,6 +61,7 @@ const handleTabUpdate = (tabSlug: string) => useTabChange(tabSlug, currentTab)
 const component = computed(() => {
     const components: Component = {
         showcase: SysadminUserShowcase,
+        api_tokens: UserApiTokens,
         details: ModelDetails,
         request_logs: TableUserRequestLogs,
         api_tokens: TableHistories,
@@ -59,11 +73,96 @@ const component = computed(() => {
 
 })
 
+const newToken = ref('')
+const isLoadingGenerate = ref(false)
+const onGenerateApiToken = async () => {
+    isLoadingGenerate.value = true
+    try {
+        const data = await axios.post(
+            route(props.apiRoutes.createToken.name, props.apiRoutes.createToken.parameters),
+            {
+                data: 'qqq'
+            }
+        )
+
+        console.log('Generate API Token response:', data.data)
+        newToken.value = data.data.token
+        
+        router.reload(
+        {
+            only: ['api_tokens']
+        }
+    )
+        
+    } catch (error) {
+        console.log('error', error)
+        notify({
+            title: trans("Something went wrong"),
+            text: trans("Failed to set location"),
+            type: "error"
+        })
+    } finally {
+        isLoadingGenerate.value = false
+    }
+
+}
+
+const isRecentlyCopied = ref(false)
+const onClickCopy = (textToCopy: string) => {
+    isRecentlyCopied.value = true
+    useCopyText(textToCopy)
+    setTimeout(() => {
+        isRecentlyCopied.value = false
+    }, 3000)
+}
 </script>
 
 <template>
+
     <Head :title="capitalize(title)" />
-    <PageHeading :data="pageHead"></PageHeading>
+    <PageHeading :data="pageHead">
+        <template #otherBefore>
+            <Button @click="() => isModalApiToken = true" label="Generate API Token" type="tertiary">
+
+            </Button>
+        </template>
+    </PageHeading>
     <Tabs :current="currentTab" :navigation="tabs['navigation']" @update:tab="handleTabUpdate" />
     <component :is="component" :data="props[currentTab as keyof typeof props]" :tab="currentTab"></component>
+
+    <Modal :isOpen="isModalApiToken" @onClose="() => (isModalApiToken = false)" width="w-full max-w-xl"
+        height="h-[500px]">
+
+        <div>
+            <!-- <div className="mx-auto flex size-12 items-center justify-center rounded-full bg-green-100">
+                xxx
+            </div> -->
+            <div class="mt-3 text-center sm:mt-5">
+                <div as="h3" class="text-base font-semibold">
+                    Generate API Token
+                </div>
+
+                <div class="text-sm text-gray-500">
+                    You can Generate a new API Token for this user. This token can be used to authenticate API requests.
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-4 mx-auto w-fit">
+            <PureInput
+                v-if="newToken"
+                v-model="newToken"
+                disabled
+                copyButton
+            />
+            
+            <Button
+                v-else
+                @click="onGenerateApiToken"
+                label="Click to Generate"
+                type="tertiary"
+                :loading="isLoadingGenerate"
+            />
+        </div>
+    </Modal>
 </template>
