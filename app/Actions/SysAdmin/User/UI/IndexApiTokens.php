114d299e867a691fed902be46f98f333a1c79bd1
Ganes556 <gustiganes@gmail.com>
2025-06-18 13:54:48 +0800

    remove no need method

--- app/Actions/SysAdmin/User/UI/IndexApiTokens.php
+++ app/Actions/SysAdmin/User/UI/IndexApiTokens.php
@@ -1,25 +1,23 @@
 <?php
 
 /*
- * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Mon, 04 Dec 2023 16:24:47 Malaysia Time, Kuala Lumpur, Malaysia
- * Copyright (c) 2023, Raul A Perusquia Flores
- */
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 18-06-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
 
 namespace App\Actions\SysAdmin\User\UI;
 
 use App\Actions\OrgAction;
-use App\Actions\SysAdmin\UI\ShowSysAdminDashboard;
 use App\Actions\SysAdmin\User\WithUsersSubNavigation;
 use App\Actions\SysAdmin\WithSysAdminAuthorization;
-use App\Http\Resources\SysAdmin\ApiTokensResource;
 use App\InertiaTable\InertiaTable;
 use App\Models\SysAdmin\User;
 use App\Services\QueryBuilder;
 use Closure;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Inertia\Inertia;
-use Inertia\Response;
 use Lorisleiva\Actions\ActionRequest;
 use Laravel\Sanctum\PersonalAccessToken;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -79,67 +77,5 @@ class IndexApiTokens extends OrgAction
                 ->column(key: 'created_at', label: __('Created At'), canBeHidden: false, sortable: true)
                 ->defaultSort('created_at');
         };
-    }
-
-    public function htmlResponse(LengthAwarePaginator $tokens, ActionRequest $request): Response
-    {
-        return Inertia::render(
-            'SysAdmin/ApiTokens',
-            [
-                'breadcrumbs' => $this->getBreadcrumbs($request->route()->getName(), ['user' => $this->user->id]),
-                'title'       => __('API Tokens'),
-                'pageHead'    => [
-                    'title' => __('API Tokens for :username', ['username' => $this->user->username]),
-                    'icon'  => [
-                        'icon'  => ['fal', 'fa-key'],
-                        'title' => __('API Tokens')
-                    ],
-                ],
-                'data' => ApiTokensResource::collection($tokens),
-                'user' => $this->user->only(['id', 'username', 'contact_name']),
-            ]
-        )->table($this->tableStructure());
-    }
-
-    public function getBreadcrumbs(string $routeName, array $routeParameters): array
-    {
-        return array_merge(
-            ShowSysAdminDashboard::make()->getBreadcrumbs(),
-            [
-                [
-                    'type'   => 'simple',
-                    'simple' => [
-                        'route' => [
-                            'name'       => 'grp.sysadmin.users.index',
-                            'parameters' => []
-                        ],
-                        'label' => __('Users'),
-                        'icon'  => 'fal fa-users'
-                    ],
-                ],
-                [
-                    'type'   => 'simple',
-                    'simple' => [
-                        'route' => [
-                            'name'       => 'grp.sysadmin.users.show',
-                            'parameters' => ['user' => $routeParameters['user']]
-                        ],
-                        'label' => $this->user->username,
-                        'icon'  => 'fal fa-user'
-                    ],
-                ],
-                [
-                    'type'   => 'simple',
-                    'simple' => [
-                        'route' => [
-                            'name'       => 'grp.sysadmin.users.tokens.index',
-                            'parameters' => ['user' => $routeParameters['user']]
-                        ],
-                        'label' => __('API Tokens'),
-                        'icon'  => 'fal fa-key'
-                    ],
-                ]
-            ]
-        );
     }
 }
