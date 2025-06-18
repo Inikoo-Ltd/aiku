be104378b7b217810674453d147462c0a2c85650
Ganes556 <gustiganes@gmail.com>
2025-06-18 14:26:16 +0800

    #1639 hydrate polls

---
+++ app/Actions/Catalogue/Shop/Hydrators/ShopHydratePolls.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 18-06-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\Catalogue\Shop\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Enums\CRM\Poll\PollTypeEnum;
+use App\Models\Catalogue\Shop;
+use App\Models\CRM\Poll;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ShopHydratePolls implements ShouldBeUnique
+{
+    use AsAction;
+    use WithEnumStats;
+
+    public function getJobUniqueId(Shop $shop): string
+    {
+        return $shop->id;
+    }
+    public function handle(Shop $shop): void
+    {
+
+        $stats         = [
+            'number_polls'                   => $shop->polls->count(),
+            'number_polls_in_registration' => $shop->polls->where('in_registration', true)->count(),
+            'number_polls_in_registration_required'       => $shop->polls->where('in_registration_required', true)->count(),
+            'number_polls_in_iris_required'       => $shop->polls->where('in_iris_required', true)->count(),
+            'number_polls_in_iris'       => $shop->polls->where('in_iris', true)->count(),
+        ];
+
+        $stats = array_merge(
+            $stats,
+            $this->getEnumStats(
+                model: 'polls',
+                field: 'type',
+                enum: PollTypeEnum::class,
+                models: Poll::class,
+                where: function ($q) use ($shop) {
+                    $q->where('shop_id', $shop->id);
+                }
+            )
+        );
+
+        $shop->crmStats()->update($stats);
+    }
+
+
+}
