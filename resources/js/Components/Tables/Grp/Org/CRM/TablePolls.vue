e65c37267b5cfe43b3a52ab0f0f258059e85f7a0
aqordeon <aqordivika@yahoo.co.id>
2025-06-19 11:21:32 +0800

    #1639 fix: Polls: show icon if show in registration

--- resources/js/Components/Tables/Grp/Org/CRM/TablePolls.vue
+++ resources/js/Components/Tables/Grp/Org/CRM/TablePolls.vue
@@ -7,6 +7,8 @@
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3"
 import Table from "@/Components/Table/Table.vue"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { trans } from "laravel-vue-i18n"
 
 
 const props = defineProps<{
@@ -21,6 +23,15 @@ const props = defineProps<{
             <Link :href="route('grp.org.shops.show.crm.polls.show', { organisation: route().params.organisation, shop: route().params.shop, poll: item.slug })" class="primaryLink">
                 {{ item.name }}
             </Link>
+        </template>
+
+        <template #cell(in_registration)="{ item }">
+            <div v-if="item.in_registration" v-tooltip="trans('Will show in registration')" class="text-center text-green-500">
+                <FontAwesomeIcon icon="fal fa-check" class="" fixed-width aria-hidden="true" />
+            </div>
+            <div v-else>
+
+            </div>
         </template>
     </Table>
 </template>
