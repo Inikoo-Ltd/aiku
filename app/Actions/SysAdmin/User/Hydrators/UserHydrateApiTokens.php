2228e8f7a0a44a8b52ae57564d8058d3abdcd201
Ganes556 <gustiganes@gmail.com>
2025-06-19 13:16:55 +0800

    hydrator user apitoken

---
+++ app/Actions/SysAdmin/User/Hydrators/UserHydrateApiTokens.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 19-06-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\SysAdmin\User\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Models\SysAdmin\User;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class UserHydrateApiTokens implements ShouldBeUnique
+{
+    use AsAction;
+    use WithEnumStats;
+
+    public function getJobUniqueId(User $user): string
+    {
+        return $user->id;
+    }
+
+    public function handle(User $user): void
+    {
+        $stats = [
+            'number_current_api_tokens' => $user->tokens()->where('expires_at', '>', now())->orWhereNull('expires_at')->count(),
+            'number_expired_api_tokens' => $user->tokens()->where('expires_at', '<=', now())->count(),
+        ];
+
+        $user->stats->update($stats);
+    }
+}
