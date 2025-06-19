7835b19447a09f50a28322e164a49dbb073d28ca
KirinZero0 <aryapermana02@gmail.com>
2025-06-19 09:58:53 +0800

    Ebay Account setup (additional settings data)

--- app/Actions/Dropshipping/Ebay/CallbackRetinaEbayUser.php
+++ app/Actions/Dropshipping/Ebay/CallbackRetinaEbayUser.php
@@ -74,6 +74,31 @@ class CallbackRetinaEbayUser extends OrgAction
                 $ebayUser->createFulfilmentPolicy();
                 $ebayUser->createPaymentPolicy();
                 $ebayUser->createReturnPolicy();
+                $defaultLocationData = [
+                    'locationKey' => 'mainWarehouse',
+                    'city' => 'Sheffield',
+                    'state' => 'England',
+                    'country' => 'GB',
+                ];
+                $ebayUser->createInventoryLocation($defaultLocationData);
+
+                $fulfilmentPolicies = $ebayUser->getFulfilmentPolicies();
+                $paymentPolicies = $ebayUser->getPaymentPolicies();
+                $returnPolicies = $ebayUser->getReturnPolicies();
+
+                $updatedSettings = [
+                    ...$ebayUser->settings,
+                    'defaults' => [
+                        'main_location_key' => Arr::get($defaultLocationData, 'locationKey'),
+                        'main_fulfilment_policy_id' => Arr::get($fulfilmentPolicies, 'fulfillmentPolicies.0.fulfillmentPolicyId'),
+                        'main_payment_policy_id' => Arr::get($paymentPolicies, 'paymentPolicies.0.paymentPolicyId'),
+                        'main_return_policy_id' => Arr::get($returnPolicies, 'returnPolicies.0.returnPolicyId'),
+                    ]
+                ];
+
+                $ebayUser = UpdateEbayUser::run($ebayUser, [
+                    'settings' => $updatedSettings
+                ]);
 
                 UpdateCustomerSalesChannel::run($ebayUser->customerSalesChannel, [
                     'state' => CustomerSalesChannelStateEnum::AUTHENTICATED
