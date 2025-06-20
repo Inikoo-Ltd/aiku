99b8df528c2d565bd2684dff1ed6bf0a044e7b48
Artha <manager.arthachan@gmail.com>
2025-06-20 13:54:53 +0800

    fix: delete portfolio

--- routes/iris/models.php
+++ routes/iris/models.php
@@ -8,6 +8,7 @@
 */
 
 use App\Actions\Iris\Portfolio\DeleteIrisPortfolioFavourites;
+use App\Actions\Iris\Portfolio\DeleteIrisPortfolioFromMultiChannels;
 use App\Actions\Iris\Portfolio\StoreIrisPortfolioFavourites;
 use App\Actions\Iris\Portfolio\StoreIrisPortfolioToAllChannels;
 use App\Actions\Iris\Portfolio\StoreIrisPortfolioToMultiChannels;
@@ -16,8 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('portfolio-all-channels', StoreIrisPortfolioToAllChannels::class)->name('all_channels.portfolio.store');
 Route::post('portfolio-multi-channels', StoreIrisPortfolioToMultiChannels::class)->name('multi_channels.portfolio.store');
 
-//Route::post('portfolio-all-channels', StoreIrisPortfolioToAllChannels::class)->name('all_channels.portfolio.store');
-//Route::post('portfolio-multi-channels', StoreIrisPortfolioToMultiChannels::class)->name('multi_channels.portfolio.store');
+Route::post('delete-portfolio-multi-channels', DeleteIrisPortfolioFromMultiChannels::class)->name('multi_channels.portfolio.store');
 
 Route::post('favourites/{product:id}', StoreIrisPortfolioFavourites::class)->name('favourites.store');
 Route::delete('un-favourites/{product:id}', DeleteIrisPortfolioFavourites::class)->name('favourites.delete');
