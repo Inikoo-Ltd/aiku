310d37eea92806e60759924dc72e0d4c4e89ebc2
KirinZero0 <aryapermana02@gmail.com>
2025-06-24 10:24:38 +0800

    product portfolios id and web images

--- app/Actions/Web/WebBlock/GetWebBlockProduct.php
+++ app/Actions/Web/WebBlock/GetWebBlockProduct.php
@@ -20,10 +20,13 @@ class GetWebBlockProduct
     {
 
         $permissions =  [];
+        $portfolios = $webpage->model->portfolios()->pluck('id')->toArray();
 
         data_set($webBlock, 'web_block.layout.data.permissions', $permissions);
         data_set($webBlock, 'web_block.layout.data.fieldValue', $webpage->website->published_layout['product']['data']['fieldValue'] ?? []);
         data_set($webBlock, 'web_block.layout.data.fieldValue.product', WebBlockProductResource::make($webpage->model)->toArray(request()));
+        data_set($webBlock, 'web_block.layout.data.fieldValue.productHasPortfolio', $portfolios);
+
         return $webBlock;
     }
 
