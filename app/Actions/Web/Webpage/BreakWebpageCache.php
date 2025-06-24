675ffdcaa432a2f7d2d2824b2115948a4ca5b07f
Raul Perusquia <raul@inikoo.com>
2025-06-24 06:34:07 +0100

    feat: Add `BreakWebpageCache` action and integrate into `PublishWebpage` process to handle cache invalidation

---
+++ app/Actions/Web/Webpage/BreakWebpageCache.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 24 Jun 2025 13:03:07 Malaysia Time, Sheffield, United Kingdom
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Web\Webpage;
+
+use App\Models\Web\Webpage;
+use Illuminate\Support\Facades\Cache;
+use Lorisleiva\Actions\Concerns\AsObject;
+
+class BreakWebpageCache
+{
+    use asObject;
+
+    public function handle(Webpage $webpage):void
+    {
+        $key = config('iris.cache.webpage.prefix').'_'.$webpage->website_id.'_in_'.$webpage->id;
+        Cache::forget($key);
+        $key = config('iris.cache.webpage.prefix').'_'.$webpage->website_id.'_out_'.$webpage->id;
+        Cache::forget($key);
+    }
+}
