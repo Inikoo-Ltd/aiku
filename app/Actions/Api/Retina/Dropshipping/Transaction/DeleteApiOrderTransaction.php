03bb4ea0cc0f99d2071edb791d263c2be07a437c
KirinZero0 <aryapermana02@gmail.com>
2025-06-25 17:13:12 +0800

    Blocking Actions if order already beyond creating

--- app/Actions/Api/Retina/Dropshipping/Transaction/DeleteApiOrderTransaction.php
+++ app/Actions/Api/Retina/Dropshipping/Transaction/DeleteApiOrderTransaction.php
@@ -12,7 +12,9 @@ namespace App\Actions\Api\Retina\Dropshipping\Transaction;
 use App\Actions\Api\Retina\Dropshipping\Resource\TransactionApiResource;
 use App\Actions\Ordering\Transaction\DeleteTransaction;
 use App\Actions\RetinaApiAction;
+use App\Enums\Ordering\Order\OrderStateEnum;
 use App\Models\Ordering\Transaction;
+use Illuminate\Http\JsonResponse;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
@@ -22,8 +24,13 @@ class DeleteApiOrderTransaction extends RetinaApiAction
     use AsAction;
     use WithAttributes;
 
-    public function handle(Transaction $transaction): Transaction
+    public function handle(Transaction $transaction): Transaction|JsonResponse
     {
+        if($transaction->order->state != OrderStateEnum::CREATING) {
+            return response()->json([
+                'message' => 'This order is already in the "' . $transaction->order->state->value . '" state and cannot be updated.',
+            ]);
+        }
         return DeleteTransaction::make()->action($transaction);
     }
 
@@ -40,7 +47,7 @@ class DeleteApiOrderTransaction extends RetinaApiAction
             ]);
     }
 
-    public function asController(Transaction $transaction, ActionRequest $request): Transaction
+    public function asController(Transaction $transaction, ActionRequest $request): Transaction|JsonResponse
     {
         $this->initialisationFromDropshipping($request);
         return $this->handle($transaction);
