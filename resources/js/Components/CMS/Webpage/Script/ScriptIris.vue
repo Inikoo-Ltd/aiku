813a344c412b1f9c2ce8a5a25a79d90b516d3791
YudhistiraA <aryarajasa0@gmail.com>
2025-07-01 18:28:24 +0800

    Setup layout in Blocks

--- resources/js/Components/CMS/Webpage/Script/ScriptIris.vue
+++ resources/js/Components/CMS/Webpage/Script/ScriptIris.vue
@@ -2,20 +2,25 @@
 import { faCube, faStar } from "@fortawesome/free-solid-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import "swiper/swiper-bundle.css" // Swiper styles
-
+import { inject } from "vue"
+import { getStyles } from "@/Composables/styles"
 library.add(faCube, faStar)
 
 const props = defineProps<{
     fieldValue: {
         value: string
     }
+    screenType: "mobile" | "tablet" | "desktop"
 }>()
-
+const layout: any = inject("layout", {})
 
 </script>
 
 <template>
-    <div class="w-full py-12 px-8 flex gap-x-10">
-        <div v-html="fieldValue?.value"></div>
+    <div id="script">
+        <div :style="getStyles(layout?.app?.webpage_layout?.container?.properties, screenType)"
+            class="w-full py-6 px-6 flex gap-x-10 editor-class overflow-x-auto font-mono">
+            <div v-html="fieldValue.value"></div>
+        </div>
     </div>
 </template>
