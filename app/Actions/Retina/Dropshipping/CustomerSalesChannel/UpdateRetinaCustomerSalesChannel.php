4306b8479f45ee39945e61cab0726cec26e588fa
KirinZero0 <aryapermana02@gmail.com>
2025-07-01 11:57:22 +0800

    Fix edit customer sales channel

--- app/Actions/Retina/Dropshipping/CustomerSalesChannel/UpdateRetinaCustomerSalesChannel.php
+++ app/Actions/Retina/Dropshipping/CustomerSalesChannel/UpdateRetinaCustomerSalesChannel.php
@@ -11,12 +11,17 @@ namespace App\Actions\Retina\Dropshipping\CustomerSalesChannel;
 use App\Actions\Dropshipping\CustomerSalesChannel\UpdateCustomerSalesChannel;
 use App\Actions\RetinaAction;
 use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Dropshipping\CustomerSalesChannelStatusEnum;
 use App\Models\Dropshipping\CustomerSalesChannel;
+use App\Rules\IUnique;
+use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 
 class UpdateRetinaCustomerSalesChannel extends RetinaAction
 {
     use WithActionUpdate;
+
+    private CustomerSalesChannel $customerSalesChannel;
 
     public function handle(CustomerSalesChannel $customerSalesChannel, array $modelData): CustomerSalesChannel
     {
@@ -25,8 +30,34 @@ class UpdateRetinaCustomerSalesChannel extends RetinaAction
         return $customerSalesChannel;
     }
 
+    public function rules(): array
+    {
+        return [
+            'reference'          => [
+                'sometimes',
+                'required',
+                'max:255',
+                'string',
+                new IUnique(
+                    table: 'customer_sales_channels',
+                    extraConditions: [
+                        ['column' => 'customer_id', 'value' => $this->customerSalesChannel->customer_id],
+                        [
+                            'column'   => 'id',
+                            'operator' => '!=',
+                            'value'    => $this->customerSalesChannel->id
+                        ]
+                    ]
+                ),
+            ],
+            'status'       => ['sometimes', Rule::enum(CustomerSalesChannelStatusEnum::class)],
+            'name' => ['sometimes', 'string', 'max:255']
+        ];
+    }
+
     public function asController(CustomerSalesChannel $customerSalesChannel, ActionRequest $request): void
     {
+        $this->customerSalesChannel = $customerSalesChannel;
         $this->initialisation($request);
 
         $this->handle($customerSalesChannel, $this->validatedData);
