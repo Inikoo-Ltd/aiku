26c0defb13dff417abaec9cc47898c6ef9121949
YudhistiraA <aryarajasa0@gmail.com>
2025-07-03 16:13:32 +0800

    fix Get BluePints

--- resources/js/Composables/getBlueprintWorkshop.ts
+++ resources/js/Composables/getBlueprintWorkshop.ts
@@ -8,21 +8,17 @@ import Header1Blueprint from "@/Components/CMS/Website/Headers/Header1/Blueprint
 import Header2Blueprint from "@/Components/CMS/Website/Headers/Header2/Blueprint"
 import BentoGridBlueprint from "@/Components/CMS/Webpage/BentoGrid/Blueprint"
 import CTA2Blueprint from "@/Components/CMS/Webpage/Cta2/Blueprint"
-import CategoriesBlueprint from "@/Components/CMS/Webpage/Categories/Blueprint"
 import CTA3Blueprint from "@/Components/CMS/Webpage/Cta3/Blueprint"
 import CTAAurora1Blueprint from "@/Components/CMS/Webpage/CtaAurora1/Blueprint"
 import CTABlueprint from "@/Components/CMS/Webpage/Cta1/Blueprint"
-import DepartmentBlueprint from "@/Components/CMS/Webpage/Department1/Blueprint"
 import GalleryBlueprint from "@/Components/CMS/Webpage/Gallery/Blueprint"
 import IframeBlueprint from "@/Components/CMS/Webpage/Iframe/Blueprint"
 import ImageBlueprint from "@/Components/CMS/Webpage/Image/Blueprint"
 import Overview2Blueprint from "@/Components/CMS/Webpage/Overview2/Blueprint"
 import OverviewBlueprint from "@/Components/CMS/Webpage/Overview/Blueprint"
-import ReviewsBlueprint from "@/Components/CMS/Webpage/Reviews/Blueprint"
 import TextBlueprint from "@/Components/CMS/Webpage/Text/Blueprint"
 import ScriptBlueprint from "@/Components/CMS/Webpage/Script/Blueprint"
 import WowsbarBannerBlueprint from "@/Components/CMS/Webpage/WowsbarBanner/Blueprint"
-import SeeAlsoBlueprint from "@/Components/CMS/Webpage/SeeAlso1/Blueprint"
 import PricingBlueprint from "@/Components/CMS/Webpage/Pricing/Blueprint"
 import TimelineBlueprint from "@/Components/CMS/Webpage/Timeline/Blueprint"
 import TextColumn from "@/Components/CMS/Webpage/TextColumn/Blueprint"
@@ -43,15 +39,14 @@ import VideoBlueprint from '@/Components/CMS/Webpage/Video/Blueprint'
 import CTAVideo1Blueprint from '@/Components/CMS/Webpage/CtaVideo1/Blueprint'
 import CollectionBluprint from '@/Components/CMS/Webpage/Collections1/Blueprint'
 import UserSubscribe from "@/Components/CMS/Webpage/UserSubscribe/Blueprint"
+import Product1Workshop from "@/Components/CMS/Webpage/Product1/Blueprint"
 
 import Cta4Blueprint from "@/Components/CMS/Webpage/Cta4/Blueprint"
 import { blueprint as SeeAlso1Blueprint  } from "@/Components/CMS/Webpage/SeeAlso1/Blueprint"
 
-import { data } from "autoprefixer"
-import CTAVideo1Workshop from "@/Components/CMS/Webpage/CtaVideo1/CtaVideo1Workshop.vue"
 
-export const getBlueprint = (componentName: string, data : object ) => {
-	const components: Component = {
+export const getBlueprint = (componentName: string, data?: object) => {
+	const components: Record<string, any> = {
 		// topbar
 		"top-bar-1-fulfilment": Topbar1Fulfilment.blueprint,
 		"top-bar-2-fulfilment": Topbar2Fulfilment.blueprint,
@@ -60,33 +55,36 @@ export const getBlueprint = (componentName: string, data : object ) => {
 		"top-bar-2": Topbar2Blueprint.blueprint,
 		"top-bar-3": Topbar3Blueprint.blueprint,
 
-		//header
+		// header
 		"header-1": Header1Blueprint.blueprint,
 		"header-2": Header2Blueprint.blueprint,
 
-		//footer
+		// footer
 		"footer-1": Footer1Blueprint.blueprint,
-		
-		//depeartement
+
+		// departments
 		"departments": DepartementBlueprint.blueprint,
 		"department-1": DepartementBlueprint.blueprint,
 
-		//sub-departement
+		// sub-department
 		"sub-departments-1": SubDepartement1Blueprint.blueprint,
 
-		//family
+		// family
 		"family-1": FamilyBluprint.blueprint,
 
-		//families list
+		// families list
 		"families-1": FamilyBluprint.blueprint,
 
-		//products-list
-		'products-1' : ProductsList1Blueprint.blueprint,
+		// products list
+		"products-1": ProductsList1Blueprint.blueprint,
 
-		//see also
-		'see-also-1' : SeeAlso1Blueprint(data).blueprint,
+		// product
+		"product-1": Product1Blueprint.blueprint,
 
+		// see also — accepts dynamic data
+		"see-also-1": () => SeeAlso1Blueprint(data).blueprint,
 
+		// misc
 		"banner": WowsbarBannerBlueprint.blueprint,
 		"bento-grid-1": BentoGridBlueprint.blueprint,
 		"bricks": GalleryBlueprint.blueprint,
@@ -98,7 +96,6 @@ export const getBlueprint = (componentName: string, data : object ) => {
 		"images": ImageBlueprint.blueprint,
 		"overview_aurora": OverviewBlueprint.blueprint,
 		"pricing": PricingBlueprint.blueprint,
-		"product-1": Product1Blueprint.blueprint,
 		"script": ScriptBlueprint.blueprint,
 		"text": TextBlueprint.blueprint,
 		"cta_aurora_1": CTAAurora1Blueprint.blueprint,
@@ -109,13 +106,15 @@ export const getBlueprint = (componentName: string, data : object ) => {
 		"timeline": TimelineBlueprint.blueprint,
 		"step-1": Timeline2Bluprint.blueprint,
 		"carousel-1": Carousel1Blueprint.blueprint,
-		"cta-video-1" : CTAVideo1Blueprint.blueprint,
-		'video-1': VideoBlueprint.blueprint,
-		'collections-1': CollectionBluprint.blueprint,
-    	"user-subscribe": UserSubscribe.blueprint,
-		"cta4" : Cta4Blueprint.blueprint
+		"cta-video-1": CTAVideo1Blueprint.blueprint,
+		"video-1": VideoBlueprint.blueprint,
+		"collections-1": CollectionBluprint.blueprint,
+		"user-subscribe": UserSubscribe.blueprint,
+		"cta4": Cta4Blueprint.blueprint,
 	}
-	return components[componentName] ?? []
+
+	const blueprint = components[componentName]
+	return typeof blueprint === 'function' ? blueprint() : blueprint ?? []
 }
 
 /* export const getBluprintPermissions = (componentName: string) => {
