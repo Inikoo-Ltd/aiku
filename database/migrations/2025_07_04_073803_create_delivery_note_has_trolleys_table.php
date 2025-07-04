e4283a14b53d177da75ecae48dfd4085cb8a43ce
Ganes556 <gustiganes@gmail.com>
2025-07-04 15:46:47 +0800

    migration

---
+++ database/migrations/2025_07_04_073803_create_delivery_note_has_trolleys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Stubs\Migrations\HasGroupOrganisationRelationship;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    use HasGroupOrganisationRelationship;
+
+    public function up(): void
+    {
+        Schema::create('delivery_note_has_trolleys', function (Blueprint $table) {
+            $table = $this->groupOrgRelationship($table);
+
+            $table->unsignedInteger('delivery_note_id')->nullable()->index();
+            $table->unsignedInteger('trolley_id')->nullable()->index();
+
+            $table->foreign('delivery_note_id')
+                ->references('id')->on('delivery_notes')
+                ->nullOnDelete();
+
+            $table->foreign('trolley_id')
+                ->references('id')->on('trolleys')
+                ->nullOnDelete();
+
+            $table->string('state')->index()->nullable();
+
+            $table->timestampsTz();
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('delivery_note_has_trolleys');
+    }
+};
