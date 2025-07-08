0e5c27cebe7f95c63a40e34cd3219721d5195495
Ganes <gustiganes@gmail.com>
2025-07-08 16:48:48 +0800

    feat: Add cancel action for orders and handle related invoice and delivery note updates

--- app/Actions/Ordering/Order/UI/GetOrderActions.php
+++ app/Actions/Ordering/Order/UI/GetOrderActions.php
@@ -175,9 +175,26 @@ class GetOrderActions
                 ],
                 default => []
             };
+
+            if ($order->state !== OrderStateEnum::CANCELLED && !app()->isProduction()) {
+                $actions[] = [
+                    'type'    => 'button',
+                    'style'   => 'danger',
+                    'icon'    => 'fal fa-times',
+                    'key'     => 'action',
+                    'label'   => __('cancel'),
+                    'tooltip' => __('Cancel Order'),
+                    'route'   => [
+                        'method'     => 'patch',
+                        'name'       => 'grp.models.order.state.cancelled',
+                        'parameters' => [
+                            'order' => $order->id
+                        ]
+                    ]
+                ];
+            }
         }
 
         return $actions;
     }
-
 }
