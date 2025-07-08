899374e3343568a67f9ff9debed9c7b33b8b8a87
KirinZero0 <aryapermana02@gmail.com>
2025-07-08 17:33:12 +0800

    NEw Order Email Send and minor fixes

---
+++ app/Actions/Comms/Email/SendNewOrderEmailToCustomer.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 08-07-2025-16h-31m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Comms\Email;
+
+use App\Actions\Comms\DispatchedEmail\StoreDispatchedEmail;
+use App\Actions\Comms\Traits\WithSendBulkEmails;
+use App\Actions\OrgAction;
+use App\Actions\Traits\Rules\WithNoStrictRules;
+use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Comms\DispatchedEmail\DispatchedEmailProviderEnum;
+use App\Enums\Comms\Outbox\OutboxCodeEnum;
+use App\Enums\Comms\Outbox\OutboxTypeEnum;
+use App\Models\Comms\DispatchedEmail;
+use App\Models\Comms\Email;
+use App\Models\Comms\Outbox;
+use App\Models\Ordering\Order;
+
+class SendNewOrderEmailToCustomer extends OrgAction
+{
+    use WithActionUpdate;
+    use WithNoStrictRules;
+    use WithSendBulkEmails;
+
+    private Email $email;
+
+    public function handle(Order $order): ?DispatchedEmail
+    {
+        $customer = $order->customer;
+        $recipient       = $customer;
+        if (!$recipient->email) {
+            return null;
+        }
+
+        /** @var Outbox $outbox */
+        $outbox = $customer->shop->outboxes()->where('code', OutboxCodeEnum::NEW_ORDER->value)->first();
+        $outboxDispatch = $customer->shop->outboxes()->where('type', OutboxTypeEnum::CUSTOMER_NOTIFICATION)->first();
+
+
+        $dispatchedEmail = StoreDispatchedEmail::run($outbox->emailOngoingRun, $recipient, [
+            'is_test'       => false,
+            'outbox_id'     => $outboxDispatch->id,
+            'email_address' => $recipient->email,
+            'provider'      => DispatchedEmailProviderEnum::SES
+        ]);
+        $dispatchedEmail->refresh();
+
+        $emailHtmlBody = $outbox->emailOngoingRun->email->liveSnapshot->compiled_layout;
+        if (!$emailHtmlBody) {
+            return null;
+        }
+
+        $baseUrl = 'https://ds.test';
+        if (app()->isProduction()) {
+            $baseUrl = 'https://'.$order->shop->website->domain;
+        }
+
+        $orderUrl = $baseUrl.'/app/dropshipping/channels/'.$order->customerSalesChannel->slug.'/orders/'.$order->slug;
+        $customerUrl = $baseUrl.'/app/dashboard';
+        return $this->sendEmailWithMergeTags(
+                $dispatchedEmail,
+                $outbox->emailOngoingRun->sender(),
+                $outbox->emailOngoingRun?->email?->subject,
+                $emailHtmlBody,
+                '',
+                additionalData: [
+                    'customer_name' => $customer->name,
+                    'order_reference' => $order->reference,
+                    'date' => $order->created_at->format('F jS, Y'),
+                    'order_link' => $orderUrl,
+                    'customer_link' => ''
+                ]
+            );
+    }
+}
--- app/Actions/Ordering/Order/SubmitOrder.php
+++ app/Actions/Ordering/Order/SubmitOrder.php
@@ -8,6 +8,7 @@
 
 namespace App\Actions\Ordering\Order;
 
+use App\Actions\Comms\Email\SendNewOrderEmailToCustomer;
 use App\Actions\Comms\Email\SendNewOrderToCustomerNotification;
 use App\Actions\CRM\Customer\Hydrators\CustomerHydrateBasket;
 use App\Actions\Dropshipping\CustomerClient\Hydrators\CustomerClientHydrateBasket;
@@ -68,6 +69,7 @@ class SubmitOrder extends OrgAction
 
         $this->orderHydrators($order);
         SendNewOrderToCustomerNotification::dispatch($order);
+        SendNewOrderEmailToCustomer::dispatch($order);
 
         return $order;
     }
