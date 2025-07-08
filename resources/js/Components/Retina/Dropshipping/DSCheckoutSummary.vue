4e961521002e935e252bd285e3e398dc5361649a
aqordeon <aqordivika@yahoo.co.id>
2025-07-08 08:22:21 +0800

    fix: Retina DS: boxstats show weight

--- resources/js/Components/Retina/Dropshipping/DSCheckoutSummary.vue
+++ resources/js/Components/Retina/Dropshipping/DSCheckoutSummary.vue
@@ -11,6 +11,7 @@ import AddressEditModal from "@/Components/Utils/AddressEditModal.vue"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { faIdCardAlt } from "@fal"
 import { library } from "@fortawesome/fontawesome-svg-core"
+import NeedToPay from "@/Components/Utils/NeedToPay.vue"
 library.add(faIdCardAlt)
 
 defineProps<{
@@ -124,7 +125,52 @@ const isModalShippingAddress = ref(false)
         </div>
 
         <div class="col-span-2">
-         Weight: {{summary.order_properties.weight}}
+            <dl v-if="false" class="relative flex items-start w-full flex-none gap-x-1">
+                <dt class="flex-none pt-0.5">
+                    <FontAwesomeIcon icon='fal fa-dollar-sign' fixed-width aria-hidden='true' class="text-gray-500" />
+                </dt>
+
+                <!-- <NeedToPay
+                     @click="onPayClick"
+                    :totalAmount="box_stats.products.payment.total_amount"
+                    :paidAmount="box_stats.products.payment.paid_amount"
+                    :payAmount="box_stats.products.payment.pay_amount"
+                    :class="[box_stats.products.payment.pay_amount ? 'hover:bg-gray-100 cursor-pointer' : '']"
+                    :currencyCode="currency.code" /> -->
+            </dl>
+
+            <div class="mt-1 flex items-center w-full flex-none gap-x-1.5">
+                <dt v-tooltip="trans('Weight')" class="flex-none">
+                    <FontAwesomeIcon icon='fal fa-weight' fixed-width aria-hidden='true' class="text-gray-500" />
+                </dt>
+                
+                <dd class="text-gray-500" v-tooltip="trans('Estimated weight of all products (in kilograms)')">
+                    {{ locale.number(summary.order_properties.weight || 0) }} kg
+                </dd>
+            </div>
+
+
+            <!-- <div v-if="delivery_note" class="mt-1 flex items-center w-full flex-none justify-between">
+                <Link
+                    :href="route(routes.delivery_note.deliveryNoteRoute.name, routes.delivery_note.deliveryNoteRoute.parameters)"
+                    class="flex items-center gap-3 gap-x-1.5 primaryLink cursor-pointer">
+                <dt class="flex-none">
+                    <FontAwesomeIcon icon='fal fa-truck' fixed-width aria-hidden='true' class="text-gray-500" />
+                </dt>
+                <dd class="text-gray-500 " v-tooltip="trans('Delivery Note')">
+                    {{ delivery_note?.reference }}
+                </dd>
+                </Link>
+                <a :href="route(routes.delivery_note.deliveryNotePdfRoute.name, routes.delivery_note.deliveryNotePdfRoute.parameters)"
+                    as="a" target="_blank" class="flex items-center">
+                    <button class="flex items-center">
+                        <div class="flex-none">
+                            <FontAwesomeIcon :icon="faFilePdf" fixed-width aria-hidden="true"
+                                class="text-gray-500 hover:text-indigo-500 transition-colors duration-200" />
+                        </div>
+                    </button>
+                </a>
+            </div> -->
         </div>
 
         <div class="col-span-3">
