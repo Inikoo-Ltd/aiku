edcb8233f21f615dc92abc8ba4ce1d563da5ba51
KirinZero0 <aryapermana02@gmail.com>
2025-07-08 12:42:11 +0800

    maya profile

---
+++ app/Http/Resources/SysAdmin/MayaUserResource.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 08-07-2025-12h-31m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Http\Resources\SysAdmin;
+
+use App\Http\Resources\Api\Dropshipping\ShopResource;
+use App\Http\Resources\Api\Dropshipping\ShopsResource;
+use App\Http\Resources\Fulfilment\FulfilmentResource;
+use App\Http\Resources\Fulfilment\FulfilmentsResource;
+use App\Http\Resources\Inventory\WarehouseResource;
+use App\Http\Resources\Inventory\WarehousesResource;
+use App\Http\Resources\SysAdmin\Organisation\OrganisationResource;
+use App\Http\Resources\SysAdmin\Organisation\OrganisationsResource;
+use App\Models\SysAdmin\User;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class MayaUserResource extends JsonResource
+{
+    public function toArray($request): array|Arrayable|JsonSerializable
+    {
+        /** @var User $user */
+        $user = $this;
+
+        return [
+            'id'                        => $this->id,
+            'username'                  => $this->username,
+            'image'                     => $user->imageSources(48, 48),
+            'email'                     => $this->email,
+            'about'                     => $user->about,
+            'status'                    => match ($this->status) {
+                true => [
+                    'tooltip' => __('active'),
+                    'icon'    => 'fal fa-check',
+                    'class'   => 'text-green-500'
+                ],
+                default => [
+                    'tooltip' => __('suspended'),
+                    'icon'    => 'fal fa-times',
+                    'class'   => 'text-red-500'
+                ]
+            },
+            'number_current_api_tokens' => $this->number_current_api_tokens,
+            'number_expired_api_tokens' => $this->number_expired_api_tokens,
+            'parent_type'               => $this->parent_type,
+            'contact_name'              => $this->contact_name,
+            'authorised_organisations'  => OrganisationResource::collection($user->authorisedOrganisations)->resolve(),
+            'authorised_shops'          => ShopResource::collection($user->authorisedShops)->resolve(),
+            'authorised_fulfilments'    => FulfilmentResource::collection($user->authorisedFulfilments)->resolve(),
+            'authorised_warehouses'     => WarehouseResource::collection($user->authorisedWarehouses)->resolve(),
+
+
+
+        ];
+    }
+}
