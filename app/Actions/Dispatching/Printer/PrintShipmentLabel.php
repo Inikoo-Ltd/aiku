49b0b199611a478788bec75671eb0dd0bc3d05ae
Raul Perusquia <raul@inikoo.com>
2025-07-08 08:03:33 +0100

    refactor: add return type annotations, improve code formatting, and adjust printer validation logic in `PrintShipmentLabel` class

--- app/Actions/Dispatching/Printer/PrintShipmentLabel.php
+++ app/Actions/Dispatching/Printer/PrintShipmentLabel.php
@@ -14,7 +14,7 @@ class PrintShipmentLabel extends OrgAction
 {
     use WithPrintNode;
 
-    public function handle(Shipment $shipment)
+    public function handle(Shipment $shipment): \Rawilk\Printing\Api\PrintNode\Resources\PrintJob
     {
         if ($shipment->combined_label_url) {
             return $this->printPdfFromPdfUri(
@@ -22,13 +22,14 @@ class PrintShipmentLabel extends OrgAction
                 printId: $this->get('printerId'),
                 pdfUri: $shipment->combined_label_url
             );
-        }elseif($shipment->label && $shipment->label_type == ShipmentLabelTypeEnum::HTML) {
+        } elseif ($shipment->label && $shipment->label_type == ShipmentLabelTypeEnum::HTML) {
             return $this->printRawBase64(
                 title: $shipment->tracking,
                 printId: $this->get('printerId'),
                 rawBase64: $shipment->label
             );
         }
+
         return $this->printPdf(
             title: $shipment->tracking,
             printId: $this->get('printerId'),
@@ -41,8 +42,8 @@ class PrintShipmentLabel extends OrgAction
      */
     public function afterValidator(Validator $validator): void
     {
-        $user = request()->user();
-        $printerId = Arr::get($user->settings, 'preferred_printer_id', null);
+        $user      = request()->user();
+        $printerId = Arr::get($user->settings, 'preferred_printer_id');
         if (!$printerId) {
             throw ValidationException::withMessages([
                 'messages' => __('You must set a preferred printer in your user settings!'),
@@ -55,12 +56,12 @@ class PrintShipmentLabel extends OrgAction
                 'messages' => __('Print by Printnode is not enabled for your group!'),
             ]);
         }
-        
-        
+
+
         $this->set('printerId', $printerId);
     }
 
-    public function asController(Shipment $shipment, ActionRequest $request)
+    public function asController(Shipment $shipment, ActionRequest $request): \Rawilk\Printing\Api\PrintNode\Resources\PrintJob
     {
         $this->initialisationFromGroup($shipment->group, $request);
 
