30affcf2f5083833f08826e0c37ccd314ba49583
Raul Perusquia <raul@inikoo.com>
2025-07-10 17:36:50 +0100

    add `dividend` and `divisor` columns to `product_has_org_stocks` for handling non-integer quantities

---
+++ database/migrations/2025_07_10_163252_add_divisor_to_product_has_org_stocks_table.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 10 Jul 2025 17:36:00 British Summer Time, Sheffield, UK
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::table('product_has_org_stocks', function (Blueprint $table) {
+            $table->unsignedSmallInteger('dividend')->default(1)->comment('helper for non integer quantities');
+            $table->unsignedSmallInteger('divisor')->default(1)->comment('helper for non integer quantities');
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('product_has_org_stocks', function (Blueprint $table) {
+            $table->dropColumn('divisor', 'dividend');
+        });
+    }
+};
