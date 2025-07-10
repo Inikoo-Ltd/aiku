190c0f17e4f1b509c8c3e5b8296ee9c90ae4b404
aqordeon <aqordivika@yahoo.co.id>
2025-07-10 14:17:46 +0800

    fix: Iris: Products height is dynamic

--- resources/js/Components/CMS/Webpage/Products1/ProductRender.vue
+++ resources/js/Components/CMS/Webpage/Products1/ProductRender.vue
@@ -177,7 +177,7 @@ const onUnselectFavourite = (product: ProductResource) => {
 
             <!-- Product Image -->
 
-            <component :is="product.url ? Link : 'div'" :href="product.url" class="block w-full mb-1 rounded sm:h-[305px] h-[180px]">
+            <component :is="product.url ? Link : 'div'" :href="product.url" class="block w-full mb-1 rounded xsm:h-[305px] xh-[180px]">
                 <Image :src="product?.web_images?.main?.gallery" alt="product image" :imageCover="true"
                     :style="{ objectFit: 'contain' }" />
             </component>
