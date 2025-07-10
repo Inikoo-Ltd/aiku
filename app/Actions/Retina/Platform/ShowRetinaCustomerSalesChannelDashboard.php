bdf8721aff4e3ebfa4306fca81b8b65e0f164c7c
aqordeon <aqordivika@yahoo.co.id>
2025-07-10 08:15:42 +0800

    fix: Retina DS: Manual channel description

--- app/Actions/Retina/Platform/ShowRetinaCustomerSalesChannelDashboard.php
+++ app/Actions/Retina/Platform/ShowRetinaCustomerSalesChannelDashboard.php
@@ -155,7 +155,7 @@ class ShowRetinaCustomerSalesChannelDashboard extends RetinaAction
             ] : null,
             'headline'  => [
                 'title' => __('Web/API order management'),
-                'description' => '<p><span >First, add desired products to your </span><strong >Portfolio</strong><span > using the </span><strong >Add to Portfolio</strong><span > button. When an order comes in, find the customer under the </span><strong >Customers</strong><span > tab (add them if new), then click </span><strong >New Order.</strong><span > Finally, enter product codes and quantities to complete the order.</span></p>'
+                'description' => '<p><span>First, add desired products to your </span><strong>My Products</strong><span> using the </span><strong>Add Products</strong><span> button. When an order comes in, find the client under the </span><strong>Clients</strong><span> tab (add them if new), then click </span><strong>Create Order.</strong><span> Finally, enter product codes and quantities to complete the order.</span></p>'
             ],
             'customer_sales_channel' => $customerSalesChannel,
             'platform'               => $customerSalesChannel->platform,
