c7a946f8b62e4e559b877755be5d7802d79aff31
KirinZero0 <aryapermana02@gmail.com>
2025-07-16 15:35:45 +0800

    Invoice pdf links in email

---
+++ app/Actions/Accounting/Invoice/IrisPdfInvoice.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 24 Apr 2023 20:23:18 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Accounting\Invoice;
+
+use App\Actions\OrgAction;
+use App\Actions\Traits\WithExportData;
+use App\Models\Accounting\Invoice;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
+use Symfony\Component\HttpFoundation\Response;
+
+class IrisPdfInvoice extends OrgAction
+{
+    use AsAction;
+    use WithAttributes;
+    use WithExportData;
+    use WithInvoicesExport;
+
+
+    public function handle(Invoice $invoice): Response
+    {
+        return $this->processDataExportPdf($invoice);
+    }
+
+
+    public function asController(Invoice $invoice): Response
+    {
+
+        return $this->handle($invoice);
+    }
+}
