4bc1fc8973cb0c1a1e62c8e075fc1073e166b36f
KirinZero0 <aryapermana02@gmail.com>
2025-07-16 18:59:14 +0800

    state fix

--- app/Actions/Retina/Dropshipping/ProductCategory/UI/IndexRetinaFamilies.php
+++ app/Actions/Retina/Dropshipping/ProductCategory/UI/IndexRetinaFamilies.php
@@ -52,7 +52,7 @@ class IndexRetinaFamilies extends RetinaAction
 
         $queryBuilder = QueryBuilder::for(ProductCategory::class);
 
-        $queryBuilder->where('product_categories.state', ProductCategoryStateEnum::ACTIVE);
+        $queryBuilder->whereIn('product_categories.state', [ProductCategoryStateEnum::ACTIVE->value, ProductCategoryStateEnum::DISCONTINUED->value]);
 
 
         $queryBuilder->leftJoin('shops', 'product_categories.shop_id', 'shops.id');
