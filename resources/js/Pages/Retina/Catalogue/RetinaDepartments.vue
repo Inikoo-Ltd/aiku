55ba6ef4f8b51157d2482214e8295964aafbbd3d
YudhistiraA <aryarajasa0@gmail.com>
2025-07-16 19:03:25 +0800

    Departements

--- resources/js/Pages/Retina/Catalogue/RetinaDepartments.vue
+++ resources/js/Pages/Retina/Catalogue/RetinaDepartments.vue
@@ -10,41 +10,21 @@ import PageHeading from '@/Components/Headings/PageHeading.vue'
 import TableDepartments from "@/Components/Tables/Grp/Org/Catalogue/TableDepartments.vue"
 import { capitalize } from "@/Composables/capitalize"
 import { PageHeading as PageHeadingTypes } from "@/types/PageHeading"
-import { routeType } from '@/types/route'
-import Tabs from "@/Components/Navigation/Tabs.vue"
-import { useTabChange } from "@/Composables/tab-change"
-import { computed, ref } from "vue"
 
 const props = defineProps<{
     pageHead: PageHeadingTypes
     title: string
-    tabs: {
-        current: string
-        navigation: {}
-    },
-    data: {},
     index?: {}
     sales?: {}
+    data : {}
 }>()
 
-const currentTab = ref<string>(props.tabs.current)
-const handleTabUpdate = (tabSlug: string) => useTabChange(tabSlug, currentTab)
-
-const component = computed(() => {
-    const components: any = {
-        index: TableDepartments,
-        sales: TableDepartments,
-    }
-
-    return components[currentTab.value]
-})
+console.log(props)
 
 </script>
 
 <template>
-    <Head :title="capitalize(title)"/>
-    <PageHeading :data="pageHead"/>
-    <Tabs :current="currentTab" :navigation="tabs.navigation" @update:tab="handleTabUpdate" />
-    <component :is="component" :key="currentTab" :tab="currentTab" :data="props[currentTab]"></component>
+    <Head :title="capitalize(title)" />
+    <PageHeading :data="pageHead" />
+    <TableDepartments :data="index"></TableDepartments> // need to work
 </template>
-
