5875f1b8779fa38db019b266b61d311a8233661a
Raul Perusquia <raul@inikoo.com>
2025-07-17 09:15:51 +0200

    fix: update PickingsResource formatting and fractional quantity calculation
    - Adjusted attribute alignment for improved readability and consistency.
    - Updated `quantity_picked_fractional` to dynamically use the actual `quantity`.

--- app/Http/Resources/Dispatching/PickingsResource.php
+++ app/Http/Resources/Dispatching/PickingsResource.php
@@ -25,30 +25,30 @@ class PickingsResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id'                                => $this->id,
-            'not_picked_reason'                 => $this->not_picked_reason,
-            'not_picked_note'                   => $this->not_picked_note,
-            'quantity_picked'                   => (float)$this->quantity,
-            'quantity_picked_fractional'        => divideWithRemainder(findSmallestFactors(0.5)),
-            'engine'                            => $this->engine,
-            'picker_name'                       => $this->picker->contact_name,
-            'type'                              => $this->type,
-            'location_code'                     => $this->location?->code,
-            'location_slug'                     => $this->location?->slug,
-            'location_id'                       => $this->location?->id,
-            'update_route'                      => [
-                'name'  => 'grp.models.picking.update',
+            'id'                         => $this->id,
+            'not_picked_reason'          => $this->not_picked_reason,
+            'not_picked_note'            => $this->not_picked_note,
+            'quantity_picked'            => (float)$this->quantity,
+            'quantity_picked_fractional' => divideWithRemainder(findSmallestFactors($this->quantity)),
+            'engine'                     => $this->engine,
+            'picker_name'                => $this->picker->contact_name,
+            'type'                       => $this->type,
+            'location_code'              => $this->location?->code,
+            'location_slug'              => $this->location?->slug,
+            'location_id'                => $this->location?->id,
+            'update_route'               => [
+                'name'       => 'grp.models.picking.update',
                 'parameters' => [
                     'picking' => $this->id
                 ],
-                'method' => 'patch'
+                'method'     => 'patch'
             ],
-            'undo_picking_route'                => [
-                'name'  => 'grp.models.picking.delete',
+            'undo_picking_route'         => [
+                'name'       => 'grp.models.picking.delete',
                 'parameters' => [
                     'picking' => $this->id
                 ],
-                'method' => 'delete'
+                'method'     => 'delete'
             ],
         ];
     }
