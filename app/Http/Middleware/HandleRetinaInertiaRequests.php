7fff50a9c90f9d97230a6e2bdaeeb5af4b236242
KirinZero0 <aryapermana02@gmail.com>
2025-07-17 17:21:09 +0800

    Hanlde Retina Inertia Req

--- app/Http/Middleware/HandleRetinaInertiaRequests.php
+++ app/Http/Middleware/HandleRetinaInertiaRequests.php
@@ -9,12 +9,14 @@
 namespace App\Http\Middleware;
 
 use App\Actions\Retina\UI\GetRetinaFirstLoadProps;
+use App\Enums\Catalogue\Shop\ShopTypeEnum;
 use App\Http\Resources\UI\LoggedWebUserResource;
 use App\Models\CRM\WebUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Inertia\Middleware;
 use App\Http\Resources\Helpers\CurrencyResource;
+use Illuminate\Support\Facades\DB;
 use Tighten\Ziggy\Ziggy;
 
 class HandleRetinaInertiaRequests extends Middleware
@@ -48,12 +50,32 @@ class HandleRetinaInertiaRequests extends Middleware
         $firstLoadOnlyProps['environment'] = app()->environment();
 
 
+        $customerSalesChannels = [];
+        if ($webUser && $request->get('shop_type') == ShopTypeEnum::DROPSHIPPING->value) {
+            $channels = DB::table('customer_sales_channels')
+                ->leftJoin('platforms', 'customer_sales_channels.platform_id', '=', 'platforms.id')
+                ->select('customer_sales_channels.id', 'customer_sales_channels.name as customer_sales_channel_name', 'platform_id', 'platforms.slug', 'platforms.code', 'platforms.name')
+                ->where('customer_id', $webUser->customer_id)
+                ->get();
+
+            foreach ($channels as $channel) {
+                $customerSalesChannels[$channel->id] = [
+                    'customer_sales_channel_id' => $channel->id,
+                    'customer_sales_channel_name' => $channel->customer_sales_channel_name,
+                    'platform_id'               => $channel->platform_id,
+                    'platform_slug'             => $channel->slug,
+                    'platform_code'             => $channel->code,
+                    'platform_name'             => $channel->name,
+                ];
+            }
+        }
         return array_merge(
             $firstLoadOnlyProps,
             [
                 'auth'     => [
                     'user'          => $webUser ? LoggedWebUserResource::make($webUser)->getArray() : null,
                     'webUser_count' => $webUser?->customer?->webUsers?->count() ?? 1,
+                    'customerSalesChannels' => $customerSalesChannels
                 ],
                 'currency' => [
                     'code'   => $website->shop->currency->code,
