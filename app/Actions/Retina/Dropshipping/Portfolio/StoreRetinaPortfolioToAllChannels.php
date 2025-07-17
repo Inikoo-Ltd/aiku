d070f9bc0cec8b1bde7cf365502e45d1b48cd334
KirinZero0 <aryapermana02@gmail.com>
2025-07-17 17:05:44 +0800

    Store To all channel

---
+++ app/Actions/Retina/Dropshipping/Portfolio/StoreRetinaPortfolioToAllChannels.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * author Arya Permana - Kirin
+ * created on 08-05-2025-16h-49m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Iris\Portfolio;
+
+use App\Actions\Dropshipping\Portfolio\StorePortfolio;
+use App\Actions\RetinaAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Catalogue\Product;
+use App\Models\Dropshipping\Portfolio;
+use Illuminate\Support\Arr;
+use Lorisleiva\Actions\ActionRequest;
+
+class StoreRetinaPortfolioToAllChannels extends RetinaAction
+{
+    use WithActionUpdate;
+
+
+    private Portfolio $portfolio;
+
+    public function handle(array $modelData): void
+    {
+        foreach ($this->customer->customerSalesChannels as $salesChannel) {
+            $items = Product::whereIn('id', Arr::get($modelData, 'item_id'))->get();
+
+
+            foreach ($items as $item) {
+                if ($salesChannel->portfolios()
+                    ->where('item_id', $item->id)
+                    ->where('item_type', $item->getMorphClass())
+                    ->exists()) {
+                    continue;
+                }
+
+                StorePortfolio::make()->action($salesChannel, $item, []);
+            }
+        }
+    }
+
+    public function rules(): array
+    {
+        return [
+            'item_id' => 'required|array|min:1',
+            'item_id.*' => 'required|integer|exists:products,id'
+        ];
+    }
+
+    public function asController(ActionRequest $request): void
+    {
+        $this->initialisation($request);
+
+        $this->handle($this->validatedData);
+    }
+}
