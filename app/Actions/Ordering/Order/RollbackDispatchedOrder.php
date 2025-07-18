a5c1ba22ddf5dc2bf56521f3f7fa5dedc1848c4f
KirinZero0 <aryapermana02@gmail.com>
2025-07-18 16:57:34 +0800

    Rollback Action

---
+++ app/Actions/Ordering/Order/RollbackDispatchedOrder.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 20 Jun 2023 20:33:11 Malaysia Time, Pantai Lembeng, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Ordering\Order;
+
+use App\Actions\Dispatching\DeliveryNote\UpdateDeliveryNote;
+use App\Actions\OrgAction;
+use App\Enums\Dispatching\DeliveryNote\DeliveryNoteStateEnum;
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Models\Ordering\Order;
+use Lorisleiva\Actions\ActionRequest;
+
+class RollbackDispatchedOrder extends OrgAction
+{
+    public function handle(Order $order)
+    {
+        if($order->invoices) {
+            $order->invoices->delete();
+        }
+
+        if($order->deliveryNotes) {
+            foreach($order->deliveryNotes as $deliveryNote) {
+                UpdateDeliveryNote::make()->action($deliveryNote,  [
+                    'state' => DeliveryNoteStateEnum::PACKED
+                ]);
+            }
+        }
+
+        UpdateOrder::make()->action($order, [
+            'state' => OrderStateEnum::PACKED
+        ]);
+    }
+
+    public function asController(Order $order, ActionRequest $request)
+    {
+        $this->initialisationFromShop($order->shop, $request);
+
+        return $this->handle($order);
+    }
+}
