d69b52b3505558746462683a33e8504bcafdb911
Raul Perusquia <raul@inikoo.com>
2025-07-22 14:53:03 +0200

    feat: add picking location IDs to org stocks table and model
    - Added `picking_location_id` and `picking_dropshipping_location_id` to `org_stocks` table with indexing.
    - Updated `OrgStock` model properties to include new fields.

--- database/migrations/2025_07_22_104058_add_location_ids_to_org_stocks_table.php
+++ database/migrations/2025_07_22_104058_add_location_ids_to_org_stocks_table.php
@@ -1,17 +1,17 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 22 Jul 2025 11:45:10 British Summer Time, Trnava, Slovakia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class () extends Migration {
+    public function up(): void
     {
         Schema::table('org_stocks', function (Blueprint $table) {
             $table->unsignedInteger('picking_location_id')->nullable()->index();
@@ -22,12 +22,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('org_stocks', function (Blueprint $table) {
             $table->dropForeign(['picking_location_id']);
