24b2327f56a411392d48f86c8e8704261de55452
KirinZero0 <aryapermana02@gmail.com>
2025-07-22 16:48:57 +0800

    Single Prodyuct Export

---
+++ app/Exports/Marketing/SingleProductExport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Exports\Marketing;
+
+use App\Enums\Catalogue\Product\ProductStateEnum;
+use App\Models\Catalogue\Asset;
+use App\Models\Catalogue\Product;
+use App\Models\Catalogue\ProductCategory;
+use App\Models\Catalogue\Shop;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class SingleProductExport implements FromQuery, WithMapping, ShouldAutoSize, WithHeadings
+{
+    use Exportable;
+
+    protected $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function query(): Relation|\Illuminate\Database\Eloquent\Builder|Asset|Builder
+    {
+        return Product::query()->where('id', $this->product->id);
+    }
+
+    /** @var Asset $row */
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->code,
+            $row->state->value,
+            $row->name,
+            $row->description,
+            $row->available_quantity,
+            $row->gross_weight,
+            $row->price,
+            $row->rrp,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Code',
+            'State',
+            'Name',
+            'Description',
+            'Quantity',
+            'Weight',
+            'Price',
+            'RRP',
+        ];
+    }
+}
