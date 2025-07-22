a4ebf0acc462ba3798a57b38bd611559b70e0c48
YudhistiraA <aryarajasa0@gmail.com>
2025-07-22 16:38:58 +0800

    pickingsessions

---
+++ resources/js/Components/Warehouse/PickingSessions/TableDeliveryNoteItemInPickingSessions.vue
@@ -0,0 +1,49 @@
+<!--
+    -  Author: Vika Aqordi <aqordivika@yahoo.co.id>
+    -  Github: aqordeon
+    -  Created: Mon, 13 September 2024 11:24:07 Bali, Indonesia
+    -  Copyright (c) 2024, Vika Aqordi
+-->
+
+<script setup lang="ts">
+import { Link, router } from "@inertiajs/vue3";
+import Table from "@/Components/Table/Table.vue";
+import { Order } from "@/types/order";
+import type { Table as TableTS } from "@/types/Table";
+import Icon from "@/Components/Icon.vue";
+import NumberWithButtonSave from "@/Components/NumberWithButtonSave.vue";
+import { debounce, get, set } from 'lodash-es';
+import { notify } from "@kyvg/vue3-notification";
+import { trans } from "laravel-vue-i18n";
+import { routeType } from "@/types/route";
+import { Collapse } from "vue-collapsed";
+import { ref, onMounted, reactive, inject } from "vue";
+import Button from "@/Components/Elements/Buttons/Button.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faArrowDown, faDebug, faClipboardListCheck, faUndoAlt, faHandHoldingBox, faListOl } from "@fal";
+import { faSkull } from "@fas";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import axios from "axios";
+import ButtonWithLink from "@/Components/Elements/Buttons/ButtonWithLink.vue";
+import { aikuLocaleStructure } from "@/Composables/useLocaleStructure";
+import Modal from "@/Components/Utils/Modal.vue"
+import { RadioButton } from "primevue"
+import FractionDisplay from "@/Components/DataDisplay/FractionDisplay.vue"
+
+library.add(faSkull, faArrowDown, faDebug, faClipboardListCheck, faUndoAlt, faHandHoldingBox, faListOl);
+
+
+defineProps<{
+    data: TableTS
+    tab?: string
+    state: string
+}>();
+
+
+</script>
+
+<template>
+    <Table :resource="data" :name="tab" class="mt-5" rowAlignTop>
+        </Table>
+ 
+</template>
