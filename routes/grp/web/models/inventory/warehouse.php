93e74b843540b4ac7cbde4d0eb98c05ad64fd26d
KirinZero0 <aryapermana02@gmail.com>
2025-07-22 17:36:15 +0800

    button starty picking

--- routes/grp/web/models/inventory/warehouse.php
+++ routes/grp/web/models/inventory/warehouse.php
@@ -31,8 +31,10 @@ Route::name('warehouse.')->prefix('warehouse/{warehouse:id}')->group(function ()
     Route::patch('location/{pallet:id}', [UpdatePalletLocation::class, 'inWarehouse'])->name('pallets.location.update');
     Route::delete('', DeleteWarehouse::class)->name('delete');
     Route::post('picking-session', StorePickingSession::class)->name('picking_session.store');
-    Route::patch('picking-session/{pickingSession:id}', UpdatePickingSession::class)->name('picking_session.update')->withoutScopedBindings();
-    Route::patch('picking-session/{pickingSession:id}/start-picking', StartPickPickingSession::class)->name('picking_session.start_picking')->withoutScopedBindings();
 });
+
+Route::patch('picking-session/{pickingSession:id}', UpdatePickingSession::class)->name('picking_session.update')->withoutScopedBindings();
+Route::patch('picking-session/{pickingSession:id}/start-picking', StartPickPickingSession::class)->name('picking_session.start_picking')->withoutScopedBindings();
+
 Route::post('organisation/{organisation:id}/shipper', StoreShipper::class)->name('shipper.store');
 Route::patch('organisation/{organisation:id}/shipper/{shipper}', UpdateShipper::class)->name('shipper.update');
