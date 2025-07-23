363cfbd5e8a01484b28b7a1b6151a14d1f05b2b8
KirinZero0 <aryapermana02@gmail.com>
2025-07-23 19:05:12 +0800

    fix packing priob

--- app/Actions/Dispatching/PickingSession/AutoFinishPackingPickingSession.php
+++ app/Actions/Dispatching/PickingSession/AutoFinishPackingPickingSession.php
@@ -20,7 +20,7 @@ class AutoFinishPackingPickingSession extends OrgAction
     use WithActionUpdate;
     public function handle(PickingSession $pickingSession): PickingSession
     {
-        $numberPacked = $pickingSession->deliveryNotes->where('state', DeliveryNoteStateEnum::PACKED)->count();
+        $numberPacked = $pickingSession->deliveryNotes->whereIn('state', [DeliveryNoteStateEnum::PACKED, DeliveryNoteStateEnum::FINALISED, DeliveryNoteStateEnum::DISPATCHED])->count();
 
         if($numberPacked == $pickingSession->number_delivery_notes) {
             $this->update($pickingSession, [
