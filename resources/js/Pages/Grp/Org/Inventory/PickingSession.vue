dccd2cce214a4461bdcbb96893381b786bc44ebe
YudhistiraA <aryarajasa0@gmail.com>
2025-07-24 15:29:49 +0800

    current tab

--- resources/js/Pages/Grp/Org/Inventory/PickingSession.vue
+++ resources/js/Pages/Grp/Org/Inventory/PickingSession.vue
@@ -9,7 +9,7 @@ import PageHeading from "@/Components/Headings/PageHeading.vue";
 import { capitalize } from "@/Composables/capitalize";
 import { PageHeading as PageHeadingTypes } from "@/types/PageHeading";
 import { useTabChange } from "@/Composables/tab-change";
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import Tabs from "@/Components/Navigation/Tabs.vue";
 import TableDeliveryNoteItemInPickingSessions from "@/Components/Warehouse/PickingSessions/TableDeliveryNoteItemInPickingSessions.vue";
 import Timeline from "@/Components/Utils/Timeline.vue";
@@ -46,6 +46,10 @@ const component = computed(() => {
   return components[currentTab.value];
 
 });
+
+watch(() => props.tabs.current, (newTab) => {
+    currentTab.value = newTab
+}, { immediate: true })
 
 
 </script>
