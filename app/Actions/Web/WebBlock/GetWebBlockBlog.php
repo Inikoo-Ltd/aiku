fd02f73850e05ca643da1d658c37fb0817609623
KirinZero0 <aryapermana02@gmail.com>
2025-07-31 18:08:57 +0800

    Blog

---
+++ app/Actions/Web/WebBlock/GetWebBlockBlog.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * author Arya Permana - Kirin
+ * created on 03-07-2025-11h-03m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Web\WebBlock;
+
+use App\Models\Web\Webpage;
+use Lorisleiva\Actions\Concerns\AsObject;
+
+class GetWebBlockBlog
+{
+    use AsObject;
+
+
+    public function handle(Webpage $webpage, array $webBlock): array
+    {
+
+        $permissions = ['edit'];
+
+        data_set($webBlock, 'web_block.layout.data.permissions', $permissions);
+        data_set($webBlock, 'web_block.layout.data.fieldValue', []);
+
+        return $webBlock;
+    }
+}
