044a5abb23d27c7c575cd6dec2ab1dec4c65f3ef
KirinZero0 <aryapermana02@gmail.com>
2025-07-31 11:00:03 +0800

    Last  ORders

--- app/Http/Resources/Sales/OrderResource.php
+++ app/Http/Resources/Sales/OrderResource.php
@@ -26,12 +26,23 @@ class OrderResource extends JsonResource
             'state_label'         => $order->state->labels()[$order->state->value],
             'state_icon'          => $order->state->stateIcon()[$order->state->value],
             'public_notes'        => $order->public_notes,
+            'customer_name'        => $order->customer->name,
+            'customer_slug'        => $order->customer->slug,
             'customer_notes'        => $order->customer_notes,
             'shipping_notes'        => $order->shipping_notes,
             'payment_amount'      => $order->payment_amount,
             'total_amount'        => $order->total_amount,
             'is_fully_paid'       => $order->total_amount == $order->payment_amount,
             'unpaid_amount'       => $order->total_amount - $order->payment_amount,
+            'created_at'          => $order->created_at,
+            'updated_at'          => $order->updated_at,
+            'submitted_at'        => $order->submitted_at,
+            'in_warehouse_at'        => $order->in_warehouse_at,
+            'handling_at'        => $order->handling_at,
+            'packed_at'        => $order->packed_at,
+            'finalised_at'        => $order->finalised_at,
+            'dispatched_at'        => $order->dispatched_at,
+            'cancelled_at'        => $order->cancelled_at,
 
         ];
     }
