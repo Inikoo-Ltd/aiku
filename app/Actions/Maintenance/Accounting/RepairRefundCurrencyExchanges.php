013cee3a9796bba5372e45a232df4281ce498f34
Raul Perusquia <raul@inikoo.com>
2025-08-01 14:32:53 +0200

    fix: ensure proper handling of null `order` in `getBoxStats` method of `IsInvoiceUI`
    - Added null check for `order` property before accessing delivery notes to avoid potential errors.

---
+++ app/Actions/Maintenance/Accounting/RepairRefundCurrencyExchanges.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 01 Aug 2025 12:48:39 Central European Summer Time, Trnava, Slovakia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Maintenance\Accounting;
+
+use App\Actions\Helpers\CurrencyExchange\GetHistoricCurrencyExchange;
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Accounting\Invoice;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class RepairRefundCurrencyExchanges
+{
+    use WithActionUpdate;
+
+    protected function handle(Invoice $invoice): void
+    {
+        $orgExchange = GetHistoricCurrencyExchange::run($invoice->shop->currency, $invoice->organisation->currency, $invoice->date);
+        $grpExchange = GetHistoricCurrencyExchange::run($invoice->shop->currency, $invoice->group->currency, $invoice->date);
+
+
+        $invoice->update([
+            'org_exchange'   => $orgExchange,
+            'grp_exchange'   => $grpExchange,
+            'org_net_amount' => $invoice->net_amount * $orgExchange,
+            'grp_net_amount' => $invoice->net_amount * $grpExchange,
+        ]);
+
+
+    }
+
+    public string $commandSignature = 'repair:invoice_exchanges';
+
+    public function asCommand(Command $command): void
+    {
+        $invoice = Invoice::find(1018279);
+        $this->handle($invoice);
+
+        $count = Invoice::whereNull('source_id')->whereDate('date', '>', '2025-01-01')
+            ->count();
+
+        $command->info("pending: $count");
+
+        Invoice::whereNull('source_id')->whereDate('date', '>', '2025-01-01')
+            ->orderBy('date')
+            ->chunk(1000, function ($invoices) {
+                foreach ($invoices as $invoice) {
+                    $this->handle($invoice);
+                }
+            });
+    }
+
+}
