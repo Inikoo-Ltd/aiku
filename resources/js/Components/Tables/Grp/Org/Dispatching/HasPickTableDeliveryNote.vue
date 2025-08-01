05f2367f5f8ac587498d2f34afc207504093eb17
YudhistiraA <aryarajasa0@gmail.com>
2025-08-01 15:08:47 +0800

    INI-18

--- resources/js/Components/Tables/Grp/Org/Dispatching/HasPickTableDeliveryNote.vue
+++ resources/js/Components/Tables/Grp/Org/Dispatching/HasPickTableDeliveryNote.vue
@@ -182,13 +182,12 @@ const onCheckedAll = ({ data, allChecked }) => {
         @onCheckedAll="(data) => onCheckedAll(data)"
         checkboxKey='id' 
         :isChecked="(item) => selectedDeliveryNotes.includes(item.id)"
-        
         :disabledCheckbox="(item) => item.picking_sessions_count > 0"
     >
     
-        <template #header-checkbox="data">
+       <!--  <template #header-checkbox="data">
             <div></div>
-        </template>
+        </template> -->
 
         <template #disable-checkbox>
             <div></div>
