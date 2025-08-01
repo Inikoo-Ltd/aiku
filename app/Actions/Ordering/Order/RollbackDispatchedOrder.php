4b9647fccba5e357b789b716e301f08dc8e044f1
KirinZero0 <aryapermana02@gmail.com>
2025-08-01 16:54:28 +0800

    Rollback fix

--- app/Actions/Ordering/Order/RollbackDispatchedOrder.php
+++ app/Actions/Ordering/Order/RollbackDispatchedOrder.php
@@ -9,9 +9,13 @@
 namespace App\Actions\Ordering\Order;
 
 use App\Actions\Dispatching\DeliveryNote\UpdateDeliveryNote;
+use App\Actions\Dispatching\DeliveryNoteItem\UpdateDeliveryNoteItem;
+use App\Actions\Ordering\Transaction\UpdateTransaction;
 use App\Actions\OrgAction;
 use App\Enums\Dispatching\DeliveryNote\DeliveryNoteStateEnum;
+use App\Enums\Dispatching\DeliveryNoteItem\DeliveryNoteItemStateEnum;
 use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Enums\Ordering\Transaction\TransactionStateEnum;
 use App\Models\Ordering\Order;
 use Lorisleiva\Actions\ActionRequest;
 
@@ -26,14 +30,32 @@ class RollbackDispatchedOrder extends OrgAction
                         'state' => DeliveryNoteStateEnum::FINALISED,
                         'dispatched_at' => null,
                     ]);
+
+                    foreach($deliveryNote->deliveryNoteItems as $item) {
+                        if($item->state == DeliveryNoteItemStateEnum::DISPATCHED) {
+                            UpdateDeliveryNoteItem::make()->action($item, [
+                                'state' => DeliveryNoteItemStateEnum::FINALISED,
+                                'dispatched_at' => null,
+                            ]);
+                        }
+                    }
                 }
             }
         }
 
-        UpdateOrder::make()->action($order, [
-            'state' => OrderStateEnum::FINALISED,
-
-        ]);
+        if($order->state == OrderStateEnum::DISPATCHED) {
+            $order = UpdateOrder::make()->action($order, [
+                'state' => OrderStateEnum::FINALISED,
+                'dispatched_at' => null
+            ]);
+            foreach($order->transactions as $transaction) {
+                if($transaction->state == TransactionStateEnum::DISPATCHED) {
+                    UpdateTransaction::make()->action($transaction, [
+                        'state' => TransactionStateEnum::FINALISED,
+                    ]);
+                }
+            }
+        }
     }
 
     public function asController(Order $order, ActionRequest $request): void
