2e7546d2fdabbe25f796e20a71e93aa52a4d7754
KirinZero0 <aryapermana02@gmail.com>
2025-08-05 10:46:40 +0800

    fix resource import

--- app/Actions/CRM/Customer/UI/EditCustomer.php
+++ app/Actions/CRM/Customer/UI/EditCustomer.php
@@ -12,6 +12,7 @@ use App\Actions\Helpers\Country\UI\GetAddressData;
 use App\Actions\OrgAction;
 use App\Actions\Traits\Authorisations\WithCRMAuthorisation;
 use App\Http\Resources\Helpers\AddressFormFieldsResource;
+use App\Http\Resources\Helpers\TaxNumberResource;
 use App\Models\CRM\Customer;
 use App\Models\Catalogue\Shop;
 use App\Models\SysAdmin\Organisation;
