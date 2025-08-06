a0d6e8b1da5a551dea8c3903fa40ce28b0c05d12
Raul Perusquia <raul@inikoo.com>
2025-08-06 06:21:55 +0200

    refactor: enhance `UpdateTradeUnitTranslations` to handle multilingual attributes and update resource logic for improved i18n handling

--- app/Actions/Goods/TradeUnit/UpdateTradeUnitTranslations.php
+++ app/Actions/Goods/TradeUnit/UpdateTradeUnitTranslations.php
@@ -17,20 +17,49 @@ class UpdateTradeUnitTranslations extends OrgAction
 {
     use asAction;
 
-    public function handle(TradeUnit $tradeUnit, array $modelData)
+    public function handle(TradeUnit $tradeUnit, array $modelData): TradeUnit
     {
+        UpdateTradeUnit::run($tradeUnit, $modelData['master']);
+
+
+        $name_i8n=[];
+        $description_i8n=[];
+        $description_title_i8n=[];
+        $description_extra_i8n=[];
+
+        foreach ($modelData['translations'] as $locale => $translation) {
+            $name_i8n[$locale] = $translation['name'];
+            $description_i8n[$locale] = $translation['description'];
+            $description_title_i8n[$locale] = $translation['description_title'];
+            $description_extra_i8n[$locale] = $translation['description_extra'];
+        }
+        $tradeUnit->name_i8n = $name_i8n;
+        $tradeUnit->description_i8n = $description_i8n;
+        $tradeUnit->description_title_i8n = $description_title_i8n;
+        $tradeUnit->description_extra_i8n = $description_extra_i8n;
+        $tradeUnit->save();
+
+
+        return $tradeUnit;
+
 
     }
 
     public function rules(): array
     {
         return [
-
+            'master' => ['required', 'array'],
+            'master.name' => 'required|string',
+            'master.description' => ['present','nullable','string','max:10000'],
+            'master.description_title' => ['present','nullable','string','max:1000'],
+            'master.description_extra' => ['present','nullable','string','max:20000'],
+            'translations'=> ['required', 'array'],
         ];
     }
 
-    public function asController(TradeUnit $tradeUnit, ActionRequest $request)
+    public function asController(TradeUnit $tradeUnit, ActionRequest $request): void
     {
+
         $this->initialisationFromGroup(group(), $request);
         $this->handle($tradeUnit, $this->validatedData);
     }
