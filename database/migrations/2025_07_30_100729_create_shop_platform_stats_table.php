166bfc109f7757315259b38ec598e7ffbe16bc22
KirinZero0 <aryapermana02@gmail.com>
2025-08-06 18:47:05 +0800

    indentation fix

--- database/migrations/2025_07_30_100729_create_shop_platform_stats_table.php
+++ database/migrations/2025_07_30_100729_create_shop_platform_stats_table.php
@@ -21,34 +21,34 @@ return new class extends Migration
             $table->foreign('shop_id')->references('id')->on('shops')->nullOnDelete();
             $table->unsignedSmallInteger('platform_id');
             $table->foreign('platform_id')->references('id')->on('platforms')->nullOnDelete();
-                $table->unsignedInteger("number_customers")->default(0);
-                foreach (CustomerStateEnum::cases() as $customerState) {
-                    $table->unsignedInteger("number_customers_state_{$customerState->snake()}")->default(0);
-                }
+            $table->unsignedInteger("number_customers")->default(0);
+            foreach (CustomerStateEnum::cases() as $customerState) {
+                $table->unsignedInteger("number_customers_state_{$customerState->snake()}")->default(0);
+            }
 
-                $table->unsignedInteger("number_products")->default(0);
-                $table->unsignedInteger("number_current_products")->default(0)->comment('state: active+discontinuing');
+            $table->unsignedInteger("number_products")->default(0);
+            $table->unsignedInteger("number_current_products")->default(0)->comment('state: active+discontinuing');
 
-                foreach (ProductStateEnum::cases() as $productState) {
-                    $table->unsignedInteger("number_products_state_{$productState->snake()}")->default(0);
-                }
+            foreach (ProductStateEnum::cases() as $productState) {
+                $table->unsignedInteger("number_products_state_{$productState->snake()}")->default(0);
+            }
 
-                $table->dateTimeTz("last_order_created_at")->nullable();
-                $table->dateTimeTz("last_order_submitted_at")->nullable();
-                $table->dateTimeTz("last_order_dispatched_at")->nullable();
-                $table->unsignedInteger("number_orders")->default(0);
+            $table->dateTimeTz("last_order_created_at")->nullable();
+            $table->dateTimeTz("last_order_submitted_at")->nullable();
+            $table->dateTimeTz("last_order_dispatched_at")->nullable();
+            $table->unsignedInteger("number_orders")->default(0);
 
-                foreach (OrderStateEnum::cases() as $case) {
-                    $table->unsignedInteger('number_orders_state_'.$case->snake())->default(0);
-                }
+            foreach (OrderStateEnum::cases() as $case) {
+                $table->unsignedInteger('number_orders_state_'.$case->snake())->default(0);
+            }
 
-                foreach (OrderStatusEnum::cases() as $case) {
-                    $table->unsignedInteger('number_orders_status_'.$case->snake())->default(0);
-                }
+            foreach (OrderStatusEnum::cases() as $case) {
+                $table->unsignedInteger('number_orders_status_'.$case->snake())->default(0);
+            }
 
-                foreach (OrderHandingTypeEnum::cases() as $case) {
-                    $table->unsignedInteger('number_orders_handing_type_'.$case->snake())->default(0);
-                }
+            foreach (OrderHandingTypeEnum::cases() as $case) {
+                $table->unsignedInteger('number_orders_handing_type_'.$case->snake())->default(0);
+            }
             $table->unsignedInteger('number_customer_sales_channels')->default(0);
             $table->unsignedInteger('number_customer_sales_channel_broken')->default(0);
 
