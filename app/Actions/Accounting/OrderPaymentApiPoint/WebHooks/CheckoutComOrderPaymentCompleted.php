8a34ed128a3281e9bb86b0bf9ec67b7276270b68
Raul Perusquia <raul@inikoo.com>
2025-08-06 08:35:51 +0200

    refactor: enhance `UpdateTradeUnitTranslations` to handle multilingual attributes and update resource logic for improved i18n handling

---
+++ app/Actions/Accounting/OrderPaymentApiPoint/WebHooks/CheckoutComOrderPaymentCompleted.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 07 May 2025 12:38:25 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Accounting\OrderPaymentApiPoint\WebHooks;
+
+use App\Actions\Accounting\OrderPaymentApiPoint\UpdateOrderPaymentApiPoint;
+use App\Actions\Accounting\Payment\StorePayment;
+use App\Actions\Accounting\WithCheckoutCom;
+use App\Actions\IrisAction;
+use App\Actions\Ordering\Order\AttachPaymentToOrder;
+use App\Actions\Ordering\Order\SubmitOrder;
+use App\Actions\Retina\Dropshipping\Orders\SettleRetinaOrderWithBalance;
+use App\Actions\Retina\Dropshipping\Orders\WithRetinaOrderPlacedRedirection;
+use App\Enums\Accounting\OrderPaymentApiPoint\OrderPaymentApiPointStateEnum;
+use App\Enums\Accounting\Payment\PaymentStateEnum;
+use App\Enums\Accounting\Payment\PaymentStatusEnum;
+use App\Enums\Accounting\Payment\PaymentTypeEnum;
+use App\Models\Accounting\OrderPaymentApiPoint;
+use App\Models\Accounting\PaymentAccountShop;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CheckoutComOrderPaymentCompleted extends IrisAction
+{
+    use AsAction;
+    use WithCheckoutCom;
+    use WithRetinaOrderPlacedRedirection;
+
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(OrderPaymentApiPoint $orderPaymentApiPoint, array $modelData): array
+    {
+        $paymentAccountShopID = Arr::get($orderPaymentApiPoint->data, 'payment_methods.checkout');
+        $paymentAccountShop   = PaymentAccountShop::find($paymentAccountShopID);
+
+        $checkoutComPayment = $this->getCheckOutPayment(
+            $paymentAccountShop,
+            $modelData['cko-payment-id']
+        );
+
+
+        $amount = Arr::get($checkoutComPayment, 'amount', 0) / 100;
+
+        $paymentData = [
+            'reference'               => Arr::get($checkoutComPayment, 'id'),
+            'amount'                  => $amount,
+            'status'                  => PaymentStatusEnum::SUCCESS,
+            'state'                   => PaymentStateEnum::COMPLETED,
+            'type'                    => PaymentTypeEnum::PAYMENT,
+            'payment_account_shop_id' => $paymentAccountShop->id,
+            'api_point_type'          => class_basename($orderPaymentApiPoint),
+            'api_point_id'            => $orderPaymentApiPoint->id,
+        ];
+
+
+        $order = DB::transaction(function () use ($orderPaymentApiPoint, $paymentAccountShop, $paymentData) {
+            $payment = StorePayment::make()->action($orderPaymentApiPoint->order->customer, $paymentAccountShop->paymentAccount, $paymentData);
+
+            $order = $orderPaymentApiPoint->order;
+
+            AttachPaymentToOrder::make()->action($order, $payment, [
+                'amount' => $payment->amount
+            ]);
+
+
+            if ($order->total_amount > $order->payment_amount && $order->customer->balance > 0) {
+                SettleRetinaOrderWithBalance::run($order);
+            }
+
+            UpdateOrderPaymentApiPoint::run(
+                $orderPaymentApiPoint,
+                [
+                    'state'        => OrderPaymentApiPointStateEnum::SUCCESS,
+                    'processed_at' => now(),
+                    'data'         => [
+                        'payment_id' => $payment->id,
+                    ]
+                ]
+            );
+
+
+            $order->refresh();
+
+            return SubmitOrder::run($order);
+        });
+
+        return [
+            'success' => true,
+            'reason'  => 'Order paid successfully',
+            'order'   => $order,
+        ];
+    }
+
+    public function rules(): array
+    {
+        return [
+            'cko-payment-id'         => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function asController(OrderPaymentApiPoint $orderPaymentApiPoint, ActionRequest $request): array
+    {
+        dd($request->all());
+        $this->initialisation($request);
+
+        return $this->handle($orderPaymentApiPoint, $this->validatedData);
+    }
+
+}
