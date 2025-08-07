950ec8e7c620dfea6a79b64cc53a362d4fbfc3b5
aqordeon <aqordivika@yahoo.co.id>
2025-08-07 12:08:37 +0800

    fix: Checkout: condition

--- resources/js/Components/Retina/Ecom/CheckoutPaymentCard.vue
+++ resources/js/Components/Retina/Ecom/CheckoutPaymentCard.vue
@@ -10,6 +10,7 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import axios from "axios"
 import { notify } from "@kyvg/vue3-notification"
 import { trans } from "laravel-vue-i18n"
+import LoadingIcon from "@/Components/Utils/LoadingIcon.vue"
 library.add(faCopy)
 
 const props = defineProps<{
@@ -40,12 +41,14 @@ console.log('Checkout payment Card props', props.data)
 const locale = inject('locale', {})
 
 const isLoading = ref(true)
+const isLoadingAfterSuccess = ref(false)
 const MAX_RETRIES = 5;
 const retryCount = ref(0);
 
 
 const hitWebhookAfterSuccess = async (paymentResponseId: string) => {
   try {
+    isLoadingAfterSuccess.value = true
     const response = await axios.post(
       route('retina.webhooks.checkout_com.order_payment_completed', {
         orderPaymentApiPoint: props.data.order_payment_api_point,
@@ -98,12 +101,15 @@ const hitWebhookAfterSuccess = async (paymentResponseId: string) => {
     retryCount.value = 0
     notify({
       title: trans('Something went wrong'),
-      text: error.data.message ? error.data.message : trans('Failed to communicate with the payment service.'),
+      text: error.data?.message || error?.message || trans('Failed to communicate with the payment service.'),
       type: 'error',
     });
+  } finally {
+    isLoadingAfterSuccess.value = false
   }
 };
 
+const paymentResponseId = ref<string | null>(null)
 onMounted(async () => {
     // isLoading.value = true
     const checkout = await loadCheckoutWebComponents({
@@ -117,6 +123,7 @@ onMounted(async () => {
     
         onPaymentCompleted: (_component, paymentResponse) => {
             console.log("Create Payment with PaymentId: ", paymentResponse.id)
+            paymentResponseId.value = paymentResponse.id
             hitWebhookAfterSuccess(paymentResponse.id)
         },
     
@@ -124,7 +131,8 @@ onMounted(async () => {
             console.log( `onChange() -> isValid: "${component.isValid()}" for "${component.type}"`, )
         },
         onError: (component, error) => {
-            console.log("onError", error, "Component", component.type)
+            console.error("onError error:", error)
+            console.error("onError component:", component)
         },
         appearance: {
             colorAction: 'rgb(15, 22, 38)',
@@ -160,23 +168,33 @@ const onClickCopy = (textToCopy: string) => {
             </Transition>
         </div>
 
+        <!-- Section: Flow Container -->
         <div class="relative min-h-[200px]">
-            <div xv-show="!isLoading" id="flow-container" class="xabsolute w-full border-b border-gray-300" />
-
-            <div v-show="isLoading" class="pointer-events-none absolute top-0 h-full w-full z-10">
-                <div class="w-full min-h-[200px] h-full xmd:h-[511px] skeleton" xclass="isLoading ? 'skeleton' : ''">
+            <template v-if="paymentResponseId">
+                <div xv-show="!isLoading" id="flow-container" class="xabsolute w-full xborder-b border-gray-300" />
+                <div v-show="isLoading" class="pointer-events-none absolute top-0 h-full w-full z-10">
+                    <div class="w-full min-h-[200px] h-full xmd:h-[511px] skeleton" xclass="isLoading ? 'skeleton' : ''">
+                    </div>
                 </div>
+            </template>
+
+            <div v-else class="h-64 flex flex-col items-center justify-center gap-y-2 bg-gray-100 border border-gray-300 rounded">
+                <FontAwesomeIcon icon="fal fa-check-circle" class="text-3xl ml-1 text-green-500" fixed-width aria-hidden="true" />
+                <div>{{ trans("Payment done. Waiting for confirmation...") }}</div>
             </div>
 
-            <Transition name="fade">
-                <div v-if="retryCount > 0"
-                    class="mt-4 px-4 py-2 rounded-lg bg-yellow-50 border border-yellow-300 text-yellow-800 text-sm font-medium flex items-center gap-2 animate-pulse">
-                    <FontAwesomeIcon :icon="faSpinner" class="animate-spin" />
-                    Retrying payment... Attempt {{ retryCount }} of {{ MAX_RETRIES }}
-                </div>
-            </Transition>
-
+            <div v-show="isLoadingAfterSuccess" class="bg-black/70 text-white text-5xl flex justify-center items-center absolute top-0 h-full w-full z-10">
+                <LoadingIcon />
+            </div>
         </div>
+
+        <Transition name="fade">
+            <div v-if="retryCount > 0"
+                class="absolute inset-0 xmt-4 px-4 py-2 rounded-lg bg-black/10 backdrop-blur-sm border border-black/50 text-yellow-600 text-lg font-medium flex items-center justify-center gap-2 xanimate-pulse">
+                <FontAwesomeIcon :icon="faSpinner" class="animate-spin" />
+                {{ trans("Retrying payment... Attempt :retryCount of :max_entries", { retryCount: retryCount, max_entries: MAX_RETRIES }) }}
+            </div>
+        </Transition>
     </div>
 </template>
 
