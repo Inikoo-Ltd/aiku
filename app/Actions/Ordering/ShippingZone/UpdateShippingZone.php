b1b80993b85bbc50f8973c5f368d7b78b75426a6
Artha <manager.arthachan@gmail.com>
2025-08-07 17:46:31 +0800

    fix: update shipping zone

--- app/Actions/Ordering/ShippingZone/UpdateShippingZone.php
+++ app/Actions/Ordering/ShippingZone/UpdateShippingZone.php
@@ -47,7 +47,7 @@ class UpdateShippingZone extends OrgAction
             'status'      => ['sometimes', 'required', 'boolean'],
             'price'       => ['sometimes', 'array'],
             'territories' => ['sometimes', 'array'],
-            'position'    => ['required', 'integer'],
+            'position'    => ['sometimes', 'integer'],
             'is_failover' => ['sometimes', 'boolean'],
 
         ];
