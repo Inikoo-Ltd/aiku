472fc7b07f1bec9c9ecc152e69287838a8c969f8
YudhistiraA <aryarajasa0@gmail.com>
2025-08-07 15:31:57 +0800

    master Product

--- resources/js/Components/Showcases/Grp/MasterProductShowcase.vue
+++ resources/js/Components/Showcases/Grp/MasterProductShowcase.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-import {routeType} from '@/types/route';
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import {faImage} from "@far";
-import {faInfoCircle} from "@fas";
-import Image from "@/Components/Image.vue";
-import Message from "primevue/message";
-import {Link} from "@inertiajs/vue3";
-import ButtonWithLink from "@/Components/Elements/Buttons/ButtonWithLink.vue";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {trans} from "laravel-vue-i18n";
-import {ref} from "vue";
-import {faAlbum, faAlbumCollection} from "@fal";
+import TranslationBox from '@/Components/TranslationBox.vue';
 
-library.add(faAlbumCollection);
+
+const props = defineProps<{
+  data:object
+}>()
+console.log(props.data)
 </script>
 
 <template>
     <div class="px-4 pb-8 m-5">
-        idk what to put
+        <TranslationBox 
+            :master="data.data" 
+            :needTranslation="data.data" 
+            :route-to-save="data.data.route_save_translations" 
+        />
     </div>
 </template>
