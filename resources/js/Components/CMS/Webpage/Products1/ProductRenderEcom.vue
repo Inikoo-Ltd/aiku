5b06164e1cd63cc9f1d6b19c7346b9b32eb831cc
aqordeon <aqordivika@yahoo.co.id>
2025-08-12 17:00:35 +0800

    #2064 feat: Retina Ecom: setup products list

---
+++ resources/js/Components/CMS/Webpage/Products1/ProductRenderEcom.vue
@@ -0,0 +1,404 @@
+<script setup lang="ts">
+import Image from '@/Components/Image.vue'
+import { useLocaleStore } from "@/Stores/locale"
+import { inject, ref } from 'vue'
+import { retinaLayoutStructure } from '@/Composables/useRetinaLayoutStructure'
+import { Link, router, usePage } from '@inertiajs/vue3'
+import { notify } from '@kyvg/vue3-notification'
+import { trans } from 'laravel-vue-i18n'
+import LoadingIcon from '@/Components/Utils/LoadingIcon.vue'
+import { faHeart } from '@far'
+import { faCircle, faStar, faHeart as fasHeart, faEllipsisV, faMedal} from '@fas'
+import { Image as ImageTS } from '@/types/Image'
+import ButtonAddPortfolio from '@/Components/Iris/Products/ButtonAddPortfolio.vue'
+import { getStyles } from "@/Composables/styles";
+import Button from '@/Components/Elements/Buttons/Button.vue'
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faQuestionCircle } from "@fal"
+import { faStarHalfAlt } from "@fas"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import InformationIcon from '@/Components/Utils/InformationIcon.vue'
+import ButtonWithLink from '@/Components/Elements/Buttons/ButtonWithLink.vue'
+import { InputNumber } from 'primevue'
+import { get, set } from 'lodash-es'
+library.add(faStarHalfAlt, faQuestionCircle)
+
+const layout = inject('layout', retinaLayoutStructure)
+
+const locale = useLocaleStore()
+
+interface ProductResource {
+    id: number
+    name: string
+    code: string
+    image?: {
+        source: ImageTS,
+    }
+    rpp?: number
+    unit: string
+    stock: number
+    rating: number
+    price: number
+    url: string | null
+    units: number
+    bestseller?: boolean
+    is_favourite?: boolean
+    top_seller : number | null
+    web_images : {
+        main : {
+            original: ImageTS,
+            gallery : ImageTS
+        }    
+    }
+    quantity_ordered: number
+    quantity_ordered_new: number
+    transaction_id: number | null
+}
+
+const props = defineProps<{
+    product: ProductResource
+    productHasPortfolio : Array<Number>
+    style?: Object|null
+}>()
+
+
+const currency = layout?.iris?.currency 
+
+// Section: Add to Favourites
+const isLoadingFavourite = ref(false)
+const onAddFavourite = (product: ProductResource) => {
+
+    // Section: Submit
+    router.post(
+        route('iris.models.favourites.store', {
+            product: product.id
+        }),
+        {
+            // item_id: [product.id]
+        },
+        {
+            preserveScroll: true,
+            preserveState: true,
+            onStart: () => { 
+                isLoadingFavourite.value = true
+            },
+            onSuccess: () => {
+                product.is_favourite = true
+            },
+            onError: errors => {
+                notify({
+                    title: trans("Something went wrong"),
+                    text: trans("Failed to add the product to favourites"),
+                    type: "error"
+                })
+            },
+            onFinish: () => {
+                isLoadingFavourite.value = false
+            },
+        }
+    )
+}
+const onUnselectFavourite = (product: ProductResource) => {
+
+    // Section: Submit
+    router.delete(
+        route('iris.models.favourites.delete', {
+            product: product.id
+        }),
+        {
+            preserveScroll: true,
+            preserveState: true,
+            onStart: () => { 
+                isLoadingFavourite.value = true
+            },
+            onSuccess: () => {
+                // notify({
+                //     title: trans("Success"),
+                //     text: trans("Added to portfolio"),
+                //     type: "success"
+                // })
+                product.is_favourite = false
+            },
+            onError: errors => {
+                notify({
+                    title: trans("Something went wrong"),
+                    text: trans("Failed to remove the product from favourites"),
+                    type: "error"
+                })
+            },
+            onFinish: () => {
+                isLoadingFavourite.value = false
+            },
+        }
+    )
+}
+
+const isLoadingSubmitQuantityProduct = ref(false)
+const onAddToBasket = (product: ProductResource) => {
+    router.post(
+        route('iris.models.transaction.store', {
+            product: product.id
+        }),
+        {
+            quantity: get(product, ['quantity_ordered_new'], product.quantity_ordered)
+        },
+        {
+            preserveScroll: true,
+            preserveState: true,
+            only: ['iris'],
+            onStart: () => { 
+                isLoadingSubmitQuantityProduct.value = true
+            },
+            onSuccess: () => {
+                product.quantity_ordered = product.quantity_ordered_new
+            },
+            onError: errors => {
+                notify({
+                    title: trans("Something went wrong"),
+                    text: errors.message || trans("Failed to add product to basket"),
+                    type: "error"
+                })
+            },
+            onFinish: () => {
+                isLoadingSubmitQuantityProduct.value = false
+            },
+        }
+    )
+}
+
+const onUpdateQuantity = (product: ProductResource) => {
+
+    // Section: Submit
+    router.post(
+        route('iris.models.transaction.update', {
+            transaction: product.transaction_id
+        }),
+        {
+            quantity: get(product, ['quantity_ordered_new'], product.quantity_ordered)
+        },
+        {
+            preserveScroll: true,
+            preserveState: true,
+            only: ['iris'],
+            onStart: () => { 
+                isLoadingSubmitQuantityProduct.value = true
+            },
+            onSuccess: () => {
+                product.quantity_ordered = product.quantity_ordered_new
+            },
+            onError: errors => {
+                notify({
+                    title: trans("Something went wrong"),
+                    text: errors.message || trans("Failed to update product quantity in basket"),
+                    type: "error"
+                })
+            },
+            onFinish: () => {
+                isLoadingSubmitQuantityProduct.value = false
+            },
+        }
+    )
+}
+
+
+
+const xxxxxxx = Math.random() > 0.5
+
+</script>
+
+<template>
+    <div class="relative flex flex-col justify-between h-full">
+
+        <!-- Top Section -->
+        <div>
+            <div v-if="product?.top_seller"
+                class="absolute top-2 left-2 border border-black text-xs font-bold px-2 py-0.5 rounded">
+                <FontAwesomeIcon :icon="faMedal" class="w-3.5 h-3.5 mr-0 md:mr-2" :class="{
+
+                        'text-[#FFD700]': product.top_seller === 1, // Gold
+                        'text-[#C0C0C0]': product.top_seller === 2, // Silver
+                        'text-[#CD7F32]': product.top_seller === 3  // Bronze
+                    }" />
+
+                <span class="hidden md:inline">BESTSELLER</span>
+            </div>
+
+            <!-- Favorite Icon -->
+            <template v-if="layout?.retina?.type != 'dropshipping' && layout?.iris?.is_logged_in">
+
+                <div v-if="isLoadingFavourite" class="absolute top-2 right-2 text-gray-500 text-xl">
+                    <LoadingIcon />
+                </div>
+                <div v-else @click="() => product.is_favourite ? onUnselectFavourite(product) : onAddFavourite(product)"
+                    class="cursor-pointer absolute top-2 right-2 group text-xl ">
+
+                    <FontAwesomeIcon 
+                        v-if="product.is_favourite" 
+                        :icon="fasHeart" 
+                        fixed-width 
+                        class="text-pink-500" 
+                    />
+                    <div v-else class="relative">
+                        <FontAwesomeIcon
+                            :icon="fasHeart"
+                            class="hidden group-hover:inline text-pink-400"
+                            fixed-width
+                        />
+                        <FontAwesomeIcon
+                            :icon="faHeart"
+                            class="inline group-hover:hidden text-pink-300"
+                            fixed-width
+                        />
+                    </div>
+
+                </div>
+            </template>
+
+
+            <!-- Icon: status (stocks) -->
+            <template v-if="layout?.iris?.is_logged_in">
+
+                <div class="absolute top-9 right-2">
+                    <div class="cursor-pointer group text-xl ">
+                        <FontAwesomeIcon
+                            v-if="product.stock"
+                            v-tooltip="trans('Product is ready stock')"
+                            :icon="faCircle"
+                            class="text-green-400 animate-pulse"
+                            fixed-width
+                        />
+                        <FontAwesomeIcon
+                            v-else
+                            v-tooltip="trans('Product is out of stock')"
+                            :icon="faCircle"
+                            class="text-red-500"
+                            fixed-width
+                        />
+                    </div>
+                </div>
+            </template>
+
+
+            <!-- Product Image -->
+            <component :is="product.url ? Link : 'div'" :href="product.url" class="block w-full mb-1 rounded sm:h-[305px] h-[180px]">
+                <Image :src="product?.web_images?.main?.gallery" alt="product image" 
+                    :style="{ objectFit: 'contain' }" />
+            </component>
+
+            <!-- Title -->
+
+            <Link v-if="product.url" :href="product.url"
+                class="text-gray-800 hover:text-gray-500 font-bold text-sm mb-1">
+            {{ product.name }}
+
+            </Link>
+            <div v-else class="text-gray-800 hover:text-gray-500 font-bold text-sm mb-1">
+                {{ product.name }}
+            </div>
+
+            <!-- SKU and RRP -->
+            <div class="flex justify-between text-xs text-gray-600 mb-1 capitalize">
+                <span>{{ product?.code }}</span>
+                <span v-if="product.rpp">
+                    RRP: {{ locale.currencyFormat((currency.code,product.rpp || 0)) }}/ {{ product.unit }}
+                </span>
+            </div>
+
+            <!-- Rating and Stock -->
+            <div class="flex justify-between items-center text-xs mb-2">
+
+                <div v-if="layout?.iris?.is_logged_in" class="flex items-center gap-1"
+                    :class="product.stock > 0 ? 'text-green-600' : 'text-red-600'">
+
+                    <FontAwesomeIcon :icon="faCircle" class="text-[8px]" />
+                    <span>({{ product.stock > 0 ? product.stock : 0 }} {{trans('available')}})</span>
+                </div>
+                <div class="flex items-center space-x-[1px] text-gray-500">
+
+                </div>
+            </div>
+
+            <!-- Prices -->
+            <div v-if="layout?.iris?.is_logged_in" class="mb-3">
+                <div class="flex justify-between text-sm ">
+                    <div>
+                        {{ trans('Price') }}: <span class="" :class="xxxxxxx ? 'line-through text-gray-400' : 'font-semibold'">{{ locale.currencyFormat(currency.code,product.price) }}</span>
+                        <span v-if="xxxxxxx" class="ml-1 font-semibold text-green-600">{{ locale.currencyFormat(currency.code,product.price) }}</span>
+                    </div>
+
+                    <div>
+                        <span v-tooltip="trans('Recommended retail price')" >{{trans('RRP')}}</span>:  <span class="font-semibold">{{ locale.currencyFormat(currency.code,product.rrp) }}</span>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Section: Coupon -->
+            <div class="mb-2">
+                <div v-if="!xxxxxxx" class="cursor-pointer rounded py-1 px-3 bg-gray-100 border border-gray-300 w-fit text-xs" >
+                    <FontAwesomeIcon icon="fas fa-star-half-alt" class="" fixed-width aria-hidden="true" />
+                    <span class="">↓5%</span>
+                    <InformationIcon :information="trans('Information')" />
+                </div>
+                <div v-else class="cursor-pointer rounded py-1 px-3 bg-green-100 border border-green-300 text-green-700 w-fit text-xs" >
+                    <FontAwesomeIcon icon="fas fa-star-half-alt" class="" fixed-width aria-hidden="true" />
+                    <span class="">↓5%</span>
+                    <InformationIcon :information="trans('Information')" />
+                </div>
+            </div>
+        </div>
+
+
+        <div>
+            <div v-if="layout?.iris?.is_logged_in" class="w-full">
+                <!-- <pre>{{ product }}</pre> -->
+                <div v-if="product.stock > 0" class="flex flex-col items-center gap-2 xmt-2">
+                    <!-- {{ get(product, ['quantity_ordered_new'], product.quantity_ordered) }} -->
+                    <InputNumber
+                        :modelValue="get(product, ['quantity_ordered_new'], product.quantity_ordered) || product.quantity_ordered"
+                        @update:modelValue="(e) => set(product, ['quantity_ordered_new'], e)"
+                        inputId="integeronly"
+                        fluid
+                        showButtons
+                        :min="0"
+                        :max="product.stock"
+                    />
+                    
+                    <Button
+                        v-if="!product.quantity_ordered"
+                        @click="() => onAddToBasket(product)"
+                        icon="fal fa-shopping-cart"
+                        :label="trans('Add to basket')"
+                        type="secondary"
+                        full
+                        :loading="isLoadingSubmitQuantityProduct"
+                        :disabled="product.quantity_ordered_new === product.quantity_ordered"
+                    />
+                    <Button
+                        v-else
+                        @click="() => onUpdateQuantity(product)"
+                        icon="fal fa-plus"
+                        :label="trans('Update quantity in basket')"
+                        type="tertiary"
+                        full
+                        :loading="isLoadingSubmitQuantityProduct"
+                        :disabled="product.quantity_ordered_new === product.quantity_ordered"
+                    />
+                </div>
+
+                <div v-else>
+                    <Button :label="trans('Out of stock')" type="tertiary" disabled full />
+                </div>
+            </div>
+
+            <Link v-else href="/app/login" class="text-center border border-gray-200 text-sm px-3 py-2 rounded text-gray-600 w-full">
+                {{ trans("Login to add to your portfolio") }}
+            </Link>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
