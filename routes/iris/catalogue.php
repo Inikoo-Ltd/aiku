7aeae8c5d722c07116ddfe329d1196a630f9843a
Raul Perusquia <raul@inikoo.com>
2025-08-19 04:09:05 -0600

    chore: add routes for downloading product data feeds
    - Introduced `data-feed.csv` route to shop and product category for CSV downloads.
    - Updated `DownloadIrisProduct` to support shop-specific feed downloads.

--- routes/iris/catalogue.php
+++ routes/iris/catalogue.php
@@ -11,6 +11,6 @@ use App\Actions\Iris\Catalogue\DownloadIrisProduct;
 
 Route::prefix('feeds')->as('feeds.')->group(function () {
     Route::get('product-category/{productCategory}/download.csv', [DownloadIrisProduct::class, 'inProductCategory'])->name('product_category.download');
-    Route::get('shop/{shop}/download.csv', DownloadIrisProduct::class)->name('shop.download');
+    Route::get('shop/{shop}/download.csv', [DownloadIrisProduct::class, 'inShop'])->name('shop.download');
     Route::get('product/{product}/download.csv', [DownloadIrisProduct::class, 'inProduct'])->name('product.download');
 });
