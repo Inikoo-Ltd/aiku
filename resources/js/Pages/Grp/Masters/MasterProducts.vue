3c1759bfca06d7ad10bc970f0b31c01ae7bc96d2
YudhistiraA <aryarajasa0@gmail.com>
2025-08-25 09:53:40 +0800

    TradeUnits Selector

--- resources/js/Pages/Grp/Masters/MasterProducts.vue
+++ resources/js/Pages/Grp/Masters/MasterProducts.vue
@@ -15,6 +15,8 @@ import { PageHeading as PageHeadingTypes } from "@/types/PageHeading";
 import Button from "@/Components/Elements/Buttons/Button.vue";
 import { ref } from "vue";
 import Dialog from "primevue/dialog";
+import { trans } from "laravel-vue-i18n";
+import ProductsSelector from "@/Components/Dropshipping/ProductsSelector.vue";
 import PureMultiselectInfiniteScroll from "@/Components/Pure/PureMultiselectInfiniteScroll.vue";
 
 library.add(faShapes, faSortAmountDownAlt, faBrowser, faSortAmountDown, faHome);
@@ -23,7 +25,7 @@ const props = defineProps<{
   pageHead: PageHeadingTypes
   title: string
   data: {}
-  familyId : Number
+  familyId: Number
 }>();
 
 // state for PrimeVue modal
@@ -35,54 +37,43 @@ const openModal = () => {
 const closeModal = () => {
   showModal.value = false;
 };
-const dummy = ref([])
 
-console.log(props)
-const save = () => {
+
+const isLoadingSubmit = ref(false)
+const save = (products) => {
   router.post(
     route('grp.models.master_family.store-assets', { masterFamily: props.familyId }),
-    {trade_units : [dummy.value]}
+    { trade_units: products }
   )
 }
 </script>
 
 <template>
+
   <Head :title="capitalize(title)" />
   <PageHeading :data="pageHead">
     <template #button-create-product="{ action }">
-      <Button
-        :style="action.style"
-        :label="action.label"
-        :icon="action.icon"
-        @click="openModal"
-        :key="`ActionButton${action.label}${action.style}`"
-        :tooltip="action.tooltip"
-       
-      />
+      <Button :style="action.style" :label="action.label" :icon="action.icon" @click="openModal"
+        :key="`ActionButton${action.label}${action.style}`" :tooltip="action.tooltip" />
     </template>
   </PageHeading>
 
   <TableMasterProducts :data="data" />
 
   <!-- PrimeVue Dialog -->
-  <Dialog v-model:visible="showModal" modal header="Create Product" :style="{ width: '40rem' }">
-    <div class="p-4">
-      <PureMultiselectInfiniteScroll
-						v-model="dummy"
-            :labelProp="'name'"
-            :value-prop="'slug'"
-             :mode="'tags'"
-						:fetchRoute="{
-              name : 'grp.json.master-product-category.recommended-trade-units',
-              parameters : {
-                masterProductCategory : route().params['masterFamily']
-              }
-            }"
-				</PureMultiselectInfiniteScroll>
-      <div class="flex justify-end gap-2">
-        <Button label="Cancel" style="secondary" @click="closeModal" />
-        <Button label="Save" style="primary" @click="save" />
-      </div>
+  <Dialog v-model:visible="showModal" modal :show-header="false" header="Create"
+    :style="{ width: '50rem', padding: '10px' }" :content-style="{ overflow: 'unset' }">
+    <div class="pt-4">
+      <ProductsSelector :headLabel="trans('Add Trade Units')"  :route-fetch="{
+      name: 'grp.json.master-product-category.recommended-trade-units',
+      parameters: {
+        masterProductCategory: route().params['masterFamily']
+      }
+    }" :isLoadingSubmit @submit="(products: {}[]) => save(products)" class="px-4" />
+
     </div>
+    
   </Dialog>
 </template>
+
+<style scoped></style>
