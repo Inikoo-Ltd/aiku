0c46274f015216bc3b74ea31a315ca3e399170ee
aqordeon <aqordivika@yahoo.co.id>
2025-08-26 10:05:51 +0800

    fix: Retina: box label on link API URL

--- resources/js/Components/Showcases/Retina/Catalouge/RetinaSubDepartementShowcase.vue
+++ resources/js/Components/Showcases/Retina/Catalouge/RetinaSubDepartementShowcase.vue
@@ -46,8 +46,10 @@ const routeAPI = window.location.origin + `/${props.data?.subDepartment?.slug}/d
 
 <template>
     <div class="px-4 pb-8 m-5">
-        <div class="py-2 w-fit">
-            <div>
+        <!-- Box: API URL -->
+        <div class="w-fit border border-gray-300 rounded overflow-hidden">
+            <div class="py-2 px-3 text-lg font-bold bg-gray-100 ">
+                <FontAwesomeIcon icon="fal fa-link" class="" fixed-width aria-hidden="true" />
                 API Url (no need password)
                 <InformationIcon
                     :information="trans('Download all products in this Sub Department. Can be use to integrate with 3rd party app')"
@@ -55,20 +57,19 @@ const routeAPI = window.location.origin + `/${props.data?.subDepartment?.slug}/d
                 :
             </div>
             
-            <div xhref="props.data.url" target="_blank" xv-tooltip="trans('Go To Website')"
-                class="xhover:bg-gray-50 ring-1 ring-gray-300 rounded overflow-hidden flex text-xxs md:text-base text-gray-500">
-                <div v-tooltip="trans('Copy url')" class="flex items-center">
+            <div class="px-3 border-t border-gray-300 overflow-hidden flex items-center text-sm md:text-base text-gray-500">
+                <div class="py-2 px-2 w-full italic">
+                    {{ routeAPI }}
+                </div>
+                <div v-tooltip="trans('Copy url')" class="flex items-center relative">
                     <CopyButton
                         :text="routeAPI"
-                        class="text-3xl px-3 py-1.5"
+                        class="text-3xl px-2 py-1.5"
                     />
-                </div>
-
-                <div class="bg-gray-200 py-2 px-2 w-full">
-                    {{ routeAPI }}
                 </div>
             </div>
         </div>
+
 
         <div class="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-4 mt-4">
             <!-- Sidebar -->
