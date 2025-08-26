611a2ebaa35971de1b829b8a42aa2b2174bc08c7
Artha <manager.arthachan@gmail.com>
2025-08-26 13:17:40 +0800

    INI-84 Add refund payment route

--- app/Actions/Accounting/Payment/RefundPayment.php
+++ app/Actions/Accounting/Payment/RefundPayment.php
@@ -8,16 +8,42 @@
 
 namespace App\Actions\Accounting\Payment;
 
+use App\Actions\OrgAction;
+use App\Enums\Accounting\Payment\PaymentTypeEnum;
 use App\Models\Accounting\Payment;
+use App\Models\SysAdmin\Organisation;
+use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
-class RefundPayment
+class RefundPayment extends OrgAction
 {
     use AsAction;
 
-    public function handle(Payment $payment)
+    public function handle(Payment $payment): Payment
     {
+        $modelData = [
+            'type' => PaymentTypeEnum::REFUND,
+            'original_payment_id' => $payment->id,
+            'amount' => $payment->amount
+        ];
 
+        return StorePayment::make()->action($payment->customer, $payment->paymentAccount, $modelData);
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        if ($this->asAction) {
+            return true;
+        }
+
+        return $request->user()->authTo("accounting.{$this->organisation->id}.edit");
+    }
+
+    public function asController(Organisation $organisation, Payment $payment, ActionRequest $request): Payment
+    {
+        $this->initialisation($organisation, $request);
+
+        return $this->handle($payment);
     }
 
 }
