cac50763153d7884c87adbdcbc064b66a49343af
Raul Perusquia <raul@inikoo.com>
2025-08-27 13:46:22 +0800

    ecom basket

--- app/Actions/Retina/Ecom/Checkout/UI/ShowRetinaEcomCheckout.php
+++ app/Actions/Retina/Ecom/Checkout/UI/ShowRetinaEcomCheckout.php
@@ -11,15 +11,16 @@
 namespace App\Actions\Retina\Ecom\Checkout\UI;
 
 use App\Actions\Accounting\OrderPaymentApiPoint\StoreOrderPaymentApiPoint;
-use App\Actions\Retina\Dropshipping\Checkout\UI\ShowRetinaDropshippingCheckout;
+use App\Actions\Accounting\Traits\CalculatesPaymentWithBalance;
 use App\Actions\Retina\Ecom\Basket\UI\IsOrder;
+use App\Actions\Retina\GetRetinaPaymentMethods;
 use App\Actions\Retina\UI\Dashboard\ShowRetinaDashboard;
 use App\Actions\RetinaAction;
 use App\Http\Resources\Sales\OrderResource;
-use App\Models\Accounting\OrderPaymentApiPoint;
 use App\Models\CRM\Customer;
 use App\Models\Ordering\Order;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\ActionRequest;
@@ -27,6 +28,7 @@ use Lorisleiva\Actions\ActionRequest;
 class ShowRetinaEcomCheckout extends RetinaAction
 {
     use IsOrder;
+    use CalculatesPaymentWithBalance;
 
     public function handle(Customer $customer): array
     {
@@ -37,8 +39,9 @@ class ShowRetinaEcomCheckout extends RetinaAction
         $paymentMethods = [];
 
         if ($order) {
-            $paymentMethods = $this->getPaymentMethods($order, $orderPaymentApiPoint);
+            $paymentMethods = GetRetinaPaymentMethods::run($order, $orderPaymentApiPoint);
         }
+
 
         return [
             'order'          => $order,
@@ -47,38 +50,74 @@ class ShowRetinaEcomCheckout extends RetinaAction
         ];
     }
 
-    public function getPaymentMethods(Order $order, OrderPaymentApiPoint $orderPaymentApiPoint): array
-    {
-        return ShowRetinaDropshippingCheckout::make()
-            ->getPaymentMethods($order, $orderPaymentApiPoint);
-    }
 
     public function asController(ActionRequest $request): array
     {
         $this->initialisation($request);
-
-        return $this->handle($this->customer);
+        $order = $this->customer->orderInBasket;
+        if (!$order) {
+            return [
+                'order'          => null,
+                'paymentMethods' => null,
+                'balance'        => null,
+            ];
+        } else {
+            return $this->handle($this->customer);
+        }
     }
 
-    public function htmlResponse(array $checkoutData): Response
+    public function htmlResponse(array $checkoutData): \Illuminate\Http\Response|Response|\Illuminate\Http\RedirectResponse
     {
         /** @var Order $order */
         $order = Arr::get($checkoutData, 'order');
+
+        if (!$order) {
+            return Redirect::route('retina.ecom.basket.show');
+        }
+
+        $paymentAmounts = $this->calculatePaymentWithBalance(
+            $order->total_amount,
+            $this->customer->balance
+        );
+
+        $toPay          = $paymentAmounts['total'];
+        $toPayByBalance = $paymentAmounts['by_balance'];
+        $toPayByOther   = $paymentAmounts['by_other'];
 
         return Inertia::render(
             'Ecom/Checkout',
             [
                 'breadcrumbs'    => $this->getBreadcrumbs(),
-                'title'          => __('Basket'),
+                'title'          => __('Checkout'),
                 'pageHead'       => [
-                    'title' => __('Basket'),
-                    'icon'  => 'fal fa-shopping-basket'
+                    'title' => $order->reference,
+                    'model' => __('Checkout'),
                 ],
                 'order'          => OrderResource::make($order)->resolve(),
-                'summary'        => $order ? $this->getOrderBoxStats($order) : null,
+                'summary'        => $this->getOrderBoxStats($order),
                 'paymentMethods' => Arr::get($checkoutData, 'paymentMethods'),
                 'balance'        => $this->customer->balance,
-                'total_amount'   => $order->total_amount
+                'total_amount'   => $order->total_amount,
+                'currency_code'  => $order->currency->code,
+                'to_pay_data'    => [
+                    'total'      => $toPay,
+                    'by_balance' => $toPayByBalance,
+                    'by_other'   => $toPayByOther
+
+                ],
+                'routes'         => [
+                    'pay_with_balance' => [
+                        'name'       => 'retina.models.order.pay_with_balance',
+                        'parameters' => [
+                            'order' => $order->id
+                        ],
+                        'method'     => 'patch'
+                    ],
+                    'back_to_basket'   => [
+                        'name'       => 'retina.ecom.basket.show	',
+                        'parameters' => []
+                    ]
+                ]
             ]
         );
     }
