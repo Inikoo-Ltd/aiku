cd8fa744434a1cb10a3f259934c4ecd1930f04b6
Raul Perusquia <raul@inikoo.com>
2025-09-03 14:44:53 +0800

    Refactor `GetTradeUnitDataForMasterProductCreation` to include validation rules, enhance trade unit handling, and add logic for organization stock data retrieval. Simplify `tradeUnits` method in `OrgStock` model.

--- app/Models/Inventory/OrgStock.php
+++ app/Models/Inventory/OrgStock.php
@@ -197,18 +197,19 @@ class OrgStock extends Model implements Auditable
     public function tradeUnits(): MorphToMany
     {
 
-        return $this->morphToMany(
-            TradeUnit::class,
-            'model',
-            'model_has_trade_units',
-            'model_id',
-            null,
-            null,
-            null,
-            'trade_units',
-        )
-            ->withPivot(['quantity', 'notes'])
-            ->withTimestamps();
+        return $this->morphToMany(TradeUnit::class, 'model', 'model_has_trade_units')->withPivot(['quantity', 'notes'])->withTimestamps();
+//        return $this->morphToMany(
+//            TradeUnit::class,
+//            'model',
+//            'model_has_trade_units',
+//            'model_id',
+//            null,
+//            null,
+//            null,
+//            'trade_units',
+//        )
+//            ->withPivot(['quantity', 'notes'])
+//            ->withTimestamps();
     }
 
     public function timeSeries(): HasMany
