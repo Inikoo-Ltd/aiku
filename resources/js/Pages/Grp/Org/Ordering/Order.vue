3452b0eb92dd09e639eaa9f50b2a93e2cf2dce87
Raul Perusquia <raul@inikoo.com>
2025-09-08 09:09:20 +0800

    Remove unused `ShipmentSection` import from Order.vue.

--- resources/js/Pages/Grp/Org/Ordering/Order.vue
+++ resources/js/Pages/Grp/Org/Ordering/Order.vue
@@ -64,7 +64,6 @@ import Icon from "@/Components/Icon.vue"
 import ButtonWithLink from "@/Components/Elements/Buttons/ButtonWithLink.vue"
 import ModalConfirmationDelete from "@/Components/Utils/ModalConfirmationDelete.vue"
 import { ToggleSwitch } from "primevue"
-import ShipmentSection from "@/Components/Warehouse/DeliveryNotes/ShipmentSection.vue"
 import ModalConfirmation from "@/Components/Utils/ModalConfirmation.vue"
 
 library.add(fadExclamationTriangle, faExclamationTriangle, faDollarSign, faIdCardAlt, faShippingFast, faIdCard, faEnvelope, faPhone, faWeight, faStickyNote, faExclamation, faTruck, faFilePdf, faPaperclip, faSpinnerThird, faMapMarkerAlt)
