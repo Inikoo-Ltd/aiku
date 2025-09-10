cd3769cbe96e958511d089e657711cc14f72234c
KirinZero0 <aryapermana02@gmail.com>
2025-09-10 14:23:02 +0800

    width height data

--- app/Actions/Traits/HasBucketImages.php
+++ app/Actions/Traits/HasBucketImages.php
@@ -24,6 +24,10 @@ trait HasBucketImages
                 'column_in_db' => 'image_id',
                 'id' => $model->image_id,
                 'images' => $model->imageSources(),
+                'dimensions' => [
+                    'width' => $model->image->width,
+                    'height' => $model->image->height
+                ]
             ],
             [
                 'label' => __('Video'),
@@ -38,6 +42,10 @@ trait HasBucketImages
                 'column_in_db' => 'front_image_id',
                 'id' => $model->front_image_id,
                 'images' => $model->imageSources(getImage:'frontImage'),
+                'dimensions' => [
+                    'width' => $model->frontImage->width,
+                    'height' => $model->frontImage->height
+                ]
             ],
             [
                 'label' => __('Left side'),
@@ -45,6 +53,10 @@ trait HasBucketImages
                 'column_in_db' => 'left_image_id',
                 'id' => $model->left_image_id,
                 'images' => $model->imageSources(getImage:'leftImage'),
+                'dimensions' => [
+                    'width' => $model->leftImage->width,
+                    'height' => $model->leftImage->height
+                ]
             ],
             [
                 'label' => __('3/4 angle side'),
@@ -52,6 +64,10 @@ trait HasBucketImages
                 'column_in_db' => '34_image_id',
                 'id' => $model->{'34_image_id'},
                 'images' => $model->imageSources(getImage:'threeQuarterImage'),
+                'dimensions' => [
+                    'width' => $model->threeQuarterImage->width,
+                    'height' => $model->threeQuarterImage->height
+                ]
             ],
             [
                 'label' => __('Right side'),
@@ -59,6 +75,10 @@ trait HasBucketImages
                 'column_in_db' => 'right_image_id',
                 'id' => $model->right_image_id,
                 'images' => $model->imageSources(getImage:'rightImage'),
+                'dimensions' => [
+                    'width' => $model->rightImage->width,
+                    'height' => $model->rightImage->height
+                ]
             ],
             [
                 'label' => __('Back side'),
@@ -66,6 +86,10 @@ trait HasBucketImages
                 'column_in_db' => 'back_image_id',
                 'id' => $model->back_image_id,
                 'images' => $model->imageSources(getImage:'backImage'),
+                'dimensions' => [
+                    'width' => $model->backImage->width,
+                    'height' => $model->backImage->height
+                ]
             ],
             [
                 'label' => __('Top side'),
@@ -73,6 +97,10 @@ trait HasBucketImages
                 'column_in_db' => 'top_image_id',
                 'id' => $model->top_image_id,
                 'images' => $model->imageSources(getImage:'topImage'),
+                'dimensions' => [
+                    'width' => $model->topImage->width,
+                    'height' => $model->topImage->height
+                ]
             ],
             [
                 'label' => __('Bottom side'),
@@ -80,6 +108,10 @@ trait HasBucketImages
                 'column_in_db' => 'bottom_image_id',
                 'id' => $model->bottom_image_id,
                 'images' => $model->imageSources(getImage:'bottomImage'),
+                'dimensions' => [
+                    'width' => $model->bottomImage->width,
+                    'height' => $model->bottomImage->height
+                ]
             ],
             [
                 'label' => __('Comparison image'),
@@ -87,6 +119,10 @@ trait HasBucketImages
                 'column_in_db' => 'size_comparison_image_id',
                 'id' => $model->size_comparison_image_id,
                 'images' => $model->imageSources(getImage:'sizeComparisonImage'),
+                'dimensions' => [
+                    'width' => $model->sizeComparisonImage->width,
+                    'height' => $model->sizeComparisonImage->height
+                ]
             ],
             [
                 'label' => __('Lifestyle image'),
@@ -94,6 +130,10 @@ trait HasBucketImages
                 'column_in_db' => 'lifestyle_image_id',
                 'id' => $model->lifestyle_image_id,
                 'images' => $model->imageSources(getImage:'lifestyleImage'),
+                'dimensions' => [
+                    'width' => $model->lifestyleImage->width,
+                    'height' => $model->lifestyleImage->height
+                ]
             ],
         ];
 
