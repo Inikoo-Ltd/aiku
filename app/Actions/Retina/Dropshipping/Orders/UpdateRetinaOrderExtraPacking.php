d082416accc87bd289bef2f2a0216baaa71cfe16
KirinZero0 <aryapermana02@gmail.com>
2025-09-11 10:15:14 +0800

    Extra Packing action

---
+++ app/Actions/Retina/Dropshipping/Orders/UpdateRetinaOrderExtraPacking.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * author Arya Permana - Kirin
+ * created on 09-05-2025-09h-20m
+ * github: https://github.com/KirinZero0
+ * copyright 2025
+*/
+
+namespace App\Actions\Retina\Dropshipping\Orders;
+
+use App\Actions\Ordering\Order\UpdateOrderExtraPacking;
+use App\Actions\RetinaAction;
+use App\Models\Ordering\Order;
+use Lorisleiva\Actions\ActionRequest;
+
+class UpdateRetinaOrderExtraPacking extends RetinaAction
+{
+    public function handle(Order $order, array $modelData): Order
+    {
+        return UpdateOrderExtraPacking::run($order, $modelData);
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $order = $request->route('order');
+
+        return $order->customer_id == $this->customer->id;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'has_extra_packing' => ['required', 'boolean'],
+        ];
+    }
+
+    public function asController(Order $order, ActionRequest $request): Order
+    {
+        $this->initialisation($request);
+
+        return $this->handle($order, $this->validatedData);
+    }
+}
