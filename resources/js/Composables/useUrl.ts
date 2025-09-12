d6d1e4aa0f34085a2f8241d642e8649c85084053
aqordeon <aqordivika@yahoo.co.id>
2025-09-12 09:13:54 +0800

    feat: Retina Ecom: Checkout will activate Basket active mode

--- resources/js/Composables/useUrl.ts
+++ resources/js/Composables/useUrl.ts
@@ -1,4 +1,5 @@
 import { usePage } from "@inertiajs/vue3"
+import { inject } from "vue"
 
 // from "http://app.aiku.test/org/sk/inventory" to "/org/sk/inventory"
 export const removeDomain = (fullUrl: string, domain: string) => {
@@ -16,7 +17,13 @@ export const isRouteSameAsCurrentUrl = (expectedRoute: string) => {
 
 // routeRoot: the route.root to indicates a group of Navigation ('grp.org.fulfilments.show.operations.pallets.current.index' is exist in 'grp.org.fulfilments.show.operations.')
 export const isNavigationActive = (layoutRoute: string, routeRoot: string | undefined) => {
+    const injectLayout = inject('layout')
+    
     if(!routeRoot) return false
 
-    return layoutRoute.includes(routeRoot)
+    const isCurrentRouteIncludesRouteRoot = layoutRoute.includes(routeRoot)
+    const isRootActiveIncludesNavRoot = injectLayout?.root_active ? injectLayout?.root_active?.includes(routeRoot) : false
+
+    console.log('2 isRootActiveIncludesNavRoot', layoutRoute, injectLayout?.root_active, isRootActiveIncludesNavRoot)
+    return isCurrentRouteIncludesRouteRoot || isRootActiveIncludesNavRoot
 }
