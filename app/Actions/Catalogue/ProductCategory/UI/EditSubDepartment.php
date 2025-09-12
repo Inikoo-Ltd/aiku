f0db30f93ad0b85f2935de0e9b4d4549606c9cfe
KirinZero0 <aryapermana02@gmail.com>
2025-09-12 14:40:43 +0800

    Hide Translation if master exist

--- app/Actions/Catalogue/ProductCategory/UI/EditSubDepartment.php
+++ app/Actions/Catalogue/ProductCategory/UI/EditSubDepartment.php
@@ -88,137 +88,139 @@ class EditSubDepartment extends OrgAction
                 ],
 
                 'formData' => [
-                    'blueprint' => [
+                    'blueprint' => array_filter(
                         [
-                            'label'  => __('Id'),
-                            'icon'   => 'fa-light fa-fingerprint',
-                            'fields' => [
-                                'code' => [
-                                    'type'  => 'input',
-                                    'label' => __('code'),
-                                    'value' => $subDepartment->code
-                                ],
-                            ]
-                        ],
-                        [
-                            'label'  => __('Name/Description'),
-                            'icon'   => 'fa-light fa-tag',
-                            'fields' => [
-                                'name' => [
-                                    'type'  => 'input',
-                                    'label' => __('name'),
-                                    'value' => $subDepartment->name
-                                ],
-                                'description_title' => [
-                                    'type'  => 'input',
-                                    'label' => __('description title'),
-                                    'value' => $subDepartment->description_title
-                                ],
-                                'description' => [
-                                    'type'  => 'textEditor',
-                                    'label' => __('description'),
-                                    'value' => $subDepartment->description
-                                ],
-                                'description_extra' => [
-                                    'type'  => 'textEditor',
-                                    'label' => __('Extra description'),
-                                    'value' => $subDepartment->description_extra
-                                ],
-                            ]
-                        ],
-                        [
-                            'label'  => __('Translations'),
-                            'icon'   => 'fa-light fa-language',
-                            'main' => $subDepartment->name,
-                            'languages_main' => $subDepartment->shop->language->code,
-                            'fields' => [
-                                'name_i8n' => [
-                                    'type'  => 'input_translation',
-                                    'label' => __('translate name'),
-                                    'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
-                                    'value' => $subDepartment->getTranslations('name_i8n')
-                                ],
-                                'description_title_i8n' => [
-                                    'type'  => 'input_translation',
-                                    'label' => __('translate description title'),
-                                    'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
-                                    'value' => $subDepartment->getTranslations('description_title_i8n')
-                                ],
-                                'description_i8n' => [
-                                    'type'  => 'textEditor_translation',
-                                    'label' => __('translate description'),
-                                    'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
-                                    'value' => $subDepartment->getTranslations('description_i8n')
-                                ],
-                                'description_extra_i8n' => [
-                                    'type'  => 'textEditor_translation',
-                                    'label' => __('translate description extra'),
-                                    'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
-                                    'value' => $subDepartment->getTranslations('description_extra_i8n')
-                                ],
-                            ]
-                        ],
-                        [
-                            'label'  => __('Properties'),
-                            'icon'   => 'fa-light fa-fingerprint',
-                            'fields' => [
-                                "image"         => [
-                                    "type"    => "crop-image-full",
-                                    "label"   => __("Image"),
-                                    "value"   => $subDepartment->imageSources(720, 480),
-                                    "required" => false,
-                                    'noSaveButton' => true,
-                                    "full"         => true
-                                ],
-                            ]
-                        ],
-                        [
-                            'label'  => __('Pricing'),
-                            'icon'   => 'fa-light fa-money-bill',
-                            'fields' => [
-                                'cost_price_ratio' => [
-                                    'type'          => 'input_number',
-                                    'bind' => [
-                                        'maxFractionDigits' => 3
+                            [
+                                'label'  => __('Id'),
+                                'icon'   => 'fa-light fa-fingerprint',
+                                'fields' => [
+                                    'code' => [
+                                        'type'  => 'input',
+                                        'label' => __('code'),
+                                        'value' => $subDepartment->code
                                     ],
-                                    'label'         => __('pricing ratio'),
-                                    'placeholder'   => __('Cost price ratio'),
-                                    'required'      => true,
-                                    'value'         => $subDepartment->cost_price_ratio,
-                                    'min'           => 0
-                                ],
-                            ]
-                        ],
-                        [
-                            'label'  => __('Department'),
-                            'icon'   => 'fa-light fa-box',
-                            'fields' => [
-                                'department_id'  =>  [
-                                    'type'    => 'select_infinite',
-                                    'label'   => __('Department'),
-                                    'options'   => [
-                                        [
-                                            'id' => $subDepartment->department?->id,
-                                            'code' => $subDepartment->department?->code
-                                        ]
-                                    ],
-                                    'fetchRoute'    => [
-                                        'name'       => 'grp.org.shops.show.catalogue.departments.index',
-                                        'parameters' => [
-                                            'organisation' => $this->organisation->slug,
-                                            'shop' => $this->shop->slug
-                                        ]
-                                    ],
-                                    'valueProp' => 'id',
-                                    'labelProp' => 'code',
-                                    'required' => false,
-                                    'value'   => $subDepartment->department->id ?? null,
                                 ]
                             ],
-                        ],
-
-
-                    ],
+                            [
+                                'label'  => __('Name/Description'),
+                                'icon'   => 'fa-light fa-tag',
+                                'fields' => [
+                                    'name' => [
+                                        'type'  => 'input',
+                                        'label' => __('name'),
+                                        'value' => $subDepartment->name
+                                    ],
+                                    'description_title' => [
+                                        'type'  => 'input',
+                                        'label' => __('description title'),
+                                        'value' => $subDepartment->description_title
+                                    ],
+                                    'description' => [
+                                        'type'  => 'textEditor',
+                                        'label' => __('description'),
+                                        'value' => $subDepartment->description
+                                    ],
+                                    'description_extra' => [
+                                        'type'  => 'textEditor',
+                                        'label' => __('Extra description'),
+                                        'value' => $subDepartment->description_extra
+                                    ],
+                                ]
+                            ],
+                            !$subDepartment->master_product_category_id ? [
+                                'label'  => __('Translations'),
+                                'icon'   => 'fa-light fa-language',
+                                'main' => $subDepartment->name,
+                                'languages_main' => $subDepartment->shop->language->code,
+                                'fields' => [
+                                    'name_i8n' => [
+                                        'type'  => 'input_translation',
+                                        'label' => __('translate name'),
+                                        'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
+                                        'value' => $subDepartment->getTranslations('name_i8n')
+                                    ],
+                                    'description_title_i8n' => [
+                                        'type'  => 'input_translation',
+                                        'label' => __('translate description title'),
+                                        'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
+                                        'value' => $subDepartment->getTranslations('description_title_i8n')
+                                    ],
+                                    'description_i8n' => [
+                                        'type'  => 'textEditor_translation',
+                                        'label' => __('translate description'),
+                                        'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
+                                        'value' => $subDepartment->getTranslations('description_i8n')
+                                    ],
+                                    'description_extra_i8n' => [
+                                        'type'  => 'textEditor_translation',
+                                        'label' => __('translate description extra'),
+                                        'languages' => GetLanguagesOptions::make()->getExtraGroupLanguages($subDepartment->shop->extra_languages),
+                                        'value' => $subDepartment->getTranslations('description_extra_i8n')
+                                    ],
+                                ]
+                            ] : null,
+                            [
+                                'label'  => __('Properties'),
+                                'icon'   => 'fa-light fa-fingerprint',
+                                'fields' => [
+                                    "image"         => [
+                                        "type"    => "crop-image-full",
+                                        "label"   => __("Image"),
+                                        "value"   => $subDepartment->imageSources(720, 480),
+                                        "required" => false,
+                                        'noSaveButton' => true,
+                                        "full"         => true
+                                    ],
+                                ]
+                            ],
+                            [
+                                'label'  => __('Pricing'),
+                                'icon'   => 'fa-light fa-money-bill',
+                                'fields' => [
+                                    'cost_price_ratio' => [
+                                        'type'          => 'input_number',
+                                        'bind' => [
+                                            'maxFractionDigits' => 3
+                                        ],
+                                        'label'         => __('pricing ratio'),
+                                        'placeholder'   => __('Cost price ratio'),
+                                        'required'      => true,
+                                        'value'         => $subDepartment->cost_price_ratio,
+                                        'min'           => 0
+                                    ],
+                                ]
+                            ],
+                            [
+                                'label'  => __('Department'),
+                                'icon'   => 'fa-light fa-box',
+                                'fields' => [
+                                    'department_id'  =>  [
+                                        'type'    => 'select_infinite',
+                                        'label'   => __('Department'),
+                                        'options'   => [
+                                            [
+                                                'id' => $subDepartment->department?->id,
+                                                'code' => $subDepartment->department?->code
+                                            ]
+                                        ],
+                                        'fetchRoute'    => [
+                                            'name'       => 'grp.org.shops.show.catalogue.departments.index',
+                                            'parameters' => [
+                                                'organisation' => $this->organisation->slug,
+                                                'shop' => $this->shop->slug
+                                            ]
+                                        ],
+                                        'valueProp' => 'id',
+                                        'labelProp' => 'code',
+                                        'required' => false,
+                                        'value'   => $subDepartment->department->id ?? null,
+                                    ]
+                                ],
+                            ],
+    
+    
+                        ]
+                    ),
                     'args'      => [
                         'updateRoute' => [
                             'name'       => 'grp.models.product_category.update',
