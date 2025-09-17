768541920edacfec8e1512528f85efe68c5d2149
Artha <manager.arthachan@gmail.com>
2025-09-17 16:56:02 +0800

    feat: add review status columns to `products` and `product_categories` tables

---
+++ database/migrations/2025_09_17_085429_add_reviews_columns_to_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->boolean('is_name_reviewed')->nullable();
+            $table->boolean('is_title_reviewed')->nullable();
+            $table->boolean('is_description_reviewed')->nullable();
+            $table->boolean('is_extra_description_reviewed')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('is_name_reviewed');
+            $table->dropColumn('is_title_reviewed');
+            $table->dropColumn('is_description_reviewed');
+            $table->dropColumn('is_extra_description_reviewed');
+        });
+    }
+};
