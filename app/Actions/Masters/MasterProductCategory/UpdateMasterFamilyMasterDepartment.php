d690cb6363205aff088d59e14ce36d2af3fced2b
Raul Perusquia <raul@inikoo.com>
2025-09-18 14:40:08 +0800

    Remove `UpdateMasterFamilyMasterDepartment` action:
    - Delete unused action located in `Actions/Masters`.
    - Adjust references across `AttachMasterFamiliesToMasterDepartment` and related files.

--- app/Actions/Masters/MasterProductCategory/UpdateMasterFamilyMasterDepartment.php
+++ app/Actions/Masters/MasterProductCategory/UpdateMasterFamilyMasterDepartment.php
@@ -1,65 +1,79 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 18 Sept 2025 14:39:46 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 namespace App\Actions\Masters\MasterProductCategory;
 
-use App\Actions\GrpAction;
 use App\Actions\Masters\MasterProductCategory\Hydrators\MasterDepartmentHydrateMasterAssets;
 use App\Actions\Masters\MasterProductCategory\Hydrators\MasterProductCategoryHydrateMasterFamilies;
 use App\Actions\Masters\MasterProductCategory\Hydrators\MasterSubDepartmentHydrateMasterAssets;
 use App\Actions\Masters\MasterShop\Hydrators\MasterShopHydrateMasterFamiliesWithNoDepartment;
+use App\Actions\OrgAction;
 use App\Actions\SysAdmin\Group\Hydrators\GroupHydrateMasterFamiliesWithNoDepartment;
+use App\Actions\Traits\Authorisations\WithMastersEditAuthorisation;
 use App\Actions\Traits\WithActionUpdate;
 use App\Enums\Catalogue\MasterProductCategory\MasterProductCategoryTypeEnum;
 use App\Models\Masters\MasterProductCategory;
-use App\Models\Masters\MasterShop;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Lorisleiva\Actions\ActionRequest;
 
-class UpdateMasterFamilyMasterDepartment extends GrpAction
+class UpdateMasterFamilyMasterDepartment extends OrgAction
 {
     use WithActionUpdate;
+    use WithMastersEditAuthorisation;
 
-    private MasterShop $masterShop;
+    /**
+     * @var \App\Models\Masters\MasterProductCategory
+     */
+    private MasterProductCategory $masterFamily;
 
-    public function handle(MasterProductCategory $family, array $modelData): MasterProductCategory
+    public function handle(MasterProductCategory $masterFamily, array $modelData): MasterProductCategory
     {
-        $oldDepartment    = $family->masterDepartment ?? null;
-        $oldSubDepartment = $family->masterSubDepartment ?? null;
+
+
+        $oldDepartment    = $masterFamily->masterDepartment ?? null;
+        $oldSubDepartment = $masterFamily->masterSubDepartment ?? null;
 
         data_set($modelData, 'master_parent_id', Arr::get($modelData, 'master_department_id'));
         data_set($modelData, 'master_department_id', Arr::get($modelData, 'master_department_id'));
         data_set($modelData, 'master_sub_department_id', null);
 
-        $family  = $this->update($family, $modelData);
-        $changes = $family->getChanges();
-        $family->refresh();
+        $masterFamily = $this->update($masterFamily, $modelData);
+        $changes      = $masterFamily->getChanges();
+        $masterFamily->refresh();
         DB::table('master_assets')
-            ->where('master_family_id', $family->id)
+            ->where('master_family_id', $masterFamily->id)
             ->update([
-                'master_department_id'     => $family->master_department_id,
+                'master_department_id'     => $masterFamily->department_id,
                 'master_sub_department_id' => null
             ]);
 
 
         if (Arr::has($changes, 'master_department_id')) {
-            MasterDepartmentHydrateMasterAssets::dispatch($family->masterDepartment);
-            MasterProductCategoryHydrateMasterFamilies::dispatch($family->masterDepartment);
+            MasterDepartmentHydrateMasterAssets::dispatch($masterFamily->masterDepartment);
+            MasterProductCategoryHydrateMasterFamilies::dispatch($masterFamily->masterDepartment);
             if ($oldDepartment) {
                 MasterDepartmentHydrateMasterAssets::dispatch($oldDepartment);
                 MasterProductCategoryHydrateMasterFamilies::dispatch($oldDepartment);
             } else {
-                MasterShopHydrateMasterFamiliesWithNoDepartment::dispatch($family->masterShop);
-                GroupHydrateMasterFamiliesWithNoDepartment::dispatch($family->group);
+
+                MasterShopHydrateMasterFamiliesWithNoDepartment::dispatch($masterFamily->masterShop);
+                GroupHydrateMasterFamiliesWithNoDepartment::dispatch($masterFamily->group);
             }
         }
 
-        if (Arr::has($changes, 'master_sub_department_id') && $oldSubDepartment) {
+        if (Arr::has($changes, 'sub_department_id') && $oldSubDepartment) {
             MasterProductCategoryHydrateMasterFamilies::dispatch($oldSubDepartment);
             MasterSubDepartmentHydrateMasterAssets::dispatch($oldSubDepartment);
         }
 
-        return $family;
+        return $masterFamily;
     }
 
 
@@ -70,17 +84,25 @@ class UpdateMasterFamilyMasterDepartment extends GrpAction
                 'required',
                 Rule::exists('master_product_categories', 'id')
                     ->where('type', MasterProductCategoryTypeEnum::DEPARTMENT)
-                    ->where('master_shop_id', $this->masterShop->id)
+                    ->where('master_shop_id', $this->masterFamily->master_shop_id)
             ]
         ];
     }
 
-    public function action(MasterProductCategory $family, array $modelData): MasterProductCategory
+    public function action(MasterProductCategory $masterFamily, array $modelData): MasterProductCategory
     {
         $this->asAction = true;
-        $this->masterShop = $family->masterShop;
-        $this->initialisation($family->group, $modelData);
+        $this->masterFamily = $masterFamily;
+        $this->initialisationFromGroup($masterFamily->group, $modelData);
 
-        return $this->handle($family, $this->validatedData);
+        return $this->handle($masterFamily, $this->validatedData);
+    }
+
+
+    public function asController(MasterProductCategory $masterFamily, ActionRequest $request): void
+    {
+        $this->masterFamily = $masterFamily;
+        $this->initialisationFromGroup($masterFamily->group, $request);
+        $this->handle($masterFamily, $this->validatedData);
     }
 }
