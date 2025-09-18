e1c315c59fa27269e8ad50ccd6a800439ca59332
Raul Perusquia <raul@inikoo.com>
2025-09-18 16:37:38 +0800

    Fix `RepairWrongProductOrgStock` & `RepairWrongMasterProductStock` quantity logic:
    - Adjust trade unit and stock quantities to ensure correct base calculations.
    - Add early exit for missing stocks in `RepairWrongMasterProductStock`.
    - Integrate `HydrateProducts` in `RepairWrongProductOrgStock` for data consistency.

--- app/Actions/Maintenance/Catalogue/RepairWrongMasterProductStock.php
+++ app/Actions/Maintenance/Catalogue/RepairWrongMasterProductStock.php
@@ -21,12 +21,43 @@ class RepairWrongMasterProductStock
 
     public function handle(MasterAsset $masterAsset, Command $command): void
     {
+
+        if($masterAsset->stocks()->count()>1
+        ){
+            dd($masterAsset->orgStocks);
+        }
+
+        $stock= $masterAsset->stocks->first();
+
+        if(!$stock){
+            return;
+        }
+
+        $base=$stock->tradeUnits->pluck('pivot.quantity')[0];
+
+
         $productData = [
             'product_code' => $masterAsset->code,
             'product_units' => $masterAsset->units,
-            'org_stocks' => $masterAsset->stocks->pluck('pivot.quantity')->toArray(),
+            'stocks' => $masterAsset->stocks->pluck('pivot.quantity')->toArray(),
             'trade_units' => $masterAsset->tradeUnits->pluck('pivot.quantity')->toArray(),
+            'base'=>$base
+
         ];
+
+
+
+        $tradeUnit=$masterAsset->tradeUnits->first();
+        $masterAsset->tradeUnits()->updateExistingPivot(
+            $tradeUnit->id,
+            ['quantity' => 1]
+        );
+
+        $stock=$masterAsset->stocks->first();
+        $masterAsset->stocks()->updateExistingPivot(
+            $stock->id,
+            ['quantity' => 1/$base]
+        );
 
         $command->info('Master Product Data:');
         $command->info(json_encode($productData, JSON_PRETTY_PRINT));
