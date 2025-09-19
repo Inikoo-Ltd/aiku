4b119dde9200c8c6742ca2b2e2dccb18b375e63c
YudhistiraA <aryarajasa0@gmail.com>
2025-09-19 10:34:53 +0800

    fix breadcrumbs

--- resources/js/Pages/Grp/Masters/MasterProduct.vue
+++ resources/js/Pages/Grp/Masters/MasterProduct.vue
@@ -78,7 +78,7 @@ console.log(props)
     <Head :title="capitalize(title)"/>
     <PageHeading :data="pageHead"/>
     <Tabs :current="currentTab" :navigation="tabs['navigation']" @update:tab="handleTabUpdate"/>
-    <div v-if="mini_breadcrumbs" class="bg-white shadow-sm rounded px-4 py-2 mx-4 mt-2 w-fit border border-gray-200 overflow-x-auto">
+    <div v-if="mini_breadcrumbs.length != 0" class="bg-white shadow-sm rounded px-4 py-2 mx-4 mt-2 w-fit border border-gray-200 overflow-x-auto">
         <Breadcrumb  :model="mini_breadcrumbs">
             <template #item="{ item, index }">
                 <div class="flex items-center gap-1 whitespace-nowrap">
