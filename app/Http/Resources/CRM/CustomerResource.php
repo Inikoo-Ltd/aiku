24ea059d213f856d043ecc0a1a7022079e0d0a41
Raul Perusquia <raul@inikoo.com>
2025-09-22 10:43:21 +0800

    Refactor `GetCustomerShowcase` to use `CustomerResource` and enhance `CustomerResource` with additional fields and tax number support.

--- app/Http/Resources/CRM/CustomerResource.php
+++ app/Http/Resources/CRM/CustomerResource.php
@@ -2,7 +2,7 @@
 
 /*
  * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Tue, 02 Apr 2024 14:30:49 Central Indonesia Time, Bali Office , Indonesia
+ * Created: Tue, 02 Apr 2024 14:30:49 Central Indonesia Time, Bali Office, Indonesia
  * Copyright (c) 2024, Raul A Perusquia Flores
  */
 
@@ -10,6 +10,7 @@ namespace App\Http\Resources\CRM;
 
 use App\Http\Resources\HasSelfCall;
 use App\Http\Resources\Helpers\AddressResource;
+use App\Http\Resources\Helpers\TaxNumberResource;
 use App\Models\CRM\Customer;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,18 +25,23 @@ class CustomerResource extends JsonResource
     {
         /** @var Customer $customer */
         $customer = $this;
+
         return [
-            'slug'                   => $customer->slug,
-            'reference'              => $customer->reference,
-            'name'                   => $customer->name,
-            'contact_name'           => $customer->contact_name,
-            'company_name'           => $customer->company_name,
-            'location'               => $customer->location,
-            'address'                => AddressResource::make($customer->address),
-            'email'                  => $customer->email,
-            'phone'                  => $customer->phone,
-            'created_at'             => $customer->created_at,
-            'number_current_customer_clients' => $this->number_current_customer_clients,
+            'id'           => $customer->id,
+            'slug'         => $customer->slug,
+            'reference'    => $customer->reference,
+            'name'         => $customer->name,
+            'contact_name' => $customer->contact_name,
+            'company_name' => $customer->company_name,
+            'location'     => $customer->location,
+            'address'      => AddressResource::make($customer->address),
+            'email'        => $customer->email,
+            'phone'        => $customer->phone,
+            'created_at'   => $customer->created_at,
+            'balance'      => $customer->balance,
+            'tax_number'   => $customer->taxNumber ? TaxNumberResource::make($customer->taxNumber)->getArray() : [],
+            'state'        => $customer->state,
+            'status'       => $customer->status,
         ];
     }
 }
