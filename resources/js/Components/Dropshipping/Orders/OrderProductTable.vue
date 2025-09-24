87bc4693910e8631177f38bc0a13f43e18d6a416
YudhistiraA <aryarajasa0@gmail.com>
2025-09-24 13:37:24 +0800

    Order Product Table

--- resources/js/Components/Dropshipping/Orders/OrderProductTable.vue
+++ resources/js/Components/Dropshipping/Orders/OrderProductTable.vue
@@ -132,16 +132,23 @@ onBeforeUnmount(() => {
 
 async function onSave() {
   const changedItems: Record<number, { newQty: number }> = {}
-  const newProducts: Record<number, { newQty: number }> = {}
+  const newProducts: Record<number, { quantity_ordered: number }> = {}
 
   rowsArray().forEach((row) => {
+    // Handle new products
     if (typeof row.id === 'string' && row.id.startsWith('new')) {
+      const edited = createNewQty[row.id]
+      const qty = edited
+        ? Number(edited.quantity_ordered)
+        : Number(row.quantity_ordered)
+
       newProducts[row.id_product] = {
-        quantity_ordered: Number(row.quantity_ordered),
+        quantity_ordered: qty,
       }
       return
     }
 
+    // Handle existing products
     const clonedItem = createNewQty[row.id]
     if (clonedItem) {
       const newQty = Number(clonedItem.quantity_ordered)
@@ -151,7 +158,10 @@ async function onSave() {
     }
   })
 
-  if (Object.keys(changedItems).length === 0 && newProducts.length === 0) return
+  // nothing to save
+  if (Object.keys(changedItems).length === 0 && Object.keys(newProducts).length === 0) {
+    return
+  }
 
   console.log("🟢 changedItems:", changedItems)
   console.log("🟡 newProducts:", newProducts)
@@ -167,7 +177,7 @@ async function onSave() {
       onFinish: () => (loadingsaveModify.value = false),
       onSuccess: () => {
         // clear state
-        Object.keys(createNewQty).forEach((k) => delete createNewQty[Number(k)])
+        Object.keys(createNewQty).forEach((k) => delete createNewQty[k])
         editingIds.value.clear()
         notify({
           title: trans('Success'),
@@ -187,6 +197,7 @@ async function onSave() {
   )
 }
 
+
 const openModal = (action: any) => {
   currentAction.value = action
   isModalProductListOpen.value = true
@@ -203,7 +214,7 @@ const addNewProduct = (products) => {
     const newItem = {
       id: existingIndex >= 0 ? props.data.data[existingIndex].id : 'new-' + ulid(),
       asset_code: product.code,
-      id_product : product.id,
+      id_product: product.id,
       price: product.price,
       quantity_ordered: product.quantity_selected,
       net_amount: product.quantity_selected * product.price,
@@ -229,127 +240,139 @@ const onDeleteNewRow = (index) => {
 </script>
 
 <template>
-  <Table :resource="data" :name="tab" :rowColorFunction="(item) => {
-    if (typeof item.id === 'string' && item.id.startsWith('new')) {
-      return 'bg-yellow-50'
-    }
-    return ''
-  }">
+  <div>
+    <Table :resource="data" :name="tab" :rowColorFunction="(item) => {
+      if (typeof item.id === 'string' && item.id.startsWith('new')) {
+        return 'bg-yellow-50'
+      }
+      return ''
+    }">
 
-    <!-- Save All Button -->
-    <template #add-on-button-in-before>
-      <div v-if="layout?.app?.environment === 'local'">
-      <Button
-        v-if="Object.keys(createNewQty).length > 0 || rowsArray().some(item => typeof item.id === 'string' && item.id.startsWith('new'))"
-        label="Save all changes" @click="onSave" :loading="loadingsaveModify" />
-      <Button label="Add New" @click="openModal" />
-      </div>
-    </template>
+      <!-- Save All Button -->
+      <template #add-on-button-in-before>
+        <div v-if="layout?.app?.environment === 'local'" class="flex items-center gap-2">
+          <!-- Add Product -->
+          <Button type="create" label="Product" size="sm" @click="openModal" :injectStyle="{ borderRadius: '3px' }" />
 
+          <!-- Divider -->
+          <div v-if="
+            Object.keys(createNewQty).length > 0 ||
+            rowsArray().some(item => typeof item.id === 'string' && item.id.startsWith('new'))
+          " class="h-5 border-r"></div>
 
-    <!-- Column: Code -->
-    <template #cell(asset_code)="{ item }">
-      <Link :href="productRoute(item)" class="primaryLink">
-      {{ item.asset_code }}
-      </Link>
-    </template>
-
-    <!-- Column: Name / Stock -->
-    <template #cell(asset_name)="{ item }">
-      <div>
-        <div>{{ item.asset_name }}</div>
-        <div v-if="item.available_quantity !== undefined && item.available_quantity < 1">
-          <Tag label="Out of stock" no-hover-color :theme="7" size="xxs" />
+          <!-- Save All Changes -->
+          <Button v-if="
+            Object.keys(createNewQty).length > 0 ||
+            rowsArray().some(item => typeof item.id === 'string' && item.id.startsWith('new'))
+          " type="save" label="Save all changes" size="sm" :injectStyle="{ borderRadius: '3px' }"
+            :loading="loadingsaveModify" @click="onSave" />
         </div>
-        <div v-else class="text-gray-500 italic text-xs">
-          Stock: {{ locale.number(item.available_quantity || 0) }} available
-        </div>
-      </div>
-    </template>
+      </template>
 
-    <!-- Column: Quantity Ordered -->
-    <template #cell(quantity_ordered)="{ item }">
-      <div class="flex items-center justify-end gap-2">
-        <!-- Editable when creating and not in edit mode -->
-        <div v-if="state === 'creating' && !editingIds.has(item.id)" class="w-fit">
-          <NumberWithButtonSave :modelValue="item.quantity_ordered" :routeSubmit="item.updateRoute"
-            :bindToTarget="{ min: 0 }" isWithRefreshModel keySubmit="quantity_ordered"
-            :isLoading="isLoading === 'quantity' + item.id" :readonly="readonly"
-            @update:modelValue="(e: number) => debounceUpdateQuantity(item.updateRoute, item.id, e)" noUndoButton
-            noSaveButton />
-        </div>
-
-        <!-- Read-only display -->
-        <div v-else-if="!editingIds.has(item.id)">
-          {{ formatQuantity(item.quantity_ordered) }}
-        </div>
-
-        <!-- Inline edit mode with original quantity displayed -->
-        <div v-else class="items-center gap-2">
-          <span class="text-gray-500 italic text-sm">
-            original: {{ formatQuantity(item.quantity_ordered) }}
-          </span>
-          <NumberWithButtonSave v-model="createNewQty[item.id].quantity_ordered" :bindToTarget="{ min: 0 }" noUndoButton
-            noSaveButton class="w-24" />
-        </div>
-      </div>
-    </template>
-
-    <template #cell(net_amount)="{ item }">
-      <div class="flex justify-end">
-        <div v-if="editingIds.has(item.id)" class="">
-          <!-- Original price tag -->
-          <div
-            class="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm font-medium shadow-sm whitespace-nowrap my-2">
-            orig: {{ locale.currencyFormat(item.currency_code, item.net_amount) }}
-          </div>
-          <!-- Estimated price tag -->
-          <div
-            class="bg-yellow-100 text-yellow-800 px-4 py-1.5 rounded-full text-sm font-medium shadow-sm whitespace-nowrap inline-flex items-center justify-center">
-            est: {{ locale.currencyFormat(item.currency_code, (item.price *
-            createNewQty[item.id].quantity_ordered).toFixed(2)) }}
-          </div>
-        </div>
-        <div v-else>
-          {{ locale.currencyFormat(item.currency_code, item.net_amount) }}
-        </div>
-      </div>
-    </template>
-
-    <!-- Column: Actions -->
-    <template #cell(actions)="{ item }">
-      <div class="flex gap-2 items-center">
-        <!-- Delete / Unselect -->
-        <Link v-if="state === 'creating'" :href="route(item.deleteRoute.name, item.deleteRoute.parameters)" as="button"
-          :method="item.deleteRoute.method" @start="() => (isLoading.value = 'unselect' + item.id)"
-          @finish="() => (isLoading.value = null)" v-tooltip="trans('Unselect this product')" :preserveScroll="true">
-        <Button v-if="!readonly" icon="fal fa-times" type="negative" size="xs"
-          :loading="isLoading === 'unselect' + item.id" />
+      <!-- Column: Code -->
+      <template #cell(asset_code)="{ item }">
+        <Link :href="productRoute(item)" class="primaryLink">
+        {{ item.asset_code }}
         </Link>
+      </template>
 
-        <!-- Edit / Cancel -->
-        <div v-if="state !== 'creating'" class="flex gap-2 items-center">
-          <button v-if="!editingIds.has(item.id) && layout?.app?.environment === 'local'"
-            class="h-9 align-bottom text-center" @click="startEdit(item)" aria-label="Edit Product Order"
-            v-tooltip="'Edit Product Order'">
-            <FontAwesomeIcon :icon="faPencil" class="h-5 text-gray-500 hover:text-gray-700" aria-hidden="true" />
-          </button>
-
-          <Button v-else-if="editingIds.has(item.id)" type="negative" v-tooltip="'Cancel edit'" :icon="faTimes"
-            @click="onCancel(item)" size="sm" aria-label="Cancel edit" />
-
-          <Button v-if="typeof item.id === 'string' && item.id.startsWith('new')" type="negative" v-tooltip="'delete'"
-            :icon="faTrashAlt" @click="() => onDeleteNewRow(item.rowIndex)" size="sm" />
+      <!-- Column: Name / Stock -->
+      <template #cell(asset_name)="{ item }">
+        <div>
+          <div>{{ item.asset_name }}</div>
+          <div v-if="item.available_quantity !== undefined && item.available_quantity < 1">
+            <Tag label="Out of stock" no-hover-color :theme="7" size="xxs" />
+          </div>
+          <div v-else class="text-gray-500 italic text-xs">
+            Stock: {{ locale.number(item.available_quantity || 0) }} available
+          </div>
         </div>
-      </div>
-    </template>
+      </template>
 
-  </Table>
+      <!-- Column: Quantity Ordered -->
+      <template #cell(quantity_ordered)="{ item }">
+        <div class="flex items-center justify-end gap-2">
+          <!-- Editable when creating and not in edit mode -->
+          <div v-if="state === 'creating' && !editingIds.has(item.id)" class="w-fit">
+            <NumberWithButtonSave :modelValue="item.quantity_ordered" :routeSubmit="item.updateRoute"
+              :bindToTarget="{ min: 0 }" isWithRefreshModel keySubmit="quantity_ordered"
+              :isLoading="isLoading === 'quantity' + item.id" :readonly="readonly"
+              @update:modelValue="(e: number) => debounceUpdateQuantity(item.updateRoute, item.id, e)" noUndoButton
+              noSaveButton />
+          </div>
 
-  <Modal :isOpen="isModalProductListOpen" @onClose="isModalProductListOpen = false" width="w-full max-w-6xl">
-    <ProductsSelectorAutoSelect
-      :headLabel="trans('Add products to Order') + ' #' + (Array.isArray(props.data) ? '' : props.data?.reference)"
-      :routeFetch="props.fetchRoute" :isLoadingSubmit="false" :listLoadingProducts="false" withQuantity
-      @submit="addNewProduct" />
-  </Modal>
+          <!-- Read-only display -->
+          <div v-else-if="!editingIds.has(item.id)">
+            {{ formatQuantity(item.quantity_ordered) }}
+          </div>
+
+          <!-- Inline edit mode with original quantity displayed -->
+          <div v-else class="items-center gap-2">
+            <span class="text-gray-500 italic text-sm">
+              original: {{ formatQuantity(item.quantity_ordered) }}
+            </span>
+            <NumberWithButtonSave v-model="createNewQty[item.id].quantity_ordered" :bindToTarget="{ min: 0 }"
+              noUndoButton noSaveButton class="w-24" />
+          </div>
+        </div>
+      </template>
+
+      <template #cell(net_amount)="{ item }">
+        <div class="flex justify-end">
+          <div v-if="editingIds.has(item.id)" class="">
+            <!-- Original price tag -->
+            <div
+              class="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm font-medium shadow-sm whitespace-nowrap my-2">
+              orig: {{ locale.currencyFormat(item.currency_code, item.net_amount) }}
+            </div>
+            <!-- Estimated price tag -->
+            <div
+              class="bg-yellow-100 text-yellow-800 px-4 py-1.5 rounded-full text-sm font-medium shadow-sm whitespace-nowrap inline-flex items-center justify-center">
+              est: {{ locale.currencyFormat(item.currency_code, (item.price *
+                createNewQty[item.id].quantity_ordered).toFixed(2)) }}
+            </div>
+          </div>
+          <div v-else>
+            {{ locale.currencyFormat(item.currency_code, item.net_amount) }}
+          </div>
+        </div>
+      </template>
+
+      <!-- Column: Actions -->
+      <template #cell(actions)="{ item }">
+        <div class="flex gap-2 items-center">
+          <!-- Delete / Unselect -->
+          <Link v-if="state === 'creating'" :href="route(item.deleteRoute.name, item.deleteRoute.parameters)"
+            as="button" :method="item.deleteRoute.method" @start="() => (isLoading.value = 'unselect' + item.id)"
+            @finish="() => (isLoading.value = null)" v-tooltip="trans('Unselect this product')" :preserveScroll="true">
+          <Button v-if="!readonly" icon="fal fa-times" type="negative" size="xs"
+            :loading="isLoading === 'unselect' + item.id" />
+          </Link>
+
+          <!-- Edit / Cancel -->
+          <div v-if="state !== 'creating'" class="flex gap-2 items-center">
+            <button v-if="!editingIds.has(item.id) && layout?.app?.environment === 'local'"
+              class="h-9 align-bottom text-center" @click="startEdit(item)" aria-label="Edit Product Order"
+              v-tooltip="'Edit Product Order'">
+              <FontAwesomeIcon :icon="faPencil" class="h-5 text-gray-500 hover:text-gray-700" aria-hidden="true" />
+            </button>
+
+            <Button v-else-if="editingIds.has(item.id)" type="negative" v-tooltip="'Cancel edit'" :icon="faTimes"
+              @click="onCancel(item)" size="sm" aria-label="Cancel edit" />
+
+            <Button v-if="typeof item.id === 'string' && item.id.startsWith('new')" type="negative" v-tooltip="'delete'"
+              :icon="faTrashAlt" @click="() => onDeleteNewRow(item.rowIndex)" size="sm" />
+          </div>
+        </div>
+      </template>
+
+    </Table>
+
+    <Modal :isOpen="isModalProductListOpen" @onClose="isModalProductListOpen = false" width="w-full max-w-6xl">
+      <ProductsSelectorAutoSelect
+        :headLabel="trans('Add products to Order') + ' #' + (Array.isArray(props.data) ? '' : props.data?.reference)"
+        :routeFetch="props.fetchRoute" :isLoadingSubmit="false" :listLoadingProducts="false" withQuantity
+        @submit="addNewProduct" />
+    </Modal>
+  </div>
 </template>
