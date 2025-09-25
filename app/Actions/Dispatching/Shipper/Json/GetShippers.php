72ac86f9270dd1fa47678b6c29cb6a83457d1e88
KirinZero0 <aryapermana02@gmail.com>
2025-09-25 15:33:48 +0800

    Get Shipper convert to query builder

--- app/Actions/Dispatching/Shipper/Json/GetShippers.php
+++ app/Actions/Dispatching/Shipper/Json/GetShippers.php
@@ -15,17 +15,43 @@ use App\Models\SysAdmin\Organisation;
 use Lorisleiva\Actions\ActionRequest;
 use App\Http\Resources\Dispatching\ShippersResource;
 use App\Models\Dispatching\Shipper;
+use App\Services\QueryBuilder;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class GetShippers extends OrgAction
 {
-    public function asController(Organisation $organisation, ActionRequest $request): \Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
+    public function asController(Organisation $organisation, ActionRequest $request): LengthAwarePaginator
     {
         $this->initialisation($organisation, $request);
 
-        return Shipper::where('organisation_id', $organisation->id)
-            ->where('status', true)
-            ->orderBy('api_shipper')
-            ->get();
+        // return Shipper::where('organisation_id', $organisation->id)
+        //     ->where('status', true)
+        //     ->orderBy('api_shipper')
+        //     ->get();
+
+        return $this->handle($organisation);
+    }
+
+    
+    public function handle(Organisation $organisation, $prefix = null): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->whereAnyWordStartWith('shippers.name', $value)
+                    ->orWhereStartWith('shippers.code', $value);
+            });
+        });
+
+        $queryBuilder = QueryBuilder::for(Shipper::class);
+        $queryBuilder->where('organisation_id', $organisation->id)
+            ->where('status', true);
+
+        return $queryBuilder
+            ->defaultSort('code')
+            ->allowedFilters([$globalSearch])
+            ->withPaginator($prefix)
+            ->withQueryString();
     }
 
     public function jsonResponse($shipper): \Illuminate\Http\Resources\Json\AnonymousResourceCollection|\Illuminate\Http\Resources\Json\JsonResource
