039dc876ae8cc1830338fe72f539570bcb73e7f5
Raul Perusquia <raul@inikoo.com>
2025-09-30 13:14:29 +0800

    Add support for refunding payments to balance and update refund handling logic across models and routes.

--- app/Http/Resources/Accounting/RefundPaymentsResource.php
+++ app/Http/Resources/Accounting/RefundPaymentsResource.php
@@ -37,7 +37,6 @@ class RefundPaymentsResource extends JsonResource
 {
     public function toArray($request): array
     {
-
         $apiRefund = false;
         if ($this->payment_account_code == 'checkout-v2') {
             $apiRefund = true;
@@ -54,29 +53,28 @@ class RefundPaymentsResource extends JsonResource
             'created_at'           => $this->created_at,
             'updated_at'           => $this->updated_at,
             'amount'               => $this->amount,
-            'manual_refund_route'                => [
+            'manual_refund_route'  => [
                 'name'   => 'grp.org.accounting.payments.show',
                 'params' => [
                     'organisation' => $this->organisation_slug,
                     'payment'      => $this->id
                 ]
             ],
-            'api_refund_route'                => [
+            'api_refund_route'     => [
                 'name'   => 'grp.org.accounting.payments.show',
                 'params' => [
                     'organisation' => $this->organisation_slug,
                     'payment'      => $this->id
                 ]
             ],
-            'balance_refund_route'                => [
-                'name'   => 'grp.org.accounting.payments.show',
+            'balance_refund_route' => [
+                'name'   => 'grp.models.payment.refund_to_balance',
                 'params' => [
-                    'organisation' => $this->organisation_slug,
                     'payment'      => $this->id
                 ]
             ],
             'currency_code'        => $this->currency_code,
-            'api_refund' => $apiRefund
+            'api_refund'           => $apiRefund
         );
     }
 }
