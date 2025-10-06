f65df352b47660bef9123a9748e3341bee0ed555
Raul Perusquia <raul@inikoo.com>
2025-10-06 11:26:17 +0800

    Refactor customer communications model to consolidate basket reminder fields and enhance subscription handling in `CustomerResource`. Update related migration and improve data structure logic.

--- app/Http/Resources/CRM/CustomerResource.php
+++ app/Http/Resources/CRM/CustomerResource.php
@@ -8,6 +8,7 @@
 
 namespace App\Http\Resources\CRM;
 
+use App\Enums\Catalogue\Shop\ShopTypeEnum;
 use App\Http\Resources\HasSelfCall;
 use App\Http\Resources\Helpers\AddressResource;
 use App\Http\Resources\Helpers\TaxNumberResource;
@@ -26,22 +27,69 @@ class CustomerResource extends JsonResource
         /** @var Customer $customer */
         $customer = $this;
 
+        $comms = $customer->comms;
+
+        $subscriptions = [
+            'newsletters' => [
+                'is_subscribed'   => $comms->is_subscribed_to_newsletter,
+                'unsubscribed_at' => $comms->newsletter_unsubscribed_at
+            ],
+            'marketing'   => [
+                'is_subscribed'   => $comms->is_subscribed_to_marketing,
+                'unsubscribed_at' => $comms->marketing_unsubscribed_at
+            ],
+
+        ];
+
+        if ($customer->shop->type == ShopTypeEnum::B2B) {
+            $subscriptions['abandoned_cart'] = [
+                'is_subscribed'   => $comms->is_subscribed_to_abandoned_cart,
+                'unsubscribed_at' => $comms->abandoned_cart_unsubscribed_at
+            ];
+
+
+            $subscriptions['reorder_reminder'] = [
+                'is_subscribed'   => $comms->is_subscribed_to_reorder_reminder,
+                'unsubscribed_at' => $comms->reorder_reminder_unsubscribed_at
+            ];
+
+            $subscriptions['basket_low_stock'] = [
+                'is_subscribed'   => $comms->is_subscribed_to_basket_low_stock,
+                'unsubscribed_at' => $comms->basket_low_stock_unsubscribed_at
+            ];
+
+            $subscriptions['to_basket_reminder'] = [
+                'is_subscribed'   => $comms->is_subscribed_to_basket_reminder,
+                'unsubscribed_at' => $comms->basket_reminder_unsubscribed_at
+            ];
+        }
+
+
         return [
-            'id'           => $customer->id,
-            'slug'         => $customer->slug,
-            'reference'    => $customer->reference,
-            'name'         => $customer->name,
-            'contact_name' => $customer->contact_name,
-            'company_name' => $customer->company_name,
-            'location'     => $customer->location,
-            'address'      => AddressResource::make($customer->address),
-            'email'        => $customer->email,
-            'phone'        => $customer->phone,
-            'created_at'   => $customer->created_at,
-            'balance'      => $customer->balance,
-            'tax_number'   => $customer->taxNumber ? TaxNumberResource::make($customer->taxNumber)->getArray() : [],
-            'state'        => $customer->state,
-            'status'       => $customer->status,
+            'id'                  => $customer->id,
+            'slug'                => $customer->slug,
+            'reference'           => $customer->reference,
+            'name'                => $customer->name,
+            'contact_name'        => $customer->contact_name,
+            'company_name'        => $customer->company_name,
+            'location'            => $customer->location,
+            'address'             => AddressResource::make($customer->address),
+            'email'               => $customer->email,
+            'phone'               => $customer->phone,
+            'created_at'          => $customer->created_at,
+            'balance'             => $customer->balance,
+            'tax_number'          => $customer->taxNumber ? TaxNumberResource::make($customer->taxNumber)->getArray() : [],
+            'state'               => $customer->state,
+            'status'              => $customer->status,
+            'email_subscriptions' => [
+                'suspended'     => [
+                    'is_suspended'    => $comms->is_suspended,
+                    'suspended_at'    => $comms->suspended_at,
+                    'suspended_cause' => $comms->suspended_cause,
+                ],
+                'subscriptions' => $subscriptions
+
+            ]
         ];
     }
 }
