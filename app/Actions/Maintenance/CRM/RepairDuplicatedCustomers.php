feb9d32c3e7735b5a3190705c809acccb7fe436e
Raul Perusquia <raul@inikoo.com>
2025-10-06 22:58:28 +0800

    Add `slug` to debug output in `RepairDuplicatedCustomers` for enhanced logging.

--- app/Actions/Maintenance/CRM/RepairDuplicatedCustomers.php
+++ app/Actions/Maintenance/CRM/RepairDuplicatedCustomers.php
@@ -45,7 +45,7 @@ class RepairDuplicatedCustomers
                         Customer::where('email', $customer->email)->where('shop_id',$customer->shop_id)->get()->each(function ($customer) {
                             $countWebUsers = DB::table('web_users')->where('customer_id', $customer->id)->count();
                             $orders=DB::table('orders')->where('customer_id', $customer->id)->count();
-                           print  ">> ".$customer->id."  WU:$countWebUsers  O:$orders  ; $customer->source_id | $customer->post_source_id   \n";
+                           print  ">> ".$customer->id."  $customer->slug  WU:$countWebUsers  O:$orders  ; $customer->source_id | $customer->post_source_id   \n";
 
                         });
 
