2c534753206f2d31f91913f96f36557bf5aadfed
YudhistiraA <aryarajasa0@gmail.com>
2025-10-07 13:14:19 +0800

    attachment

--- app/Actions/Traits/HasBucketAttachment.php
+++ app/Actions/Traits/HasBucketAttachment.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Wed, 10 Sept 2025 09:46:39 Malaysia Time, Kuala Lumpur, Malaysia
- * Copyright (c) 2025, Raul A Perusquia Flores
- */
-
 namespace App\Actions\Traits;
 
 use App\Models\Catalogue\Product;
@@ -16,77 +10,85 @@ trait HasBucketAttachment
 {
     public function getAttachmentData(MasterAsset|Product|TradeUnit $model): array
     {
+        $public = [
+            [
+                'label' => __('IFRA'),
+                'scope' => 'IFRA',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('SDS'),
+                'scope' => 'SDS',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('Allergen Declarations'),
+                'scope' => 'allergen_declarations',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('Declaration of Conformity'),
+                'scope' => 'declaration_of_conformity',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('CPSR'),
+                'scope' => 'CPSR',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+        ];
+
+        $private = [
+            [
+                'label' => __('UFRA Private'),
+                'scope' => 'ifra_private',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('SDS Private'),
+                'scope' => 'sds_private',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('Allergen Declarations Private'),
+                'scope' => 'allergen_declarations_private',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('CPSR Private'),
+                'scope' => 'cpsr_private',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+            [
+                'label' => __('DOC Private'),
+                'scope' => 'doc_private',
+                'id'    => null,
+                'file'  => null,
+                'size'  => '2kb',
+            ],
+        ];
+
         return [
-            [
-                'label'        => __('IFRA'),
-                'scope'        => 'IFRA',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('SDS'),
-                'scope'        => 'SDS',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('Allergen Declarations'),
-                'scope'        => 'allergen_declarations',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('Declaration of Conformity'),
-                'scope'        => 'declaration_of_conformity',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('CPSR '),
-                'scope'        => 'CPSR ',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('UFRA Private'),
-                'scope'        => 'ifra_private',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('SDS Private'),
-                'scope'        => 'sds_private',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('Allergen Declarations Private'),
-                'scope'        => 'allergen_declarations_private',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('CPSR Private'),
-                'scope'        => 'cpsr_private',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
-            [
-                'label'        => __('DOC Private'),
-                'scope'        => 'doc_private',
-                'id'           => null,
-                'file'         => null,
-                'size'         => '2kb',
-            ],
+            'public'  => $public,
+            'private' => $private,
         ];
     }
 }
