a696060655ba2f35c0391d9b06b7581c9934427b
Raul Perusquia <raul@inikoo.com>
2025-10-12 10:49:22 +0800

    Mock `getJobUniqueId` in `RetinaFulfilmentUITest` setup to return consistent value.

--- tests/Feature/Retina/RetinaFulfilmentUITest.php
+++ tests/Feature/Retina/RetinaFulfilmentUITest.php
@@ -53,6 +53,9 @@ beforeAll(function () {
 });
 beforeEach(function () {
     ReindexWebpageLuigiData::shouldRun();
+    ReindexWebpageLuigiData::mock()
+        ->shouldReceive('getJobUniqueId')
+        ->andReturn(1);
     $this->organisation = createOrganisation();
     $this->warehouse    = createWarehouse();
     $this->fulfilment   = createFulfilment($this->organisation);
