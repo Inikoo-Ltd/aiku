eec8e2071eda3795cc22730a0940064bdefaa326
YudhistiraA <aryarajasa0@gmail.com>
2025-10-13 11:05:13 +0800

    Skeleton Topbar

--- resources/js/Components/CMS/Website/TopBars/Template/Topbar1/Topbar1Iris.vue
+++ resources/js/Components/CMS/Website/TopBars/Template/Topbar1/Topbar1Iris.vue
@@ -7,6 +7,7 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import { getStyles } from "@/Composables/styles"
 import { checkVisible, textReplaceVariables } from "@/Composables/Workshop"
 import ButtonWithLink from "@/Components/Elements/Buttons/ButtonWithLink.vue"
+import { Skeleton } from "primevue"
 
 import { trans } from "laravel-vue-i18n"
 import SwitchLanguage from "@/Components/Iris/SwitchLanguage.vue"
@@ -68,7 +69,8 @@ const layout = inject("layout", {})
 
 <template>
     <div></div>
-    <div id="top_bar_1_iris" class="py-1 px-4 flex flex-col md:flex-row md:justify-between gap-x-4 sticky top-0 z-50" :style="{
+    <div id="top_bar_1_iris" class="py-1 px-4 flex flex-col md:flex-row md:justify-between gap-x-4 sticky top-0 z-50"
+        :style="{
         ...getStyles(layout?.app?.webpage_layout?.container?.properties, screenType),
         margin: 0,
         ...getStyles(model?.container?.properties, screenType)
@@ -86,12 +88,11 @@ const layout = inject("layout", {})
 
         <div class="hidden md:flex justify-between md:justify-start items-center gap-x-1 flex-wrap md:flex-nowrap">
             <SwitchLanguage
-                v-if="layout.app.environment !== 'production' && Object.values(layout.iris.website_i18n?.language_options || {})?.length"
-            />
-            
+                v-if="layout.app.environment !== 'production' && Object.values(layout.iris.website_i18n?.language_options || {})?.length" />
+
             <!-- Section: My account -->
-            <ButtonWithLink type="transparent"  class="bg-transparent"
-                v-if="checkVisible(model?.profile?.visible || null, isLoggedIn) && layout.retina?.type == 'dropshipping'"
+            <ButtonWithLink type="transparent" class="bg-transparent"
+                v-if="checkVisible(model?.profile?.visible || null, isLoggedIn) && layout.retina?.type == 'dropshipping' && !layout.iris_varnish.isFetching"
                 v-tooltip="trans('My account')" url="/app/dashboard" :noHover="true">
                 <template #label>
                     <span class="text-white"> {{ trans('My account') }}</span>
@@ -99,11 +100,12 @@ const layout = inject("layout", {})
             </ButtonWithLink>
 
             <!-- Section: Profile -->
-            <ButtonWithLink v-if="checkVisible(model?.profile?.visible || null, isLoggedIn)"
-                v-tooltip="trans('Profile')" :url="layout.retina?.type == 'b2b' ? '/app/dashboard' : '/app/profile'" icon="fal fa-user" type="transparent" :noHover="true">
-                 <template #icon>
-                    <FontAwesomeIcon icon="fal fa-user" :style="{ color: 'white' }" fixed-width
-                        aria-hidden="true" />
+            <ButtonWithLink
+                v-if="checkVisible(model?.profile?.visible || null, isLoggedIn) && !layout.iris_varnish.isFetching"
+                v-tooltip="trans('Profile')" :url="layout.retina?.type == 'b2b' ? '/app/dashboard' : '/app/profile'"
+                icon="fal fa-user" type="transparent" :noHover="true">
+                <template #icon>
+                    <FontAwesomeIcon icon="fal fa-user" :style="{ color: 'white' }" fixed-width aria-hidden="true" />
                 </template>
                 <template #label>
                     <span class="text-white"
@@ -113,18 +115,18 @@ const layout = inject("layout", {})
 
             <!-- Section: Favourite -->
             <ButtonWithLink
-                v-if="checkVisible(model?.favourite?.visible || null, isLoggedIn) && layout.retina?.type !== 'dropshipping'"
-                v-tooltip="trans('Favourites')" url="/app/favourites" icon="fal fa-heart" type="transparent" :noHover="true">
-                 <template #icon>
-                    <FontAwesomeIcon icon="fal fa-heart" :style="{ color: 'white' }" fixed-width
-                        aria-hidden="true" />
+                v-if="checkVisible(model?.favourite?.visible || null, isLoggedIn) && layout.retina?.type !== 'dropshipping' && !layout.iris_varnish.isFetching"
+                v-tooltip="trans('Favourites')" url="/app/favourites" icon="fal fa-heart" type="transparent"
+                :noHover="true">
+                <template #icon>
+                    <FontAwesomeIcon icon="fal fa-heart" :style="{ color: 'white' }" fixed-width aria-hidden="true" />
                 </template>
                 <template #label>
                     <span class="text-white" v-if="model?.favourite?.text === `{{ favourites_count }}`"
                         v-html="textReplaceVariables(model?.favourite?.text, layout.iris_variables)" />
-                    <span  class="text-white" v-else-if="model?.favourite?.text === `{{ favourites_count }} favourites`">
+                    <span class="text-white" v-else-if="model?.favourite?.text === `{{ favourites_count }} favourites`">
                         {{ layout.iris_variables?.favourites_count }} {{ layout.iris_variables?.favourites_count > 1 ?
-                            trans("favourites") : trans("favourite") }}
+                        trans("favourites") : trans("favourite") }}
                     </span>
                 </template>
             </ButtonWithLink>
@@ -132,25 +134,23 @@ const layout = inject("layout", {})
 
             <!-- Section: Basket (cart) -->
             <ButtonWithLink
-                v-if="checkVisible(model?.cart?.visible || null, isLoggedIn) && layout.retina?.type == 'b2b'"
+                v-if="checkVisible(model?.cart?.visible || null, isLoggedIn) && layout.retina?.type == 'b2b' && !layout.iris_varnish.isFetching"
                 url="/app/basket" :noHover="true" type="transparent">
                 <template #icon>
                     <FontAwesomeIcon icon="fal fa-shopping-cart" :style="{ color: 'white' }" fixed-width
                         aria-hidden="true" />
                 </template>
                 <template #label>
-                    <span
-                        class="text-white"
-                        xv-html="textReplaceVariables(model?.cart?.text, layout.iris_variables)"
-                        v-html="textReplaceVariables(`{{ items_count }} ${trans('items')} ({{ cart_amount }})`, layout.iris_variables)"
-                    >
+                    <span class="text-white" xv-html="textReplaceVariables(model?.cart?.text, layout.iris_variables)"
+                        v-html="textReplaceVariables(`{{ items_count }} ${trans('items')} ({{ cart_amount }})`, layout.iris_variables)">
                     </span>
                 </template>
             </ButtonWithLink>
 
             <!-- Section: Register -->
-            <ButtonWithLink v-if="checkVisible(model?.register?.visible || null, isLoggedIn)" url="/app/register"
-                icon="fal fa-user-plus" type="transparent" :noHover="true">
+            <ButtonWithLink
+                v-if="checkVisible(model?.register?.visible || null, isLoggedIn) && !layout.iris_varnish.isFetching"
+                url="/app/register" icon="fal fa-user-plus" type="transparent" :noHover="true">
                 <template #icon>
                     <FontAwesomeIcon icon="fal fa-user-plus" :style="{ color: 'white' }" fixed-width
                         aria-hidden="true" />
@@ -165,11 +165,11 @@ const layout = inject("layout", {})
             </ButtonWithLink>
 
             <!-- Section: Login -->
-            <ButtonWithLink v-if="checkVisible(model?.login?.visible || null, isLoggedIn)" :url="urlLoginWithRedirect()"
-                icon="fal fa-sign-in" type="transparent" :noHover="true" >
+            <ButtonWithLink
+                v-if="checkVisible(model?.login?.visible || null, isLoggedIn) && !layout.iris_varnish.isFetching"
+                :url="urlLoginWithRedirect()" icon="fal fa-sign-in" type="transparent" :noHover="true">
                 <template #icon>
-                    <FontAwesomeIcon icon="fal fa-sign-in" :style="{ color: 'white' }" fixed-width
-                        aria-hidden="true" />
+                    <FontAwesomeIcon icon="fal fa-sign-in" :style="{ color: 'white' }" fixed-width aria-hidden="true" />
                 </template>
                 <template #label>
                     <span class="text-white">
@@ -179,8 +179,9 @@ const layout = inject("layout", {})
             </ButtonWithLink>
 
             <!-- Section: Logout -->
-            <ButtonWithLink v-if="checkVisible(model?.logout?.visible || null, isLoggedIn)" url="/app/logout"
-                method="post" :data="{}" icon="fal fa-sign-out" type="transparent" :noHover="true">
+            <ButtonWithLink
+                v-if="checkVisible(model?.logout?.visible || null, isLoggedIn) && !layout.iris_varnish.isFetching"
+                url="/app/logout" method="post" :data="{}" icon="fal fa-sign-out" type="transparent" :noHover="true">
                 <template #icon>
                     <FontAwesomeIcon icon="fal fa-sign-out" :style="{ color: 'white' }" fixed-width
                         aria-hidden="true" />
@@ -191,6 +192,14 @@ const layout = inject("layout", {})
                     </span>
                 </template>
             </ButtonWithLink>
+
+
+            <div  v-if="layout.iris_varnish.isFetching" class="flex flex-col md:flex-row md:justify-between gap-x-4 sticky top-0 z-50">
+                <Skeleton  width="6rem" height="1.5rem"
+                    class="rounded-xl opacity-70 bg-white/10 animate-pulse mx-4" />
+                <Skeleton  width="6rem" height="1.5rem"
+                    class="rounded-xl opacity-70 bg-white/10 animate-pulse mx-4" />
+            </div>
         </div>
     </div>
 </template>
