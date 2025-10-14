979159e8e091a170224249441aaf2d6437d78779
Artha <manager.arthachan@gmail.com>
2025-10-14 13:37:49 +0800

    feat: implement create and delete actions for Retina back-in-stock reminders with route configuration

---
+++ app/Actions/Retina/CRM/StoreRetinaBackInStockReminder.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 30 Sept 2025 12:28:30 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Retina\CRM;
+
+use App\Actions\CRM\BackInStockReminder\StoreBackInStockReminder;
+use App\Actions\RetinaAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Catalogue\Product;
+use App\Models\CRM\Customer;
+use Lorisleiva\Actions\ActionRequest;
+
+class StoreRetinaBackInStockReminder extends RetinaAction
+{
+    use WithActionUpdate;
+
+
+    public function handle(Customer $customer, Product $product, array $modelData): void
+    {
+        StoreBackInStockReminder::make()->action($customer, $product, $modelData);
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $product = $request->route()->parameter('product');
+        if ($product->shop_id !== $this->shop->id) {
+            return false;
+        }
+        return true;
+    }
+
+    public function asController(Product $product, ActionRequest $request): void
+    {
+        $this->initialisation($request);
+
+        $this->handle($this->customer, $product, $this->validatedData);
+    }
+}
