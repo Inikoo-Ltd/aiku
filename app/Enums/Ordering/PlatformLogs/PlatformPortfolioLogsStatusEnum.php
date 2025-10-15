7fe29a76ea91f9c5ac2665181cea311194750611
Artha <manager.arthachan@gmail.com>
2025-10-15 15:18:45 +0800

    HELP-466 feat: add `PlatformPortfolioLogs` model, migration, and enums for logging platform portfolio actions

---
+++ app/Enums/Ordering/PlatformLogs/PlatformPortfolioLogsStatusEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 20 Jun 2023 20:24:56 Malaysia Time, Pantai Lembeng, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\Ordering\PlatformLogs;
+
+use App\Enums\EnumHelperTrait;
+
+enum PlatformPortfolioLogsStatusEnum: string
+{
+    use EnumHelperTrait;
+
+    case OK = 'ok';
+    case FAIL = 'fail';
+
+    public function labels(): array
+    {
+        return [
+            'ok'    => 'Ok',
+            'fail'  => 'Fail'
+        ];
+    }
+}
---
+++ app/Models/PlatformPortfolioLogs.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Wed, 15 Oct 2025 14:48:34 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlatformPortfolioLogs extends Model
+{
+}
