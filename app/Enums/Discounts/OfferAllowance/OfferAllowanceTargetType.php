162b0aec4780e9111e6257cc25886efdb19c5277
Raul Perusquia <raul@inikoo.com>
2025-10-17 01:42:19 +0800

    Rename `allowances` to `trigger_data` in `offers` table and model, adjust related logic, and extend `offer_allowances` with new fields (`class`, `type`, `target_id`, `target_data`).

---
+++ app/Enums/Discounts/OfferAllowance/OfferAllowanceTargetType.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 16 Oct 2025 17:13:17 Central Indonesia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\Discounts\OfferAllowance;
+
+enum OfferAllowanceTargetType: string
+{
+    case ALL_PRODUCTS_IN_ORDER = 'all_products_in_order';
+}
