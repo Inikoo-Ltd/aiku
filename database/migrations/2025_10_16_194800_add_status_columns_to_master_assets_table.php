3f9907751ce67e8f2ed2c88c96edf309f93d0d7d
Raul Perusquia <raul@inikoo.com>
2025-10-17 14:58:11 +0800

    migratiosn

---
+++ database/migrations/2025_10_16_194800_add_status_columns_to_master_assets_table.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 16 Oct 2025 19:49:17 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+
+    public function up(): void
+    {
+        Schema::table('master_assets', function (Blueprint $table) {
+            $table->string('stocks_status')->nullable()->index();
+            $table->string('products_status')->nullable()->index();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('master_assets', function (Blueprint $table) {
+            $table->dropColumn('stocks_status');
+            $table->dropColumn('products_status');
+        });
+    }
+};
