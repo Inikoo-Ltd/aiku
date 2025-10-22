09d66467655d4cd8dc7c5a6adcaa3a8fee64df37
aqordeon <aqordivika@yahoo.co.id>
2025-10-22 17:05:01 +0800

    feat: Sidebar: Layout Panel

--- resources/js/Pages/Grp/Org/Web/Workshop/Sidebar/Blueprint.ts
+++ resources/js/Pages/Grp/Org/Web/Workshop/Sidebar/Blueprint.ts
@@ -9,52 +9,47 @@ export default {
 					label: "Background",
 					type: "background",
 				},
-				{
-					key: ["border"],
-					label: "Border",
-					type: "border",
-				},
-				{
-                    key: ["shadow"],
-                    label : "Shadow",
-                    type: "shadow",
-                },
-                {
-                    key: ["shadowColor"],
-                    label : "Shadow Color",
-                    type: "color",
-                },
-			],
-		},
-        {
-			name: "Navigation",
-			key: ["navigation_container", "properties"],
-			replaceForm: [
+				// {
+				// 	key: ["border"],
+				// 	label: "Border",
+				// 	type: "border",
+				// },
 				{
 					key: ["text"],
+					label: "Text",
 					type: "textProperty",
 				},
 			],
 		},
-         {
-			name: "Subnavigation",
-			key: ["sub_navigation", "properties"],
-			replaceForm: [
-				{
-					key: ["text"],
-					type: "textProperty",
-				},
-			],
-		},
-        {
-			name: "Link Subnavigation",
-			key: ["sub_navigation_link", "properties"],
-			replaceForm: [
-				{
-					key: ["text"],
-					type: "textProperty",
-				},
-			],
-		},
+        // {
+		// 	name: "Navigation",
+		// 	key: ["navigation_container", "properties"],
+		// 	replaceForm: [
+		// 		{
+		// 			key: ["text"],
+		// 			type: "textProperty",
+		// 		},
+		// 	],
+		// },
+        //  {
+		// 	name: "Subnavigation",
+		// 	key: ["sub_navigation", "properties"],
+		// 	replaceForm: [
+		// 		{
+		// 			key: ["text"],
+		// 			type: "textProperty",
+		// 		},
+		// 	],
+		// },
+        // {
+		// 	name: "Link Subnavigation",
+		// 	key: ["sub_navigation_link", "properties"],
+		// 	replaceForm: [
+		// 		{
+		// 			key: ["text"],
+		// 			type: "textProperty",
+		// 		},
+		// 	],
+		// },
 	],
 }
