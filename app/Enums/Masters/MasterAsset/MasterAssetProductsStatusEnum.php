8e41fc9f165a22af5f990f79276d6aa592560e3f
Raul Perusquia <raul@inikoo.com>
2025-10-22 08:44:56 +0800

    feature: add enums for `products_status` and `stocks_status` in `MasterAsset` model

---
+++ app/Enums/Masters/MasterAsset/MasterAssetProductsStatusEnum.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 21 Oct 2025 20:37:16 Central Indonesia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\Masters\MasterAsset;
+
+use App\Enums\EnumHelperTrait;
+
+enum MasterAssetProductsStatusEnum: string
+{
+    use EnumHelperTrait;
+
+    case NA = 'na';
+    case FOR_SALE = 'for_sale';
+    case NO_FOR_SALE = 'no_for_sale';
+    case ORPHAN = 'orphan';
+
+
+    public static function labels(): array
+    {
+        return [
+            'na'            => __('No applicable'),
+            'active'        => __('Stocking'),
+            'discontinuing' => __('Discontinuing'),
+            'discontinued'  => __('Discontinued'),
+            'orphan'        => __('Orphan'),
+        ];
+    }
+
+
+}
