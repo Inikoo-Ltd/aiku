ff25cb88292769d78f26c83c5829ef0f2cbc8dab
YudhistiraA <aryarajasa0@gmail.com>
2025-10-23 16:44:42 +0800

    Carousel Background image

--- resources/js/Components/CMS/Webpage/CarouselImageBackground/CardBlueprint.ts
+++ resources/js/Components/CMS/Webpage/CarouselImageBackground/CardBlueprint.ts
@@ -1,13 +1,22 @@
 export default {
 	blueprint: [
 		{
+			name:"Image",
 			label: "Image",
 			key: ["image"],
 			replaceForm: [
 				{
 					key: ["source"],
 					label: "Image",
-					type: "upload_image",
+					type: "image-cropped",
+					props_data: {
+						stencilProps: {
+							aspectRatio: 1 / 1,
+							movable: true,
+							scalable: true,
+							resizable: true,
+						},
+					},
 				},
 				{
 					key: ["alt"],
@@ -17,11 +26,13 @@ export default {
 			],
 		},
 		{
+			name : 'Text',
 			key: ["text"],
 			label: "Text",
 			type: "editorhtml",
 		},
 		{
+			name : 'Link',
 			key: ["link"],
 			label: "Link",
 			type: "link",
