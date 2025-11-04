d593ddae45feaede1c63e9877622146fd7bcd9ce
Raul Perusquia <raul@inikoo.com>
2025-11-05 06:59:39 +0800

    refactor: adjust `woo:update-inventory` schedule from every two hours to hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -127,7 +127,7 @@ class Kernel extends ConsoleKernel
             monitorSlug: 'CheckAllEbayChannels',
         );
 
-        $schedule->command('woo:update-inventory')->everyTwoHours()->withoutOverlapping()->sentryMonitor(
+        $schedule->command('woo:update-inventory')->hourly()->withoutOverlapping()->sentryMonitor(
             monitorSlug: 'UpdateWooStockInventories',
         );
 
