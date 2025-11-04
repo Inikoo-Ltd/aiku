84e4fd1a360795fb704b002425d7a181670630ba
YudhistiraA <aryarajasa0@gmail.com>
2025-11-04 10:41:48 +0800

    INI-293

--- resources/js/Components/CMS/Webpage/Text/Blueprint.ts
+++ resources/js/Components/CMS/Webpage/Text/Blueprint.ts
@@ -1,6 +1,12 @@
 export default {
 	blueprint: [
 		{
+			label: "Responsive Visibility",
+			key: ["container", "properties", "visibility"],
+			type: "visibility",
+			useIn: ["desktop", "tablet", "mobile"],
+		},
+		{
 			name: "Layout",
 			key: ["container", "properties"],
 			replaceForm: [
