285763087a79ff87eca242799b1f2c5694f04153
Artha <manager.arthachan@gmail.com>
2025-11-04 11:29:11 +0800

    feat: add GetAnnouncementTemplates action with route and resource implementation
    - Introduced `GetAnnouncementTemplates` action to retrieve Announcement Templates.
    - Added API route `announcement-templates.index`.
    - Created `AnnouncementTemplatesResource` for standardized API responses.
    - Implemented `AnnouncementTemplate` model with media relations.

--- routes/grp/web/json.php
+++ routes/grp/web/json.php
@@ -78,6 +78,7 @@ use App\Actions\Masters\MasterProductCategory\Json\GetMasterDepartmentAndMasterS
 use App\Actions\Ordering\Order\UI\IndexRecentOrderTransactionUploads;
 use App\Actions\Procurement\OrgSupplierProducts\Json\GetOrgSupplierProducts;
 use App\Actions\SysAdmin\User\GetSupervisorUsers;
+use App\Actions\Web\Announcement\UI\GetAnnouncementTemplates;
 use App\Actions\Web\WebBlockHistory\GetWebBlockHistories;
 use App\Actions\Web\WebBlockType\GetWebBlockTypes;
 use App\Actions\Web\Webpage\Json\GetWebpagesForCollection;
@@ -85,6 +86,7 @@ use App\Actions\Web\Website\GetWebsiteCloudflareUniqueVisitors;
 use Illuminate\Support\Facades\Route;
 
 Route::get('web-block-types', GetWebBlockTypes::class)->name('web-block-types.index');
+Route::get('announcement-templates', GetAnnouncementTemplates::class)->name('announcement_templates.index');
 
 Route::get('comms/outboxes/{outbox}/users', GetOutboxUsers::class)->name('outbox.users.index');
 
