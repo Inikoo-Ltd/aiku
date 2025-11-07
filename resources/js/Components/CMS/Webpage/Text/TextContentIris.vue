2d3e10ca34e15d3f8347a0feadc6877f6311f79e
YudhistiraA <aryarajasa0@gmail.com>
2025-11-07 15:35:04 +0800

    INI-244 text block reponsive text

--- resources/js/Components/CMS/Webpage/Text/TextContentIris.vue
+++ resources/js/Components/CMS/Webpage/Text/TextContentIris.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getStyles } from '@/Composables/styles'
-import { inject } from 'vue';
+import { inject, computed } from 'vue';
+import { get, isPlainObject, cloneDeep, set } from 'lodash-es'
 
 const props = defineProps<{
     fieldValue: {
@@ -12,6 +13,35 @@ const props = defineProps<{
     screenType: 'mobile' | 'tablet' | 'desktop'
 }>()
 const layout: any = inject("layout", {})
+
+const valueForField = computed({
+  get() {
+    const rawVal = get(props.fieldValue, ['value'])
+    if (!isPlainObject(rawVal)) return rawVal
+
+    if (!rawVal?.use_responsive) {
+      return rawVal?.desktop ?? ''
+    }
+
+    const view = props.screenType!
+    return rawVal?.[view] ?? rawVal?.desktop ?? ''
+  },
+  set(newVal) {
+    const rawVal = cloneDeep(get(props.fieldValue, ['value']))
+
+    if (!isPlainObject(rawVal)) {
+      set(props.fieldValue, ['value'], newVal)
+      return
+    }
+    if (!rawVal?.use_responsive) {
+      set(props.fieldValue, ['value', 'desktop'], newVal)
+    } else {
+      set(props.fieldValue, ['value', props.screenType], newVal)
+    }
+
+  }
+})
+
 </script>
 
 <template>
@@ -20,7 +50,7 @@ const layout: any = inject("layout", {})
             ...getStyles(layout?.app?.webpage_layout?.container?.properties, screenType),
             ...getStyles(fieldValue.container?.properties, screenType)
         }">
-            <div class="editor-class" v-html="fieldValue?.value"></div>
+            <div class="editor-class" v-html="valueForField"></div>
         </div>
 
     </div>
