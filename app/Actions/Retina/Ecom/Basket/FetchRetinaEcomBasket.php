f7a5bee27cbf8e830a8543af073957bec2f7397e
louis-perez <louisprzn@gmail.com>
2025-11-10 16:16:16 +0800

    INI-330: Route to fetch Order Details (retina.ecom.basket.fetch)

---
+++ app/Actions/Retina/Ecom/Basket/FetchRetinaEcomBasket.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 30-04-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\Retina\Ecom\Basket;
+
+use App\Actions\Ordering\Order\UI\GetOrderDeliveryAddressManagement;
+use App\Actions\Retina\Ecom\Basket\UI\IsOrder;
+use App\Actions\Retina\Ecom\Basket\UI\IndexBasketTransactions;
+use App\Actions\Retina\Ecom\Orders\IndexRetinaEcomOrders;
+use App\Actions\RetinaAction;
+use App\Enums\Catalogue\Charge\ChargeStateEnum;
+use App\Enums\Catalogue\Charge\ChargeTypeEnum;
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Http\Resources\Catalogue\ChargeResource;
+use App\Http\Resources\Fulfilment\RetinaEcomBasketTransactionsResources;
+use App\Http\Resources\Helpers\AddressResource;
+use App\Models\CRM\Customer;
+use App\Models\Ordering\Order;
+use App\Http\Resources\Sales\OrderResource;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+
+class FetchRetinaEcomBasket extends RetinaAction
+{
+    use IsOrder;
+
+    public function handle(ActionRequest $request): Order|null
+    {
+        $customer = auth()->user()->customer()?->first() ?? null;
+        if (!$customer?->current_order_in_basket_id) {
+            return null;
+        }
+
+        return Order::where('id', $customer->current_order_in_basket_id)->where('customer_id', $customer->id)->where('state', OrderStateEnum::CREATING)->first();
+    }
+
+    public function asController(ActionRequest $request): Order|null
+    {
+        $this->initialisation($request);
+
+        return $this->handle($request);
+    }
+
+    public function jsonResponse(Order $order): Array|null
+    {
+        if(!$order) return null;
+        $orderArr = $this->getOrderBoxStats($order); 
+        $orderArr['products']['product_detail'] = RetinaEcomBasketTransactionsResources::collection(IndexBasketTransactions::run($order));
+        return $orderArr;
+    }
+}
