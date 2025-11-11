09f5334d760542b0bf8ce05ee200e5f593b595a9
aqordeon <aqordivika@yahoo.co.id>
2025-11-11 13:37:21 +0800

    fix: ProductRenderEcom: height size on image on mobile

--- resources/js/Components/CMS/Webpage/Products1/ProductRenderEcom.vue
+++ resources/js/Components/CMS/Webpage/Products1/ProductRenderEcom.vue
@@ -254,7 +254,7 @@ const profitMargin = computed(() => {
 
             <!-- Product Image -->
             <component :is="product.url ? Link : 'div'" :href="product.url"
-                class="block w-full mb-1 rounded sm:h-[305px] h-[180px] relative">
+                class="block w-full mb-1 rounded xsm:h-[305px] h-[180px] relative">
                 <slot name="image" :product="product">
                     <Image :src="product?.web_images?.main?.gallery" alt="product image"
                         :style="{ objectFit: 'contain' }" />
