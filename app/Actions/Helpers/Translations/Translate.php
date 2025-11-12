3980ca144ce3053d570eebc1a9dc473c2f3d61d4
YudhistiraA <aryarajasa0@gmail.com>
2025-11-12 15:31:38 +0800

    INI-352 set in local

--- app/Actions/Helpers/Translations/Translate.php
+++ app/Actions/Helpers/Translations/Translate.php
@@ -34,9 +34,9 @@ class Translate extends OrgAction
                 return $text ?? '';
             }
 
-            /* if (app()->environment('local')) {
+            if (app()->environment('local')) {
                 return $text;
-            } */
+            } 
 
 
             $translationEngineService   = new TranslationEngineService();
