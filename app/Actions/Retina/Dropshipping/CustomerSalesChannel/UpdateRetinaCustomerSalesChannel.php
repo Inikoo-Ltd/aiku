433c0d9ed58d1b08ee9181a36983a05849d57941
Artha <manager.arthachan@gmail.com>
2025-11-17 16:58:44 +0800

    fix: fulfillment policies ebay

--- app/Actions/Retina/Dropshipping/CustomerSalesChannel/UpdateRetinaCustomerSalesChannel.php
+++ app/Actions/Retina/Dropshipping/CustomerSalesChannel/UpdateRetinaCustomerSalesChannel.php
@@ -54,7 +54,10 @@ class UpdateRetinaCustomerSalesChannel extends RetinaAction
             'is_vat_adjustment' => ['sometimes', 'boolean'],
             'tax_category_id'   => ['sometimes', 'integer', Rule::exists('tax_categories', 'id')],
             'status'       => ['sometimes', Rule::enum(CustomerSalesChannelStatusEnum::class)],
-            'name' => ['sometimes', 'string', 'max:255']
+            'name' => ['sometimes', 'string', 'max:255'],
+            'shipping_service'              => ['sometimes', 'string', 'max:255'],
+            'shipping_price'              => ['sometimes', 'string', 'max:255'],
+            'shipping_max_dispatch_time'              => ['sometimes', 'string', 'max:255']
         ];
     }
 
---
+++ database/migrations/2025_11_17_083507_add_policies_to_ebay_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ebay_users', function (Blueprint $table) {
+            $table->string('fulfillment_policy_id')->nullable();
+            $table->string('payment_policy_id')->nullable();
+            $table->string('return_policy_id')->nullable();
+            $table->string('location_key')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ebay_users', function (Blueprint $table) {
+            $table->dropColumn('fulfillment_policy_id');
+            $table->dropColumn('payment_policy_id');
+            $table->dropColumn('return_policy_id');
+            $table->dropColumn('location_key');
+        });
+    }
+};
