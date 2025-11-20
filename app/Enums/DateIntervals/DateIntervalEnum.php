6999593f85b993c547e550e052ce3bf89900c75d
Raul Perusquia <raul@inikoo.com>
2025-11-20 20:40:06 +0800

    Refactor `allExceptHistorical` method in `DateIntervalEnum` to simplify logic by removing unnecessary transformations.

--- app/Enums/DateIntervals/DateIntervalEnum.php
+++ app/Enums/DateIntervals/DateIntervalEnum.php
@@ -123,13 +123,10 @@ enum DateIntervalEnum: string
 
     public static function allExceptHistorical(): array
     {
-        return array_map(
-            fn ($case) => $case->value,
-            array_values(array_filter(
-                self::cases(),
-                fn ($case) => !in_array($case, [self::YESTERDAY, self::LAST_WEEK, self::LAST_MONTH])
-            ))
-        );
+        return array_values(array_filter(
+            self::cases(),
+            fn ($case) => !in_array($case, [self::YESTERDAY, self::LAST_WEEK, self::LAST_MONTH])
+        ));
     }
 
 
