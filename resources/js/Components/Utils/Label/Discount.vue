650ad451395ef89abae0db55d5fa4db78d733347
aqordeon <aqordivika@yahoo.co.id>
2025-11-21 13:09:18 +0800

    feat: RightSideBasket: loading indicator on visit product, tight Order Summary

--- resources/js/Components/Utils/Label/Discount.vue
+++ resources/js/Components/Utils/Label/Discount.vue
@@ -21,7 +21,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="bg-pink-500/20 px-1 py-0.5 text-xs border flex items-center border-pink-500/50 rounded w-fit text-pink-700" >
+    <div class="bg-pink-500/20 px-1 py-0.5 text-xs border flex items-center border-pink-500/50 rounded-sm w-fit text-pink-700" >
         <FontAwesomeIcon icon="fas fa-badge-percent" class="text-pink-500 text-sm align-middle" fixed-width aria-hidden="true" />
         <span class="ml-0.5 font-bold mr-1">{{ offers_data?.o?.p }}</span> {{ offers_data?.o?.l }}
     </div>
