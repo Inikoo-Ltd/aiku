09c606be97d1b69d0750307b08eec320892a05a0
andiferdiawan <andiferdiawan22@gmail.com>
2025-11-27 13:17:35 +0800

    feat: Add route for assigning chat session to self

--- routes/api/grp/grp_api.php
+++ routes/api/grp/grp_api.php
@@ -74,4 +74,7 @@ Route::get('/sessions', GetChatSessions::class)->name('sessions.index');
    Route::post('/sessions/{chatSession:ulid}/assign', AssignChatToAgent::class)
     ->name('chat.sessions.assign');
 
+    Route::post('/sessions/{chatSession:ulid}/assign-to-self', [AssignChatToAgent::class, 'assignToSelf'])
+    ->name('chat.sessions.assign.self');
+
 });
--- routes/iris/json.php
+++ routes/iris/json.php
@@ -7,32 +7,32 @@
  * copyright 2025
 */
 
-use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInCollection;
-use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInProduct;
-use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInProductCategory;
-use App\Actions\Catalogue\Product\Json\GetIrisInStockProductsInCollection;
-use App\Actions\Catalogue\Product\Json\GetIrisInStockProductsInProductCategory;
+use Illuminate\Support\Facades\Route;
+use App\Actions\Iris\IrisLogWebUserRequest;
+use App\Actions\Helpers\Tag\Json\GetIrisTags;
+use App\Actions\Iris\Json\GetIrisFirstHitData;
+use App\Actions\Iris\Basket\FetchIrisEcomBasket;
+use App\Actions\Helpers\Brand\Json\GetIrisBrands;
+use App\Actions\Helpers\Tag\Json\GetIrisShopTags;
+use App\Actions\Iris\Json\GetRetinaEcomCustomerData;
+use App\Actions\Helpers\Brand\Json\GetIrisShopBrands;
+use App\Actions\CRM\WebUser\Retina\Json\GetRedirectUrl;
 use App\Actions\Catalogue\Product\Json\GetIrisLastOrderedProducts;
-use App\Actions\Catalogue\Product\Json\GetIrisOutOfStockProductsInCollection;
-use App\Actions\Catalogue\Product\Json\GetIrisOutOfStockProductsInProductCategory;
-use App\Actions\Catalogue\Product\Json\GetIrisPortfoliosInCollection;
-use App\Actions\Catalogue\Product\Json\GetIrisPortfoliosInProductCategory;
 use App\Actions\Catalogue\Product\Json\GetIrisProductEcomOrdering;
 use App\Actions\Catalogue\Product\Json\GetIrisProductsInCollection;
+use App\Actions\Catalogue\Product\Json\GetIrisPortfoliosInCollection;
 use App\Actions\Catalogue\Product\Json\GetIrisProductsInProductCategory;
-use App\Actions\CRM\WebUser\Retina\Json\GetRedirectUrl;
-use App\Actions\Dropshipping\CustomerSalesChannel\Json\GetCustomerProductCategorySalesChannelIds;
+use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInProduct;
+use App\Actions\Catalogue\Product\Json\GetIrisInStockProductsInCollection;
+use App\Actions\Catalogue\Product\Json\GetIrisPortfoliosInProductCategory;
+use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInCollection;
+use App\Actions\Catalogue\Product\Json\GetIrisOutOfStockProductsInCollection;
+use App\Actions\Catalogue\Product\Json\GetIrisInStockProductsInProductCategory;
+use App\Actions\Catalogue\Product\Json\GetIrisBasketTransactionsInProductCategory;
+use App\Actions\Catalogue\Product\Json\GetIrisOutOfStockProductsInProductCategory;
 use App\Actions\Dropshipping\CustomerSalesChannel\Json\GetCustomerProductSalesChannelIds;
-use App\Actions\Helpers\Brand\Json\GetIrisBrands;
-use App\Actions\Helpers\Brand\Json\GetIrisShopBrands;
-use App\Actions\Helpers\Tag\Json\GetIrisShopTags;
-use App\Actions\Helpers\Tag\Json\GetIrisTags;
-use App\Actions\Iris\IrisLogWebUserRequest;
-use App\Actions\Iris\Basket\FetchIrisEcomBasket;
-use App\Actions\Iris\Json\GetIrisFirstHitData;
-use App\Actions\Iris\Json\GetRetinaEcomCustomerData;
+use App\Actions\Dropshipping\CustomerSalesChannel\Json\GetCustomerProductCategorySalesChannelIds;
 use App\Actions\Retina\Dropshipping\CustomerSalesChannel\UI\IndexRetinaDropshippingCustomerSalesChannels;
-use Illuminate\Support\Facades\Route;
 
 Route::middleware(["retina-auth:retina"])->group(function () {
     Route::get('product-category/{productCategory:id}/portfolio-data', GetIrisPortfoliosInProductCategory::class)->name('product_category.portfolio_data');
