59433894905e5ecec4b12570f5ba82842196d387
Raul Perusquia <raul@inikoo.com>
2025-11-27 14:57:19 +0800

    Add `PayOrderWithCustomerBalance` action to handle customer balance payment for orders
    - Implemented logic to validate order payment status, customer balance, and active payment accounts.
    - Added transaction support for creating payments, attaching them to orders, and updating customer credit transactions.
    - Enhanced feedback mechanism with session-based notifications for payment outcomes.

--- app/Actions/CRM/Customer/PayOrderWithCustomerBalance.php
+++ app/Actions/CRM/Customer/PayOrderWithCustomerBalance.php
@@ -9,26 +9,99 @@
 
 namespace App\Actions\CRM\Customer;
 
-
-
 use App\Actions\Accounting\CreditTransaction\StoreCreditTransaction;
+use App\Actions\Accounting\Payment\StorePayment;
+use App\Actions\Ordering\Order\AttachPaymentToOrder;
 use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
-use App\Enums\Accounting\CreditTransaction\CreditTransactionReasonEnum;
 use App\Enums\Accounting\CreditTransaction\CreditTransactionTypeEnum;
+use App\Enums\Accounting\Payment\PaymentStateEnum;
+use App\Enums\Accounting\Payment\PaymentStatusEnum;
+use App\Models\Accounting\PaymentAccountShop;
 use App\Models\Ordering\Order;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class PayOrderWithCustomerBalance extends OrgAction
 {
     use WithActionUpdate;
-    public function handle(Order $order)
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(Order $order): array
     {
-        dd($order);
+
+        if ($order->payment_amount == $order->total_amount) {
+            return [
+                'success' => false,
+                'reason'  => 'Order has been paid',
+            ];
+        }
+
+        if ($order->customer->balance < $order->total_amount) {
+            return [
+                'success' => false,
+                'reason'  => 'Insufficient balance',
+            ];
+        }
+
+        $paymentAccountShop = PaymentAccountShop::where('shop_id', $order->shop_id)->where('type', 'account')->where('state', 'active')->first();
+        if (!$paymentAccountShop) {
+            return [
+                'success' => false,
+                'reason'  => 'PaymentAccountShop not found',
+            ];
+        }
+
+        $customer = $order->customer;
+
+        $paymentData = [
+            'reference'               => 'cu-'.$customer->id.'-bal-'.Str::random(10),
+            'amount'                  => $order->total_amount,
+            'status'                  => PaymentStatusEnum::SUCCESS,
+            'state'                   => PaymentStateEnum::COMPLETED,
+            'payment_account_shop_id' => $paymentAccountShop->id
+        ];
+        DB::transaction(function () use ($order, $customer, $paymentAccountShop, $paymentData) {
+            $payment = StorePayment::make()->action($customer, $paymentAccountShop->paymentAccount, $paymentData);
+
+            AttachPaymentToOrder::make()->action($order, $payment, [
+                'amount' => $payment->amount
+            ]);
+
+
+            $creditTransactionData = [
+                'amount'     => -$payment->amount,
+                'type'       => CreditTransactionTypeEnum::PAYMENT,
+                'payment_id' => $payment->id,
+            ];
+            StoreCreditTransaction::make()->action($customer, $creditTransactionData);
+
+            return $order;
+        });
+
+        return [
+            'success' => true,
+        ];
+
     }
 
-    public function asController(Order $order)
+    /**
+     * @throws \Throwable
+     */
+    public function asController(Order $order): void
     {
         $this->initialisationFromShop($order->shop, []);
-        $this->handle($order);
+        $result = $this->handle($order);
+
+        request()->session()->flash('notification', [
+            'status'  => $result['success'] ? 'success' : 'error',
+            'title'   => $result['type'],
+            'description' => ''
+        ]);
     }
+
+
+
 }
