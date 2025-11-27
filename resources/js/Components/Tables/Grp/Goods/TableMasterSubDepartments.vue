8923912773c97387d3e9bebeb566acb35ca177e8
ekayudinata <ekayudinatha@gmail.com>
2025-11-27 13:28:48 +0800

    INI-456 update table sub departments

--- resources/js/Components/Tables/Grp/Goods/TableMasterSubDepartments.vue
+++ resources/js/Components/Tables/Grp/Goods/TableMasterSubDepartments.vue
@@ -7,12 +7,25 @@
 <script setup lang="ts">
 import Table from '@/Components/Table/Table.vue'
 import {RouteParams} from "@/types/route-params";
-import {Link} from '@inertiajs/vue3'
+import {Link, router} from '@inertiajs/vue3'
+import { ref } from 'vue';
+import { faFolderTree } from "@fal";
+import Button from "@/Components/Elements/Buttons/Button.vue";
+import PureMultiselectInfiniteScroll from "@/Components/Pure/PureMultiselectInfiniteScroll.vue";
+import Dialog from 'primevue/dialog';
+import axios from "axios";
 
 defineProps<{
     data: object,
     tab?: string
 }>()
+
+
+const selectedSubDepartment = ref([])
+const tableKey = ref(1)
+const visibleDialog = ref(false);
+const selectedParentId = ref(null)
+const loadingChangeParent = ref(false)
 
 function masterSubDepartmentRoute(subDepartment: {}) {
     switch (route().current()) {
@@ -49,10 +62,77 @@ function masterSubDepartmentRoute(subDepartment: {}) {
     }
 }
 
+const onSaveChangeParent = async () => {
+    if (!selectedParentId.value) {
+        alert("Please choose a parent first.");
+        return;
+    }
+
+    try {
+        loadingChangeParent.value = true;
+
+        const requests = selectedSubDepartment.value.map((subDepartmentId: number) => {
+            return axios.patch(
+                route("grp.models.master_product_category.update", {
+                    masterProductCategory: subDepartmentId,
+                }),
+                {
+                    master_department_or_master_sub_department_id: selectedParentId.value,
+                }
+            );
+        });
+
+        await Promise.all(requests);
+
+        visibleDialog.value = false;
+        selectedSubDepartment.value = [];
+        selectedParentId.value = null;
+        tableKey.value++
+
+        router.reload()
+
+    } catch (error) {
+        console.error("Failed updating parent", error);
+    } finally {
+        loadingChangeParent.value = false;
+    }
+};
+
+const onChangeCheked = (checked: boolean, item: { id: number }) => {
+    if (!selectedSubDepartment.value) return
+
+    if (checked) {
+        if (!selectedSubDepartment.value.includes(item.id)) {
+            selectedSubDepartment.value.push(item.id)
+        }
+    } else {
+        selectedSubDepartment.value = selectedSubDepartment.value.filter(id => id != item.id)
+    }
+}
+
+const onCheckedAll = (handle: { allChecked: boolean, data: Array<{id: number}> }) => {
+    if (handle.allChecked) {
+        // Set selectedSubDepartment with all data.id values
+        selectedSubDepartment.value = handle.data.map(item => item.id);
+    } else {
+        // Clear selectedSubDepartment
+        selectedSubDepartment.value = [];
+    }
+}
+
 </script>
 
 <template>
-    <Table :resource="data" :name="tab" class="mt-5">
+    <Table @onCheckedAll="onCheckedAll" :resource="data" :name="tab" class="mt-5" :isCheckBox="true" :key="tableKey"
+    @onChecked="(item) => onChangeCheked(true, item)" @onUnchecked="(item) => onChangeCheked(false, item)"
+        checkboxKey='id' :isChecked="(item) => selectedSubDepartment.includes(item.id)" ref="_table">
+           <template #add-on-button>
+            <div v-if="selectedSubDepartment.length != 0">
+                <Button :icon="faFolderTree" label="Assign to another" @click="visibleDialog = true" :size="'xs'"
+                    type="secondary" />
+            </div>
+        </template>
+
         <template #cell(code)="{ item: subDepartment }">
             <Link v-if="masterSubDepartmentRoute(subDepartment)" :href="masterSubDepartmentRoute(subDepartment)"
                   class="primaryLink">
@@ -60,4 +140,22 @@ function masterSubDepartmentRoute(subDepartment: {}) {
             </Link>
         </template>
     </Table>
+
+     <Dialog v-model:visible="visibleDialog" modal header="Edit Parent" :style="{ width: '25rem' }"
+        :content-style="{ overflow: 'visible', paddingLeft: '20px', paddingRight: '20px', }">
+        <div>
+            <PureMultiselectInfiniteScroll :fetchRoute="{
+                name: 'grp.json.master_shop.master_departments_and_sub_departments',
+                parameters: {
+                    masterShop: (route().params as RouteParams).masterShop
+                }
+            }" :required="true" valueProp="id" type_label="department-and-sub-department" labelProp="code"
+            v-model="selectedParentId" />
+        </div>
+
+        <div class="flex justify-end gap-2 mt-4">
+            <Button type="tertiary" label="Cancel" @click="visibleDialog = false" />
+            <Button type="save" label="Save" @click="onSaveChangeParent"  :loading="loadingChangeParent"/>
+        </div>
+    </Dialog>
 </template>
