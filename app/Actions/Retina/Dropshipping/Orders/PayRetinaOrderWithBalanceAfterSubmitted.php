31c54f2709b7afbcca96a582c9078ece9e61f3b0
Raul Perusquia <raul@inikoo.com>
2025-11-27 15:27:16 +0800

    Add support for `TRANSFER_OUT` type in `DecreaseCreditTransactionCustomer` action
    - Extended type mapping logic based on `reason` to include `TRANSFER_OUT`.
    - Improved readability by adjusting spacing.

--- app/Actions/Retina/Dropshipping/Orders/PayRetinaOrderWithBalanceAfterSubmitted.php
+++ app/Actions/Retina/Dropshipping/Orders/PayRetinaOrderWithBalanceAfterSubmitted.php
@@ -7,31 +7,41 @@
  * Copyright: 2025
 */
 
-
 namespace App\Actions\Retina\Dropshipping\Orders;
 
-use App\Actions\Accounting\CreditTransaction\StoreCreditTransaction;
-use App\Actions\Accounting\Payment\StorePayment;
-use App\Actions\Ordering\Order\AttachPaymentToOrder;
-use App\Actions\Ordering\Order\UpdateState\SubmitOrder;
+use App\Actions\CRM\Customer\PayOrderWithCustomerBalance;
 use App\Actions\RetinaAction;
-use App\Enums\Accounting\CreditTransaction\CreditTransactionTypeEnum;
-use App\Enums\Accounting\Payment\PaymentStateEnum;
-use App\Enums\Accounting\Payment\PaymentStatusEnum;
-use App\Enums\Accounting\Payment\PaymentTypeEnum;
-use App\Models\Accounting\PaymentAccountShop;
 use App\Models\Ordering\Order;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Lorisleiva\Actions\ActionRequest;
-use Sentry;
 
 class PayRetinaOrderWithBalanceAfterSubmitted extends RetinaAction
 {
-    
-    public function handle(Order $order, bool $submitOrder = true): array
+    public function handle(Order $order): array
     {
-        dd('xxxxxx');
+        return PayOrderWithCustomerBalance::run($order);
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $order = $request->route()->parameter('order');
+        if ($order->customer_id == $this->customer->id) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function asController(Order $order, ActionRequest $request): void
+    {
+        $this->initialisation($request);
+
+        $result = $this->handle($order);
+        request()->session()->flash('notification', [
+            'status'      => $result['success'] ? 'success' : 'error',
+            'title'       => Arr::get($result, 'reason', ''),
+            'description' => ''
+        ]);
     }
 
 
