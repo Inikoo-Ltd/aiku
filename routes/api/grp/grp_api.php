e301288d75b22b21f23948e92fa6c5f8f4dde487
andiferdiawan <andiferdiawan22@gmail.com>
2025-11-27 13:49:18 +0800

    chore: Clean up unused imports and improve code readability

--- routes/api/grp/grp_api.php
+++ routes/api/grp/grp_api.php
@@ -14,12 +14,9 @@ use App\Actions\Api\Group\Group\ShowApiGroup;
 use App\Actions\Api\Group\Order\ShowApiOrder;
 use App\Actions\Api\Group\Shop\IndexApiShops;
 use App\Actions\Api\Group\Order\IndexApiOrders;
-use App\Actions\CRM\ChatSession\GetChatMessages;
-use App\Actions\CRM\ChatSession\GetChatSessions;
-use App\Actions\CRM\ChatSession\SendChatMessage;
+
 use App\Actions\Api\Group\Invoice\ShowApiInvoice;
-use App\Actions\CRM\ChatSession\StoreChatSession;
-use App\Actions\CRM\ChatSession\AssignChatToAgent;
+
 
 use App\Actions\Api\Group\Customer\ShowApiCustomer;
 use App\Actions\Api\Group\Invoice\IndexApiInvoices;
@@ -60,21 +57,4 @@ Route::middleware(['auth:sanctum', 'treblle'])->group(function () {
             });
         });
     });
-});
-
-
-
-// JUST FOR TESTING API
-Route::prefix('chats')->as('chats.')->group(function () {
-Route::get('/sessions', GetChatSessions::class)->name('sessions.index');
-   Route::post('/sessions', StoreChatSession::class)->name('chats.sessions.store');
-   Route::post('/messages/{chatSession:ulid}/send', SendChatMessage::class)->name('chat.messages.send');
-   Route::get('/sessions/{chatSession:ulid}/messages', GetChatMessages::class)->name('chat.sessions.messages');
-
-   Route::post('/sessions/{chatSession:ulid}/assign', AssignChatToAgent::class)
-    ->name('chat.sessions.assign');
-
-    Route::post('/sessions/{chatSession:ulid}/assign-to-self', [AssignChatToAgent::class, 'assignToSelf'])
-    ->name('chat.sessions.assign.self');
-
 });
