221ed4f94609e382d5b26c70921f27a8c6750f41
YudhistiraA <aryarajasa0@gmail.com>
2025-12-03 12:01:17 +0800

    Refactor Products compoenents

--- resources/js/Components/CMS/Webpage/Products/Ecommerce/ListProductsEcomWorkshop.vue
+++ resources/js/Components/CMS/Webpage/Products/Ecommerce/ListProductsEcomWorkshop.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { faFilter, faSearch, faLayerGroup } from "@fas";
 import { ref, computed, inject, watch } from "vue";
-import FilterProducts from "@/Components/CMS/Webpage/Products1/FilterProduct.vue";
+import FilterProducts from "@/Components/CMS/Webpage/Products/FilterProduct.vue";
 import Drawer from "primevue/drawer";
 import Button from "@/Components/Elements/Buttons/Button.vue";
 import PureInput from "@/Components/Pure/PureInput.vue";
