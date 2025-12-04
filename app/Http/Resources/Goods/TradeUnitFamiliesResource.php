15979bc48bf8a264e0c7606f403edefa8898aabd
Raul Perusquia <raul@inikoo.com>
2025-12-04 22:45:29 +0800

    INI-110 🛠️ Update: Add trade unit status statistics to `TradeUnitFamilyStats`, adjust resource and index actions to reflect changes, implement hydration logic, and include supporting migration.

--- app/Http/Resources/Goods/TradeUnitFamiliesResource.php
+++ app/Http/Resources/Goods/TradeUnitFamiliesResource.php
@@ -10,16 +10,31 @@ namespace App\Http\Resources\Goods;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $slug
+ * @property mixed $code
+ * @property mixed $name
+ * @property mixed $id
+ * @property mixed $number_trade_units
+ * @property mixed $number_trade_units_status_in_process
+ * @property mixed $number_trade_units_status_active
+ * @property mixed $number_trade_units_status_discontinued
+ * @property mixed $number_trade_units_status_anomality
+ */
 class TradeUnitFamiliesResource extends JsonResource
 {
     public function toArray($request): array
     {
         return [
-            'slug'                    => $this->slug,
-            'code'                    => $this->code,
-            'name'                    => $this->name,
-            'id'                      => $this->id,
-            'number_trade_units'      => $this->number_trade_units
+            'slug'                                   => $this->slug,
+            'code'                                   => $this->code,
+            'name'                                   => $this->name,
+            'id'                                     => $this->id,
+            'number_trade_units'                     => $this->number_trade_units,
+            'number_trade_units_status_in_process'   => $this->number_trade_units_status_in_process,
+            'number_trade_units_status_active'       => $this->number_trade_units_status_active,
+            'number_trade_units_status_discontinued' => $this->number_trade_units_status_discontinued,
+            'number_trade_units_status_anomality'    => $this->number_trade_units_status_anomality,
         ];
     }
 }
