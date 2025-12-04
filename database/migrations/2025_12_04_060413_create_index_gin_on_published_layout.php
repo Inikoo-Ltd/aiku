311b2b8f09c0c11b3ec676c08c23f8cbd71317d5
louis-perez <louisprzn@gmail.com>
2025-12-04 14:13:39 +0800

    Create INDEX Gin on Published Layout

---
+++ database/migrations/2025_12_04_060413_create_index_gin_on_published_layout.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public $withinTransaction = false;
+
+    public function up(): void
+    {
+        // Need to use DB Statement for GIN Indexing. Cannot use Schema. Needed for UpdateWebBlockToWebsiteAndChild, otherwise it would be hell to query it
+        DB::statement("
+            CREATE INDEX CONCURRENTLY IF NOT EXISTS webpages_published_layout_gin
+            ON webpages
+            USING GIN (published_layout)
+        ");
+
+    }
+
+
+    public function down(): void
+    {
+        DB::statement("DROP INDEX CONCURRENTLY IF EXISTS webpages_published_layout_gin");
+    }
+};
