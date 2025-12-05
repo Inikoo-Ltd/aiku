3ba413a2c62d2363c5680dd57ca896382627be7c
Artha <manager.arthachan@gmail.com>
2025-12-05 08:39:33 +0800

    fix: enhance WooCommerce channel command to display detailed status and improve error message handling

--- app/Actions/Dropshipping/WooCommerce/PingActiveWooChannel.php
+++ app/Actions/Dropshipping/WooCommerce/PingActiveWooChannel.php
@@ -12,6 +12,9 @@ use App\Actions\Traits\WithActionUpdate;
 use App\Enums\Ordering\Platform\PlatformTypeEnum;
 use App\Models\Dropshipping\CustomerSalesChannel;
 use App\Models\Dropshipping\Platform;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class PingActiveWooChannel
@@ -21,12 +24,12 @@ class PingActiveWooChannel
 
     public string $commandSignature = 'woo:ping_active_channel';
 
-    public function asCommand(): void
+    public function asCommand(Command $command): void
     {
-        $this->handle();
+        $this->handle($command);
     }
 
-    public function handle(): void
+    public function handle(Command $command): void
     {
         $platform = Platform::where('type', PlatformTypeEnum::WOOCOMMERCE)->first();
 
@@ -48,6 +51,28 @@ class PingActiveWooChannel
                         'ping_error_count' => 0
                     ]);
                 }
+
+                $errorMsg = '-';
+
+                if (is_string(Arr::get($customerSalesChannel->user->data, '0'))) {
+                    $errorMsg = Arr::get($customerSalesChannel->user->data, '0');
+                } elseif (is_array(Arr::get($customerSalesChannel->user->data, '0'))) {
+                    $errorMsg = Arr::get($customerSalesChannel->user->data, '0.message');
+                }
+
+                $statusData = [
+                    ['Customer Sales Channel', $customerSalesChannel->slug],
+                    ['Platform Status', $customerSalesChannel->platform_status ? 'Yes' : 'No'],
+                    ['Can Connect to Platform', $customerSalesChannel->can_connect_to_platform ? 'Yes' : 'No'],
+                    ['Exist in Platform', $customerSalesChannel->exist_in_platform ? 'Yes' : 'No'],
+                    ['Ban', $customerSalesChannel->ban_stock_update_util ?? '-'],
+                    ['Ping Error Count', $customerSalesChannel->ping_error_count],
+                    ['Error Message', Str::substr($errorMsg, 0, 120)],
+                ];
+
+
+                $command->info("\nCustomer Sales Channel Status:");
+                $command->table(['Field', 'Value'], $statusData);
             }
         }
     }
