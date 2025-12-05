81f6db8cb3b73bf04f1bd1ba98cf3eb7006e70b0
Artha <manager.arthachan@gmail.com>
2025-12-05 08:39:33 +0800

    fix: enhance WooCommerce channel command to display detailed status and improve error message handling

--- app/Actions/Dropshipping/WooCommerce/CheckWooChannel.php
+++ app/Actions/Dropshipping/WooCommerce/CheckWooChannel.php
@@ -35,7 +35,7 @@ class CheckWooChannel
             $existInPlatform      = true;
         } else {
             $this->update($wooCommerceUser, [
-                'data' => $connection
+                'data' => $connection ?? []
             ]);
         }
 
