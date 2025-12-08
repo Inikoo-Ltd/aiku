30a8874179ca95666b4e3aa15de08d6b7c8615ef
ekayudinata <ekayudinatha@gmail.com>
2025-12-08 11:23:30 +0800

    INI-284 fix passkey issue

--- app/Actions/UI/Profile/ShowProfileIndexPassKeys.php
+++ app/Actions/UI/Profile/ShowProfileIndexPassKeys.php
@@ -3,23 +3,18 @@
 namespace App\Actions\UI\Profile;
 
 use App\Actions\OrgAction;
-use App\Actions\Traits\Actions\WithActionButtons;
 use App\Actions\Traits\WithPassKeyAction;
-use App\Actions\UI\WithInertia;
-use App\Enums\UI\SysAdmin\ProfileTabsEnum;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class ShowProfileIndexPassKeys extends OrgAction
 {
     use AsAction;
 
-    use WithPassKeyAction;
+    // use WithPassKeyAction;
 
-    
-    public function asController()
-    {
-        return $this->generatePasskeyOptions();
-    }
+
+    // public function asController()
+    // {
+    //     return $this->generatePasskeyOptions();
+    // }
 }
