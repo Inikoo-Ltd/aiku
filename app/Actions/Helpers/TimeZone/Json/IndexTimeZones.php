ce0b7ffb402af07f6172ee59374d8749fb98b118
aqordeon <aqordivika@yahoo.co.id>
2025-12-09 11:35:42 +0800

    INI-494 Timezone: fix query, show in production

--- app/Actions/Helpers/TimeZone/Json/IndexTimeZones.php
+++ app/Actions/Helpers/TimeZone/Json/IndexTimeZones.php
@@ -60,12 +60,16 @@ class IndexTimeZones
             $label  = $this->formatLabel($tz, $offset);
 
             return [
-                'value'        => $tz,
-                'label'        => $label,
-                'offset'       => $offset, // seconds from UTC
-                'offset_label' => $this->formatGmtOffset($offset),
+            'value'        => $tz,
+            'label'        => $label,
+            'offset'       => $offset, // seconds from UTC
+            'offset_label' => $this->formatGmtOffset($offset),
             ];
         }, $timezones);
+
+        usort($data, function (array $a, array $b): int {
+            return $a['offset'] <=> $b['offset'];
+        });
 
         if ($q !== null && $q !== '') {
             $needle = Str::lower($q);
@@ -81,7 +85,8 @@ class IndexTimeZones
 
     public function asController(): JsonResponse
     {
-        $q = request()->string('q')->toString();
+        // $q = request()->string('q')->toString();
+        $q = request()->input('filter.global');  // from parameter ?filter[global]=
 
         return response()->json($this->handle($q));
     }
