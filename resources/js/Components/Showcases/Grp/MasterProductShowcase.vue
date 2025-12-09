e45171f2c959201f04ea2899c25c5e37b8618a24
aqordeon <aqordivika@yahoo.co.id>
2025-12-09 16:37:01 +0800

    feat: ProductShowcase: new component LabelSKU

--- resources/js/Components/Showcases/Grp/MasterProductShowcase.vue
+++ resources/js/Components/Showcases/Grp/MasterProductShowcase.vue
@@ -11,6 +11,7 @@ import {
 	faShieldAlt,
 	faInfoCircle,
 	faChevronDown,
+	faLock,
 	faChevronUp,
 	faBox,
 	faVideo,
@@ -33,6 +34,7 @@ import { useLayoutStore } from "@/Stores/layout"
 import { provide } from "vue"
 import FractionDisplay from '@/Components/DataDisplay/FractionDisplay.vue'
 import SalesAnalyticsCompact from '@/Components/Product/SalesAnalyticsCompact.vue'
+import LabelSKU from '@/Components/Utils/Product/LabelSKU.vue'
 
 
 library.add(
@@ -48,6 +50,7 @@ library.add(
 	faShieldAlt,
 	faInfoCircle,
 	faChevronDown,
+	faLock,
 	faChevronUp,
 	faBox,
 	faVideo
@@ -198,10 +201,7 @@ const tradeUnitRoute = (tradeUnit: TradeUnit) => {
         [tradeUnit.slug])
 }
 
-const isModalProductForSale = ref(false);
-const isModalSKUDetail = ref(false);
-
-console.log(props);
+const isModalProductForSale = ref(false)
 
 </script>
 
@@ -217,20 +217,11 @@ console.log(props);
 		</div>
 		
 		<div v-if="data.availability_status || data.trade_units.length > 0" class="text-md text-gray-800 whitespace-pre-wrap justify-self-end self-center items-center flex">
-			<div v-tooltip="data.trade_units.length > 1 ? trans('Click to view all trade units detail') : ''" class="border border-solid hover:opacity-80 py-1 px-3 rounded-md hover:cursor-pointer me-2 flex border-green-600" v-on:click="isModalSKUDetail = true">
-				<div v-if="data.trade_units.length == 1" class="text-teal-600 whitespace-nowrap w-full">
-					<span class=""> &#8623; SKU </span>
-					<span class="font-bold">
-						<FractionDisplay v-if="data.trade_units[0].pick_fractional" :fractionData="data.trade_units[0].pick_fractional" />
-					</span>
-				</div>
-				<div v-else class="text-teal-600 whitespace-nowrap w-full">
-					<span class=""> Multi Trade Units</span>
-				</div>
-				<div class="border-s border-green-600 text-gray-700 whitespace-nowrap font-bold ms-2 ps-2">
-					{{ data.masterProduct.units + " " + data.masterProduct.unit }}
-				</div>
-			</div>
+			<LabelSKU
+				:product="data.masterProduct"
+				:trade_units="data.trade_units"
+				:routeFunction="tradeUnitRoute"
+			/>
 			
 			<span v-if="data.availability_status"
 				v-on:click="isModalProductForSale = true"
@@ -360,45 +351,8 @@ console.log(props);
 			</div>	
 		</div>
 	</Modal>
-
-	<Modal :isOpen="isModalSKUDetail" @onClose="isModalSKUDetail = false" width="max-w-3xl w-full">
-		<div class="grid grid-cols-2 font-bold mb-4">
-			<div class="text-left text-lg">
-				{{ trans('Trade Unit SKU Details') }}
-			</div>
-        </div>
-		<div class="grid grid-cols-5 mt-3 text-sm font-bold">
-			<div class="text-left">
-				Code
-			</div>
-			<div class="text-left col-span-3">
-				Name
-			</div>
-			<div class="text-right">
-				SKU
-			</div>	
-		</div>
-		<div v-for="tUnit in data.trade_units" :key="tUnit.tradeUnit.id" class="grid grid-cols-5 mt-3 text-sm min-h-8">
-			<div class="text-left flex items-center">
-				<Link :href="tradeUnitRoute(tUnit.tradeUnit)" class="primaryLinkxx">
-					{{ tUnit.tradeUnit.code }}
-				</Link>
-			</div>
-			<div class="text-left col-span-3 flex items-center">
-				{{ tUnit.tradeUnit.name }}
-			</div>
-			<div class="justify-items-end text-teal-600 whitespace-nowrap">
-				<span class="border border-solid hover:opacity-80 py-1 px-3 rounded-md hover:cursor-pointer flex border-green-600 w-fit">
-					<span> &#8623; SKU </span>
-					<span class="font-bold ms-2">
-						<FractionDisplay v-if="tUnit.pick_fractional" :fractionData="tUnit.pick_fractional" />
-					</span>
-				</span>
-			</div>
-		</div>
-	</Modal>
-	
 </template>
+
 <style lang="scss">
     .primaryLinkxx {
         background: linear-gradient(to top, var(--theme-color-3), var(--theme-color-3));
