dda535394912ca12d9c8948a8871de9e39fadf78
Raul Perusquia <raul@inikoo.com>
2025-12-15 14:16:54 +0800

    ♻️ Refactor: Simplify `GetProductShowcase` action by removing unused trade unit logic and integrating brand and tags resources.

--- app/Http/Resources/Inventory/OrgStocksInProductResource.php
+++ app/Http/Resources/Inventory/OrgStocksInProductResource.php
@@ -11,24 +11,33 @@ namespace App\Http\Resources\Inventory;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $id
+ * @property mixed $slug
+ * @property mixed $code
+ * @property mixed $name
+ * @property mixed $pivot_notes
+ * @property mixed $unit_value
+ * @property mixed $discontinued_in_organisation_at
+ * @property mixed $family_slug
+ * @property mixed $family_code
+ * @property mixed $pivot_quantity
+ */
 class OrgStocksInProductResource extends JsonResource
 {
     public function toArray($request): array
     {
-
         return [
-            // 'id'                              => $this->id,
-            'org_stock_id'                              => $this->id,
+            'org_stock_id'                    => $this->id,
             'slug'                            => $this->slug,
             'code'                            => $this->code,
             'name'                            => $this->name,
-            'notes'                            => $this->pivot_notes,
+            'notes'                           => $this->pivot_notes,
             'unit_value'                      => $this->unit_value,
             'discontinued_in_organisation_at' => $this->discontinued_in_organisation_at,
             'family_slug'                     => $this->family_slug,
             'family_code'                     => $this->family_code,
-            // 'pivot_quantity'                  => $this->pivot_quantity,
-            'quantity'                  => $this->pivot_quantity,
+            'quantity'                        => $this->pivot_quantity,
         ];
     }
 }
