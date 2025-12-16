111cf503ff6fca4379d52a16500f4f4caaecec6f
YudhistiraA <aryarajasa0@gmail.com>
2025-12-16 13:33:09 +0800

    set Button add chart  mobile

--- resources/js/Components/CMS/Webpage/Products2/Products2Render.vue
+++ resources/js/Components/CMS/Webpage/Products2/Products2Render.vue
@@ -188,7 +188,7 @@ const toggleBackInStock = () =>
                 </div>
 
                 <!-- BUTTON -->
-                <div class="flex items-center md:justify-end">
+                <div class="flex items-center justify-end">
                     <template v-if="layout?.iris?.is_logged_in">
                         <!-- In stock -->
                         <NewAddToCartButton v-if="product.stock > 0 && basketButton" :hasInBasket="hasInBasket"
