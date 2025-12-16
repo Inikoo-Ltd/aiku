d0e668f0a7e180a01512e5ba754b124b0a07bd6e
Raul Perusquia <raul@inikoo.com>
2025-12-16 11:18:51 +0800

    INI-49 Feature: Add `UpdateOrderShippingEngineAsAuto` action and corresponding route to handle auto shipping engine updates for orders.

--- routes/grp/web/models/ordering/order.php
+++ routes/grp/web/models/ordering/order.php
@@ -22,6 +22,7 @@ use App\Actions\Ordering\Order\SaveOrderModification;
 use App\Actions\Ordering\Order\SwitchOrderDeliveryAddress;
 use App\Actions\Ordering\Order\UpdateOrder;
 use App\Actions\Ordering\Order\UpdateOrderDeliveryAddress;
+use App\Actions\Ordering\Order\UpdateOrderShippingEngineAsAuto;
 use App\Actions\Ordering\Order\UpdateOrderShippingEngineAsManual;
 use App\Actions\Ordering\Order\UpdateOrderShippingTBCAmount;
 use App\Actions\Ordering\Order\UpdateState\CancelOrder;
@@ -85,12 +86,8 @@ Route::name('order.')->prefix('order/{order:id}')->group(function () {
 
     Route::patch('set-shipping-engine-manual', UpdateOrderShippingEngineAsManual::class)->name('set_shipping_engine_manual');
     Route::patch('set-shipping-tbc-amount', UpdateOrderShippingTBCAmount::class)->name('set_shipping_tbc_amount');
-
-
+    Route::get('set-shipping-engine-auto', UpdateOrderShippingEngineAsAuto::class)->name('set_shipping_engine_auto');
 });
-
-
-
 
 Route::name('picking.')->prefix('picking/{picking:id}')->group(function () {
     Route::patch('update', UpdatePicking::class)->name('update');
