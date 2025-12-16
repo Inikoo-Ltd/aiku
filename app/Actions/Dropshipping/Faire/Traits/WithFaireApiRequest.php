35d97aadb934688bb51a030f1aa9dd8e307db112
itzArtha <manager.arthachan@gmail.com>
2025-12-16 16:20:49 +0800

    Add necessary imports to `WithFaireApiRequest` trait for API, logging, and caching functionality.

--- app/Actions/Dropshipping/Faire/Traits/WithFaireApiRequest.php
+++ app/Actions/Dropshipping/Faire/Traits/WithFaireApiRequest.php
@@ -2,6 +2,12 @@
 
 namespace App\Actions\Dropshipping\Faire\Traits;
 
+use Carbon\Carbon;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 trait WithFaireApiRequest
 {
     /**
