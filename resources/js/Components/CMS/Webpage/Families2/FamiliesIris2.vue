a35f7f328c2f2005f7b8d2838a2c55b8a1db2161
YudhistiraA <aryarajasa0@gmail.com>
2025-12-17 16:37:43 +0800

    INI-664 collection bug

--- resources/js/Components/CMS/Webpage/Families2/FamiliesIris2.vue
+++ resources/js/Components/CMS/Webpage/Families2/FamiliesIris2.vue
@@ -65,7 +65,7 @@ const refreshCarousel = async (delay = 100) => {
 
 const allItems = computed(() => [
   ...(props.fieldValue?.families || []),
-  ...(props.fieldValue?.collections || [])
+/*   ...(props.fieldValue?.collections || []) */
 ])
 
 const spaceBetween = computed(() => (props.screenType === 'mobile' ? 8 : 24))
