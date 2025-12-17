09f50eacb8b8a972137ae1e8e6184b2f60a760dd
aqordeon <aqordivika@yahoo.co.id>
2025-12-17 10:29:13 +0800

    feat: Toggle: add loading state and adjust styles

--- resources/js/Components/Pure/Toggle.vue
+++ resources/js/Components/Pure/Toggle.vue
@@ -4,17 +4,21 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { faTimes } from "@fal"
 import { faCheck } from "@far"
 import { library } from "@fortawesome/fontawesome-svg-core"
+import LoadingIcon from '../Utils/LoadingIcon.vue'
 library.add(faTimes, faCheck)
 
 const props = defineProps<{
     modelValue: boolean|undefined
     classes?: string
+    loading?: boolean
 }>()
 
 const emit = defineEmits<{
     (e: 'update:modelValue', value: boolean): void
 }>()
 
+
+// TODO: make toggle smaller h-6 w-12 to h-5 w-10
 </script>
 
 
@@ -23,16 +27,20 @@ const emit = defineEmits<{
         :modelValue="modelValue"
         @update:modelValue="(val) => emit('update:modelValue', val)"
         :class="[
-            modelValue ? 'bg-green-500' : 'bg-slate-300',
+            loading
+                ? 'bg-slate-500 cursor-not-allowed'
+                : modelValue
+                    ? 'bg-green-500'
+                    : 'bg-slate-300',
             classes,
         ]"
         class="pr-1 relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
     >
         <span aria-hidden="true" :class="modelValue ? 'translate-x-6 bg-white ' : 'translate-x-0 bg-gray-50'"
             class="flex items-center justify-center pointer-events-none h-full w-1/2 transform rounded-full shadow-lg ring-0 transition">
-            <FontAwesomeIcon v-if="modelValue" icon='far fa-check' class='text-sm text-green-600' fixed-width aria-hidden='true' />
+            <LoadingIcon v-if="loading" />
+            <FontAwesomeIcon v-else-if="modelValue" icon='far fa-check' class='text-sm text-green-600' fixed-width aria-hidden='true' />
             <FontAwesomeIcon v-else icon='fal fa-times' class='text-sm text-red-500' fixed-width aria-hidden='true' />
         </span>
-
     </Switch>
 </template>
