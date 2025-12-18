a71c5be6900c436fc2f7a8c9acd13a056486371b
oggiesutrisna <oggiesutrisna@COMPUTERS-2B.localdomain>
2025-12-18 14:52:19 +0800

    fix: Specify correct foreign key in OrderReturn orders relationship

--- app/Models/Dispatching/OrderReturn.php
+++ app/Models/Dispatching/OrderReturn.php
@@ -165,7 +165,7 @@ class OrderReturn extends Model implements Auditable
 
     public function orders(): BelongsToMany
     {
-        return $this->belongsToMany(Order::class, 'return_order')->withTimestamps();
+        return $this->belongsToMany(Order::class, 'return_order', 'return_id', 'order_id')->withTimestamps();
     }
 
     public function returnItems(): HasMany
