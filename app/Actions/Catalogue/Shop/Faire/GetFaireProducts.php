106ae8266cad7b951f81ae697818fa1acb3a220a
itzArtha <manager.arthachan@gmail.com>
2025-12-29 15:09:31 +0800

    Update `GetFaireProducts` to store products directly using `StoreProduct` action and enhance command handling logic.

--- app/Actions/Catalogue/Shop/Faire/GetFaireProducts.php
+++ app/Actions/Catalogue/Shop/Faire/GetFaireProducts.php
@@ -2,49 +2,45 @@
 
 namespace App\Actions\Catalogue\Shop\Faire;
 
+use App\Actions\Catalogue\Product\StoreProduct;
 use App\Actions\OrgAction;
 use App\Enums\Catalogue\Shop\ShopEngineEnum;
 use App\Enums\Catalogue\Shop\ShopStateEnum;
 use App\Enums\Catalogue\Shop\ShopTypeEnum;
-use App\Models\Catalogue\Product;
 use App\Models\Catalogue\Shop;
+use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 
 class GetFaireProducts extends OrgAction
 {
-    public string $commandSignature = 'faire:products';
+    public string $commandSignature = 'faire:products {shop}';
 
-    public function handle(Shop $shop): array
+    public function handle(Shop $shop): void
     {
         $products = $shop->getFaireProducts([
             'limit' => 250
         ]);
 
-        $variantSkus = [];
-        $awProducts    = [];
-
         foreach (Arr::get($products, 'products', []) as $product) {
             foreach ($product['variants'] as $variant) {
-                $variantSkus[] = $variant['sku'];
-
-                $awProduct = Product::where('shop_id', 41)
-                    ->where('code', 'ILIKE', "%{$variant['sku']}%")
-                    ->first();
-
-                if ($awProduct->code) {
-                    $awProducts[] = $awProduct->code;
-                }
+                StoreProduct::make()->action($shop, [
+                    'code' => $variant['sku'],
+                    'name' => $product['name'] - $variant['name'],
+                    'description' => $product['description'],
+                    'rrp' => $variant['retail_price_cents'],
+                    'price' => $variant['wholesale_price_cents'],
+                    'external_id' => $variant['id'],
+                    'is_main' => true
+                ]);
             }
         }
-
-
-        dd(array_diff($variantSkus, $awProducts));
     }
 
-    public function asCommand(): void
+    public function asCommand(Command $command): void
     {
         $shop = Shop::where('type', ShopTypeEnum::EXTERNAL)->where('engine', ShopEngineEnum::FAIRE)
             ->where('state', ShopStateEnum::OPEN)
+            ->where('slug', $command->argument('shop'))
             ->first();
 
         $this->handle($shop);
