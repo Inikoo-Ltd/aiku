eb2e4b246f028ad50525f08ca9fc1d75fa74c43f
stewicca <stewicalf@gmail.com>
2025-12-29 14:45:04 +0800

    INI-208 Add asset time series records and model updates

--- app/Models/Catalogue/AssetTimeSeriesRecord.php
+++ app/Models/Catalogue/AssetTimeSeriesRecord.php
@@ -11,6 +11,20 @@ namespace App\Models\Catalogue;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
+ * @property int $asset_time_series_id
+ * @property string $sales
+ * @property string $sales_org_currency
+ * @property string $sales_grp_currency
+ * @property int $invoices
+ * @property int $refunds
+ * @property int $orders
+ * @property int $delivery_notes
+ * @property int $customers_invoiced
+ * @property \Illuminate\Support\Carbon|null $from
+ * @property \Illuminate\Support\Carbon|null $to
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder<static>|AssetTimeSeriesRecord newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|AssetTimeSeriesRecord newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|AssetTimeSeriesRecord query()
@@ -21,4 +35,12 @@ class AssetTimeSeriesRecord extends Model
     protected $table = 'asset_time_series_records';
 
     protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'from' => 'datetime',
+            'to' => 'datetime',
+        ];
+    }
 }
