ca31ced3afcac8deb2c017fcd0af25b5ebcf3f1e
itzArtha <manager.arthachan@gmail.com>
2025-12-31 13:32:01 +0800

    Refactor `UpdateFaireInventoryQuantity` action to replace `Order` with `Product` in method signature and streamline logic for inventory updates.

--- app/Actions/Catalogue/Shop/External/Faire/UpdateFaireInventoryQuantity.php
+++ app/Actions/Catalogue/Shop/External/Faire/UpdateFaireInventoryQuantity.php
@@ -3,27 +3,14 @@
 namespace App\Actions\Catalogue\Shop\External\Faire;
 
 use App\Actions\OrgAction;
+use App\Models\Catalogue\Product;
 use App\Models\Catalogue\Shop;
-use App\Models\Ordering\Order;
 
 class UpdateFaireInventoryQuantity extends OrgAction
 {
-    public function handle(Shop $shop, Order $order): array
+    public function handle(Shop $shop, Product $product): array
     {
-        $shipments = [];
-        foreach ($order->deliveryNotes as $deliveryNote) {
-            foreach ($deliveryNote->shipments as $shipment) {
-                $shipments[] = [
-                    'shipping_type' => 'SHIP_ON_YOUR_OWN',
-                    'order_id' => $order->external_id,
-                    'carrier'  => $shipment->shipper?->name,
-                    'tracking_code' => $shipment->tracking
-                ];
-            }
-        }
-
-        return $shop->updateShippingFaireOrder($order->external_id, [
-            'shipments' => $shipments
-        ]);
+        // TODO: Implement updateInventoryQuantity method.
+        return $shop->updateInventoryQuantity($product->external_id);
     }
 }
