3faf629f17b1b924e6c761bc3c060a8f0674a30e
itzArtha <manager.arthachan@gmail.com>
2026-01-06 10:56:46 +0800

    Simplify `allowedForShop` logic in `ShopPermissionsEnum` by removing engine-specific handling and updating

--- app/Enums/SysAdmin/Authorisation/ShopPermissionsEnum.php
+++ app/Enums/SysAdmin/Authorisation/ShopPermissionsEnum.php
@@ -76,10 +76,7 @@ enum ShopPermissionsEnum: string
 
     private static function allowedForShop(Shop $shop): array
     {
-        return match ($shop->engine->value) {
-            ShopEngineEnum::FAIRE->value => self::permissionsForFaireShop(),
-            default => self::cases(),
-        };
+        return self::cases();
     }
 
     private static function permissionsForFaireShop(): array
@@ -91,12 +88,17 @@ enum ShopPermissionsEnum: string
             self::PRODUCTS_EDIT,
             self::PRODUCTS_VIEW,
 
+            self::ORDERS,
             self::ORDERS_VIEW,
             self::ORDERS_EDIT,
+
+            self::CRM,
             self::CRM_VIEW,
+            self::CRM_EDIT,
             self::CRM_PROSPECTS_VIEW,
 
             self::SUPERVISOR_PRODUCTS,
+            self::SUPERVISOR_ORDERS,
         ];
     }
 }
