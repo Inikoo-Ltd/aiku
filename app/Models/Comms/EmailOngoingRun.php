7de53fd0b439db6f9adc8cc9c11dfb1b3f5187fb
ekayudinata <ekayudinatha@gmail.com>
2026-01-14 11:18:08 +0800

    INI-72 update sender on the emailOngoingRun

--- app/Models/Comms/EmailOngoingRun.php
+++ app/Models/Comms/EmailOngoingRun.php
@@ -116,14 +116,13 @@ class EmailOngoingRun extends Model
 
     public function sender(): string
     {
-        if (app()->environment('production')) {
-            /** @var Shop $parent */
-            $parent = $this->shop;
-            $sender = $parent->senderEmail?->email_address ?? $parent->email;
-        } else {
-            $sender = config('app.email_address_in_non_production_env');
-        }
+        /** @var Shop $parent */
+        $parent = $this->shop;
 
-        return $sender;
+        $email = app()->environment('production')
+            ? ($parent->senderEmail?->email_address ?? $parent->email)
+            : config('app.email_address_in_non_production_env');
+
+        return "{$parent->name} <{$email}>";
     }
 }
