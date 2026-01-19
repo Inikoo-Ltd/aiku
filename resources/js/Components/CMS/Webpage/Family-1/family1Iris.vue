5a9aeeabda17d06d488379ca2739635de3ec0074
aqordeon <aqordivika@yahoo.co.id>
2026-01-19 15:24:00 +0800

    INI-840 feat: Iris Family: show Offers label

--- resources/js/Components/CMS/Webpage/Family-1/family1Iris.vue
+++ resources/js/Components/CMS/Webpage/Family-1/family1Iris.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref, inject, computed } from 'vue'
 import { faCube, faLink } from "@fal"
-import { faStar, faCircle } from "@fas"
+import { faStar, faCircle, faBadgePercent } from "@fas"
 import { faChevronCircleLeft, faChevronCircleRight } from '@far'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { getStyles } from "@/Composables/styles"
 import { trans } from 'laravel-vue-i18n'
 import Button from '@/Components/Elements/Buttons/Button.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCube, faLink, faStar, faCircle, faChevronCircleLeft, faChevronCircleRight)
+library.add(faCube, faLink, faStar, faCircle, faBadgePercent, faChevronCircleLeft, faChevronCircleRight)
 
 const props = defineProps<{
   fieldValue: {
@@ -22,9 +23,16 @@ const props = defineProps<{
 
       }[]
       offers_data?: {
-        vol_gr: {
-            volume: number
-            discount: number
+        [key: string]: {
+          state: string
+          duration: string
+          label: string
+          allowances: {
+            class: string  // 'discount'
+            type: string   // 'percentage_off'
+            label: string  // '5.0%'
+          }[]
+          note: string
         }
       }
     }
@@ -42,62 +50,64 @@ const toggleShowExtra = () => {
 const layout: any = inject("layout", {})
 console.log('ssss',props)
 
-const promoData = computed(() => {
-  const vol = props.fieldValue?.family?.offers_data?.vol_gr
-  if (!vol?.volume || !vol?.discount) return null
+// const promoData = computed(() => {
+//   const vol = props.fieldValue?.family?.offers_data?.vol_gr
+//   if (!vol?.volume || !vol?.discount) return null
 
-  return {
-    title: trans("Special Volume Deal"),
-    headline: trans(`Buy ${vol.volume} items, get ${vol.discount}% OFF`),
-    description: trans(`Perfect for bulk buyers. Save more when you purchase ${vol.volume} or more items.`),
-    cta: trans("Start Saving Now")
-  }
-})
+//   return {
+//     title: trans("Special Volume Deal"),
+//     headline: trans(`Buy ${vol.volume} items, get ${vol.discount}% OFF`),
+//     description: trans(`Perfect for bulk buyers. Save more when you purchase ${vol.volume} or more items.`),
+//     cta: trans("Start Saving Now")
+//   }
+// })
 </script>
 
 <template>
 
-  <div id="family-1">
-    <!-- <pre>{{ props.fieldValue?.family?.active_offers }}</pre> -->
-        <section v-if="promoData && layout.iris.is_logged_in"
-            class="relative mx-[10px]  sm:mx-[50px] mt-6 overflow-hidden rounded-md p-6 shadow-md"
-            aria-label="Colorful Volume Promotion">
-            <!-- Gradient Background -->
-            <div class="absolute inset-0 bg-gradient-to-br from-pink-400 via-purple-400 to-indigo-400 opacity-90"></div>
-
-            <!-- Decorative Blurs -->
-            <div class="absolute -top-10 -right-10 h-40 w-40 rounded-full bg-yellow-300 opacity-30 blur-3xl"></div>
-            <div class="absolute -bottom-10 -left-10 h-40 w-40 rounded-full bg-cyan-300 opacity-30 blur-3xl"></div>
-
-            <!-- Content -->
-            <div class="relative z-10 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 text-white">
-
-                <div>
-                    <p class="text-xs uppercase tracking-wide font-semibold opacity-90 mb-1">
-                        {{ promoData.title }}
-                    </p>
-
-                    <h2 class="text-xl font-bold leading-tight">
-                        {{ promoData.headline }}
-                    </h2>
-
-                    <p class="text-sm mt-1 max-w-[520px] opacity-90">
-                        {{ promoData.description }}
-                    </p>
-                </div>
-
-                <a href="#list-products-ecom-iris" class="flex-shrink-0">
-                    <Button :label="promoData.cta">
-                    </Button>
-                </a>
-
-            </div>
-        </section>
+  <div id="family-1-iris">
 
   
 
     <div :style="{...getStyles(layout?.app?.webpage_layout?.container?.properties, screenType), ...getStyles(fieldValue?.container?.properties), width : 'auto' }"  class="py-4 px-[10px] sm:px-[50px]"
       aria-label="Family Description Section">
+      
+        <!-- Section: listing Offers -->
+        <div v-if="fieldValue?.family?.offers_data && layout.iris.is_logged_in"
+            class="grid gap-x-4 mt-4 gap-y-2"
+            :class="fieldValue?.family?.offers_data?.length > 1 ? 'md:grid-cols-2' : ''"
+        >
+            <section v-for="offer in fieldValue?.family?.offers_data"
+                class="relative xmx-[10px] sm:xmx-[50px] overflow-hidden rounded-md px-6 py-3 shadow-md mb-2"
+                aria-label="Colorful Volume Promotion">
+                <!-- Gradient Background -->
+                <div class="absolute inset-0 bg-gradient-to-br from-pink-400 via-purple-400 to-purple-200 opacity-90"></div>
+                <!-- Decorative Blurs -->
+                <div class="absolute -top-10 -right-10 h-40 w-40 rounded-full bg-yellow-300 opacity-30 blur-3xl"></div>
+                <div class="absolute -bottom-10 -left-10 h-40 w-40 rounded-full bg-cyan-300 opacity-30 blur-3xl"></div>
+                <!-- Content -->
+                <div class="relative z-10 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 text-white">
+                    <div>
+                        <div class="">
+                            <span v-for="(allowance, idx) in offer.allowances" class="text-xs">
+                                <span v-if="idx != 0"> | </span>
+                                {{ allowance.label }}
+                            </span>
+                        </div>
+                        <p class="text-sm uppercase tracking-wide font-semibold xopacity-90 mb-1">
+                            {{ offer.label }}
+                        </p>
+                        <p v-if="offer.note" class="text-xs mt-1 max-w-[520px] opacity-90">
+                            {{ offer.note }}
+                        </p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon v-if="offer.allowances.some((v: { type: string }) => v.type === 'percentage_off')" v-tooltip="trans('Offer with percentage off')" icon="fas fa-badge-percent" class="text-2xl xopacity-60 align-middle" fixed-width aria-hidden="true" />
+                        <!-- <FontAwesomeIcon v-if="offer.allowances.some((v: { type: string }) => v.type === 'percentage_off')" icon="fas fa-badge-dollar" class="text-2xl opacity-60" fixed-width aria-hidden="true" /> -->
+                    </div>
+                </div>
+            </section>
+        </div>
 
       <!-- Description Title (SEO: Heading) -->
       <h1 v-if="fieldValue.family.name" class="text-[1.5rem] leading-[2rem] font-semibold text-gray-800">
