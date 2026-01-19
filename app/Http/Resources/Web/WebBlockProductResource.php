695eba1c67404cad73aec7f56eec0d752ed0cbd1
Raul Perusquia <raul@inikoo.com>
2026-01-19 18:56:20 +0800

    Remove unused `$gr_offer` variable and related logic from `WebBlockProductResource`.

--- app/Http/Resources/Web/WebBlockProductResource.php
+++ app/Http/Resources/Web/WebBlockProductResource.php
@@ -75,7 +75,6 @@ class WebBlockProductResource extends JsonResource
             }
         }
 
-        $gr_offer = $this->family->getGROffer;
 
         return [
             'luigi_identity'                    => $product->getLuigiIdentity(),
@@ -112,10 +111,6 @@ class WebBlockProductResource extends JsonResource
             'is_on_demand'                      => $isOnDemand,
             'is_back_in_stock'                  => $product->backInStockReminders,
             'back_in_stock'                     => $back_in_stock,
-            'available_gr_offer_to_use'             => $gr_offer ? [
-                'slug'          => $gr_offer->slug,
-                'trigger_data'  => $gr_offer->trigger_data,
-            ] : [],
             ...$offers
         ];
     }
