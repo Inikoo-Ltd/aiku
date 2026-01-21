73205120bada1a76eec5b2322b98652c784ba3c0
itzArtha <manager.arthachan@gmail.com>
2026-01-21 13:41:56 +0800

    Add PickingTrolley and PickedBay models with corresponding migrations

---
+++ app/Models/Inventory/PickingTrolley.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use App\Models\Traits\HasHistory;
+use App\Models\Traits\HasUniversalSearch;
+use App\Models\Traits\InWarehouse;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class PickingTrolley extends Model implements Auditable
+{
+    use SoftDeletes;
+    use HasSlug;
+    use HasUniversalSearch;
+    use HasFactory;
+    use HasHistory;
+    use InWarehouse;
+
+    protected $guarded = [];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('code')
+            ->doNotGenerateSlugsOnUpdate()
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
