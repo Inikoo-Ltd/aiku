354ee888c8a1a9a63942b37c641a88c3085401cb
stewicca <stewicalf@gmail.com>
2026-01-26 10:13:14 +0800

    INI-849 feat: add InvoiceCategoryTimeSeries and related models, migrations, and processing logic

---
+++ app/Models/Accounting/InvoiceCategoryTimeSeriesRecord.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Author: stewicca <stewicalf@gmail.com>
+ * Copyright (c) 2026, Steven Wicca Alfredo
+ */
+
+namespace App\Models\Accounting;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $invoice_category_time_series_id
+ * @property string $frequency
+ * @property string|null $sales
+ * @property string|null $sales_org_currency
+ * @property string|null $sales_grp_currency
+ * @property string|null $lost_revenue
+ * @property string|null $lost_revenue_org_currency
+ * @property string|null $lost_revenue_grp_currency
+ * @property int|null $invoices
+ * @property int|null $refunds
+ * @property int|null $customers_invoiced
+ * @property \Illuminate\Support\Carbon|null $from
+ * @property \Illuminate\Support\Carbon|null $to
+ * @property string|null $period
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|InvoiceCategoryTimeSeriesRecord newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|InvoiceCategoryTimeSeriesRecord newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|InvoiceCategoryTimeSeriesRecord query()
+ * @mixin \Eloquent
+ */
+class InvoiceCategoryTimeSeriesRecord extends Model
+{
+    protected $table = 'invoice_category_time_series_records';
+
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'from' => 'datetime',
+            'to'   => 'datetime',
+        ];
+    }
+}
