eaa2ae09d53e35f6d8129c7c434ae20d5082ab55
Raul Perusquia <raul@inikoo.com>
2026-01-27 15:07:20 +0800

    INI-641 Update `UpdateTransactionChargeAmount` action to handle discretionary charges and percentage discounts, refactor logic, and add validation for charge transactions

--- app/Actions/Ordering/Transaction/UpdateTransactionChargeAmount.php
+++ app/Actions/Ordering/Transaction/UpdateTransactionChargeAmount.php
@@ -9,14 +9,14 @@
 
 namespace App\Actions\Ordering\Transaction;
 
-use App\Actions\Ordering\Order\CalculateOrderDiscounts;
-use App\Actions\Ordering\Order\Hydrators\OrderHydrateDiscretionaryOffersData;
 use App\Actions\OrgAction;
 use App\Actions\Traits\Rules\WithNoStrictRules;
 use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Catalogue\Charge\ChargeTypeEnum;
 use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Models\Billables\Charge;
 use App\Models\Ordering\Transaction;
-use Illuminate\Support\Arr;
+use Illuminate\Validation\Validator;
 use Lorisleiva\Actions\ActionRequest;
 
 class UpdateTransactionChargeAmount extends OrgAction
@@ -28,7 +28,6 @@ class UpdateTransactionChargeAmount extends OrgAction
 
     public function handle(Transaction $transaction, array $modelData): Transaction
     {
-
         if (in_array($transaction->order->state, [
             OrderStateEnum::DISPATCHED,
             OrderStateEnum::FINALISED,
@@ -37,13 +36,37 @@ class UpdateTransactionChargeAmount extends OrgAction
             abort(403);
         }
 
-        //        if (Arr::get($modelData, 'discretionary_offer') == 0) {
-        //            $modelData['discretionary_offer'] = null;
-        //        }
-        //
-        //        $transaction->update($modelData);
-        //        OrderHydrateDiscretionaryOffersData::run($transaction->order);
-        //        CalculateOrderDiscounts::run($transaction->order);
+        /** @var \App\Models\Billables\Charge $charge */
+        $charge = $transaction->model;
+
+        $offersData = [];
+
+        if ($charge->type == ChargeTypeEnum::DISCRETIONARY->value) {
+            $dataToUpdate = [
+                'gross_amount' => $modelData['amount'],
+                'net_amount'   => $modelData['amount']
+            ];
+        } else {
+            $dataToUpdate = [
+                'net_amount' => $modelData['amount']
+            ];
+            if ($transaction->gross_amount > 0 && $transaction->gross_amount > $modelData['amount']) {
+                $discountFactor = $modelData['amount'] / $transaction->gross_amount;
+
+                $offersData = [
+                    'v' => 1,
+                    'o' => [
+                        't'  => 'percentage_off',
+                        'pf' => $discountFactor,
+                        'p'  => percentage($discountFactor, 1),
+                        'l'  => '',
+                    ]
+                ];
+            }
+        }
+        $dataToUpdate['offers_data'] = $offersData;
+
+        UpdateTransaction::run($transaction, $dataToUpdate);
 
 
         return $transaction;
@@ -52,8 +75,15 @@ class UpdateTransactionChargeAmount extends OrgAction
     public function rules(): array
     {
         return [
-            'amount'       => [ 'numeric', 'min:0'],
+            'amount' => ['numeric', 'min:0'],
         ];
+    }
+
+    public function afterValidator(Validator $validator, ActionRequest $request): void
+    {
+        if (!$this->transaction->model instanceof Charge) {
+            $validator->errors()->add('message', __('Transaction must be a charge'));
+        }
     }
 
 
