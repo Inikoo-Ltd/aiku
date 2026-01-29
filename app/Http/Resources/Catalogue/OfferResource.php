c2350740a7fbf471abaf0a33906af76f012d85c3
aqordeon <aqordivika@yahoo.co.id>
2026-01-29 09:56:20 +0800

    fix: Ensure basic offer data is merged safely in OfferResource

--- app/Http/Resources/Catalogue/OfferResource.php
+++ app/Http/Resources/Catalogue/OfferResource.php
@@ -63,6 +63,6 @@ class OfferResource extends JsonResource
             ],
         ];
 
-        return array_merge($customOfferData, $basicOfferData);
+        return array_merge($customOfferData, $basicOfferData ?? []);
     }
 }
