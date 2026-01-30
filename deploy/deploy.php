9531d42031040711b01f8163346fe3b7f6c991a2
Raul Perusquia <raul@inikoo.com>
2026-01-30 12:47:08 +0800

    Add optional delay parameter to `varnish:restart` command and update usage in deployment script

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -181,7 +181,7 @@ task('deploy:flush-varnish', function () {
     if ($shouldFlush) {
         writeln('SSR checksum changed (or missing). Flushing Varnish cache via artisan varnish...');
         try {
-            artisan('varnish:restart', ['skipIfNoEnv', 'showOutput'])();
+            artisan('varnish:restart 10', ['skipIfNoEnv', 'showOutput'])();
             writeln('Varnish cache flush command executed.');
         } catch (\Throwable $e) {
             writeln('Error flushing Varnish cache: '.$e->getMessage());
