34f4f8002d4c8c221c5fd8ffb04088a31e5d8559
Raul Perusquia <raul@inikoo.com>
2026-02-02 15:56:26 +0800

    Add `has_gr_vol_discount` property to MasterProductCategory model and migration

--- database/migrations/2026_01_30_072427_add_has_gr_vol_discount_to_master_product_categories_table.php
+++ database/migrations/2026_01_30_072427_add_has_gr_vol_discount_to_master_product_categories_table.php
@@ -1,28 +1,25 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 02 Feb 2026 15:49:31 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('master_product_categories', function (Blueprint $table) {
             $table->boolean('has_gr_vol_discount')->default(false);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('master_product_categories', function (Blueprint $table) {
             $table->dropColumn('has_gr_vol_discount');
