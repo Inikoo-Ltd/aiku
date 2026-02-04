ce4723649d6a1c56a011c2d8db48c852bb36a1cc
Raul Perusquia <raul@inikoo.com>
2026-02-04 20:20:48 +0800

    Add `last_external_shop_products_fetched_at` field to `shops` table

--- database/migrations/2026_02_04_072617_add_last_external_shop_products_fetched_at_to_shops_table.php
+++ database/migrations/2026_02_04_072617_add_last_external_shop_products_fetched_at_to_shops_table.php
@@ -1,29 +1,25 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 04 Feb 2026 20:20:41 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class extends Migration {
+
+    public function up(): void
     {
         Schema::table('shops', function (Blueprint $table) {
             $table->dateTimeTz('last_external_shop_products_fetched_at')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('shops', function (Blueprint $table) {
             $table->dropColumn('last_external_shop_products_fetched_at');
