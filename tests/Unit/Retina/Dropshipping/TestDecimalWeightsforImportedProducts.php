005d5e2c47b4b7658d3ba4fdae5b89f9d67c9e2d
oggiesutrisna <sutrisna.oggie@gmail.com>
2026-02-05 16:12:10 +0800

    INI-977 test: add test for decimal weights in imported products

---
+++ tests/Unit/Retina/Dropshipping/TestDecimalWeightsforImportedProducts.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit\Retina\Dropshipping;
+
+use PHPUnit\Framework\TestCase;
+
+class TestDecimalWeightsforImportedProducts extends TestCase
+{
+    public function pro(): void {
+        $this->assertTrue(true);
+    }
+}
