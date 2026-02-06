f67d6e9b342b0b7e5987c5091c88a8353f66f902
stewicca <stewicalf@gmail.com>
2026-02-06 09:01:31 +0800

    INI-986 Improve Sales Channels Resource and Table with Active Icons
    - Updated SalesChannelsResource to return an icon representation of the
      'is_active' status.
    - Enhanced TableSalesChannels component to display the active status
      icon
      and formatted sales values.

--- resources/js/Components/Tables/Grp/SalesChannels/TableSalesChannels.vue
+++ resources/js/Components/Tables/Grp/SalesChannels/TableSalesChannels.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { inject } from 'vue'
+import Icon from '@/Components/Icon.vue'
 import Table from '@/Components/Table/Table.vue'
+import { aikuLocaleStructure } from '@/Composables/useLocaleStructure'
 
-const props = defineProps<{
+const locale = inject("locale", aikuLocaleStructure)
+
+defineProps<{
     data: {}
 }>()
 </script>
 
 <template>
-    <Table :resource="data" class="mt-5"></Table>
+    <Table :resource="data" class="mt-5">
+        <template #cell(is_active)="{ item }">
+             <Icon :data="item.is_active" class="px-1" />
+        </template>
+
+        <template #cell(type)="{ item }">
+            {{ item.type === 'na' ? 'NA' : item.type.charAt(0).toUpperCase() + item.type.slice(1) }}
+        </template>
+
+        <template #cell(sales)="{ item }">
+            <span class="tabular-nums">{{ locale.currencyFormat('GBP', item.sales) }}</span>
+        </template>
+    </Table>
 </template>
