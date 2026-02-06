6ab07cff8e7b90b5d647ab65b8c6bcb4c38e6a74
Raul Perusquia <raul@inikoo.com>
2026-02-06 16:51:49 +0800

    Add `product_price_currency_exchange` column to `shops` table with updated precision and nullable default

--- database/migrations/2026_02_06_061737_add_product_price_currency_exchange_to_shops_table.php
+++ database/migrations/2026_02_06_061737_add_product_price_currency_exchange_to_shops_table.php
@@ -1,29 +1,24 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 06 Feb 2026 16:51:21 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class extends Migration {
+
+    public function up(): void
     {
         Schema::table('shops', function (Blueprint $table) {
-            $table->decimal('product_price_currency_exchange', 16, 2)->default(0)->nullable();
+            $table->decimal('product_price_currency_exchange', 16)->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::table('shops', function (Blueprint $table) {
             $table->dropColumn('product_price_currency_exchange');
