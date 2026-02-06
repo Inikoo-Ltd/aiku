93b350f7d2e77d7eed6f336ac45896694b8504af
Raul Perusquia <raul@inikoo.com>
2026-02-06 11:53:48 +0800

    Update `RepairCustomersOrdersInBasketId` to handle B2B shop type check earlier and improve readability

--- app/Actions/Maintenance/CRM/RepairCustomersOrdersInBasketId.php
+++ app/Actions/Maintenance/CRM/RepairCustomersOrdersInBasketId.php
@@ -23,21 +23,17 @@ class RepairCustomersOrdersInBasketId
 
     protected function handle(Customer $customer, Command $command): void
     {
-        if ($customer->shop->type != ShopTypeEnum::B2B) {
-            return;
-        }
-
         $order = Order::where('customer_id', $customer->id)->where('state', OrderStateEnum::CREATING)->first();
 
         $oldOrder = $customer->current_order_in_basket_id;
         if ($order) {
             $customer->update([
-                'current_order_in_basket_id' => $order?->id,
+                'current_order_in_basket_id' => $order->id,
             ]);
         }
         if ($order && $oldOrder != $order->id) {
             CustomerHydrateBasket::run($customer->id);
-            $command->info("Customer {$customer->slug}: $oldOrder  ->   {$order->id}");
+            $command->info("Customer $customer->slug: $oldOrder  ->   $order->id");
         }
     }
 
@@ -57,15 +53,10 @@ class RepairCustomersOrdersInBasketId
             ->orderBy('id')
             ->chunkById(500, function ($customers) use ($command) {
                 foreach ($customers as $customer) {
-                    $this->handle($customer, $command);
-                }
-            }, 'id');
+                    if ($customer->shop->type != ShopTypeEnum::B2B) {
+                        return;
+                    }
 
-
-        Customer::withTrashed()
-            ->orderBy('id')
-            ->chunkById(500, function ($customers) use ($command) {
-                foreach ($customers as $customer) {
                     $this->handle($customer, $command);
                 }
             }, 'id');
