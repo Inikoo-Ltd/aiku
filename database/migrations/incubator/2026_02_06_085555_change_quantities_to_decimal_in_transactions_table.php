6399dba098683e8f3ebd98778bcd3e306e711ff4
Raul Perusquia <raul@inikoo.com>
2026-02-06 17:05:14 +0800

    Change quantities to decimal in `transactions` and `invoice_transactions` tables for improved precision and consistency.

---
+++ database/migrations/incubator/2026_02_06_085555_change_quantities_to_decimal_in_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->decimal('quantity_ordered', 16, 6)->nullable()->change();
+            $table->decimal('quantity_bonus', 16, 6)->nullable()->default(0)->change();
+            $table->decimal('quantity_dispatched', 16, 6)->nullable()->default(0)->change();
+            $table->decimal('quantity_fail', 16, 6)->nullable()->default(0)->change();
+            $table->decimal('quantity_cancelled', 16, 6)->nullable()->default(0)->change();
+            $table->decimal('quantity_picked', 16, 6)->nullable()->change();
+            $table->decimal('submitted_quantity_ordered', 16, 6)->default(0)->change();
+        });
+
+        Schema::table('invoice_transactions', function (Blueprint $table) {
+            $table->decimal('quantity', 16, 6)->nullable()->change();
+        });
+    }
+
+
+    public function down(): void
+    {
+    }
+};
