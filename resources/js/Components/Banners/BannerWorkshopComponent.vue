ee0ff3fbaa9638e2c0a917ef9f740d63234e8c70
YudhistiraA <aryarajasa0@gmail.com>
2026-02-09 16:58:53 +0800

    INI-947 refactor banner

--- resources/js/Components/Banners/BannerWorkshopComponent.vue
+++ resources/js/Components/Banners/BannerWorkshopComponent.vue
@@ -1,62 +1,95 @@
 <script setup lang="ts">
-import { ref, reactive, onBeforeMount, watch, onBeforeUnmount, computed } from "vue"
+import { ref, computed } from "vue"
 import SlidesWorkshop from "@/Components/Banners/SlidesWorkshop.vue"
 import SliderLandscape from "@/Components/Banners/Slider/SliderLandscape.vue"
 import SliderSquare from "@/Components/Banners/Slider/SliderSquare.vue"
 import SlidesWorkshopAddMode from "@/Components/Banners/SlidesWorkshopAddMode.vue"
 import ScreenView from "@/Components/ScreenView.vue"
-
+import { BannerWorkshop } from "@/types/BannerWorkshop"
+import { routeType } from "@/types/route"
 
 const props = defineProps<{
-    data: any
-    imagesUploadRoute: any
-    user: any
-    banner: any
-    galleryRoute:{
-        stock_images : routeType,
-        uploaded_images : routeType
-    }
+  modelValue: BannerWorkshop
+  imagesUploadRoute: any
+  banner: any
+  galleryRoute: {
+    stock_images: routeType
+    uploaded_images: routeType
+  }
 }>()
 
-const jumpToIndex = ref('')
-const screenView = ref("")
+const emits = defineEmits<{
+  (e: "update:modelValue", val: BannerWorkshop): void
+}>()
+
+const jumpToIndex = ref<string>("")
+const screenView = ref<string>("")
+
+const data = computed<BannerWorkshop>({
+  get() {
+    return props.modelValue
+  },
+  set(v) {
+    console.log('components', v)
+    emits("update:modelValue", v)
+  }
+})
+const hasSlides = computed(() => {
+  return props.modelValue?.components?.some((item: any) => item.ulid != null)
+})
 
 </script>
 
 <template>
-    <div v-if="data.components?.filter((item: any) => item.ulid != null).length > 0" class="w-full">
-        <!-- Button: Screen -->
-        <div class="flex justify-end pr-2">
-            <ScreenView @screenView="(val) => (screenView = val)" />
-        </div>
+  <div v-if="hasSlides" class="w-full">
+    <div class="flex justify-end pr-2">
+      <ScreenView @screenView="val => (screenView.value = val)" />
+    </div>
 
-        <!-- Banner: Square or Landscape -->
-        <div class="flex pr-0.5"
-            :class="[data.type === 'square' ? 'justify-start 2xl:justify-center' : 'justify-center']"
-        >
-            <div v-if="data.type === 'square'"
-                class="w-full min-h-[250px] max-h-[400px]"
-            >
-                <SliderSquare :data="data" :jumpToIndex="jumpToIndex" :view="screenView" />
-            </div>
-            <SliderLandscape v-else :data="data" :jumpToIndex="jumpToIndex" :view="screenView" />
-        </div>
-        
-        <!-- Editor -->
-        <SlidesWorkshop 
-            :bannerType="banner.type" 
-            class="clear-both mt-2 p-2.5" 
-            :data="data" 
-            @jumpToIndex="(UlidOfSlide) => jumpToIndex = UlidOfSlide"
-            :imagesUploadRoute="imagesUploadRoute" 
-            :user="user" 
-            :screenView="screenView" 
-            :galleryRoute="galleryRoute"
+    <!-- Banner preview -->
+    <div
+      class="flex pr-0.5"
+      :class="[props.modelValue.type === 'square'
+        ? 'justify-start 2xl:justify-center'
+        : 'justify-center']"
+    >
+      <div
+        v-if="props.modelValue.type === 'square'"
+        class="w-full min-h-[250px] max-h-[400px]"
+      >
+        <SliderSquare
+          :data="props.modelValue"
+          :jumpToIndex="jumpToIndex"
+          :view="screenView"
         />
+      </div>
+
+      <SliderLandscape
+        v-else
+        :data="props.modelValue"
+        :jumpToIndex="jumpToIndex"
+        :view="screenView"
+      />
     </div>
 
-    <!-- Section: Add slide if there is not exist -->
-    <div v-else>
-        <SlidesWorkshopAddMode :data="data" :imagesUploadRoute="imagesUploadRoute" :galleryRoute="galleryRoute"/>
-    </div>
+    <!-- Editor -->
+    <SlidesWorkshop
+      :bannerType="props.modelValue.type"
+      class="clear-both mt-2 p-2.5"
+      v-model="data"
+      @jumpToIndex="val => (jumpToIndex = val)"
+      :imagesUploadRoute="imagesUploadRoute"
+      :screenView="screenView"
+      :galleryRoute="galleryRoute"
+    />
+  </div>
+
+  <!-- Empty state -->
+  <div v-else>
+    <SlidesWorkshopAddMode
+      :data="props.modelValue"
+      :imagesUploadRoute="imagesUploadRoute"
+      :galleryRoute="galleryRoute"
+    />
+  </div>
 </template>
