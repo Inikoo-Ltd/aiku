402d664ff4128c235b7f0d1858232f92303aa572
Raul Perusquia <raul@inikoo.com>
2026-02-10 17:56:15 +0800

    Refactor image update actions by extracting shared logic into `CanUpdateImages` trait and updating related actions for consistency and reusability.

---
+++ app/Actions/Maintenance/Masters/CopyMasterFamilyImagesFromAnotherMaster.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 10 Feb 2026 17:55:44 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Maintenance\Masters;
+
+use App\Actions\Masters\MasterProductCategory\UpdateMasterProductCategory;
+use App\Actions\Masters\MasterProductCategory\UpdateMasterProductCategoryImages;
+use App\Enums\Catalogue\MasterProductCategory\MasterProductCategoryTypeEnum;
+use App\Models\Masters\MasterProductCategory;
+use App\Models\Masters\MasterShop;
+use Illuminate\Console\Command;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CopyMasterFamilyImagesFromAnotherMaster
+{
+    use asAction;
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(MasterShop $from, MasterShop $to, Command $command): void
+    {
+        foreach ($to->getMasterFamilies() as $masterFamily) {
+            $fromFamily = MasterProductCategory::where('master_shop_id', $from->id)->where('type', MasterProductCategoryTypeEnum::FAMILY)
+                ->whereRaw("lower(code) = lower(?)", [$masterFamily->code])
+                ->first();
+
+            if ($fromFamily && $fromFamily->image && $masterFamily->image_id != $fromFamily->image_id) {
+                $command->info("Updating ".$masterFamily->code.'  '.$masterFamily->image_id.'  -> '.$fromFamily->image_id);
+                UpdateMasterProductCategoryImages::run(
+                    masterProductCategory: $masterFamily,
+                    modelData: [
+                        'image_id' => $fromFamily->image_id
+                    ],
+                    updateDependants: true
+                );
+            }
+        }
+    }
+
+
+    public function getCommandSignature(): string
+    {
+        return 'repair:copy_master_family_images_from_another_master {from} {to}';
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function asCommand(Command $command): int
+    {
+        $fromShop = MasterShop::where('slug', $command->argument('from'))->firstOrFail();
+        $toShop   = MasterShop::where('slug', $command->argument('to'))->firstOrFail();
+
+        $this->handle($fromShop, $toShop, $command);
+
+        return 0;
+    }
+
+
+}
