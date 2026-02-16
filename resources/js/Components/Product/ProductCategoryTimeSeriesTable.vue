665f61b8dd60878297688b10012174d524b7a7bf
stewicca <stewicalf@gmail.com>
2026-02-16 14:52:07 +0800

    INI-1089 Add invoices listing for products
    Add IndexInvoicesInProduct UI action and Inertia page to list invoices
    related to a product (both MasterAsset and Asset). Expose a filter_date
    field from time series resources to enable linking, update the
    ProductCategoryTimeSeriesTable to link invoice counts to the new
    invoices
    route, and register routes for master and shop product invoice views.

--- resources/js/Components/Product/ProductCategoryTimeSeriesTable.vue
+++ resources/js/Components/Product/ProductCategoryTimeSeriesTable.vue
@@ -4,11 +4,12 @@
   - Copyright (c) 2025, Steven Wicca Alfredo
   -->
 
-<script setup lang="ts">
-import Table from "@/Components/Table/Table.vue"
-import { useLocaleStore } from "@/Stores/locale"
-import { usePage } from '@inertiajs/vue3'
+<script setup lang='ts'>
 import { computed } from 'vue'
+import { Link } from '@inertiajs/vue3'
+import { usePage } from '@inertiajs/vue3'
+import Table from '@/Components/Table/Table.vue'
+import { useLocaleStore } from '@/Stores/locale'
 
 const props = defineProps<{
     data: object
@@ -18,33 +19,55 @@ const props = defineProps<{
 const locale = useLocaleStore()
 const page = usePage()
 
-// Get currency from page props (organization or shop currency)
 const currency = computed(() => {
     return page.props?.auth?.organisation?.currency?.code ||
            page.props?.shop?.currency?.code ||
-           'USD'
+           'GBP'
 })
+
+const getInvoicesRoute = (filterDate?: string) => {
+    const currentRouteName = route().current() as string
+    if (!currentRouteName) {
+        return null
+    }
+
+    const invoicesRouteName = currentRouteName.replace(/\.show$/, '.invoices')
+    if (invoicesRouteName === currentRouteName) {
+        return null
+    }
+
+    if (route().has(invoicesRouteName)) {
+        const { tab, ...routeParams } = route().params as Record<string, string>
+
+        const params = filterDate
+            ? { ...routeParams, 'between[date]': filterDate }
+            : routeParams
+
+        return route(invoicesRouteName, params)
+    }
+
+    return null
+}
 </script>
 
 <template>
-    <Table
-        :resource="data"
-        :name="tab"
-        class="mt-5"
-    >
+    <Table :resource="data" :name="tab" class="mt-5">
         <template #cell(period)="{ item }">
             <span class="font-medium text-gray-700">{{ item.period }}</span>
         </template>
 
         <template #cell(sales)="{ item }">
             <div :class="item.sales >= 0 ? 'text-gray-700' : 'text-red-500'">
-                {{ locale.currencyFormat(currency, item.sales) }}
+                {{ locale.currencyFormat(item.currency_code ?? currency, item.sales) }}
             </div>
         </template>
 
         <template #cell(invoices)="{ item }">
             <div class="text-gray-700">
-                {{ locale.number(item.invoices) }}
+                <Link v-if="getInvoicesRoute(item.filter_date)" :href="getInvoicesRoute(item.filter_date)" class="primaryLink">
+                    {{ locale.number(item.invoices) }}
+                </Link>
+                <span v-else>{{ locale.number(item.invoices) }}</span>
             </div>
         </template>
 
