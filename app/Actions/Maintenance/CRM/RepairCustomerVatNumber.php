edc91d31e617cf953cfa76b21a94e59356f8bcaf
louis-perez <louisprzn@gmail.com>
2026-02-23 17:24:49 +0800

    INI-1160: RepairCustomerVatNumber file

---
+++ app/Actions/Maintenance/CRM/RepairCustomerVatNumber.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * author Louis Perez
+ * created on 23-02-2026-17h-14m
+ * github: https://github.com/louis-perez
+ * copyright 2026
+*/
+
+namespace App\Actions\Maintenance\CRM;
+
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Helpers\TaxNumber;
+use Illuminate\Console\Command;
+
+class RepairCustomerVatNumber
+{
+    use WithActionUpdate;
+
+
+    protected function handle(TaxNumber $taxNumber): void
+    {
+        $taxNumber->updateQuietly(['number' => preg_replace('/^GR/', 'EL', $taxNumber->number)]);
+    }
+
+    public string $commandSignature = 'customers:repair_vat';
+
+    public function asCommand(Command $command): void
+    {
+        $query = TaxNumber::where('country_code', 'GR')->get();
+        $total = $query->count();
+
+        if ($total === 0) {
+            $command->info('No invoice transactions to repair.');
+
+            return;
+        }
+
+        $bar = $command->getOutput()->createProgressBar($total);
+        $bar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%');
+
+        foreach ($query as $taxNumber) {
+            $this->handle($taxNumber);
+            $bar?->advance();
+        }
+
+        
+        $bar->finish();
+        $command->newLine();
+        $command->info('VAT repair completed');
+    }
+
+}
