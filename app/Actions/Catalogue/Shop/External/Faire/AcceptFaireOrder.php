c4a0c68fb4572f180817550f6a18cd93fbd1b54a
Raul Perusquia <raul@inikoo.com>
2026-02-23 13:05:24 +0800

    INI-1154 Add support for handling `commission_amount` in orders and transactions
    - Updated `GetFaireOrders` to compute and store commission values.
    - Refactored `DeleteOrder` to use transactions for data integrity during deletion.
    - Added `commission_amount` validation and storage in `orders` and `transactions`.
    - Adjusted Faire product price calculation to account for `unit_multiplier`.

--- app/Actions/Catalogue/Shop/External/Faire/AcceptFaireOrder.php
+++ app/Actions/Catalogue/Shop/External/Faire/AcceptFaireOrder.php
@@ -3,25 +3,25 @@
 namespace App\Actions\Catalogue\Shop\External\Faire;
 
 use App\Actions\OrgAction;
-use App\Models\Catalogue\Shop;
 use App\Models\Ordering\Order;
 use Illuminate\Console\Command;
 
 class AcceptFaireOrder extends OrgAction
 {
-    public $commandSignature = 'faire:order_accepted {shop} {order}';
+    public $commandSignature = 'faire:order_accepted {order}';
 
-    public function handle(Shop $shop, Order $order): array
+    public function handle(Order $order): array
     {
+        $shop = $order->shop;
         $acceptedOrder = $shop->acceptFaireOrder($order->external_id);
-
-        DownloadFairePackingPdfSlip::run($shop, $order);
+        DownloadFairePackingPdfSlip::dispatch($order);
 
         return $acceptedOrder;
     }
 
     public function asCommand(Command $command): void
     {
-        $this->handle(Shop::where('slug', $command->argument('shop'))->first(), Order::where('slug', $command->argument('order'))->first());
+        $order = Order::where('slug', $command->argument('order'))->firstOrFail();
+        $this->handle($order);
     }
 }
