6178ed154deb80314e440e06530f99213625ff6d
aqordeon <aqordivika@yahoo.co.id>
2026-02-25 09:37:16 +0800

    fix: ctrans: support trans with replacement

--- resources/js/Composables/useTrans.ts
+++ resources/js/Composables/useTrans.ts
@@ -5,9 +5,21 @@
  * Copyright: 2026
 */
 import { trans } from 'laravel-vue-i18n'
+import { ReplacementsInterface } from 'laravel-vue-i18n/interfaces/replacements'
 
 // Method: Custom Translation function that falls back to the original text if translation is not found
-export const ctrans = (text?: string) => {  
+export const ctrans = (text: string, replacements: ReplacementsInterface = {}) => {  
     if (!text) return ''
-    return trans(text) || text
+
+    if (trans(text, replacements)) {
+        return trans(text, replacements)
+    } else {
+        Object.keys(replacements).forEach(key => {
+            const value = replacements[key];
+            const regex = new RegExp(`:${key}`, 'g')
+            text = text.replace(regex, value.toString())
+        })
+
+        return text
+    }
 }
