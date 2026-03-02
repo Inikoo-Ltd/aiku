59e414d96ffa8877b994f90f76783ff32ca263e5
Raul Perusquia <raul@inikoo.com>
2026-03-02 14:54:23 +0800

    Update `RepairOrderAmountsAfterMigration` to sync invoice transaction amounts and calculate invoice totals.

--- app/Actions/Maintenance/Ordering/RepairOrderAmountsAfterMigration.php
+++ app/Actions/Maintenance/Ordering/RepairOrderAmountsAfterMigration.php
@@ -8,10 +8,12 @@
 
 namespace App\Actions\Maintenance\Ordering;
 
+use App\Actions\Accounting\Invoice\CalculateInvoiceTotals;
 use App\Actions\Helpers\CurrencyExchange\GetHistoricCurrencyExchange;
 use App\Actions\Ordering\Order\CalculateOrderTotalAmounts;
 use App\Actions\Traits\WithActionUpdate;
 use App\Actions\Traits\WithFixedAddressActions;
+use App\Models\Accounting\InvoiceTransaction;
 use App\Models\Ordering\Order;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -23,38 +25,23 @@ class RepairOrderAmountsAfterMigration
 
     public function handle(Order $order): void
     {
-
         foreach ($order->transactions as $transaction) {
-            if($transaction->model_type != 'Product'){
+            if ($transaction->model_type != 'Product') {
                 continue;
             }
 
-
-
-            $discountFactor=.9;
-
-
-
-
-            /** @var \App\Models\Catalogue\Product $product */
-            $product = $transaction->model;
-            $currentHistoricAsset=$product->historicAsset;
-
-            $gross = $currentHistoricAsset->price * $transaction->quantity_ordered;
-            $net   = $gross * $discountFactor;
-
-            $modelData = [
-                'gross_amount'     => $gross,
-                'net_amount'       => $net,
-                'grp_net_amount' => $net * $transaction['grp_exchange'],
-                'org_net_amount'   => $net * $transaction['org_exchange'],
-            ];
-
-
-
-            $transaction->update($modelData);
+            $invoiceTransaction = InvoiceTransaction::where('transaction_id', $transaction->id)->first();
+            if ($invoiceTransaction) {
+                $invoiceTransaction->update([
+                    'gross_amount'   => $transaction->gross_amount,
+                    'net_amount'     => $transaction->net_amount,
+                    'grp_net_amount' => $transaction->grp_net_amount,
+                    'org_net_amount' => $transaction->org_net_amount,
+                ]);
+            }
         }
-        CalculateOrderTotalAmounts::run($order);
+        $invoice=$order->invoices()->first();
+        CalculateInvoiceTotals::run($invoice);
     }
 
 
@@ -63,7 +50,6 @@ class RepairOrderAmountsAfterMigration
     public function asCommand(Command $command): void
     {
         $order = Order::where('slug', $command->argument('order'))->firstOrFail();
-
 
 
         $this->handle($order);
