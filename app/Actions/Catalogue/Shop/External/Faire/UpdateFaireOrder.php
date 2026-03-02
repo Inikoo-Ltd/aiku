4b4deb9e2fbec1aed229aac8cd6e8fca7358de65
Raul Perusquia <raul@inikoo.com>
2026-03-02 20:36:21 +0800

    Add `UpdateFaireOrder` action to handle orders from Faire marketplace, update product transactions, and recalculate totals.

---
+++ app/Actions/Catalogue/Shop/External/Faire/UpdateFaireOrder.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Actions\Catalogue\Shop\External\Faire;
+
+use App\Actions\Accounting\Invoice\CalculateInvoiceTotals;
+use App\Actions\Catalogue\Product\UpdateProduct;
+use App\Actions\CRM\Customer\StoreCustomer;
+use App\Actions\CRM\Customer\UpdateCustomer;
+use App\Actions\Helpers\Country\UI\IsEuropeanUnion;
+use App\Actions\Helpers\CurrencyExchange\GetCurrencyExchange;
+use App\Actions\Ordering\Order\CalculateOrderTotalAmounts;
+use App\Actions\Ordering\Order\StoreOrder;
+use App\Actions\Ordering\Order\UpdateState\SendOrderToWarehouse;
+use App\Actions\Ordering\Order\UpdateState\SubmitOrder;
+use App\Actions\Ordering\Transaction\StoreTransaction;
+use App\Actions\OrgAction;
+use App\Enums\Catalogue\Shop\ShopEngineEnum;
+use App\Enums\Catalogue\Shop\ShopTypeEnum;
+use App\Enums\Ordering\Order\OrderPayDetailedStatusEnum;
+use App\Enums\Ordering\Order\OrderPayStatusEnum;
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Models\Accounting\InvoiceTransaction;
+use App\Models\Catalogue\Product;
+use App\Models\Catalogue\Shop;
+use App\Models\CRM\Customer;
+use App\Models\Helpers\Country;
+use App\Models\Helpers\Currency;
+use App\Models\Helpers\TaxCategory;
+use App\Models\Ordering\Order;
+use App\Models\Ordering\Transaction;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+
+class UpdateFaireOrder extends OrgAction
+{
+    /**
+     * @throws \Throwable
+     */
+    public function handle(Order $order, Command|null $command = null): void
+    {
+        $shop = $order->shop;
+
+        $orderFaireData = $shop->getFaireOrder($order->external_id);
+
+        foreach ($orderFaireData['items'] as $item) {
+            $transaction = Transaction::where('order_id', $order->id)->where('marketplace_id', $item['id'])->first();
+
+            /** @var Product $product */
+            $product = $transaction->model;
+
+
+            $product = UpdateProduct::run(
+                $product,
+                [
+                    'price' => $product->units * Arr::get($item,'price.amount_minor') / 100,
+                ]
+            );
+
+            $currencyCode = Arr::get($item, 'price.currency');
+            $currency     = Currency::where('code', $currencyCode)->first();
+            $exchange     = GetCurrencyExchange::run($currency, $shop->currency);
+
+
+
+            $netAmount = $exchange * $item['quantity'] * Arr::get($item,'price.amount_minor') / 100;
+
+            $orgExchange = GetCurrencyExchange::run($shop->currency, $shop->organisation->currency);
+            $grpExchange = GetCurrencyExchange::run($shop->currency, $shop->group->currency);
+
+
+            $transaction->update([
+                'historic_asset_id' => $product->current_historic_asset_id,
+                'gross_amount'      => $netAmount,
+                'net_amount'        => $netAmount,
+                'grp_net_amount'    => $netAmount * $grpExchange,
+                'org_net_amount'    => $netAmount * $orgExchange,
+            ]);
+
+            $invoiceTransaction = InvoiceTransaction::where('transaction_id', $transaction->id)->first();
+            if($invoiceTransaction) {
+
+
+
+
+
+                $invoiceTransaction->update([
+                    'historic_asset_id' => $product->current_historic_asset_id,
+                    'gross_amount'      => $netAmount,
+                    'net_amount'        => $netAmount,
+                    'grp_net_amount'    => $netAmount * $grpExchange,
+                    'org_net_amount'    => $netAmount * $orgExchange,
+                ]);
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+        CalculateOrderTotalAmounts::run($order);
+        $invoice=$order->invoices()->first();
+        if($invoice) {
+            CalculateInvoiceTotals::run($invoice);
+        }
+
+    }
+
+
+    public string $commandSignature = 'faire:update_order {order}';
+
+    /**
+     * @throws \Throwable
+     */
+    public function asCommand(Command $command): int
+    {
+        $order = Order::where('slug', $command->argument('order'))->firstOrFail();
+        $this->handle($order, $command);
+
+        return 0;
+    }
+}
