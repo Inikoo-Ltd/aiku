387ce49cb5e409ced730c60521e27079da1188e0
Raul Perusquia <raul@inikoo.com>
2026-03-02 14:13:05 +0800

    Refactor `ChangePickingBaysDeliveryNote` to use database transactions and hydrate picked bay delivery notes.

--- app/Actions/Dispatching/DeliveryNote/UpdateState/ChangePickingBaysDeliveryNote.php
+++ app/Actions/Dispatching/DeliveryNote/UpdateState/ChangePickingBaysDeliveryNote.php
@@ -10,31 +10,39 @@
 namespace App\Actions\Dispatching\DeliveryNote\UpdateState;
 
 use App\Actions\Dispatching\PickedBay\AttachDeliveryNoteToPickedBay;
+use App\Actions\Dispatching\PickedBay\Hydrators\PickedBayHydrateNumberDeliveryNotes;
 use App\Actions\OrgAction;
 use App\Models\Dispatching\DeliveryNote;
 use App\Models\Inventory\PickedBay;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 
 class ChangePickingBaysDeliveryNote extends OrgAction
 {
+    /**
+     * @throws \Throwable
+     */
     public function handle(DeliveryNote $deliveryNote, array $modelData): DeliveryNote
     {
-        // $pickedBay = null;
-        // if (Arr::has($modelData, 'picked_bay') && $modelData['picked_bay']) {
-        //     $pickedBay = PickedBay::find($modelData['picked_bay']);
-        // }
-        // if ($pickedBay) {
-        //     AttachDeliveryNoteToPickedBay::run($pickedBay, $deliveryNote);
-        // }
+        return DB::transaction(function () use ($deliveryNote, $modelData) {
+            foreach ($deliveryNote->pickedBays as $pickedBay) {
+                DB::table('picked_bay_has_delivery_notes')
+                    ->where('delivery_note_id', $deliveryNote->id)->where('picked_bay_id', $pickedBay->id)->delete();
+                PickedBayHydrateNumberDeliveryNotes::run($pickedBay->id);
+            }
 
-        // return UpdateDeliveryNoteStateToPicked::run($deliveryNote);
+            $pickedBay = null;
+            if (Arr::has($modelData, 'picked_bay') && $modelData['picked_bay']) {
+                $pickedBay = PickedBay::find($modelData['picked_bay']);
+            }
+            if ($pickedBay) {
+                AttachDeliveryNoteToPickedBay::run($pickedBay, $deliveryNote);
+            }
 
-        dd('ccccccc', $modelData);
-
-
-
+            return $deliveryNote;
+        });
     }
 
 
@@ -49,6 +57,9 @@ class ChangePickingBaysDeliveryNote extends OrgAction
         ];
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function asController(DeliveryNote $deliveryNote, ActionRequest $request): void
     {
         $this->initialisationFromShop($deliveryNote->shop, $request);
