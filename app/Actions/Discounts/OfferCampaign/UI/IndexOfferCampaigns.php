a48df7f839b8dcb9e2b3121c4614d7b91e8a79b9
stewicca <stewicalf@gmail.com>
2026-03-02 08:36:40 +0800

    Use offer_campaigns.state and remove date filter

--- app/Actions/Discounts/OfferCampaign/UI/IndexOfferCampaigns.php
+++ app/Actions/Discounts/OfferCampaign/UI/IndexOfferCampaigns.php
@@ -62,7 +62,7 @@ class IndexOfferCampaigns extends OrgAction
             'offer_campaigns.code',
             'offer_campaigns.name',
             'offer_campaigns.type',
-            'offer_campaigns.offers_state',
+            'offer_campaigns.state',
             'offer_campaigns.status',
             'shops.slug as shop_slug',
             'shops.name as shop_name',
@@ -114,7 +114,6 @@ class IndexOfferCampaigns extends OrgAction
             $emptyStateData['description'] = __("There are no offer campaigns in this shop");
 
             $table->withGlobalSearch();
-            $table->betweenDates(['date']);
             $table->withEmptyState($emptyStateData);
             $table->withModelOperations($modelOperations);
 
