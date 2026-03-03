23ce3f37f9c82927d4c2207978b1dabe996772c4
Raul Perusquia <raul@inikoo.com>
2026-03-03 23:12:16 +0800

    Refactor `StoreShipmentFromFaire` to handle multiple shipments and improve shipper assignment logic. Fix validation condition in `StoreShipper`.

--- app/Actions/Dispatching/Shipment/StoreShipmentFromFaire.php
+++ app/Actions/Dispatching/Shipment/StoreShipmentFromFaire.php
@@ -25,37 +25,34 @@ class StoreShipmentFromFaire extends OrgAction
 
         $faireOrder = GetSpecificFaireOrder::run($order);
 
-        $faireShipment = Arr::get($faireOrder, 'shipments.0');
 
-        /*if (app()->environment('local')) {
-            $faireShipment = [
-                'id'                 => 'FAIRE-'.Str::random(10),
-                'carrier'            => 'ups-faire',
-                'tracking_code'      => Str::random(10),
-                'shipping_label_url' => 'https://www.google.com'
-            ];
-        }*/
-
-        if (!$faireShipment) {
+        if (count($faireOrder['shipments']) == 0) {
             throw ValidationException::withMessages(['message' => __('No shipment found for this order')]);
         }
 
-        $shipperCode = Arr::get($faireShipment, 'carrier').'-faire';
-        $shipper     = Shipper::where('code', $shipperCode)->where('organisation_id', $deliveryNote->organisation_id)->first();
+        $shipment = null;
+        foreach (Arr::get($faireOrder, 'shipments') as $faireShipment) {
+            $shipperCode = Arr::get($faireShipment, 'carrier').'-faire';
+            $shipper     = Shipper::where('code', $shipperCode)->where('organisation_id', $deliveryNote->organisation_id)->first();
 
-        if (!$shipper) {
-            $shipper = StoreShipper::make()->action($order->organisation, [
-                'code'     => Arr::get($faireShipment, 'carrier').'-faire',
-                'name'     => Arr::get($faireShipment, 'carrier')." (Faire)",
-                'trade_as' => Arr::get($faireShipment, 'carrier')
+
+            if (!$shipper) {
+                $shipper = StoreShipper::make()->action($order->organisation, [
+                    'code'     => Arr::get($faireShipment, 'carrier').'-faire',
+                    'name'     => Arr::get($faireShipment, 'carrier')." (Faire)",
+                    'trade_as' => Arr::get($faireShipment, 'carrier')
+                ]);
+            }
+
+            $shipment = StoreShipment::make()->action($deliveryNote, $shipper, [
+                'reference'          => Arr::get($faireShipment, 'id'),
+                'tracking'           => Arr::get($faireShipment, 'tracking_code'),
+                'combined_label_url' => Arr::get($faireShipment, 'shipping_label_url')
             ]);
         }
 
-        return StoreShipment::make()->action($deliveryNote, $shipper, [
-            'reference'          => Arr::get($faireShipment, 'id'),
-            'tracking'           => Arr::get($faireShipment, 'tracking_code'),
-            'combined_label_url' => Arr::get($faireShipment, 'shipping_label_url')
-        ]);
+
+        return $shipment;
     }
 
     public string $commandSignature = 'delivery_note:faire_shipment {delivery_note}';
