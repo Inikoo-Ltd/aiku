1bb5e1c8f04143efdf8a611ac2bc6116eab68758
itzArtha <manager.arthachan@gmail.com>
2026-03-06 11:27:06 +0800

    feat: make `customer_sales_channel_id` nullable in Allegro users table

---
+++ database/migrations/2026_03_05_075839_change_customer_sales_channel_id_to_allegro_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('allegro_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('customer_sales_channel_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('allegro_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('customer_sales_channel_id')->change();
+        });
+    }
+};
