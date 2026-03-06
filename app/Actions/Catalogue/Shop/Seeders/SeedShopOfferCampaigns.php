3a9e5fcc1fa1b7d305c2fb56070bfa5876955987
Raul Perusquia <raul@inikoo.com>
2026-03-06 12:04:36 +0800

    Update `SeedShopOfferCampaigns` to sync existing offer campaigns with updated status and code

--- app/Actions/Catalogue/Shop/Seeders/SeedShopOfferCampaigns.php
+++ app/Actions/Catalogue/Shop/Seeders/SeedShopOfferCampaigns.php
@@ -19,6 +19,7 @@ use App\Enums\Discounts\Offer\OfferStateEnum;
 use App\Enums\Discounts\OfferCampaign\OfferCampaignTypeEnum;
 use App\Enums\Discounts\OfferAllowance\OfferAllowanceStateEnum;
 use App\Models\Catalogue\Shop;
+use App\Models\Discounts\OfferCampaign;
 use Illuminate\Console\Command;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -41,6 +42,15 @@ class SeedShopOfferCampaigns extends GrpAction
             $code = $case->codes()[$case->value];
 
             if ($shop->offerCampaigns()->where('code', $code)->exists()) {
+                /** @var OfferCampaign $offerCampaign */
+                $offerCampaign = $shop->offerCampaigns()->where('code', $code)->first();
+                $offerCampaign->update(
+                    [
+                        'code'   => $code,
+                        'status' => $case->defaultStatus()
+                    ]
+                );
+
                 continue;
             }
 
