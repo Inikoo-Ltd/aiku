26c734a720ec2f4e5e20e0f34c780b1124eb04ee
Raul Perusquia <raul@inikoo.com>
2026-03-09 15:05:20 +0800

    Refactor `StoreVolGrGift`: improve product handling by restructuring default product logic, enhance validation rules for nested product data, and update redirect routing for offer display.

--- app/Actions/Discounts/Offer/VolGr/StoreVolGrGift.php
+++ app/Actions/Discounts/Offer/VolGr/StoreVolGrGift.php
@@ -19,6 +19,8 @@ use App\Models\Discounts\Offer;
 use App\Models\Discounts\OfferCampaign;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -28,7 +30,6 @@ class StoreVolGrGift extends OrgAction
 
     public function handle(OfferCampaign $offerCampaign, $modelData): Offer
     {
-
         if ($offerId = Arr::get($offerCampaign->data, 'vol_gr_gift_offer_id')) {
             $offer = Offer::find($offerId);
             if ($offer) {
@@ -52,9 +53,17 @@ class StoreVolGrGift extends OrgAction
             ]
         );
 
+        $products = [];
+        foreach ($modelData['products'] as $product) {
+            $products[] = [
+                'id'      => $product['id'],
+                'default' => (bool)Arr::get($product, 'default', false)
+            ];
+        }
+
+
         $allowanceData = [
-            'products' => $modelData['products'],
-            'default'  => Arr::get($modelData, 'default'),
+            'products' => $products,
         ];
 
         data_set(
@@ -82,9 +91,10 @@ class StoreVolGrGift extends OrgAction
     public function rules(): array
     {
         return [
-            'amount'   => ['numeric', 'required'],
-            'products' => ['required', 'array'],
-            'default'  => ['sometimes', 'nullable', 'integer']
+            'amount'             => ['numeric', 'required'],
+            'products'           => ['required', 'array'],
+            'products.*.id'      => ['required', 'integer', Rule::exists('products', 'id')->where('shop_id', $this->shop->id)],
+            'products.*.default' => ['sometimes', 'boolean'],
         ];
     }
 
@@ -95,8 +105,12 @@ class StoreVolGrGift extends OrgAction
         return $this->handle($offerCampaign, $this->validatedData);
     }
 
-    public function htmlResponse(): RedirectResponse
+    public function htmlResponse(Offer $offer): RedirectResponse
     {
-        return back();
+        return Redirect::route('grp.org.shops.show.discounts.offers.show', [
+            'organisation' => $this->organisation,
+            'shop'         => $this->shop,
+            'offer'        => $offer->id
+        ]);
     }
 }
