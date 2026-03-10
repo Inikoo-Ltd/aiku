e2e15eb3bf1bd824fe59b1ae1bcb3aeb76a36442
RifqiTaw <rifqitaufiqurrohman1@gmail.com>
2026-03-10 11:59:32 +0800

    refactor: adjust modalcreatecustomer & route

--- app/Actions/Discounts/OfferCampaign/StoreCustomerOffers.php
+++ app/Actions/Discounts/OfferCampaign/StoreCustomerOffers.php
@@ -3,14 +3,26 @@
 namespace App\Actions\Discounts\OfferCampaign;
 
 use Lorisleiva\Actions\Concerns\AsAction;
+use Illuminate\Http\Request;
 
 class StoreCustomerOffers
 {
     use AsAction;
 
-    public function handle()
+    public function handle(array $data)
     {
-        // nanti logic create offer
-        dd('hello customer');
+        // create offer logic
+    }
+
+    public function asController(Request $request, $organisation, $shop, $offerCampaign)
+    {
+        $data = $request->all();
+
+        dd([
+            'organisation' => $organisation,
+            'shop' => $shop,
+            'offerCampaign' => $offerCampaign,
+            'payload' => $data
+        ]);
     }
 }
