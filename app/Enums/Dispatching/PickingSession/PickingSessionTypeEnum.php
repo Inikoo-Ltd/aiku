0c675a2aebb6ee452057b0e29a88cf617be2ec91
andiferdiawan <andiferdiawan22@gmail.com>
2026-03-17 16:55:28 +0800

    refactor: add enum helper traits and blueprint method
    Add EnumHelperTrait and HasTabs traits to PickingSessionTypeEnum to improve code reuse and maintainability. Implement blueprint method to centralize UI-related data (title and icon) for each enum case.

--- app/Enums/Dispatching/PickingSession/PickingSessionTypeEnum.php
+++ app/Enums/Dispatching/PickingSession/PickingSessionTypeEnum.php
@@ -2,8 +2,14 @@
 
 namespace App\Enums\Dispatching\PickingSession;
 
+use App\Enums\EnumHelperTrait;
+use App\Enums\HasTabs;
+
 enum PickingSessionTypeEnum: string
 {
+    use EnumHelperTrait;
+    use HasTabs;
+
     case DROPSHIPPING = 'dropshipping';
     case FULFILMENT   = 'fulfilment';
 
@@ -12,6 +18,20 @@ enum PickingSessionTypeEnum: string
         return match ($this) {
             self::DROPSHIPPING => __('Dropshipping'),
             self::FULFILMENT   => __('Fulfilment'),
+        };
+    }
+
+    public function blueprint(): array
+    {
+        return match ($this) {
+            self::DROPSHIPPING => [
+                'title' => __('Dropshipping'),
+                'icon'  => 'fal fa-shopping-cart',
+            ],
+            self::FULFILMENT   => [
+                'title' => __('Fulfilment'),
+                'icon'  => 'fal fa-warehouse-alt',
+            ],
         };
     }
 }
