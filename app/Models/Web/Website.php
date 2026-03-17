b464ce7da7803925e0da9c86c7d3759fd94c6e2b
louis-perez <louisprzn@gmail.com>
2026-03-17 16:28:13 +0800

    Rework Department Page (further changes)

--- app/Models/Web/Website.php
+++ app/Models/Web/Website.php
@@ -346,6 +346,16 @@ class Website extends Model implements Auditable, HasMedia
         return $this->belongsTo(Snapshot::class, 'live_products_snapshot_id');
     }
 
+    public function unpublishedFamiliesOverviewSnapshot(): BelongsTo
+    {
+        return $this->belongsTo(Snapshot::class, 'unpublished_families_overview_snapshot_id');
+    }
+
+    public function liveFamiliesOverviewSnapshot(): BelongsTo
+    {
+        return $this->belongsTo(Snapshot::class, 'live_families_overview_snapshot_id');
+    }
+
     public function liveSnapshot(): BelongsTo
     {
         return $this->belongsTo(Snapshot::class, 'live_snapshot_id');
---
+++ database/migrations/2026_03_17_071516_modify_layout_style_column_under_webpage.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('webpages', function (Blueprint $table) {
+            $table->dropColumn(['layout_style']);
+            
+            $table->string('layout_style')->default('main_page');
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('webpages', function (Blueprint $table) {
+            $table->dropColumn(['layout_style']);
+
+            $table->unsignedInteger('layout_style')->default(1);
+        });
+    }
+};
