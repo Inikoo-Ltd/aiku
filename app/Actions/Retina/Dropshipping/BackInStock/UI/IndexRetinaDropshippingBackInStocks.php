63940200f591cb1026d9469b46ff62b73ccfe3af
Raul Perusquia <raul@inikoo.com>
2026-03-22 11:15:22 +0800

    Refactor `IndexRetinaDropshippingBackInStocks` action: remove unused `getBasketTransactions` method and redundant imports.

--- app/Actions/Retina/Dropshipping/BackInStock/UI/IndexRetinaDropshippingBackInStocks.php
+++ app/Actions/Retina/Dropshipping/BackInStock/UI/IndexRetinaDropshippingBackInStocks.php
@@ -13,7 +13,6 @@ use App\Actions\Retina\UI\Dashboard\ShowRetinaDashboard;
 use App\Actions\RetinaAction;
 use App\Http\Resources\CRM\CustomerBackInStockRemindersResource;
 use App\Models\CRM\Customer;
-use App\Models\Ordering\Order;
 use Closure;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Inertia\Inertia;
@@ -32,83 +31,44 @@ class IndexRetinaDropshippingBackInStocks extends RetinaAction
         return IndexCustomerBackInStockReminders::make()->tableStructure($customer, $prefix);
     }
 
-    private function getBasketTransactions(Customer $customer): array
-    {
-        if (!$customer->current_order_in_basket_id) {
-            return [];
-        }
-
-        $order = Order::find($customer->current_order_in_basket_id);
-        if (!$order) {
-            return [];
-        }
-
-        // Get transactions the same way as ShowRetinaEcomBasket
-        $transactions = $order->transactions()
-            ->whereIn('model_type', ['Product', 'Service'])
-            ->with(['asset.product'])
-            ->get();
-
-        $basketTransactions = [];
-        /** @var \App\Models\Ordering\Transaction $transaction */
-        foreach ($transactions as $transaction) {
-            // Use product ID as a key to match with favorites data (products.id)
-            $productId = $transaction->asset?->product?->id;
-
-            if ($productId) {
-                $basketTransactions[$productId] = [
-                    'id' => $transaction->id,
-                    'quantity_ordered' => (int) $transaction->quantity_ordered,
-                    'asset_id' => $transaction->asset_id,
-                    'product_id' => $productId,
-                ];
-            }
-        }
-
-        return $basketTransactions;
-    }
-
-
     public function asController(ActionRequest $request): LengthAwarePaginator
     {
         $this->initialisation($request);
+
         return $this->handle($this->customer);
     }
 
 
-
     public function htmlResponse(LengthAwarePaginator $productFavorites, ActionRequest $request): Response
     {
-        $basketTransactions = $this->getBasketTransactions($this->customer);
-
         return Inertia::render(
             'Dropshipping/RetinaDropshippingBackInStocks',
             [
-                'breadcrumbs' => $this->getBreadcrumbs(),
-                'title'       => __('Back In Stocks'),
-                'pageHead' => [
-                    'title'         => __('Back In Stocks'),
-                    'icon'          => 'fal fa-heart',
+                'breadcrumbs'               => $this->getBreadcrumbs(),
+                'title'                     => __('Back In Stocks'),
+                'pageHead'                  => [
+                    'title' => __('Back In Stocks'),
+                    'icon'  => 'fal fa-heart',
                 ],
-                'data'     => CustomerBackInStockRemindersResource::collection($productFavorites),
-                'basketTransactions' => $basketTransactions,
-                'attachToFavouriteRoute' => [
+                'data'                      => CustomerBackInStockRemindersResource::collection($productFavorites),
+                'basketTransactions'        => [],
+                'attachToFavouriteRoute'    => [
                     'name' => 'retina.models.product.favourite'
                 ],
-                'detachToFavouriteRoute' => [
+                'detachToFavouriteRoute'    => [
                     'name' => 'retina.models.product.unfavourite'
                 ],
-                'attachBackInStockRoute' => [
+                'attachBackInStockRoute'    => [
                     'name' => 'retina.models.remind_back_in_stock.store'
                 ],
-                'detachBackInStockRoute' => [
+                'detachBackInStockRoute'    => [
                     'name' => 'retina.models.remind_back_in_stock.delete'
                 ],
-                'addToBasketRoute' => [
+                'addToBasketRoute'          => [
                     'name' => 'retina.models.product.add-to-basket'
                 ],
                 'updateBasketQuantityRoute' => [
-                    'name' => 'retina.models.transaction.update',
+                    'name'   => 'retina.models.transaction.update',
                     'method' => 'patch'
                 ]
             ]
@@ -124,8 +84,6 @@ class IndexRetinaDropshippingBackInStocks extends RetinaAction
                 ]
             );
     }
-
-
 
 
 }
