1646b4023af141a16b9faff786ce9406b6c98ec4
louis-perez <louisprzn@gmail.com>
2026-03-25 15:53:22 +0800

    Create CheckOrderDetailFromShopify to read Order Details (Would help debugging easier)

---
+++ app/Actions/Dropshipping/Shopify/Fulfilment/CheckOrderDetailFromShopify.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * author Louis Perez
+ * created on 25-03-2026-14h-24m
+ * github: https://github.com/louis-perez
+ * copyright 2026
+*/
+
+namespace App\Actions\Dropshipping\Shopify\Fulfilment;
+
+use App\Actions\Dropshipping\Shopify\WithShopifyApi;
+use App\Actions\OrgAction;
+use App\Models\Dropshipping\ShopifyUser;
+use App\Models\Ordering\Order;
+use Illuminate\Console\Command;
+use Illuminate\Validation\ValidationException;
+
+class CheckOrderDetailFromShopify extends OrgAction
+{
+    use WithShopifyApi;
+
+    public function handle(Order $order): void
+    {
+        $orderId = data_get($order->data, 'shopify_data.order.id', '');
+
+        /** @var ShopifyUser $shopifyUser */
+        $shopifyUser = $order->customerSalesChannel->user;
+
+        if (!$shopifyUser || !$orderId) {
+            return;
+        }
+
+        $mutation = <<<'MUTATION'
+            query getOrder($id: ID!) {
+                order(id: $id) {
+                    id
+                    name
+                    createdAt
+                    fulfillable
+                    displayFulfillmentStatus
+                    lineItems(first: 100) {
+                        edges {
+                            node {
+                                id
+                                name
+                                sku
+                                quantity
+                                nonFulfillableQuantity
+                                variant {
+                                    id
+                                }
+                                product {
+                                    id
+                                }
+                            }
+                        }
+                    }
+                    fulfillments(first: 10) {
+                        name
+                        status
+                        createdAt
+                        fulfillmentLineItems(first: 10) {
+                            edges {
+                                node {
+                                    id
+                                    lineItem {
+                                        name
+                                        sku
+                                        currentQuantity
+                                        unfulfilledQuantity
+                                        variant {
+                                            id
+                                        }
+                                        product {
+                                            id
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    nonFulfillableLineItems(first: 100) {
+                        edges {
+                            node {
+                                id
+                                sku
+                                name
+                                currentQuantity
+                                nonFulfillableQuantity
+                                variant {
+                                    id
+                                }
+                                product {
+                                    id
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        MUTATION;
+
+        $variables = [
+            'id' => $orderId
+        ];
+
+        try {
+            list($status, $response) = $this->doPost($shopifyUser, $mutation, $variables);
+        } catch (\Exception $e) {
+            throw ValidationException::withMessages(['message' => $e->getMessage()]);
+        }
+
+        $orderResponse = data_get($response['body']->toArray(), 'data.order', []);
+        // Keep it this way. Command only used on local anyway
+        dd($orderResponse);
+    }
+
+    
+    public string $commandSignature = 'shopify:check_order_details {order}';
+
+    public function asCommand(Command $command): void
+    {
+        $order = Order::where('slug', $command->argument('order'))->first();
+
+        if ($order) {
+            $this->handle($order);
+        }
+    }
+}
