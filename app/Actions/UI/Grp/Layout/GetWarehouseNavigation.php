856dd8f0dd40fbb0876541ae9b55f62195fb7734
Raul Perusquia <raul@inikoo.com>
2026-03-26 14:11:44 +0800

    Add `skus_families` navigation to warehouse, remove redundant logic from organisation navigation

--- app/Actions/UI/Grp/Layout/GetWarehouseNavigation.php
+++ app/Actions/UI/Grp/Layout/GetWarehouseNavigation.php
@@ -27,6 +27,22 @@ class GetWarehouseNavigation
             "stocks.$warehouse->id.view",
             "fulfilment.$warehouse->id.view",
         ])) {
+
+            $navigation['skus_families'] = [
+                'label'   => __('SKUs Families'),
+                'tooltip' => __('SKUs Families'),
+                'icon'    => ['fal', 'fa-boxes-alt'],
+                'route'   => [
+                    'name'       => 'grp.org.warehouses.show.inventory.org_stock_families.index',
+                    'parameters' => [
+                        'organisation' => $warehouse->organisation->slug,
+                        'warehouse'    => $warehouse->slug,
+                        'tab'          => 'sales',
+                    ],
+                ],
+                'topMenu' => [],
+            ];
+
             $navigation["inventory"] = [
                 "root"    => "grp.org.warehouses.show.inventory.",
                 "label"   => __("Inventory"),
@@ -101,7 +117,15 @@ class GetWarehouseNavigation
                     ],
                 ],
             ];
+
+
+
+
+
         }
+
+
+
 
         $navigation = $this->getLocationsNavs($user, $warehouse, $navigation);
 
