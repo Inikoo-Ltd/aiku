76ce7bbe2e62d03b09137105819c316292d0d3bb
ekayudinata <ekayudinatha@gmail.com>
2026-03-30 10:29:53 +0800

    INI-1360 feat(comms): refactor PriceChangeNotification and improve email handling

---
+++ app/Actions/Comms/Outbox/PriceChangeNotification/ProcessPriceChangeRecipients.php
@@ -0,0 +1,180 @@
+<?php
+
+/*
+ * Author: eka yudinata (https://github.com/ekayudinata)
+ * Created: Tuesday, 25 Mar 2026 09:43:00 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2026, eka yudinata
+ */
+
+namespace App\Actions\Comms\Outbox\PriceChangeNotification;
+
+use App\Actions\Comms\DispatchedEmail\StoreDispatchedEmail;
+use App\Actions\Comms\EmailBulkRun\StoreEmailBulkRunRecipient;
+use App\Actions\Comms\EmailBulkRun\UpdateEmailBulkRunRecipientStoredAt;
+use App\Actions\Comms\EmailDeliveryChannel\SendEmailDeliveryChannel;
+use App\Actions\Comms\EmailDeliveryChannel\StoreEmailDeliveryChannel;
+use App\Actions\Comms\EmailDeliveryChannel\UpdateEmailDeliveryChannel;
+use App\Models\Catalogue\Product;
+use App\Models\Comms\EmailBulkRun;
+use App\Models\CRM\Customer;
+use App\Models\Comms\Outbox;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ProcessPriceChangeRecipients
+{
+    use AsAction;
+
+    public string $jobQueue = 'ses';
+
+    public function handle(?int $emailBulkRunId, array $customers): void
+    {
+        if (!$emailBulkRunId) {
+            return;
+        }
+
+        $emailBulkRun = EmailBulkRun::find($emailBulkRunId);
+
+        if (!$emailBulkRun) {
+            return;
+        }
+
+        $outbox = Outbox::find($emailBulkRun->outbox_id);
+
+        if (!$outbox) {
+            return;
+        }
+
+        $emailDeliveryChannel = StoreEmailDeliveryChannel::run($emailBulkRun);
+
+        foreach ($customers as $customer) {
+            $customerModel = Customer::find($customer['id']);
+            if (!$customerModel) {
+                continue;
+            }
+
+            $dispatchedEmail = StoreDispatchedEmail::run(
+                $emailBulkRun,
+                $customerModel,
+                [
+                    'outbox_id'     => $outbox->id,
+                    'email_address' => $customer['email'],
+                    'data->additional_data' => [
+                        'products' => $this->generateProductLinks($customer['product_ids'])
+                    ]
+                ]
+            );
+
+            StoreEmailBulkRunRecipient::run(
+                $emailBulkRun,
+                [
+                    'dispatched_email_id' => $dispatchedEmail->id,
+                    'recipient_type'      => class_basename($customerModel),
+                    'recipient_id'        => $customerModel->id,
+                    'channel'             => $emailDeliveryChannel->id,
+                    'recipient_name'      => $customerModel->name,
+                ]
+            );
+        }
+
+        // After processing the chunk, update and dispatch the delivery channel
+        UpdateEmailDeliveryChannel::run(
+            $emailDeliveryChannel,
+            [
+                'number_emails' => $emailBulkRun->recipients()->where('channel', $emailDeliveryChannel->id)->count()
+            ]
+        );
+        UpdateEmailBulkRunRecipientStoredAt::run($emailBulkRun);
+
+        SendEmailDeliveryChannel::dispatch($emailDeliveryChannel);
+    }
+
+    public function generateProductLinks(string $productIds): string
+    {
+        try {
+            $productIds = explode(',', $productIds);
+        } catch (\Throwable $th) {
+            \Log::error('Error parsing product IDs: ' . $th->getMessage());
+            return '';
+        }
+
+        $date = Carbon::now()->format('d M y');
+
+        $html = '';
+
+        $html .= '<table width="100%" cellpadding="8" cellspacing="0"
+        style="font-family: Helvetica, Arial, sans-serif;
+               font-size: 14px;
+               border-collapse: collapse;">';
+
+
+        $html .= '
+        <tr style="border-bottom:1px solid #e5e7eb;">
+            <th align="left" style="color:#555;">' . __('Product') . '</th>
+            <th align="center" style="color:#555;">' . __('New Price') . ' (' . $date . ')</th>
+        </tr>';
+
+        foreach ($productIds as $productId) {
+            $dataProduct = Product::find($productId);
+
+            if (!$dataProduct) {
+                continue;
+            }
+
+            $productImage = Arr::get(
+                $dataProduct->imageSources(200, 200),
+                'png'
+            );
+
+            $newPrice = $dataProduct->price ?? 0;
+
+            if ($dataProduct->webpage) {
+                $url  = $dataProduct->webpage->getCanonicalUrl();
+                $name = $dataProduct->name;
+
+                $html .= '
+                <tr style="border-bottom:1px solid #f1f5f9;">
+                    <td style="vertical-align:middle;">
+                        <table cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="padding-right:12px;">';
+
+                if ($productImage) {
+                    $html .= '
+                    <img src="' . $productImage . '"
+                         width="60"
+                         height="60"
+                         style="display:block;
+                                border-radius:6px;
+                                object-fit:cover;" />';
+                }
+
+                $html .= '
+                                </td>
+                                <td style="vertical-align:middle;">
+                                    <a ses:no-track href="' . $url . '"
+                                       style="color:#2563eb;
+                                        text-decoration:underline;
+                                        font-weight:600;">'
+                    . $name .
+                    '</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+
+                    <td align="center"
+                        style="font-weight:600;
+                               color:#16a34a;">'
+                    . number_format($newPrice, 2) .
+                    '</td>
+                </tr>';
+            }
+        }
+
+        $html .= '</table>';
+
+        return $html;
+    }
+}
