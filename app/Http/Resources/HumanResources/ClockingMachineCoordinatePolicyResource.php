2b0cde7d0ec30c124bbcdb042e494d8b6a69dc28
andiferdiawan <andiferdiawan22@gmail.com>
2026-04-01 16:20:49 +0800

    feat: add number and scope_name fields to clocking machine policy resource

--- app/Http/Resources/HumanResources/ClockingMachineCoordinatePolicyResource.php
+++ app/Http/Resources/HumanResources/ClockingMachineCoordinatePolicyResource.php
@@ -10,9 +10,11 @@ class ClockingMachineCoordinatePolicyResource extends JsonResource
     {
         return [
             'id'                => $this->id,
+            'number'            => $this->number,
             'organisation_id'   => $this->organisation_id,
             'scope_type'        => $this->scope_type,
             'scope_id'          => $this->scope_id,
+            'scope_name'        => $this->scope_name,
             'clocking_machine_id' => $this->clocking_machine_id,
             'mode'              => $this->mode?->value ?? $this->mode,
             'is_active'         => (bool) $this->is_active,
