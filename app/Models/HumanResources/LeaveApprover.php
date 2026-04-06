d659c7f030f19873f043af69cfb9aeb42cea5ec1
Oggie Sutrisna <sutrisna.oggie@gmail.com>
2026-04-06 09:53:58 +0800

    fix: remove `softDeletes` for LeaveApprover.php error

--- app/Models/HumanResources/LeaveApprover.php
+++ app/Models/HumanResources/LeaveApprover.php
@@ -7,7 +7,6 @@ use App\Models\SysAdmin\User;
 use App\Models\Traits\InOrganisation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
@@ -19,7 +18,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property int $organisation_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read \App\Models\SysAdmin\Group|null $group
  * @property-read Organisation $organisation
  * @property-read User $user
@@ -28,16 +26,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover bySequence(int $sequence)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover withTrashed(bool $withTrashed = true)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|LeaveApprover withoutTrashed()
  * @mixin \Eloquent
  */
 class LeaveApprover extends Model
 {
     use InOrganisation;
-    use SoftDeletes;
 
     protected $casts = [
         'is_active' => 'boolean',
