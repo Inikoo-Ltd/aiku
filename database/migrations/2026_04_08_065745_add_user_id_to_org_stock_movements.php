e9be59956d753e6149110bc0503fc769f1bf5573
Raul Perusquia <raul@inikoo.com>
2026-04-08 15:37:33 +0800

    Update: modify `user_id` in `org_stock_movements` to unsigned small integer, add index, and set null on delete

--- database/migrations/2026_04_08_065745_add_user_id_to_org_stock_movements.php
+++ database/migrations/2026_04_08_065745_add_user_id_to_org_stock_movements.php
@@ -1,30 +1,26 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 08 Apr 2026 15:37:23 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    public function up(): void
     {
         Schema::table('org_stock_movements', function (Blueprint $table) {
-            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedSmallInteger('user_id')->index()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
-            $table->index('user_id');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('org_stock_movements', function (Blueprint $table) {
             $table->dropColumn(['user_id']);
