d6ce3964d9b3548d24b8a862ae1fa989e2cf9f5b
YudhistiraA <aryarajasa0@gmail.com>
2026-04-10 10:20:10 +0800

    INI- 1406 add empty state component for template selection in HeaderWorkshop

--- resources/js/Pages/Grp/Org/Web/Workshop/Header/HeaderWorkshop.vue
+++ resources/js/Pages/Grp/Org/Web/Workshop/Header/HeaderWorkshop.vue
@@ -306,6 +306,14 @@ watch(currentView, (newValue) => {
                             :blueprint="getBlueprint(usedTemplates[selectedTab.key].code)"
                             :uploadImageRoute="uploadImageRoute" :panelOpen="panelActive" 
                             @update:model-value="e => usedTemplates[selectedTab.key].data.fieldValue = e"/>
+
+                        <div v-else>
+                            <EmptyState :data="{ description: 'No template selected', title: 'Pick Template' }">
+                                <template #button-empty-state>
+                                    <Button label="template" icon="fas fa-th-large" @click="isModalOpen = true" />
+                                </template>
+                            </EmptyState>
+                        </div>
                     </div>
                 </div>
             </div>
