ca65528c195137bbb4dd949a7cdab6276d9db1d3
Raul Perusquia <raul@inikoo.com>
2026-04-14 10:57:29 +0800

    Add `UndoSetAsWaitingWarehouse` action and route for reverting delivery note item state adjustments

---
+++ app/Actions/Dispatching/Picking/UndoSetAsWaitingWarehouse.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 09 Apr 2026 10:04:34 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Dispatching\Picking;
+
+use App\Actions\Dispatching\DeliveryNote\Hydrators\DeliveryNoteHydrateWaitingItems;
+use App\Actions\Dispatching\DeliveryNoteItem\CalculateDeliveryNoteItemTotalPicked;
+use App\Actions\OrgAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Dispatching\DeliveryNoteItem\DeliveryNoteItemStateEnum;
+use App\Models\Dispatching\DeliveryNoteItem;
+use App\Models\SysAdmin\User;
+use Illuminate\Support\Arr;
+use Lorisleiva\Actions\ActionRequest;
+
+class UndoSetAsWaitingWarehouse extends OrgAction
+{
+    use WithActionUpdate;
+
+    private DeliveryNoteItem $deliveryNoteItem;
+    protected ?User $user = null;
+
+    public function handle(DeliveryNoteItem $deliveryNoteItem, array $modelData): DeliveryNoteItem
+    {
+        $dataToUpdate = [
+            'state'                      => DeliveryNoteItemStateEnum::HANDLING_BLOCKED,
+            'quantity_waiting_warehouse' => $modelData['quantity'],
+            'has_waiting_warehouse'      => true,
+            'is_handled'                 => true,
+        ];
+        if (Arr::has($modelData, 'note')) {
+            $dataToUpdate['notes'] = $modelData['note'];
+        }
+        $deliveryNoteItem->update(
+            $dataToUpdate
+        );
+        DeliveryNoteHydrateWaitingItems::run($deliveryNoteItem->delivery_note_id);
+        CalculateDeliveryNoteItemTotalPicked::make()->action($deliveryNoteItem);
+
+        return $deliveryNoteItem;
+    }
+
+
+
+
+    public function asController(DeliveryNoteItem $deliveryNoteItem, ActionRequest $request): DeliveryNoteItem
+    {
+        $this->initialisationFromShop($deliveryNoteItem->shop, $request);
+
+        return $this->handle($deliveryNoteItem, $this->validatedData);
+    }
+
+
+
+
+}
