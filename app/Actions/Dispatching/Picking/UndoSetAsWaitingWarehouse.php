e4cf4d7e293f2d16acaddbcea4499affefcc4370
Raul Perusquia <raul@inikoo.com>
2026-04-14 11:00:14 +0800

    Refactor: simplify `UndoSetAsWaitingWarehouse` action by removing unused parameters, refining update logic, and ensuring consistent state handling

--- app/Actions/Dispatching/Picking/UndoSetAsWaitingWarehouse.php
+++ app/Actions/Dispatching/Picking/UndoSetAsWaitingWarehouse.php
@@ -1,8 +1,7 @@
 <?php
-
 /*
  * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Thu, 09 Apr 2026 10:04:34 Malaysia Time, Kuala Lumpur, Malaysia
+ * Created: Tue, 14 Apr 2026 11:00:07 Malaysia Time, Kuala Lumpur, Malaysia
  * Copyright (c) 2026, Raul A Perusquia Flores
  */
 
@@ -15,7 +14,6 @@ use App\Actions\Traits\WithActionUpdate;
 use App\Enums\Dispatching\DeliveryNoteItem\DeliveryNoteItemStateEnum;
 use App\Models\Dispatching\DeliveryNoteItem;
 use App\Models\SysAdmin\User;
-use Illuminate\Support\Arr;
 use Lorisleiva\Actions\ActionRequest;
 
 class UndoSetAsWaitingWarehouse extends OrgAction
@@ -25,17 +23,20 @@ class UndoSetAsWaitingWarehouse extends OrgAction
     private DeliveryNoteItem $deliveryNoteItem;
     protected ?User $user = null;
 
-    public function handle(DeliveryNoteItem $deliveryNoteItem, array $modelData): DeliveryNoteItem
+    public function handle(DeliveryNoteItem $deliveryNoteItem): DeliveryNoteItem
     {
-        $dataToUpdate = [
-            'state'                      => DeliveryNoteItemStateEnum::HANDLING_BLOCKED,
-            'quantity_waiting_warehouse' => $modelData['quantity'],
-            'has_waiting_warehouse'      => true,
-            'is_handled'                 => true,
-        ];
-        if (Arr::has($modelData, 'note')) {
-            $dataToUpdate['notes'] = $modelData['note'];
+
+        if($deliveryNoteItem->quantity_waiting_warehouse==0){
+            return $deliveryNoteItem;
         }
+
+        $dataToUpdate = [
+            'state'                      => DeliveryNoteItemStateEnum::HANDLING,
+            'quantity_waiting_warehouse' => 0,
+            'has_waiting_warehouse'      => false,
+            'is_handled'                 => false,
+        ];
+
         $deliveryNoteItem->update(
             $dataToUpdate
         );
@@ -52,7 +53,7 @@ class UndoSetAsWaitingWarehouse extends OrgAction
     {
         $this->initialisationFromShop($deliveryNoteItem->shop, $request);
 
-        return $this->handle($deliveryNoteItem, $this->validatedData);
+        return $this->handle($deliveryNoteItem);
     }
 
 
