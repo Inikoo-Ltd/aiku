59b124f37c4a7ea818c395868be9ccca59b38bbb
aqordeon <aqordivika@yahoo.co.id>
2026-04-17 14:42:19 +0800

    feat: Retina Basket: show Missed Offer (local)

--- resources/js/Pages/Retina/Ecom/RetinaEcomBasket.vue
+++ resources/js/Pages/Retina/Ecom/RetinaEcomBasket.vue
@@ -119,6 +119,7 @@ const props = defineProps<{
         status: boolean
         meter: number[]
     }
+    missed_offers: Record<string, { label: string }>
 }>()
 
 
@@ -575,6 +576,7 @@ const onChangeInsurance = async (val: boolean) => {
         :currency_code="order?.currency_code"
         :isInBasket="true"
         :updateRoute="routes.update_route"
+        :missed_offers
     />
     
     <template v-if="order">
