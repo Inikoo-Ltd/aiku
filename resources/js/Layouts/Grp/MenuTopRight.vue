1613cd5e7b7962d28ee5f071e8402b14141f38c3
stewicca <stewicalf@gmail.com>
2026-04-17 13:31:42 +0800

    Adjust waiting badge logic and remove menu scroll

--- resources/js/Layouts/Grp/MenuTopRight.vue
+++ resources/js/Layouts/Grp/MenuTopRight.vue
@@ -131,7 +131,7 @@ const isUserMac = navigator.platform.includes('Mac')  // To check the user's Ope
                             </div>
                         </template>
                         <template #content="{ close }">
-                            <div class="max-h-96 overflow-y-auto">
+                            <div>
                                 <p class="text-xs font-semibold text-gray-500 uppercase tracking-wide mb-2">
                                     {{ trans('Dispatching Waiting Items') }}
                                 </p>
