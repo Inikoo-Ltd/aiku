6d873de8887b0a01e30d1ae6e6e30aaba4838310
Raul Perusquia <raul@inikoo.com>
2026-04-17 12:06:56 +0800

    Refactor: remove user request logging UI, routes, and associated tests

--- database/migrations/2026_04_17_021103_add_index_page_and_follow_link_on_webpage.php
+++ database/migrations/2026_04_17_021103_add_index_page_and_follow_link_on_webpage.php
@@ -1,7 +1,11 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 17 Apr 2026 12:06:30 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
