20421bb1271f3cbcc8b030cb614fa485f1e4dd8f
Raul Perusquia <raul@inikoo.com>
2026-04-20 15:49:09 +0545

    Refactor: add type hinting for `Organisation` in `getOrganisation` method to improve code clarity

--- app/Models/SysAdmin/User.php
+++ app/Models/SysAdmin/User.php
@@ -335,7 +335,9 @@ class User extends Authenticatable implements HasMedia, Auditable
 
     public function getOrganisation(): ?Organisation
     {
-        return $this->getOrganisations()->first();
+        /** @var Organisation $organisation */
+        $organisation= $this->getOrganisations()->first();
+        return $organisation;
     }
 
 
