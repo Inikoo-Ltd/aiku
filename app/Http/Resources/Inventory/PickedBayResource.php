7aa4a52a202efb9fadb620532de8fbcb15cb785f
louis-perez <louisprzn@gmail.com>
2026-04-23 16:29:38 +0800

    INI-1471: Enable Delete Picked Bays & Trolley Added support to view list of delivery notes used in picked bays

--- app/Http/Resources/Inventory/PickedBayResource.php
+++ app/Http/Resources/Inventory/PickedBayResource.php
@@ -24,10 +24,39 @@ class PickedBayResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        /** @var PickedBay $product */
+        $pickedBay = $this->resource;
+
+        // Don't worry, won't run if relationship is not eager loaded
+        $deliveryNotes = [];
+        if ($pickedBay->relationLoaded('deliveryNotes')) {
+            $deliveryNotes = $pickedBay->deliveryNotes->map(function ($deliveryNote) {
+                $weight = $deliveryNote->estimated_weight ?? 0;
+                if ($weight < 1000) {
+                    $weight = $weight.' g';
+                } elseif ($weight < 10000) {
+                    $weight = round($weight / 1000, 1).' Kg';
+                } else {
+                    $weight = round($weight / 1000).' Kg';
+                }
+                return [
+                    'id'            => $deliveryNote->id,
+                    'slug'          => $deliveryNote->slug,
+                    'date'          => $deliveryNote->date,
+                    'weight'        => $weight,
+                    'number_items'  => $deliveryNote->number_items,
+                    'reference'     => $deliveryNote->reference,
+                    'state'         => $deliveryNote->state->value,
+                    'state_icon'    => $deliveryNote->state->stateIcon()[$deliveryNote->state->value],
+                ];
+            })->toArray();
+        }
+
         return [
             'id'                       => $this->id,
             'code'                     => $this->code,
             'slug'                     => $this->slug,
+            'delivery_notes'           => $deliveryNotes,
             'current_delivery_note_id' => $this->current_delivery_note_id,
         ];
     }
--- resources/js/Components/Tables/Grp/Org/Inventory/TablePickedBays.vue
+++ resources/js/Components/Tables/Grp/Org/Inventory/TablePickedBays.vue
@@ -5,10 +5,19 @@
   -->
 
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3'
+import { Link, router } from '@inertiajs/vue3'
 import Table from '@/Components/Table/Table.vue'
 import { useLocaleStore } from '@/Stores/locale'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { trans } from 'laravel-vue-i18n';
+import { faExpandAlt, faMonument, faYinYang } from '@fal';
+import Modal from '@/Components/Utils/Modal.vue';
+import { ref } from 'vue';
+import Icon from "@/Components/Icon.vue"
+import { useFormatTime } from '@/Composables/useFormatTime';
+import Button from "@/Components/Elements/Buttons/Button.vue"
+import { notify } from '@kyvg/vue3-notification';
+
 
 const props = defineProps<{
 	data: {}
@@ -16,6 +25,10 @@ const props = defineProps<{
 }>()
 
 const locale = useLocaleStore()
+
+const isOpenPickedBayModal = ref(false);
+const selectedDeliveryNotes = ref([])
+const isLoadingDelete = ref(null);
 
 function bayRoute(bay: bay) {
 	switch (route().current()) {
@@ -29,6 +42,65 @@ function bayRoute(bay: bay) {
 				[bay.organisation_slug, bay.slug])
 	}
 }
+
+const deliveryNoteRoute = (bay: {}) => {
+    switch (route().current()) {
+        case 'grp.org.warehouses.show.dispatching.picked_bays.show':
+        case 'grp.org.warehouses.show.dispatching.picked_bays.index':
+            return route(
+                'grp.org.warehouses.show.dispatching.delivery_notes.show',
+                {
+                    organisation: route().params['organisation'],
+                    warehouse: route().params['warehouse'],
+                    deliveryNote: bay.current_delivery_note.slug
+                })
+        default:
+            return '#'
+    }
+}
+
+const openModalPickBay = (deliveryNotes: []) => {
+	isOpenPickedBayModal.value = true;
+	selectedDeliveryNotes.value = deliveryNotes;
+}
+
+const resetModalPickBay = () => {
+	isOpenPickedBayModal.value = false;
+	selectedDeliveryNotes.value = [];
+}
+
+const routeDeliveryNote = (deliveryNote: any) => {
+	return route("grp.helpers.redirect_delivery_notes", [deliveryNote.id])
+}
+
+const deletePickBay = (pickedBay: any) => {
+	router.delete(route('grp.models.picked_bays.delete', {
+					pickedBay: pickedBay.id
+	}), {
+		onStart: () => {
+			isLoadingDelete.value = pickedBay.id;
+		},
+		onSuccess: () => {
+			notify({
+				title: "Success",
+				text: trans("Picked Bay has been successfully deleted"),
+				type: "success",
+			})
+		}, 
+		onError: (err) => {
+			const errMsg = err.pickedBay;
+			notify({
+				title: "Failed",
+				text: errMsg ??  trans("Failed to delete Picked Bay"),
+				type: "error",
+			})
+		}, 
+		onFinish: () => {
+			isLoadingDelete.value = null;
+		}
+	})
+}
+
 </script>
 
 <template>
@@ -39,5 +111,78 @@ function bayRoute(bay: bay) {
 				{{ bay['code'] }}
 			</Link>
 		</template>
+
+		<template #cell(delivery_notes)="{ item: bay }">
+			<span v-if="bay.delivery_notes.length" class="border rounded-md border-yellow-400 px-2 py-1 text-yellow-600 bg-yellow-200 cursor-pointer pulse-animate hover:opacity-[80%] ease-in-out transition" @click="openModalPickBay(bay.delivery_notes)">
+				{{ trans("Used in :_countDeliveryNotes Delivery Notes", {_countDeliveryNotes: bay.delivery_notes.length}) }}
+				<FontAwesomeIcon :icon="faExpandAlt" class="ml-2"/>
+			</span>
+            <span v-else class="italic text-xs opacity-60">
+                {{ trans('No current delivery note') }}
+            </span>
+		</template>
+
+		<template #cell(actions)="{item: bay}">
+			<Button
+				v-if="!bay.delivery_notes.length"
+				v-tooltip="trans('Delete picking bay')"
+				@click="deletePickBay(bay)"
+				:type="'negative'"
+				icon="fal fa-skull"
+				:size="'xs'"
+				:loading="isLoadingDelete == bay.id"
+			/>
+		</template>
 	</Table>
+
+	<Modal :isOpen="isOpenPickedBayModal" @onClose="resetModalPickBay" :width="'xl:w-[700px] w-4/5 px-0 py-2'">
+		<div class="px-4 pb-2 pt-2">
+			<FontAwesomeIcon :icon="faMonument" class="mr-2" /> 
+			<span class="font-medium">
+				{{ trans("Picked Bays - Delivery Notes") }}
+			</span>
+		</div>
+		<hr class="border-gray-500">
+		<div class="pt-2 px-4 pb-2">
+			<div class="grid grid-cols-12 border-b border-gray-300 pt-1 pb-2">
+				<span class="pl-2">
+					<FontAwesomeIcon :icon="faYinYang" />
+				</span>
+				<span class="col-span-3">
+					{{ trans("Reference") }}
+				</span>
+				<span class="col-span-4 text-right">
+                	{{ trans("Date") }}
+				</span>
+				<span class="col-span-2 text-right">
+					{{ trans("Weight")}}
+				</span>
+				<span class="col-span-2 text-right pr-2">
+                	{{ trans("Items") }}
+				</span>
+			</div>
+			<div v-for="deliveryNote in selectedDeliveryNotes" class="grid grid-cols-12 py-2">
+				<span class="pl-2">
+					<Icon :data="deliveryNote.state_icon"/>
+				</span>
+				<span class="col-span-3">
+					<Link :href="routeDeliveryNote(deliveryNote)" class="secondaryLink">
+						{{ deliveryNote.reference }}
+					</Link>
+				</span>
+				<span class="col-span-4 text-right">
+                	{{ useFormatTime(deliveryNote.date, { 
+						localeCode: locale.language.code, 
+						formatTime: "EEE, do MMM yy, HH:mm"
+					}) }}
+				</span>
+				<span class="col-span-2 text-right">
+					{{ deliveryNote.weight }}	
+				</span>
+				<span class="col-span-2 text-right pr-2">
+                	{{ deliveryNote.number_items }}
+				</span>
+			</div>
+		</div>
+	</Modal>
 </template>
