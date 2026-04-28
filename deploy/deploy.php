8ee3346d6f2b9bd9d081bd009bd5b5900f254bb3
Raul Perusquia <raul@inikoo.com>
2026-04-28 21:16:52 +0800

    Refactor ListSelectorTradeUnit.vue: clean imports, fix inconsistencies, and improve readability; update deploy script to handle NVM for non-staging builds

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -60,9 +60,19 @@ task('deploy:migrate', function () {
 });
 desc('🏗️ Build vue app');
 task('deploy:build', function () {
+
+
+
     $frontEndChanged = get('front_end_changed');
     if ($frontEndChanged) {
-        run("cd {{release_path}} && {{bin/npm}} run build");
+        if ( currentHost()->getAlias() !== 'staging') {
+            set('use_nvm', 'source ~/.nvm/nvm.sh && nvm use');
+            run("cd {{release_path}} && {{use_nvm}} && npm run build");
+        }else{
+            run("cd {{release_path}} && {{bin/npm}} run build");
+        }
+
+
     } else {
         // No FE changes: reuse built assets from the previous release
         writeln('No front-end changes detected. Reusing built assets from previous release if available.');
