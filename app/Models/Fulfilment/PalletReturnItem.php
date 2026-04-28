b74ba85c0575e018c0fa4f15408fcd8f63797dae
andiferdiawan <andiferdiawan22@gmail.com>
2026-04-28 13:39:09 +0800

    feat: add quantity_not_picked field to PalletReturnItem

--- app/Models/Fulfilment/PalletReturnItem.php
+++ app/Models/Fulfilment/PalletReturnItem.php
@@ -32,6 +32,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property numeric $quantity_picked
+ * @property numeric $quantity_not_picked
  * @property string|null $not_setup_reason
  * @property int|null $picking_session_id
  * @property numeric $quantity_waiting_crm
