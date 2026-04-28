c35d117e1813a6d70550bf75ce6fd6e5ae3f1a0b
Raul Perusquia <raul@inikoo.com>
2026-04-28 22:03:34 +0800

    Update deploy script: use `nvm use --lts` for staging builds

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -63,7 +63,7 @@ task('deploy:migrate', function () {
 desc('Modified npm:install');
 task('npm:my_install', function () {
     if ( currentHost()->getAlias() == 'staging') {
-        set('use_nvm', 'source ~/.nvm/nvm.sh && nvm use');
+        set('use_nvm', 'source ~/.nvm/nvm.sh && nvm use --lts');
         run("cd {{release_path}} && {{use_nvm}} && npm ci");
     }else {
         run('cd {{release_path}} && {{bin/npm}} ci');
