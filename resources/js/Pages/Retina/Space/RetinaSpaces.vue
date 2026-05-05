35ba6e755cbc95432385dddb01288f6834350b42
Raul Perusquia <raul@inikoo.com>
2026-05-05 23:35:24 +0800

    Remove unused imports, commented code, and improve spacing in Retina components for better readability and maintainability.

--- resources/js/Pages/Retina/Space/RetinaSpaces.vue
+++ resources/js/Pages/Retina/Space/RetinaSpaces.vue
@@ -6,69 +6,67 @@
 
 <script setup lang="ts">
 import Table from "@/Components/Table/Table.vue"
-import { RecurringBill } from "@/types/recurring_bill"
 import { Link } from "@inertiajs/vue3"
-import { Pallet } from "@/types/Pallet"
 import { faFileInvoiceDollar, faHandHoldingUsd } from "@fal"
 import { useLocaleStore } from "@/Stores/locale"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { useFormatTime } from "@/Composables/useFormatTime"
-library.add(faFileInvoiceDollar, faHandHoldingUsd)
 import { capitalize } from "@/Composables/capitalize"
 import { PageHeadingTypes } from "@/types/PageHeading"
-import { Head } from '@inertiajs/vue3'
-  import PageHeading from '@/Components/Headings/PageHeading.vue'
+import { Head } from "@inertiajs/vue3"
+import PageHeading from "@/Components/Headings/PageHeading.vue"
+library.add(faFileInvoiceDollar, faHandHoldingUsd)
 
-const props = defineProps<{
-	data: object
-	tab?: string
-	title: string
-	pageHead: PageHeadingTypes
+defineProps<{
+    data: object
+    tab?: string
+    title: string
+    pageHead: PageHeadingTypes
 }>()
 
 const locale = useLocaleStore()
 
 function spaceRoute(space) {
-	console.log(space)
-	switch (route().current()) {
-		case "grp.org.fulfilments.show.crm.customers.show.spaces.index":
-			return route("grp.org.fulfilments.show.crm.customers.show.spaces.show", [
-				route().params["organisation"],
-				route().params["fulfilment"],
-				route().params["fulfilmentCustomer"],
-				space.slug,
-			])
-		default:
-			return ''
-	}
+    console.log(space)
+    switch (route().current()) {
+        case "grp.org.fulfilments.show.crm.customers.show.spaces.index":
+            return route("grp.org.fulfilments.show.crm.customers.show.spaces.show", [
+                route().params["organisation"],
+                route().params["fulfilment"],
+                route().params["fulfilmentCustomer"],
+                space.slug
+            ])
+        default:
+            return ""
+    }
 }
 </script>
 
 <template>
-	<Head :title="capitalize(title)" />
-	<PageHeading :data="pageHead"></PageHeading>
-	<Table :resource="data" :name="tab" class="mt-5">
-		<!-- <template #cell(state)="{ item: space }">
-			<pre>{{ space}}</pre>
-		</template> -->
-		
-		<template #cell(reference)="{ item: space }">
-			<Link :href="spaceRoute(space)" class="primaryLink">
-				{{ space["reference"] }}
-			</Link>
-		</template>
+    <Head :title="capitalize(title)" />
+    <PageHeading :data="pageHead"></PageHeading>
+    <Table :resource="data" :name="tab" class="mt-5">
+        <!-- <template #cell(state)="{ item: space }">
+            <pre>{{ space}}</pre>
+        </template> -->
 
-		<template #cell(start_at)="{ item: space }">
-			{{ useFormatTime(space["start_at"]) }}
-		</template>
+        <template #cell(reference)="{ item: space }">
+            <Link :href="spaceRoute(space)" class="primaryLink">
+                {{ space["reference"] }}
+            </Link>
+        </template>
 
-		<template #cell(end_at)="{ item: space }">
-			{{ useFormatTime(space["end_at"]) }}
-		</template>
-		<template #cell(rental)="{ item: space }">
+        <template #cell(start_at)="{ item: space }">
+            {{ useFormatTime(space["start_at"]) }}
+        </template>
+
+        <template #cell(end_at)="{ item: space }">
+            {{ useFormatTime(space["end_at"]) }}
+        </template>
+        <template #cell(rental)="{ item: space }">
 			<span v-tooltip="space.rental_name">
 				{{ space["rental"] }}
 			</span>
-		</template>
-	</Table>
+        </template>
+    </Table>
 </template>
