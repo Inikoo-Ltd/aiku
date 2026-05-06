5f462e7a9494e28df9a6cb32fd0ba25d33cbeed3
Raul Perusquia <raul@inikoo.com>
2026-05-07 00:28:36 +0800

    Remove delay from `OrgStockHydrateQuantityInLocations` dispatch in `FetchAuroraStockLocations`.

--- app/Actions/Transfers/Aurora/FetchAuroraStockLocations.php
+++ app/Actions/Transfers/Aurora/FetchAuroraStockLocations.php
@@ -52,7 +52,7 @@ class FetchAuroraStockLocations extends FetchAuroraAction
             ], 2, false);
 
 
-            OrgStockHydrateQuantityInLocations::run($orgStock->id)->delay(2);
+            OrgStockHydrateQuantityInLocations::run($orgStock->id);
         }
 
         return [];
