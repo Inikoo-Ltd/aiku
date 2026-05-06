a6fd94d67a48098719896495261ba4b3285e6b3b
louis-perez <louisprzn@gmail.com>
2026-05-06 11:32:21 +0800

    WebBlockRecommendationFromMaster ensure same format & correct data is sent

--- app/Actions/Web/WebBlock/GetWebBlockRecommendationsFromMaster.php
+++ app/Actions/Web/WebBlock/GetWebBlockRecommendationsFromMaster.php
@@ -9,7 +9,8 @@
 
 namespace App\Actions\Web\WebBlock;
 
-
+use App\Actions\Catalogue\Product\Json\GetIrisProductsInRecommendation;
+use App\Http\Resources\Catalogue\IrisProductsInWebpageResource;
 use App\Models\Web\Webpage;
 use Lorisleiva\Actions\Concerns\AsObject;
 use App\Http\Resources\Catalogue\ProductsWebpageResource;
@@ -26,10 +27,9 @@ class GetWebBlockRecommendationsFromMaster
         $webBlockType = data_get($webBlock, 'type', '');
         $webPublishedLayout = $webpage->website->published_layout;
 
-
         data_set($webBlock, 'web_block.layout.data.permissions', $permissions);
-      
-        data_set( $webBlock,'web_block.layout.data.fieldValue.products_recommended',ProductsWebpageResource::collection($webpage->model->relatedProducts)->toArray(request()));
+        data_set($webBlock,'web_block.layout.data.fieldValue.products_recommended', IrisProductsInWebpageResource::collection(GetIrisProductsInRecommendation::run($webpage->model)->toArray(request())));
+        
         return $webBlock;
     }
 }
