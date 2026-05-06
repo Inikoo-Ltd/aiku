65bcbc4a9eccf4379fe5e117646f2a2b5d6d8e68
louis-perez <louisprzn@gmail.com>
2026-05-06 14:43:45 +0800

    ReturnDeliveryNote update / remove handler

--- routes/grp/web/models/goods_in/return_delivery_note.php
+++ routes/grp/web/models/goods_in/return_delivery_note.php
@@ -9,9 +9,12 @@
 
 use App\Actions\GoodsIn\ReturnDeliveryNote\CancelReturnDeliveryNote;
 use App\Actions\GoodsIn\ReturnDeliveryNote\SetReturningReturnDeliveryNote;
+use App\Actions\GoodsIn\ReturnDeliveryNote\UnassignReturnDeliveryNoteHandler;
 use Illuminate\Support\Facades\Route;
 
 Route::name('return_delivery_note.')->prefix('return-delivery-note/{returnDeliveryNote:id}')->group(function () {
+    Route::patch('unassign', UnassignReturnDeliveryNoteHandler::class)->name('unassign');
+    
     Route::name('state.')->prefix('state')->group(function () {
         Route::patch('cancel', CancelReturnDeliveryNote::class)->name('cancel');
         Route::patch('handling', SetReturningReturnDeliveryNote::class)->name('returning');
