8478670668ceaa19be63b3de77cadb35c32c3385
ekayudinata <ekayudinatha@gmail.com>
2026-05-07 11:04:50 +0800

    feat(Mailshot): Add prospect-specific merge tags
    Create GetProspectMailshotMergeTags action for prospect mailshots
    Add ProspectMailshotMergeTagsEnum with prospect-specific tags
    Update ShowProspectMailshotWorkshop to use prospect merge tags
    Replace generic mailshot merge tags with prospect-specific ones

---
+++ app/Enums/Comms/Mailshot/ProspectMailshotMergeTagsEnum.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Author: eka yudinata (https://github.com/ekayudinata)
+ * Created: Thursday, 7 May 2026 10:45:48 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2026, eka yudinata
+ */
+
+namespace App\Enums\Comms\Mailshot;
+
+use App\Enums\EnumHelperTrait;
+
+enum ProspectMailshotMergeTagsEnum: string
+{
+    use EnumHelperTrait;
+
+    case PROSPECT_NAME = 'Prospect Name';
+    case PROSPECT_EMAIL = 'Prospect Email';
+    case PROSPECT_PHONE = 'Prospect Phone';
+    case PROSPECT_COMPANY_NAME = 'Prospect Company Name';
+
+
+    public static function tags(): array
+    {
+        return [
+            [
+                'name'  => __('Prospect Name'),
+                'value' => '[Prospect Name]'
+            ],
+            [
+                'name'  => __('Prospect Email'),
+                'value' => '[Prospect Email]'
+            ],
+            [
+                'name'  => __('Prospect Phone'),
+                'value' => '[Prospect Phone]'
+            ],
+            [
+                'name'  => __('Prospect Company Name'),
+                'value' => '[Prospect Company Name]'
+            ],
+        ];
+    }
+}
