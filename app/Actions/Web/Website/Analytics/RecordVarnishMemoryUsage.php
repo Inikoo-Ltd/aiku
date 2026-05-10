903c34be299f1495ee6161bf16d7df5a32668c60
Raul Perusquia <raul@inikoo.com>
2026-05-10 22:53:37 +0800

    refactor: enhance `RecordVarnishMemoryUsage` with console command support, improve error handling

--- app/Actions/Web/Website/Analytics/RecordVarnishMemoryUsage.php
+++ app/Actions/Web/Website/Analytics/RecordVarnishMemoryUsage.php
@@ -8,6 +8,7 @@
 
 namespace App\Actions\Web\Website\Analytics;
 
+use Illuminate\Console\Command;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Sentry\Unit;
 
@@ -15,7 +16,7 @@ class RecordVarnishMemoryUsage
 {
     use AsAction;
 
-    public function handle(): void
+    public function handle(?Command $command = null): void
     {
         $output = shell_exec('sudo /usr/bin/varnishstat -f "SMA.s0.g_bytes" -j');
         if ($output) {
@@ -27,6 +28,7 @@ class RecordVarnishMemoryUsage
                 : round($gBytes / 1024 / 1024, 2);
 
             if ($megabytes && is_numeric($megabytes)) {
+                $command?->line("Varnish Memory Usage: $megabytes Mb ('.config('app.server_name').')");
                 \Sentry\traceMetrics()->gauge(
                     'varnish.used_memory',
                     $megabytes,
@@ -35,9 +37,24 @@ class RecordVarnishMemoryUsage
                     ],
                     Unit::megabyte()
                 );
+            } else {
+                $command?->error("Error: ".$output);
             }
         } else {
-            echo "Failed to retrieve varnish metrics.";
+            $command?->error("Failed to retrieve varnish metrics.");
         }
     }
+
+    public function getCommandSignature(): string
+    {
+        return 'metrics:varnish_memory_usage';
+    }
+
+    public function asCommand(Command $command): int
+    {
+        $this->handle($command);
+
+        return 0;
+    }
+
 }
