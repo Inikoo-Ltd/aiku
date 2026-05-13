e5b5a19585e78583c6e340dcce69b249a7bb453a
Raul Perusquia <raul@inikoo.com>
2026-05-13 22:08:13 +0800

    chore: update `composer.json` dependencies, remove `nicmart/tree`, add `CrawlWebsite` action with Spatie Crawler integration

---
+++ app/Actions/Web/Website/CrawlWebsite.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 13 May 2026 20:16:00 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Web\Website;
+
+use App\Models\Web\Website;
+use Illuminate\Console\Command;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Spatie\Crawler\Crawler;
+use Spatie\Crawler\CrawlProgress;
+use Spatie\Crawler\CrawlResponse;
+
+class CrawlWebsite
+{
+    use AsAction;
+
+    public function handle(Website $website): void
+    {
+
+        Crawler::create($website->storefront->canonical_url)
+            ->internalOnly()
+            ->depth(2)
+            ->shouldCrawl(function (string $url) {
+                return !str_contains($url, '/app') && !str_contains($url, '/search');
+            })
+            ->onCrawled(function (string $url, CrawlResponse $response, CrawlProgress $progress) {
+                echo "[$progress->urlsProcessed/$progress->urlsFound] $url\n";
+            })
+
+            ->start();
+    }
+
+    public function getCommandSignature():string
+    {
+        return 'website:crawl {website?}';
+    }
+
+    public function asCommand(Command $command):int
+    {
+        if($command->argument('website')){
+            $website=Website::where('slug',$command->argument('website'))->firstOrFail();
+            $this->handle($website);
+            return 0;
+        }
+
+        return 0;
+    }
+}
