8512038df43f0c55eabe29adc6da8ada3229a490
Raul Perusquia <raul@inikoo.com>
2026-05-16 01:04:28 +0800

    chore: adjust crawl command to set depth level in production deployments

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -209,7 +209,7 @@ task(
             writeln('SSR checksum changed (or missing). Flushing Varnish cache via artisan varnish...');
             run('cd {{release_path}} && pwd && ./restart_varnish.sh');
             if (currentHost()->get('environment') === 'production' && currentHost()->getAlias() !== 'aiku') {
-                artisan('crawl --deployment', ['skipIfNoEnv', 'showOutput'])();
+                artisan('crawl -d 3 --deployment', ['skipIfNoEnv', 'showOutput'])();
             }
         } else {
             writeln('SSR checksum unchanged. Skipping Varnish cache flush.');
