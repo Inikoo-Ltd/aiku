8080501507f847ae872de61c653554d9badaddf6
Raul Perusquia <raul@inikoo.com>
2026-05-18 17:10:50 +0800

    chore: add additional delays and update artisan commands in deployment script

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -201,7 +201,9 @@ task(
             run('sleep 2');
             run('cd {{release_path}} && pwd && ./restart_varnish.sh');
             if (currentHost()->get('environment') === 'production' && currentHost()->getAlias() !== 'aiku') {
+                run('sleep 2');
                 artisan('crawl -d 2 --deployment --seeder', ['skipIfNoEnv', 'showOutput'])();
+                run('sleep 3');
                 artisan('crawl -d 3 --deployment', ['skipIfNoEnv', 'showOutput'])();
             }
         } else {
