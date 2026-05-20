f0a0985cdbeae156f90af07f7a2ad0c3c21a7f54
YudhistiraA <aryarajasa0@gmail.com>
2026-05-20 13:53:50 +0800

    Refactor Iris components and add new web block actions
    - Updated import paths for family components in getIrisComponents.ts to reflect new structure.
    - Created GetIrisWebBlockFamilyDescription.php to handle family description retrieval for Iris web blocks.
    - Created GetWebBlockFamilyDescription.php for workshop web blocks to manage family descriptions with permissions.
    - Added Family2ExtraDescriptionIris.vue component for enhanced family description display.
    - Implemented family1Iris.vue, family2Iris.vue, and family3Iris.vue components for family block rendering with offers and descriptions.

---
+++ app/Actions/Web/WebBlock/Iris/GetIrisWebBlockFamilyDescription.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * author Louis Perez
+ * created on 16-03-2026-14h-26m
+ * github: https://github.com/louis-perez
+ * copyright 2026
+*/
+
+namespace App\Actions\Web\WebBlock\Iris;
+
+use App\Http\Resources\Web\WebBlockFamilyResource;
+use App\Models\Web\Webpage;
+use Lorisleiva\Actions\Concerns\AsObject;
+use Illuminate\Support\Arr;
+
+class GetIrisWebBlockFamilyDescription
+{
+    use AsObject;
+
+    public function handle(Webpage $webpage, array $webBlock): array
+    {
+
+        $webBlockType = data_get($webBlock, 'type', '');
+        $webPublishedLayout = $webpage->website->published_layout;
+
+        data_set($webBlock, 'web_block.layout.data.fieldValue', data_get($webPublishedLayout, "family_description.$webBlockType.fieldValue", []));
+        data_set($webBlock, 'web_block.layout.data.fieldValue.family', WebBlockFamilyResource::make($webpage->model)->toArray(request()));
+
+
+        return [
+            'type' => $webBlock['type'],
+            'structure' => Arr::get(
+                $webBlock,
+                'web_block.layout.data.fieldValue',
+                []
+            ),
+        ];
+    }
+
+}
