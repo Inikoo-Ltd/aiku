af4e9dec09443f1f6fb9c9c9c00fffbb736afdee
YudhistiraA <aryarajasa0@gmail.com>
2026-05-20 16:03:23 +0800

    Put currency price in Shipping Zone

--- app/Actions/Billables/ShippingZone/UI/EditShippingZone.php
+++ app/Actions/Billables/ShippingZone/UI/EditShippingZone.php
@@ -96,7 +96,8 @@ class EditShippingZone extends OrgAction
                                 'price' => [
                                     'type'  => 'pricing_zone',
                                     'label' => __('Price'),
-                                    'value' => $shippingZone->price
+                                    'value' => $shippingZone->price,
+                                    'currency' =>  $shippingZone->shop->currency
                                 ],
                             ],
                         ]
