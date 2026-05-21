b57d96630ed281e1ff5f6f775ed828bf97eaa2cd
Raul Perusquia <raul@inikoo.com>
2026-05-22 00:49:24 +0800

    chore: clear and cache config during testing DB dump generation

--- generate_testing_db_dumps.sh
+++ generate_testing_db_dumps.sh
@@ -20,8 +20,11 @@ echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
 dropdb --if-exists -p "${DB_PORT}" -U "${USER}" -h "${HOST}" -f -w ${DB}
 createdb -p "${DB_PORT}" -U "${USER}" -h "${HOST}"  --template=template0 --lc-collate="${DB_COLLATE}" --lc-ctype="${DB_COLLATE}" ${DB}
 echo "🌱 Migrating and seeding database"
+${PHP} artisan config:clear
+${PHP} artisan cache:clear
 ${PHP} artisan --env=testing migrate
 ${PHP} artisan --env=testing db:seed
 echo -e "💾 Saving ${PURPLE}fresh_with_assets.dump${NONE}"
 pg_dump -Fc -p "${DB_PORT}" -U "${USER}" -f "tests/datasets/db_dumps/aiku.dump" ${DB}
 echo "Test DB dumped 👍"
+${PHP} artisan config:cache
