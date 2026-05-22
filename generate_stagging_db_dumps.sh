e544088814ad65daac94a66874a511f7de163e4c
Raul Perusquia <raul@inikoo.com>
2026-05-22 12:40:18 +0800

    chore: add script for staging DB dumps and simplify testing DB dump generation

--- generate_stagging_db_dumps.sh
+++ generate_stagging_db_dumps.sh
@@ -3,25 +3,22 @@ PURPLE='\033[01;35m'
 ITALIC='\e[3m'
 NONE='\033[00m'
 
-DB=aiku_test
+DB=seed_staging
 DB_PORT=5432
 DB_COLLATE=C.UTF-8
 PHP=php8.4
-USER=aiku
+USER=staging
 HOST=localhost
-
-PHP="${1:-$PHP}"
-USER="${2:-$USER}"
-HOST="${3:-$HOST}"
-DB_PORT="${4:-$DB_PORT}"
-DB_COLLATE="${5:-$DB_COLLATE}"
 
 echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
 dropdb --if-exists -p "${DB_PORT}" -U "${USER}" -h "${HOST}" -f -w ${DB}
 createdb -p "${DB_PORT}" -U "${USER}" -h "${HOST}"  --template=template0 --lc-collate="${DB_COLLATE}" --lc-ctype="${DB_COLLATE}" ${DB}
 echo "🌱 Migrating and seeding database"
+${PHP} artisan config:clear
+${PHP} artisan cache:clear
 ${PHP} artisan --env=testing migrate
 ${PHP} artisan --env=testing db:seed
-echo -e "💾 Saving ${PURPLE}fresh_with_assets.dump${NONE}"
-pg_dump -Fc -p "${DB_PORT}" -U "${USER}" -f "tests/datasets/db_dumps/aiku.dump" ${DB}
-echo "Test DB dumped 👍"
+echo -e "💾 Saving ${PURPLE}seed_staging.dump${NONE}"
+pg_dump -Fc -p "${DB_PORT}" -U "${USER}" -f "tests/datasets/db_dumps/seed_staging.dump" ${DB}
+echo "Staging seed  DB dumped 👍"
+${PHP} artisan config:cache
