ff52adbfb38b756c26756e86a5a99f62560f6363
Raul Perusquia <raul@inikoo.com>
2026-06-06 10:28:01 +0800

    chore: add DevOps dashboard, navigation menu, routes, and related Vue component

---
+++ app/Actions/DevOps/Request/IndexDevopsRequests.php
@@ -0,0 +1,319 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Sat, 06 Jun 2026 09:22:41 Indochina Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\DevOps\Request;
+
+use App\Actions\Inventory\UI\ShowInventoryDashboard;
+use App\Actions\Inventory\Warehouse\UI\ShowWarehouse;
+use App\Actions\OrgAction;
+use App\Actions\Overview\ShowGroupOverviewHub;
+use App\Actions\Traits\Authorisations\Inventory\WithWarehouseAuthorisation;
+use App\Enums\UI\Inventory\WarehouseTabsEnum;
+use App\Http\Resources\Inventory\WarehouseAreaResource;
+use App\InertiaTable\InertiaTable;
+use App\Models\Inventory\Warehouse;
+use App\Models\Inventory\WarehouseArea;
+use App\Models\SysAdmin\Group;
+use App\Models\SysAdmin\Organisation;
+use App\Services\QueryBuilder;
+use Closure;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class IndexDevopsRequests extends OrgAction
+{
+
+    public function asController(ActionRequest $request): LengthAwarePaginator
+    {
+        $group=group();
+        $this->initialisationFromGroup($group, $request);
+
+        return $this->handle();
+    }
+
+    public function handle($prefix = null): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->whereWith('warehouse_areas.route_path', $value);
+            });
+        });
+
+        if ($prefix) {
+            InertiaTable::updateQueryBuilderParameters($prefix);
+        }
+
+        $queryBuilder = QueryBuilder::for(WarehouseArea::class);
+
+
+
+
+        return $queryBuilder
+            ->defaultSort('warehouse_areas.code')
+            ->select(
+                [
+                    'warehouse_areas.code',
+                    'warehouse_areas.id',
+                    'warehouse_areas.name',
+                    'warehouse_areas.picking_position',
+                    'number_locations',
+                    'warehouses.slug as warehouse_slug',
+                    'warehouse_areas.slug',
+                    'warehouse_area_stats.stock_value as stock_value',
+                    'warehouse_area_stats.number_empty_locations as number_empty_locations',
+                    'organisations.slug as organisation_slug',
+                    'organisations.name as organisation_name'
+                ]
+            )
+            ->leftJoin('warehouse_area_stats', 'warehouse_area_stats.warehouse_area_id', 'warehouse_areas.id')
+            ->leftJoin('warehouses', 'warehouse_areas.warehouse_id', 'warehouses.id')
+            ->leftJoin('organisations', 'warehouse_areas.organisation_id', 'organisations.id')
+            ->when($parent, function ($query) use ($parent) {
+                if (class_basename($parent) == 'Warehouse') {
+                    $query->where('warehouse_areas.warehouse_id', $parent->id);
+                }
+
+                if (class_basename($parent) == 'Organisation') {
+                    $query->where('warehouse_areas.organisation_id', $parent->id);
+                }
+            })
+            ->allowedSorts(['code', 'name', 'number_locations','picking_position', 'stock_value', 'number_empty_locations'])
+            ->allowedFilters([$globalSearch])
+            ->withPaginator($prefix, tableName: request()->route()->getName())
+            ->withQueryString();
+    }
+
+    public function tableStructure($parent, ?array $modelOperations = null, $prefix = null): Closure
+    {
+        return function (InertiaTable $table) use ($parent, $modelOperations, $prefix) {
+            if ($prefix) {
+                $table
+                    ->name($prefix)
+                    ->pageName($prefix.'Page');
+            }
+            $table
+                ->withGlobalSearch()
+                ->withModelOperations($modelOperations)
+                ->withEmptyState(
+                    match (class_basename($parent)) {
+                        'Organisation' => [
+                            'title'       => __("No warehouse areas found"),
+                            'description' => $this->canEdit && $parent->stats->number_warehouses == 0 ? __('Get started by creating a warehouse area. ✨')
+                                : __("In fact, is no even create a warehouse yet 🤷🏽‍♂️"),
+                            'count'       => $parent->stats->number_warehouse_areas,
+                            'action'      => $this->canEdit && $parent->stats->number_warehouses == 0 ? [
+                                'type'    => 'button',
+                                'style'   => 'create',
+                                'tooltip' => __('New warehouse'),
+                                'label'   => __('warehouse'),
+                                'route'   => [
+                                    'name'       => 'grp.org.warehouses.create',
+                                    'parameters' => [$parent->slug]
+                                ]
+                            ] : null
+                        ],
+                        'Warehouse' => [
+                            'title'       => __("No warehouse areas found"),
+                            'description' => $this->canEdit ? __('Get started by creating a new warehouse area. ✨')
+                                : null,
+                            'count'       => $parent->stats->number_warehouse_areas,
+                            'action'      => $this->canEdit ? [
+                                'type'    => 'button',
+                                'style'   => 'create',
+                                'tooltip' => __('New warehouse area'),
+                                'label'   => __('warehouse area'),
+                                'route'   => [
+                                    'name'       => 'grp.org.warehouses.show.infrastructure.warehouse_areas.create',
+                                    'parameters' => [
+                                        $parent->organisation->slug,
+                                        $parent->slug
+                                    ]
+                                ]
+                            ] : null
+                        ],
+                        default => null
+                    }
+                )
+                ->column(key: 'name', label: __('Name'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'picking_position', label: __('Picking Order'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'stock_value', label: __('stock value'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'number_empty_locations', label: __('empty locations'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'number_locations', label: __('locations'), canBeHidden: false, sortable: true)
+                ->defaultSort('code');
+        };
+    }
+
+    public function jsonResponse(LengthAwarePaginator $warehouseAreas): AnonymousResourceCollection
+    {
+        return WarehouseAreaResource::collection($warehouseAreas);
+    }
+
+    public function htmlResponse(LengthAwarePaginator $warehouseAreas, ActionRequest $request): Response
+    {
+        $title = __('warehouse areas');
+
+        $scope     = $this->parent;
+        $container = null;
+        if (class_basename($scope) == 'Warehouse') {
+            $container = [
+                'icon'    => ['fal', 'fa-warehouse'],
+                'tooltip' => __('Warehouse'),
+                'label'   => Str::possessive($scope->name)
+            ];
+        }
+
+        $icon = [
+            'icon'  => ['fal', 'fa-map-signs'],
+            'title' => $title
+        ];
+
+        if ($scope instanceof Group) {
+            $icon = [
+                'icon'  => ['fal', 'fa-industry-alt'],
+                'title' => $title
+            ];
+        }
+
+        return Inertia::render(
+            'Org/Warehouse/WarehouseAreas',
+            [
+                'breadcrumbs' => $this->getBreadcrumbs(
+                    $request->route()->getName(),
+                    $request->route()->originalParameters()
+                ),
+                'title'       => $title,
+                'pageHead'    => [
+                    'title'     => $title,
+                    'container' => $container,
+                    'icon'      => $icon,
+                    'actions'   => [
+                        $this->canEdit && $request->route()->getName() == 'grp.org.warehouses.show.infrastructure.warehouse_areas.index' ? [
+                            'type'   => 'buttonGroup',
+                            'key'    => 'upload-add',
+                            'button' => [
+                                [
+                                    'type'  => 'button',
+                                    'style' => 'primary',
+                                    'icon'  => ['fal', 'fa-upload'],
+                                    'label' => 'upload',
+                                    'route' => [
+                                        'name'       => 'grp.models.warehouse.warehouse-areas.upload',
+                                        'parameters' => [
+                                            $this->parent->id
+                                        ]
+                                    ]
+                                ],
+                                [
+
+                                    'type'  => 'button',
+                                    'style' => 'create',
+                                    'label' => __('Areas'),
+                                    'route' => [
+                                        'name'       => 'grp.org.warehouses.show.infrastructure.warehouse_areas.create',
+                                        'parameters' => $request->route()->originalParameters()
+                                    ]
+
+                                ]
+                            ]
+                        ] : null,
+                    ]
+                ],
+                'data'        => WarehouseAreaResource::collection($warehouseAreas),
+                'upload_warehouse_areas' => [
+                    'title' => [
+                        'label' => __('Upload locations'),
+                        'information' => __('The list of column file:')
+                    ],
+                    'progressDescription'   => __('Importing locations'),
+                    'preview_template'    => [
+                        'header' => ['code', 'name'],
+                        'rows' => [
+                            [
+                                'code' => '01-CODE',
+                                'name' => '01 code',
+                            ],
+                        ]
+                    ],
+                    'upload_spreadsheet'    => [
+                        'event'           => 'action-progress',
+                        'channel'         => 'grp.personal.'.$this->organisation->id,
+                        'required_fields' => ['code', 'name'],
+                        'template'        => [
+                            'label' => 'Download template (.xlsx)',
+                        ],
+                        'route'           => [
+                            'upload'   => [
+                                'name'       => 'grp.models.warehouse.warehouse-areas.upload',
+                                'parameters' => [
+                                    $this->parent->id
+                                ]
+                            ]
+                        ],
+                    ]
+                ],
+            ]
+        )->table($this->tableStructure($this->parent));
+    }
+
+    public function getBreadcrumbs(string $routeName, array $routeParameters): array
+    {
+        $headCrumb = function (array $routeParameters = []) {
+            return [
+                [
+                    'type'   => 'simple',
+                    'simple' => [
+                        'route' => $routeParameters,
+                        'label' => __('Warehouse areas'),
+                        'icon'  => 'fal fa-bars'
+                    ],
+                ],
+            ];
+        };
+
+        return match ($routeName) {
+            'grp.overview.inventory.warehouses-areas.index' =>
+            array_merge(
+                ShowGroupOverviewHub::make()->getBreadcrumbs(),
+                $headCrumb(
+                    [
+                        'name'       => $routeName,
+                        'parameters' => $routeParameters
+                    ]
+                )
+            ),
+            'grp.org.warehouse-areas.index' =>
+            array_merge(
+                (new ShowInventoryDashboard())->getBreadcrumbs($routeParameters),
+                $headCrumb(
+                    [
+                        'name' => 'grp.oms.warehouse-areas.index',
+                        null
+                    ]
+                )
+            ),
+            'grp.org.warehouses.show.infrastructure.warehouse_areas.index',
+            =>
+            array_merge(
+                ShowWarehouse::make()->getBreadcrumbs($routeParameters),
+                $headCrumb([
+                    'name'       => 'grp.org.warehouses.show.infrastructure.warehouse_areas.index',
+                    'parameters' =>
+                        [
+                            $routeParameters['organisation'],
+                            Warehouse::where('slug', $routeParameters['warehouse'])->first()->slug
+                        ]
+                ])
+            ),
+            default => []
+        };
+    }
+}
--- routes/grp/web/chat.php
+++ routes/grp/web/chat.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 05 Jun 2026 19:25:55 Indochina Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
 
 use App\Actions\CRM\ChatSession\UI\ShowGroupChatDashboard;
 use App\Actions\CRM\Agent\UI\ShowGroupAgents;
