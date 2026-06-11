bb874c9347abd14ae93a894bb2a4be8a73d0d14e
Raul Perusquia <raul@inikoo.com>
2026-06-11 14:01:02 +0800

    chore: add ActivateScheduledOffers action to process scheduled offers for activation

---
+++ app/Actions/Discounts/Offer/ActivateScheduledOffers.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 11 Jun 2026 12:58:50 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Discounts\Offer;
+
+use App\Enums\Discounts\Offer\OfferStateEnum;
+use App\Models\Discounts\Offer;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ActivateScheduledOffers
+{
+    use AsAction;
+
+    public function handle(): void
+    {
+        //Find all scheduled offers
+        $scheduledOffers = Offer::where('state', OfferStateEnum::IN_PROCESS)
+            ->whereNull('source_id')//Do not touch old aurora offers
+            ->where('start_at', '<=', now())->get();
+
+        foreach ($scheduledOffers as $offer) {
+            ActivateOffer::run($offer);
+        }
+    }
+}
