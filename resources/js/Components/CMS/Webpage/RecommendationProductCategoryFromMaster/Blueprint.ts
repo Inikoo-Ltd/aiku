f8fa6b6d88fbe83c605598694f04aa7427135ff7
RifqiTaw <rifqitaufiqurrohman1@gmail.com>
2026-06-19 08:13:51 +0800

    refactor: comment out unused blueprint fields in Recommendation components workshop for clarity & information

--- resources/js/Components/CMS/Webpage/RecommendationProductCategoryFromMaster/Blueprint.ts
+++ resources/js/Components/CMS/Webpage/RecommendationProductCategoryFromMaster/Blueprint.ts
@@ -4,36 +4,36 @@ export const blueprint = (data?: { website_id?: number }) => {
     // console.log('SeeAlso1 Blueprint data:', data)
     return {
         blueprint: [
-        {
-            label: "# Id ",
-            key: ["id"],
-            type: "text",
-            information: trans("id selector is used to select one unique element!"),
-        },
-        {
-            label: "Responsive Visibility",
-            key: ["container", "properties", "visibility"],
-            type: "visibility",
-            useIn: ["desktop", "tablet", "mobile"],
-        },
-        {
-            name: "Settings",
-            key: ["settings"],
-            replaceForm: [
-                {
-                    key: ["per_row"],
-                    label: trans("Show Each Row"),
-                    type: "number",
-                    useIn : ["desktop", "tablet", "mobile"],
-                },
-                {
-                    key: ["product_category"],
-                    label: "Show Product Category",
-                    type: "select_product_category",
-                    props_data : {}
-                },
-            ],
-        },
+        // {
+        //     label: "# Id ",
+        //     key: ["id"],
+        //     type: "text",
+        //     information: trans("id selector is used to select one unique element!"),
+        // },
+        // {
+        //     label: "Responsive Visibility",
+        //     key: ["container", "properties", "visibility"],
+        //     type: "visibility",
+        //     useIn: ["desktop", "tablet", "mobile"],
+        // },
+        // {
+        //     name: "Settings",
+        //     key: ["settings"],
+        //     replaceForm: [
+        //         {
+        //             key: ["per_row"],
+        //             label: trans("Show Each Row"),
+        //             type: "number",
+        //             useIn : ["desktop", "tablet", "mobile"],
+        //         },
+        //         {
+        //             key: ["product_category"],
+        //             label: "Show Product Category",
+        //             type: "select_product_category",
+        //             props_data : {}
+        //         },
+        //     ],
+        // },
         {
             name: trans("Related Products Category Web Block"),
             key: ["recommendation_settings"],
