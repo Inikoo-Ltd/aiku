9f7ed23084b992e7a028a5a5f4f42328ce865242
stewicca <stewicalf@gmail.com>
2026-06-22 14:10:40 +0800

    feat: add casts/attributes methods and PHPDoc to outbox time series models

--- app/Models/Comms/OutboxTimeSeriesRecord.php
+++ app/Models/Comms/OutboxTimeSeriesRecord.php
@@ -11,6 +11,21 @@ namespace App\Models\Comms;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
+ * @property int $outbox_time_series_id
+ * @property string $frequency
+ * @property int|null $runs
+ * @property int|null $dispatched_emails
+ * @property int|null $opened_emails
+ * @property int|null $clicked_emails
+ * @property int|null $bounced_emails
+ * @property int|null $subscribed
+ * @property int|null $unsubscribed
+ * @property \Illuminate\Support\Carbon|null $from
+ * @property \Illuminate\Support\Carbon|null $to
+ * @property string|null $period
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OutboxTimeSeriesRecord newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OutboxTimeSeriesRecord newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OutboxTimeSeriesRecord query()
@@ -21,4 +36,12 @@ class OutboxTimeSeriesRecord extends Model
     protected $table = 'outbox_time_series_records';
 
     protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'from' => 'datetime',
+            'to'   => 'datetime',
+        ];
+    }
 }
