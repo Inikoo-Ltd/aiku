3fc606185bfe9f81ad5c1c60fb8450cc381ddec0
Raul Perusquia <raul@inikoo.com>
2026-06-22 16:47:34 +0800

    refactor: add check for `ProductCategoryTypeEnum` in `CloseDiscontinuedWebpage` to ensure only `FAMILY` categories are processed

--- app/Actions/Web/Webpage/CloseDiscontinuedWebpage.php
+++ app/Actions/Web/Webpage/CloseDiscontinuedWebpage.php
@@ -9,6 +9,7 @@
 namespace App\Actions\Web\Webpage;
 
 use App\Enums\Catalogue\ProductCategory\ProductCategoryStateEnum;
+use App\Enums\Catalogue\ProductCategory\ProductCategoryTypeEnum;
 use App\Enums\Web\Redirect\RedirectTypeEnum;
 use App\Enums\Web\Webpage\WebpageStateEnum;
 use App\Models\Catalogue\Product;
@@ -27,6 +28,13 @@ class CloseDiscontinuedWebpage
                 'error' => 'model_not_product_or_product_category '.get_class($webpage->model)
             ];
         }
+
+        if( $webpage->model instanceof ProductCategory && $webpage->model->type !== ProductCategoryTypeEnum::FAMILY ) {
+            return [
+                'error' => 'product_category_not_family'
+            ];
+        }
+
 
         if($webpage->state !== WebpageStateEnum::LIVE) {
             return [
