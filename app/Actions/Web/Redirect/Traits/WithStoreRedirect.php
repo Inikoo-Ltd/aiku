ab23d23590be0242f016d991d4cad7e27326edcc
Raul Perusquia <raul@inikoo.com>
2026-06-23 15:06:17 +0800

    feat: add `GetAuroraCanonicalAndCreateRedirect` action and enhance redirect handling with validation, defaults, and input sanitization

--- app/Actions/Web/Redirect/Traits/WithStoreRedirect.php
+++ app/Actions/Web/Redirect/Traits/WithStoreRedirect.php
@@ -9,7 +9,7 @@ trait WithStoreRedirect
 {
     abstract public function set(string $key, mixed $value): static;
 
-    public function prepareForValidation(ActionRequest $request)
+    public function prepareForValidation(ActionRequest $request): void
     {
         $initialFromUrl = $request->input('from_url') ?? $this->from_url;
 
@@ -27,7 +27,7 @@ trait WithStoreRedirect
             $fromUrl = trim($fromUrl, '/'); // Sanitize start & end slash
             $fromPath = array_last(explode('/', $fromUrl)); // Get last path
 
-            $url = 'https://' . $website->domain . '/' . $fromUrl;
+            $url = 'https://www.' . $website->domain . '/' . $fromUrl;
 
             $this->set('from_url', $url);
             $this->set('from_path', $fromPath);
