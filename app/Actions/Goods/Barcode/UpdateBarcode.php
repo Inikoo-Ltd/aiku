42960146e8649ba19d653d17790ddaf8d6bbbf2a
louis-perez <louisprzn@gmail.com>
2026-06-29 14:23:56 +0800

    INI-1700: Barcode update trade unit implementation (SyncBarcodeToTradeUnit, ProductsHydrateBarcodeFromTradeUnit) & Edit Barcode (Only on local at the moment)

--- app/Actions/Goods/Barcode/UpdateBarcode.php
+++ app/Actions/Goods/Barcode/UpdateBarcode.php
@@ -8,11 +8,16 @@
 
 namespace App\Actions\Goods\Barcode;
 
+use App\Actions\Goods\Barcode\Hydrators\GroupHydrateBarcodes;
+use App\Actions\Goods\TradeUnit\UpdateTradeUnit;
 use App\Actions\GrpAction;
 use App\Actions\Traits\WithActionUpdate;
 use App\Enums\Helpers\Barcode\BarcodeStatusEnum;
 use App\Http\Resources\Helpers\BarcodeResource;
+use App\Models\Goods\TradeUnit;
 use App\Models\Helpers\Barcode;
+use App\Rules\IUnique;
+use Illuminate\Support\Arr;
 use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 
@@ -24,7 +29,38 @@ class UpdateBarcode extends GrpAction
 
     public function handle(Barcode $barcode, array $modelData): Barcode
     {
-        return $this->update($barcode, $modelData, ['data']);
+        $previousActiveTradeUnit = $barcode->tradeUnitsActive->first();
+
+        if (Arr::has($modelData, 'trade_unit')) {
+            $newTradeUnitId = Arr::pull($modelData, 'trade_unit', null);
+            $newTradeUnit = TradeUnit::find($newTradeUnitId);
+
+            SyncBarcodeToTradeUnit::make()->action($barcode, $newTradeUnit);
+
+            $barcode->refresh();
+
+            if ($barcode->tradeUnitsActive()->exists()) {
+                data_set($modelData, 'status', BarcodeStatusEnum::USED);
+            } else {
+                data_set($modelData, 'status', BarcodeStatusEnum::AVAILABLE);
+            }
+        }
+
+        $barcode = $this->update($barcode, $modelData, ['data']);
+
+        $changes = Arr::except($barcode->getChanges(), ['updated_at', 'last_fetched_at']);
+
+        if (Arr::hasAny($changes, ['status'])) {
+            GroupHydrateBarcodes::run($barcode->group);
+        }
+
+        if (Arr::hasAny($changes, ['number'])) {
+            UpdateTradeUnit::make()->action($previousActiveTradeUnit, [
+                'barcode_id'        => $barcode->id
+            ]);
+        }
+
+        return $barcode;
     }
 
     public function authorize(ActionRequest $request): bool
@@ -39,11 +75,34 @@ class UpdateBarcode extends GrpAction
     public function rules(): array
     {
         $rules = [
-            'number' => ['sometimes', 'required', 'numeric'],
-            'note'   => ['sometimes', 'nullable', 'string', 'max:1000'],
-            'data'   => ['sometimes', 'nullable', 'array'],
-            'status' => ['sometimes', 'required', Rule::enum(BarcodeStatusEnum::class)],
-
+            'number'        => [
+                'sometimes',
+                'required',
+                'numeric',
+                new IUnique(
+                    table: 'barcodes',
+                    extraConditions: [
+                        ['column' => 'deleted_at', 'operator' => 'null'],
+                    ]
+                ),
+            ],
+            'note'          => ['sometimes', 'nullable', 'string', 'max:1000'],
+            'data'          => ['sometimes', 'nullable', 'array'],
+            'status'        => ['sometimes', 'required', Rule::enum(BarcodeStatusEnum::class)],
+            'trade_unit'    => [
+                'sometimes',
+                'nullable',
+                'exists:trade_units,id',
+                new IUnique(
+                    table: 'model_has_barcodes',
+                    column: 'model_id',
+                    extraConditions: [
+                        ['column' => 'status', 'value' => true],
+                        ['column' => 'model_type', 'value'  => class_basename(TradeUnit::class)]
+                    ],
+                    caseSensitive: false
+                ),
+            ],
         ];
 
         if (!$this->strict) {
@@ -55,9 +114,9 @@ class UpdateBarcode extends GrpAction
 
     public function asController(Barcode $barcode, ActionRequest $request): Barcode
     {
-        $this->initialisation(app('group'), $request);
+        $this->initialisation(group(), $request);
 
-        return $this->handle($barcode, $request->validated());
+        return $this->handle($barcode, $this->validatedData);
     }
 
     public function action(Barcode $barcode, array $modelData, int $hydratorsDelay = 0, bool $strict = true, bool $audit = true): Barcode
