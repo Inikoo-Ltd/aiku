e30c321f0537d8e7466eb044a5a1109d4cee9f4c
louis-perez <louisprzn@gmail.com>
2026-06-29 16:44:03 +0800

    RepairShopForbiddenCountries

--- app/Actions/Catalogue/Shop/UpdateShop.php
+++ app/Actions/Catalogue/Shop/UpdateShop.php
@@ -82,7 +82,7 @@ class UpdateShop extends OrgAction
         }
 
         if (Arr::has($modelData, 'banned_countries')) {
-            dd('xxxxxx', $modelData);  // TODO
+            data_set($modelData, 'banned_country_regions', Arr::pull($modelData, 'banned_countries'));
         }
 
         if (Arr::has($modelData, 'dispatch_require_shipping')) {
@@ -475,6 +475,10 @@ class UpdateShop extends OrgAction
             'bank_transfer_instructions_for_email'                    => ['sometimes', 'nullable', 'string', 'max:10000'],
             'follow_master_pricing'                                   => ['sometimes', 'boolean'],
             'banned_countries'                                        => ['sometimes', 'nullable', 'array'],
+            'banned_countries.*'                                      => ['required', 'array'],
+            'banned_countries.*.postcode'                             => ['sometimes', 'string', 'nullable'],
+            'banned_countries.*.billing'                              => ['required', 'boolean',],
+            'banned_countries.*.delivery'                             => ['required', 'boolean',],
         ];
 
         $channelIds = SalesChannel::pluck('id');
