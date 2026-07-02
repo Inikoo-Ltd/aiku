59082b484e13719cbd73967cd93e8e4467536393
itzArtha <manager.arthachan@gmail.com>
2026-07-02 16:08:05 +0800

    feat: add action classes to fetch and cancel Allegro orders via API

---
+++ app/Actions/Dropshipping/Allegro/Order/FetchCancelledAllegroOrders.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Mon, 10 Mar 2025 16:53:20 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Dropshipping\Allegro\Order;
+
+use App\Actions\RetinaAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Ordering\Platform\PlatformTypeEnum;
+use App\Models\Dropshipping\CustomerSalesChannel;
+use App\Models\Dropshipping\Platform;
+
+class FetchCancelledAllegroOrders extends RetinaAction
+{
+    use WithActionUpdate;
+
+    public string $commandSignature = 'allegro:fetch-cancelled-orders';
+
+    public function handle(): void
+    {
+        $platform = Platform::where('type', PlatformTypeEnum::ALLEGRO)->first();
+
+        $customerSalesChannels = CustomerSalesChannel::where('platform_id', $platform->id)
+            ->where('can_connect_to_platform', true)
+            ->where('exist_in_platform', true)
+            ->where('platform_status', true)
+            ->get();
+
+        /** @var CustomerSalesChannel $customerSalesChannel */
+        foreach ($customerSalesChannels as $customerSalesChannel) {
+            if ($customerSalesChannel->user) {
+                GetCancelledAllegroOrdersFromApi::dispatch($customerSalesChannel->user);
+            }
+        }
+    }
+
+    public function asCommand(): void
+    {
+        $this->handle();
+    }
+}
