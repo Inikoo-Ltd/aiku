0a759457418b9a0fc9f04f3063f805e96ea1f984
stewicca <stewicalf@gmail.com>
2026-07-03 10:12:35 +0800

    feat: add --shop and --organisation scope options to redo time series commands

--- app/Actions/Inventory/OrgStock/RedoOrgStockTimeSeries.php
+++ app/Actions/Inventory/OrgStock/RedoOrgStockTimeSeries.php
@@ -24,7 +24,7 @@ class RedoOrgStockTimeSeries implements ShouldBeUnique
     }
 
     public string $jobQueue = 'long-low-priority';
-    public string $commandSignature = 'org-stocks:redo_time_series {--from= : Start date (Y-m-d)} {--to= : End date (Y-m-d)} {--a|async : Run asynchronously}';
+    public string $commandSignature = 'org-stocks:redo_time_series {--O|organisation= : Organisation slug} {--from= : Start date (Y-m-d)} {--to= : End date (Y-m-d)} {--a|async : Run asynchronously}';
 
     public function __construct()
     {
