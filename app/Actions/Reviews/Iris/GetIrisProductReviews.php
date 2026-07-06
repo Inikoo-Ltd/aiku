4450b85f688916538722eb3159747f318362ec30
Raul Perusquia <raul@inikoo.com>
2026-07-06 14:15:41 +0800

    refactor: enhance review actions with validation, filtering, and pagination logic
    - Updated `GetIrisProductReviews` to include rating filters, review scopes, and pagination rules.
    - Modified `GetIrisReviews` to optionally return paginated reviews and introduced a structured JSON response method.
    - Refactored `ReviewsInIrisResource` by standardizing property access and simplifying resource handling.

--- app/Actions/Reviews/Iris/GetIrisProductReviews.php
+++ app/Actions/Reviews/Iris/GetIrisProductReviews.php
@@ -7,22 +7,73 @@
 
 namespace App\Actions\Reviews\Iris;
 
+use App\Enums\Catalogue\Review\ReviewScopeEnum;
+use App\Enums\Catalogue\Review\ReviewStateEnum;
+use App\Enums\Catalogue\Review\ReviewStatusEnum;
+use App\Http\Resources\Inventory\LocationsResource;
 use App\Models\Catalogue\Product;
-use App\Models\CRM\Customer;
 use App\Models\Reviews\Review;
 use App\Services\QueryBuilder;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Arr;
 use Lorisleiva\Actions\Concerns\AsObject;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class GetIrisProductReviews
 {
     use asObject;
 
-    public function handle(Product $product): array
+    public function handle(Product $product,$prefix=null): LengthAwarePaginator
     {
-        $setting=Arr::get($product->shop->settings,'reviews',[]);
+        $setting = Arr::get($product->shop->settings, 'reviews.validation_scope.product', []);
 
         $queryBuilder = QueryBuilder::for(Review::class);
+        if (Arr::get($setting, 'enabled', false)) {
+            if (Arr::get($setting, 'scope') == 'group') {
+                $queryBuilder->where('group_id', $product->group_id);
+            } else {
+                $queryBuilder->where('organisation_id', $product->organisation_id);
+            }
+            $queryBuilder->where('master_product_id', $product->master_product_id);
+        } else {
+            $queryBuilder->where('shop_id', $product->shop_id);
+            $queryBuilder->where('product_id', $product->id);
+        }
+
+        $minRating = Arr::get($product->shop->settings, 'reviews.minimum_rating_to_show', 3);
+
+        $queryBuilder->where('reviews.rating_main', '>=', $minRating)
+            ->where('reviews.state', ReviewStateEnum::PUBLISHED)
+            ->where('reviews.is_public', true)
+            ->where('reviews.review_status', ReviewStatusEnum::APPROVED)
+            ->where('reviews.scope', ReviewScopeEnum::PRODUCT);
+
+        $allowedSort = [
+            'rating_main',
+        ];
+
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->whereStartWith('reviews.message', $value);
+            });
+        });
+
+        return $queryBuilder
+            ->select([
+                'reviews.id',
+                'customers.contact_name',
+            ])
+            ->leftJoin('customers', 'customers.id', '=', 'reviews.customer_id')
+            ->defaultSort('-created_at')
+            ->allowedSorts($allowedSort)
+            ->allowedFilters([$globalSearch])
+            ->withPaginator($prefix, tableName: request()->route()->getName())
+            ->withQueryString();
+
+
     }
+
+   Ω
 
 }
--- app/Actions/Reviews/Iris/GetIrisReviews.php
+++ app/Actions/Reviews/Iris/GetIrisReviews.php
@@ -7,25 +7,35 @@
 
 namespace App\Actions\Reviews\Iris;
 
+use App\Http\Resources\Catalogue\ReviewsInIrisResource;
+use App\Http\Resources\Inventory\LocationsResource;
 use App\Models\Catalogue\Product;
 use App\Models\Catalogue\ProductCategory;
 use App\Models\Web\Webpage;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class GetIrisReviews
 {
     use AsAction;
 
-    public function handle(Webpage $webpage): array
+    public function handle(Webpage $webpage): ?LengthAwarePaginator
     {
         $model = $webpage->model;
 
         if ($model instanceof Product) {
             GetIrisProductReviews::run($model);
         } elseif ($model instanceof ProductCategory) {
-            GetIrisProductCategoryReviews::run($model);
+           // GetIrisProductCategoryReviews::run($model);
         } else {
-            GetIrisShopReviews::run($webpage->shop);
+           // GetIrisShopReviews::run($webpage->shop);
         }
+        return null;
+    }
+
+    public function jsonResponse(LengthAwarePaginator $reviews): AnonymousResourceCollection
+    {
+        return ReviewsInIrisResource::collection($reviews);
     }
 }
