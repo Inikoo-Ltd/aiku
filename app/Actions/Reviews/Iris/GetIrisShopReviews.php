88a3631560cd940c3c94e3852e9e933a98c45096
Raul Perusquia <raul@inikoo.com>
2026-07-06 15:36:43 +0800

    refactor: centralize logic for fetching reviews into `WithGetIrisReviewsTrait` and simplify actions

--- app/Actions/Reviews/Iris/GetIrisShopReviews.php
+++ app/Actions/Reviews/Iris/GetIrisShopReviews.php
@@ -10,13 +10,9 @@
 namespace App\Actions\Reviews\Iris;
 
 use App\Actions\Reviews\Iris\Traits\WithGetIrisReviewsTrait;
-use App\Enums\Catalogue\Review\ReviewReactionTargetEnum;
 use App\Models\Catalogue\Shop;
-use App\Models\CRM\WebUser;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\Concerns\AsObject;
-use Spatie\QueryBuilder\AllowedFilter;
 
 class GetIrisShopReviews
 {
@@ -25,66 +21,6 @@ class GetIrisShopReviews
 
     public function handle(Shop $shop, $prefix = null): LengthAwarePaginator
     {
-        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
-            $query->where(function ($query) use ($value) {
-                $query->whereStartWith('reviews.message', $value);
-            });
-        });
-
-        $queryBuilder = $this->getBaseQuery($shop);
-
-        $allowedSort = [
-            'rating_main',
-        ];
-        
-        $select = [
-            'reviews.id',
-            'customers.contact_name',
-            'customers.location',
-            'reviews.rating_main',
-            'reviews.message',
-            'reviews.translations',
-            'reviews.published_at',
-            'reviews.web_images',
-            'reviews.likes',
-            'reviews.dislikes',
-            'reviews.replay_likes',
-            'reviews.replay_dislikes',
-            'reviews.reply_message as reply',
-            'reply_users.contact_name as reply_by',
-            'reviews.created_at',
-            DB::raw("'{$shop->language_id}' as language_id")
-        ];
-
-        if (auth()->check()) {
-            /** @var WebUser $webUser */
-            $webUser = auth()->user();
-            if ($webUser->customer) {
-                $select[] = 'review_reactions.type as review_reaction';
-                $select[] = 'reply_reactions.type as reply_reaction';
-
-                $queryBuilder
-                    ->leftJoin('review_reactions', function ($join) use ($webUser) {
-                        $join->on('review_reactions.review_id', 'reviews.id')
-                            ->where('review_reactions.customer_id', $webUser->customer->id)
-                            ->where('review_reactions.target', ReviewReactionTargetEnum::REVIEW);
-                    })
-                    ->leftJoin('review_reactions as reply_reactions', function ($join) use ($webUser) {
-                        $join->on('reply_reactions.review_id', 'reviews.id')
-                            ->where('reply_reactions.customer_id', $webUser->customer->id)
-                            ->where('reply_reactions.target', ReviewReactionTargetEnum::REVIEW_REPLY);
-                    });
-            }
-        }
-
-        return $queryBuilder
-            ->leftJoin('customers', 'customers.id', '=', 'reviews.customer_id')
-            ->leftJoin('users as reply_users', 'reviews.reply_by', '=', 'reply_users.id')
-            ->select($select)
-            ->defaultSort('-created_at')
-            ->allowedSorts($allowedSort)
-            ->allowedFilters([$globalSearch])
-            ->withPaginator($prefix, tableName: request()->route()->getName())
-            ->withQueryString();
+        return $this->getIrisReviews($shop, $prefix);
     }
 }
--- app/Http/Resources/Catalogue/ReviewsInIrisResource.php
+++ app/Http/Resources/Catalogue/ReviewsInIrisResource.php
@@ -1,15 +1,39 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 06 Jul 2026 15:35:14 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
 namespace App\Http\Resources\Catalogue;
 
-use App\Models\Reviews\Review;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $language_id
+ * @property mixed $contact_name
+ * @property mixed $id
+ * @property mixed $rating_main
+ * @property mixed|string $location
+ * @property mixed $translations
+ * @property mixed $published_at
+ * @property mixed $web_images
+ * @property mixed $message
+ * @property mixed $likes
+ * @property mixed $reply_by
+ * @property mixed $dislikes
+ * @property mixed $review_reaction
+ * @property mixed $replay_likes
+ * @property mixed $replay_dislikes
+ * @property mixed $reply_reaction
+ * @property mixed $reply
+ */
 class ReviewsInIrisResource extends JsonResource
 {
     public function toArray($request): array
     {
-        
+
         $languageId = $this->language_id;
 
         return [
@@ -18,7 +42,7 @@ class ReviewsInIrisResource extends JsonResource
             'customer_location'     => is_string($this->location) ? json_decode($this->location, true) : $this->location,
             'rating'                => $this->rating_main,
             'message'               => $this->message,
-            'message_translated'    => data_get($this->translations, "message.$languageId", null),
+            'message_translated'    => data_get($this->translations, "message.$languageId"),
             'date'                  => $this->published_at,
             'web_images'            => $this->web_images,
             'likes'                 => $this->likes,
@@ -28,7 +52,7 @@ class ReviewsInIrisResource extends JsonResource
             'reply_dislikes'        => $this->replay_dislikes,
             'reply_reactions'       => $this->reply_reaction,
             'reply'                 => $this->reply,
-            'reply_translated'      => data_get($this->translations, "reply_message.$languageId", null),
+            'reply_translated'      => data_get($this->translations, "reply_message.$languageId"),
             'reply_by'              => $this->reply_by,
         ];
     }
