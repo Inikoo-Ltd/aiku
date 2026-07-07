0035e90d60bbec6f10b97717095f057fe37a1111
itzArtha <manager.arthachan@gmail.com>
2026-07-07 13:26:56 +0800

    refactor: streamline shipping rate processing and eliminate repetitive code in Allegro dropshipping actions

--- app/Actions/Dropshipping/Allegro/User/ProcessShippingRates.php
+++ app/Actions/Dropshipping/Allegro/User/ProcessShippingRates.php
@@ -20,66 +20,52 @@ class ProcessShippingRates extends RetinaAction
     use WithAttributes;
     use WithActionUpdate;
 
-    public function handle(AllegroUser $allegroUser, array $data): void
+    public function handle(AllegroUser $allegroUser): array
     {
-        $shippingZones = [
-            'SK' => [[1, '2.70'], [2, '2.80'], [5, '2.90'], [15, '3.00'], [30, '3.50']],            // Zone 1
-            'CZ' => [[1, '3.90'], [2, '4.30'], [5, '4.85'], [15, '5.85'], [30, '6.20']],            // Zone 2
-            'PL' => [[1, '4.40'], [2, '4.80'], [5, '5.00'], [10, '6.00'], [15, '7.00'], [30, '7.50']], // Zone 4
-            'HU' => [[1, '4.50'], [2, '4.70'], [5, '4.80'], [10, '5.60'], [30, '6.20']],            // Zone 5
-        ];
-
-        $dispatchCountry = $allegroUser->customerSalesChannel->customer?->address?->country_code ?? 'SK';
-
-        $weightKg = (float) Arr::get($data, 'weight', 1.0);
-
-        $resolveAmount = function (array $brackets, float $weight): ?string {
-            foreach ($brackets as [$upTo, $amount]) {
-                if ($weight <= $upTo) {
-                    return $amount;
-                }
-            }
-            return null;
-        };
-
         try {
-            $deliveryMethods = collect(Arr::get($allegroUser->getDeliveryMethods(), 'deliveryMethods'));
+            $shippingZones = [
+                'SK' => [[1, '2.70'], [2, '2.80'], [5, '2.90'], [15, '3.00'], [30, '3.50']],
+                'CZ' => [[1, '3.90'], [2, '4.30'], [5, '4.85'], [15, '5.85'], [30, '6.20']],
+                'PL' => [[1, '4.40'], [2, '4.80'], [5, '5.00'], [10, '6.00'], [15, '7.00'], [30, '7.50']],
+                'HU' => [[1, '4.50'], [2, '4.70'], [5, '4.80'], [10, '5.60'], [30, '6.20']]
+            ];
 
-            $rates = [];
-            foreach ($shippingZones as $country => $brackets) {
-                $deliveryMethod = $deliveryMethods->firstWhere('destinationCountry', $country);
-                if (! $deliveryMethod) {
-                    continue;
-                }
+            $countryCode = strtoupper((string) $allegroUser->customerSalesChannel->customer?->address?->country_code);
+            $brackets = Arr::get($shippingZones, $countryCode);
+            $amount = $brackets[0][1] ?? null;
 
-                $amount = $resolveAmount($brackets, $weightKg);
-                if ($amount === null) {
-                    continue;
-                }
+            $deliveryMethods = $allegroUser->getDeliveryMethods();
+            $deliveryMethod = collect(Arr::get($deliveryMethods, 'deliveryMethods'))->firstWhere('destinationCountry', $countryCode);
 
-                $firstCurrency = Arr::get($deliveryMethod, 'shippingRatesConstraints.firstItemRate.currency');
-                $nextCurrency  = Arr::get($deliveryMethod, 'shippingRatesConstraints.nextItemRate.currency');
-
-                $rates[] = [
-                    'deliveryMethod'        => ['id' => Arr::get($deliveryMethod, 'id')],
-                    'maxQuantityPerPackage' => 1,
-                    'firstItemRate'         => ['currency' => $firstCurrency, 'amount' => $amount],
-                    'nextItemRate'          => ['currency' => $nextCurrency, 'amount' => '0.00'],
-                ];
-            }
-
-            $shipping = ! empty($rates)
+            $shipping = ($deliveryMethod && $amount !== null)
                 ? $allegroUser->createShippingRates([
-                    'name'            => 'AW-EU-'.$allegroUser->customerSalesChannel->slug.'-'.number_format($weightKg, 3, '.', ''),
-                    'type'            => 'PHYSICAL',
-                    'dispatchCountry' => $dispatchCountry,
-                    'rates'           => $rates,
+                    'name' => 'AW-EU-'.$allegroUser->customerSalesChannel->slug . '-' . $countryCode,
+                    'rates' => [
+                        [
+                            'deliveryMethod' => [
+                                'id' => Arr::get($deliveryMethod, 'id'),
+                            ],
+                            'maxQuantityPerPackage' => 5,
+                            'maxPackageWeight' => [
+                                'value' => 30,
+                                'unit' => 'KILOGRAM'
+                            ],
+                            'firstItemRate' => [
+                                'currency' => Arr::get($deliveryMethod, 'shippingRatesConstraints.firstItemRate.currency'),
+                                'amount' => $amount
+                            ],
+                            'nextItemRate' => [
+                                'currency' => Arr::get($deliveryMethod, 'shippingRatesConstraints.nextItemRate.currency'),
+                                'amount' => '0'
+                            ],
+                        ]
+                    ]
                 ])
                 : [];
         } catch (\Exception $e) {
             $shipping = [];
         }
 
-        data_set($data, 'shipping_id', Arr::get($shipping, 'id'));
+        return $shipping;
     }
 }
