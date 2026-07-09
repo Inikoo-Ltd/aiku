8df506367ed5ca554f6f0e340975cdd15dfa718a
itzArtha <manager.arthachan@gmail.com>
2026-07-09 09:47:39 +0800

    feat: implement UpcomingTransactionsResource with serialization logic for upcoming transactions

---
+++ app/Http/Resources/Ordering/UpcomingTransactionsResource.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 20 Jun 2023 20:33:32 Malaysia Time, Pantai Lembeng, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Http\Resources\Ordering;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $id
+ * @property mixed $customer_id
+ * @property mixed $product_id
+ * @property mixed $order_id
+ * @property mixed $transaction_id
+ * @property mixed $quantity
+ * @property mixed $notes
+ * @property mixed $type
+ * @property mixed $state
+ * @property mixed $product_code
+ * @property mixed $product_name
+ * @property mixed $created_at
+ * @property mixed $updated_at
+ */
+class UpcomingTransactionsResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id'             => $this->id,
+            'customer_id'    => $this->customer_id,
+            'product_id'     => $this->product_id,
+            'product_code'   => $this->product_code,
+            'product_name'   => $this->product_name,
+            'order_id'       => $this->order_id,
+            'transaction_id' => $this->transaction_id,
+            'quantity'       => $this->quantity,
+            'notes'          => $this->notes,
+            'type'           => $this->type,
+            'state'          => $this->state,
+            'created_at'     => $this->created_at,
+            'updated_at'     => $this->updated_at,
+            'update' => [
+                'name'       => 'grp.models.upcoming_transaction.update',
+                'parameters' => [
+                    'upcomingTransaction' => $this->id
+                ]
+            ],
+            'delete' => [
+                'name'       => 'grp.models.upcoming_transaction.delete',
+                'parameters' => [
+                    'upcomingTransaction' => $this->id
+                ]
+            ]
+        ];
+    }
+}
