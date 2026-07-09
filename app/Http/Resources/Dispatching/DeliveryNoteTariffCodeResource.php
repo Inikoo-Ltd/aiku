ed65ad4e1e4ca5f5831c0ee1d2e4d0f95f7e3316
stewicca <stewicalf@gmail.com>
2026-07-09 17:07:22 +0800

    feat: delivery note Tariff codes / Origin tab with column-selectable CSV/XLSX export

---
+++ app/Http/Resources/Dispatching/DeliveryNoteTariffCodeResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\Dispatching;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property string $tariff_code
+ * @property string|null $description
+ * @property string|null $origin
+ * @property bool $dg
+ * @property string|null $un_numbers
+ * @property string|null $parts
+ * @property int $num_parts
+ * @property numeric $units
+ * @property numeric $weight
+ * @property numeric $amount
+ */
+class DeliveryNoteTariffCodeResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'tariff_code' => $this->tariff_code,
+            'description' => $this->description,
+            'origin'      => $this->origin,
+            'dg'          => (bool)$this->dg,
+            'un_numbers'  => $this->un_numbers,
+            'parts'       => $this->parts ? explode(', ', $this->parts) : [],
+            'num_parts'   => (int)$this->num_parts,
+            'units'       => (float)$this->units,
+            'weight'      => (float)$this->weight,
+            'amount'      => (float)$this->amount,
+        ];
+    }
+}
