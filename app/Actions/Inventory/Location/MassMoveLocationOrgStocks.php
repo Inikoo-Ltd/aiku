b850a7acee24f05db3616305bba68291a81f12d8
louis-perez <louisprzn@gmail.com>
2026-07-10 15:07:46 +0800

    Mass Move Location Org stocks

---
+++ app/Actions/Inventory/Location/MassMoveLocationOrgStocks.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Actions\Inventory\Location;
+
+use App\Actions\Inventory\LocationOrgStock\DeleteLocationOrgStock;
+use App\Actions\Inventory\LocationOrgStock\MoveOrgStockToOtherLocation;
+use App\Actions\Inventory\LocationOrgStock\StoreLocationOrgStock;
+use App\Actions\OrgAction;
+use App\Actions\Traits\Authorisations\Inventory\WithWarehouseEditAuthorisation;
+use App\Models\Inventory\Location;
+use Illuminate\Support\Facades\DB;
+use Lorisleiva\Actions\ActionRequest;
+
+class MassMoveLocationOrgStocks extends OrgAction
+{
+    use WithWarehouseEditAuthorisation;
+
+    public function handle(Location $location, array $modelData): Location
+    {
+        $targetLocation = Location::find(data_get($modelData, 'location_id'));
+
+        DB::transaction(function () use ($location, $targetLocation) {
+            foreach ($location->locationOrgStocks as $locationOrgStock) {
+                $targetLocationOrgStock = $targetLocation->locationOrgStocks()->where('org_stock_id', $locationOrgStock->org_stock_id)->first();
+
+                if ($targetLocationOrgStock) {
+                    MoveOrgStockToOtherLocation::make()->action($locationOrgStock, $targetLocationOrgStock, [
+                        'quantity'  => $locationOrgStock->quantity
+                    ]);
+                } else {
+                    StoreLocationOrgStock::make()->action($locationOrgStock->orgStock, $targetLocation, $locationOrgStock->only([
+                        'data', 
+                        'settings', 
+                        'notes', 
+                        'picking_priority', 
+                        'type', 
+                        'quantity'
+                    ]));
+                }
+                DeleteLocationOrgStock::make()->action($locationOrgStock);
+            }
+        });
+
+        $location->refresh();
+
+        return $location;
+    }
+    
+    public function rules()
+    {
+        return [
+            'location_id'   => ['required', 'exists:location,id']
+        ];
+    }
+
+    public function asController(Location $location, ActionRequest $request): Location
+    {
+        $this->initialisationFromWarehouse($location->warehouse, $request);
+
+        return $this->handle($location, $this->validatedData);
+    }
+}
