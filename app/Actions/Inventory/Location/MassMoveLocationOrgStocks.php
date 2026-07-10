e94edb8a0ab5775816548d32fd2fcb1986884ed0
Raul Perusquia <raul@inikoo.com>
2026-07-10 15:14:25 +0800

    feat: add `mass_move_stock` route and implement bulk stock movement logic with optional deletion

--- app/Actions/Inventory/Location/MassMoveLocationOrgStocks.php
+++ app/Actions/Inventory/Location/MassMoveLocationOrgStocks.php
@@ -8,6 +8,7 @@ use App\Actions\Inventory\LocationOrgStock\StoreLocationOrgStock;
 use App\Actions\OrgAction;
 use App\Actions\Traits\Authorisations\Inventory\WithWarehouseEditAuthorisation;
 use App\Models\Inventory\Location;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\ActionRequest;
 
@@ -15,29 +16,34 @@ class MassMoveLocationOrgStocks extends OrgAction
 {
     use WithWarehouseEditAuthorisation;
 
+    /**
+     * @throws \Throwable
+     */
     public function handle(Location $location, array $modelData): Location
     {
         $targetLocation = Location::find(data_get($modelData, 'location_id'));
 
-        DB::transaction(function () use ($location, $targetLocation) {
+        DB::transaction(function () use ($location, $targetLocation, $modelData) {
             foreach ($location->locationOrgStocks as $locationOrgStock) {
                 $targetLocationOrgStock = $targetLocation->locationOrgStocks()->where('org_stock_id', $locationOrgStock->org_stock_id)->first();
 
-                if ($targetLocationOrgStock) {
-                    MoveOrgStockToOtherLocation::make()->action($locationOrgStock, $targetLocationOrgStock, [
-                        'quantity'  => $locationOrgStock->quantity
-                    ]);
-                } else {
-                    StoreLocationOrgStock::make()->action($locationOrgStock->orgStock, $targetLocation, $locationOrgStock->only([
-                        'data', 
-                        'settings', 
-                        'notes', 
-                        'picking_priority', 
-                        'type', 
-                        'quantity'
+                if (!$targetLocationOrgStock) {
+                    $targetLocationOrgStock = StoreLocationOrgStock::make()->action($locationOrgStock->orgStock, $targetLocation, $locationOrgStock->only([
+                        'data',
+                        'settings',
+                        'notes',
+                        'picking_priority',
+                        'type'
                     ]));
                 }
-                DeleteLocationOrgStock::make()->action($locationOrgStock);
+
+                MoveOrgStockToOtherLocation::make()->action($locationOrgStock, $targetLocationOrgStock, [
+                    'quantity' => $locationOrgStock->quantity
+                ]);
+
+                if (Arr::get($modelData, 'remove_after_move', false)) {
+                    DeleteLocationOrgStock::make()->action($locationOrgStock);
+                }
             }
         });
 
@@ -45,14 +51,18 @@ class MassMoveLocationOrgStocks extends OrgAction
 
         return $location;
     }
-    
-    public function rules()
+
+    public function rules(): array
     {
         return [
-            'location_id'   => ['required', 'exists:location,id']
+            'location_id'       => ['required', 'exists:location,id'],
+            'remove_after_move' => ['sometimes', 'nullable', 'boolean']
         ];
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function asController(Location $location, ActionRequest $request): Location
     {
         $this->initialisationFromWarehouse($location->warehouse, $request);
