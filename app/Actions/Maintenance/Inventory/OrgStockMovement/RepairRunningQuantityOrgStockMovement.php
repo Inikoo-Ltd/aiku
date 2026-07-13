753f19f0391ba779d296b819bdcdaefa39e9bda3
Raul Perusquia <raul@inikoo.com>
2026-07-13 15:05:07 +0800

    Add async processing to `RepairRunningQuantityOrgStockMovement` and implement unique job handling.

--- app/Actions/Maintenance/Inventory/OrgStockMovement/RepairRunningQuantityOrgStockMovement.php
+++ app/Actions/Maintenance/Inventory/OrgStockMovement/RepairRunningQuantityOrgStockMovement.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection PhpUnused */
 
 /*
  * Author Louis Perez
@@ -11,32 +12,43 @@ namespace App\Actions\Maintenance\Inventory\OrgStockMovement;
 
 use App\Actions\Inventory\OrgStockMovement\CalculateRunningQuantityOrgStockMovement;
 use App\Models\Inventory\OrgStock;
+use App\Models\Inventory\OrgStockMovement;
 use App\Models\SysAdmin\Organisation;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Lorisleiva\Actions\Concerns\AsAction;
 
-class RepairRunningQuantityOrgStockMovement
+class RepairRunningQuantityOrgStockMovement implements ShouldBeUnique
 {
     use AsAction;
 
-    public function handle(OrgStock $orgStock, Command $command)
+    public string $jobQueue = 'sales_slave_historic';
+
+    public function getJobUniqueId(OrgStock $orgStock): string
     {
+        return $orgStock->id;
+    }
+
+
+    public function handle(OrgStock $orgStock, Command $command): void
+    {
+        /** @var OrgStockMovement $movement */
         foreach (
-            $orgStock->orgStockMovements()->orderBy('date', 'asc')->get() as $movement
+            $orgStock->orgStockMovements()->orderBy('date')->get() as $movement
         ) {
-            $command->info("Repairing: {$orgStock->slug} {$movement->date}");
+            $command->info("Repairing: $orgStock->slug $movement->date");
             CalculateRunningQuantityOrgStockMovement::run($movement);
         }
     }
 
-    public string $commandSignature = 'repair:running_quantity_org_stock_movement {--org_stock_slug=} {--organisation=}';
+    public string $commandSignature = 'repair:running_quantity_org_stock_movement {--org_stock_slug=} {--organisation=} {--async}';
 
-    public function asCommand(Command $command)
+    public function asCommand(Command $command): int
     {
-        $orgStockSlug = $command->option('org_stock_slug');
+        $orgStockSlug     = $command->option('org_stock_slug');
         $organisationSlug = $command->option('organisation');
-        $organisation = null;
-        
+        $organisation     = null;
+
         if ($organisationSlug) {
             $organisation = Organisation::where('slug', $organisationSlug)->first();
         }
@@ -51,11 +63,19 @@ class RepairRunningQuantityOrgStockMovement
             $orgStocks->where('organisation_id', $organisation->id);
         }
 
+        $async = (bool)$command->option('async');
+
         $orgStocks
-            ->chunkById(250, function ($orgStockChunk) use ($command) {
-                foreach($orgStockChunk as $orgStock) {
-                    $this->handle($orgStock, $command);
+            ->chunkById(250, function ($orgStockChunk) use ($command, $async) {
+                foreach ($orgStockChunk as $orgStock) {
+                    if ($async) {
+                        $this->dispatch($orgStock, $command);
+                    } else {
+                        $this->handle($orgStock, $command);
+                    }
                 }
             });
+
+        return 0;
     }
 }
