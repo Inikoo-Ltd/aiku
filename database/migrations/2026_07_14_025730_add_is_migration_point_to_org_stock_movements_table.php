42983bd3f765af1772de3a4344fbcbfc103d10eb
Raul Perusquia <raul@inikoo.com>
2026-07-14 11:15:03 +0800

    Add `is_migration_point` attribute to `OrgStockMovement` and corresponding database migration.

---
+++ database/migrations/2026_07_14_025730_add_is_migration_point_to_org_stock_movements_table.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 14 Jul 2026 11:14:51 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2026, Raul A Perusquia Flores
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+
+    public function up(): void
+    {
+        Schema::table('org_stock_movements', function (Blueprint $table) {
+            $table->boolean('is_migration_point')->default(false);
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('org_stock_movements', function (Blueprint $table) {
+            $table->dropColumn('is_migration_point');
+        });
+    }
+};
