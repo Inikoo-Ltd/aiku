2939d1f555586a27bf8dfc9842bd3dea2e6f6cab
stewicca <stewicalf@gmail.com>
2026-07-16 14:47:34 +0800

    INI-1780 feat(org-stock-showcase): add printable PDF barcode label and show unit barcode only

--- app/Actions/Inventory/OrgStock/UI/GetOrgStockShowcase.php
+++ app/Actions/Inventory/OrgStock/UI/GetOrgStockShowcase.php
@@ -42,6 +42,14 @@ class GetOrgStockShowcase
                 'currency_code'      => $orgStock->organisation->currency->code,
                 'sales_data'         => GetOrgStockTimeSeriesData::run($orgStock),
                 'barcodes'           => GetOrgStockBarcodes::run($orgStock),
+                'label_route'        => [
+                    'name'       => 'grp.org.warehouses.show.inventory.org_stocks.label',
+                    'parameters' => [
+                        'organisation' => $warehouse->organisation->slug,
+                        'warehouse'    => $warehouse->slug,
+                        'orgStock'     => $orgStock->slug,
+                    ],
+                ],
                 'is_quantity_excess' => $orgStock->quantity_status === OrgStockQuantityStatusEnum::EXCESS,
                 'stocks_management'  => [
                     'routes'          => [
