e9e4d54415309d14b5f58a76c3a1dd25a55d2621
itzArtha <manager.arthachan@gmail.com>
2026-07-23 11:52:14 +0800

    enhance: add cloneImages method to handle image copying in CloneProductImagesFromTradeUnits for better code organization and functionality

--- app/Actions/Catalogue/Product/CloneProductImagesFromTradeUnits.php
+++ app/Actions/Catalogue/Product/CloneProductImagesFromTradeUnits.php
@@ -65,6 +65,40 @@ class CloneProductImagesFromTradeUnits implements ShouldBeUnique
         $this->syncProductImages($tradeUnit, $product);
     }
 
+    protected function cloneImages(TradeUnit|MasterProductCategory|MasterAsset|MasterCollection|Model $source, Product|ProductCategory|Collection|Model $target): void
+    {
+        $images   = [];
+        $position = 1;
+
+        foreach ($source->images as $image) {
+            if($source->images()
+                ->where('model_id', $target->id)
+                ->where('model_type', $target::class)
+                ->where('scope', 'photo')
+                ->where('media_id', $image->id)
+                ->where('sub_scope', 'main')
+                ->exists()) {
+                continue;
+            }
+
+            $images[$image->id] = [
+                'is_public'       => true,
+                'scope'           => 'photo',
+                'sub_scope'       => $image->pivot->sub_scope,
+                'caption'         => $image->pivot->caption,
+                'organisation_id' => $target->organisation_id ?? null,
+                'group_id'        => $target->group_id ?? null,
+                'position'        => $position++,
+                'created_at'      => now(),
+                'updated_at'      => now(),
+                'data'            => '{}',
+            ];
+        }
+
+        $target->images()->sync($images);
+    }
+
+
     public string $commandSignature = 'catalogue:product:clone-images-from-trade-units  {parent?} {slug?}';
 
     public function asCommand(Command $command): int
