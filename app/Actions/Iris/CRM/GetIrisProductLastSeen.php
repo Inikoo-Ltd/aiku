79d0d6cb158ed2f7b1a0a6d173f04cd6de48f94c
itzArtha <manager.arthachan@gmail.com>
2026-07-24 10:33:06 +0800

    fix: add customer and cookie as filter

--- app/Actions/Iris/CRM/GetIrisProductLastSeen.php
+++ app/Actions/Iris/CRM/GetIrisProductLastSeen.php
@@ -9,18 +9,23 @@ use App\Models\Web\Webpage;
 use App\Services\QueryBuilder;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Arr;
 use Lorisleiva\Actions\ActionRequest;
 
 class GetIrisProductLastSeen extends IrisAction
 {
     private Webpage $webpage;
 
-    public function handle(Webpage $webpage)
+    public function handle(Webpage $webpage, array $modelData)
     {
         $queryBuilder = QueryBuilder::for(Product::class)
             ->join('product_last_seens', 'product_last_seens.webpage_id', '=', 'products.webpage_id')
             ->leftJoin('webpages', 'webpages.id', '=', 'products.webpage_id')
             ->where('product_last_seens.shop_id', $webpage->shop_id)
+            ->where(function ($query) use ($modelData) {
+                $query->where('customer_id', '=', Arr::get($modelData, 'user_id'))
+                    ->orWhere('cookie_id', '=', Arr::get($modelData, 'user_id'));
+            })
             ->where('product_last_seens.webpage_id', '=', $webpage->id);
 
         $queryBuilder->select([
@@ -44,7 +49,7 @@ class GetIrisProductLastSeen extends IrisAction
         $this->webpage = $webpage;
         $this->initialisation($request);
 
-        return $this->handle($webpage);
+        return $this->handle($webpage, $request->all());
     }
 
     public function jsonResponse($lastSeen): AnonymousResourceCollection
