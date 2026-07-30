3aa59a52b04a8bd5a7cb9817c42c0c109a6ba092
stewicca <stewicalf@gmail.com>
2026-07-30 15:06:54 +0800

    refactor: 💅 redesign supplier showcase and limit supplier tabs to showcase and history

--- app/Enums/UI/SupplyChain/SupplierTabsEnum.php
+++ app/Enums/UI/SupplyChain/SupplierTabsEnum.php
@@ -16,62 +16,57 @@ enum SupplierTabsEnum: string
     use EnumHelperTrait;
     use HasTabs;
 
-
     case SHOWCASE = 'showcase';
-    case PURCHASES_SALES = 'purchase_sales';
-    case HISTORY = 'history';
+    case HISTORY  = 'history';
 
-
-    case SYSTEM_USERS = 'system_users';
-
-    case ATTACHMENTS = 'attachments';
-    case IMAGES = 'images';
-    case FEEDBACKS = 'feedbacks';
-
+    // case PURCHASES_SALES = 'purchase_sales';
+    // case ATTACHMENTS     = 'attachments';
+    // case SYSTEM_USERS    = 'system_users';
+    // case IMAGES          = 'images';
+    // case FEEDBACKS       = 'feedbacks';
 
     public function blueprint(): array
     {
         return match ($this) {
-            SupplierTabsEnum::PURCHASES_SALES => [
-                'title' => __('Purchases/Sales'),
-                'icon'  => 'fal fa-money-bill',
-            ],
             SupplierTabsEnum::SHOWCASE => [
                 'title' => __('Supplier'),
                 'icon'  => 'fas fa-info-circle',
-            ],
-
-
-            SupplierTabsEnum::FEEDBACKS => [
-                'title' => __('Issues'),
-                'icon'  => 'fal fa-poop',
-                'type'  => 'icon',
-                'align' => 'right',
-            ],
-            SupplierTabsEnum::IMAGES => [
-                'title' => __('Images'),
-                'icon'  => 'fal fa-camera-retro',
-                'type'  => 'icon',
-                'align' => 'right',
-            ],
-            SupplierTabsEnum::ATTACHMENTS => [
-                'title' => __('Attachments'),
-                'icon'  => 'fal fa-paperclip',
-                'type'  => 'icon',
-                'align' => 'right',
-            ],
-            SupplierTabsEnum::SYSTEM_USERS => [
-                'title' => __('System Users'),
-                'icon'  => 'fal fa-paper-plane',
-                'type'  => 'icon',
-                'align' => 'right',
             ],
             SupplierTabsEnum::HISTORY => [
                 'title' => __('Changelog'),
                 'icon'  => 'fal fa-clock',
                 'type'  => 'icon',
-                'align' => 'right'
+                'align' => 'right',
             ],
+
+            // SupplierTabsEnum::PURCHASES_SALES => [
+            //     'title' => __('Purchases/Sales'),
+            //     'icon'  => 'fal fa-money-bill',
+            // ],
+            // SupplierTabsEnum::ATTACHMENTS => [
+            //     'title' => __('Attachments'),
+            //     'icon'  => 'fal fa-paperclip',
+            //     'type'  => 'icon',
+            //     'align' => 'right',
+            // ],
+            // SupplierTabsEnum::SYSTEM_USERS => [
+            //     'title' => __('System Users'),
+            //     'icon'  => 'fal fa-paper-plane',
+            //     'type'  => 'icon',
+            //     'align' => 'right',
+            // ],
+            // SupplierTabsEnum::IMAGES => [
+            //     'title' => __('Images'),
+            //     'icon'  => 'fal fa-camera-retro',
+            //     'type'  => 'icon',
+            //     'align' => 'right',
+            // ],
+            // SupplierTabsEnum::FEEDBACKS => [
+            //     'title' => __('Issues'),
+            //     'icon'  => 'fal fa-poop',
+            //     'type'  => 'icon',
+            //     'align' => 'right',
+            // ],
         };
     }
 }
