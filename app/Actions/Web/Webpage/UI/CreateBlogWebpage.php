d4e29279844f525052ba3056c32a799e46b0a2eb
YudhistiraA <aryarajasa0@gmail.com>
2026-08-05 09:50:45 +0800

    feat: add 'Blog' option to blog category selection in Create and Edit webpages

--- app/Actions/Web/Webpage/UI/CreateBlogWebpage.php
+++ app/Actions/Web/Webpage/UI/CreateBlogWebpage.php
@@ -103,6 +103,7 @@ class CreateBlogWebpage extends OrgAction
                                     'placeholder' => __('Select a blog category'),
                                     'mode'        => 'single',
                                     'options'     => [
+                                        ['value' => 'blog', 'label' => __('Blog')],
                                         ['value' => 'davids_travel_blog', 'label' => __("David's Travel Blog")],
                                         ['value' => 'tips', 'label' => __('Tips')],
                                     ],
