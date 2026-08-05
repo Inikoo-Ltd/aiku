ea1ed10788ab9be759dc468687f5ae0a57ef0fcd
itzArtha <manager.arthachan@gmail.com>
2026-08-05 10:09:12 +0800

    revert back

--- app/Actions/Catalogue/Product/StoreProductFromMasterProduct.php
+++ app/Actions/Catalogue/Product/StoreProductFromMasterProduct.php
@@ -14,13 +14,8 @@ use App\Actions\Helpers\Translations\TranslateModel;
 use App\Actions\Web\Webpage\PublishWebpage;
 use App\Enums\Catalogue\Product\ProductStateEnum;
 use App\Enums\Catalogue\Product\ProductStatusEnum;
-use App\Enums\Catalogue\Shop\ShopEngineEnum;
-use App\Enums\Catalogue\Shop\ShopTypeEnum;
 use App\Models\Catalogue\Product;
-use App\Models\Catalogue\ProductCategory;
-use App\Models\Catalogue\Shop;
 use App\Models\Masters\MasterAsset;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Arr;
 
 class StoreProductFromMasterProduct extends OrgAction
@@ -36,130 +31,99 @@ class StoreProductFromMasterProduct extends OrgAction
             return;
         }
 
-        foreach ($masterAsset->masterFamily->productCategories as $productCategory) {
-            $this->storeInShop($masterAsset, $modelData, $productCategory->shop, $productCategory, $generateVariant, $ignoreCreateWebpage);
-        }
+        $productCategories = $masterAsset->masterFamily->productCategories;
 
-        foreach ($this->getExternalShops($masterAsset) as $externalShop) {
-            $this->storeInShop($masterAsset, $modelData, $externalShop, null, generateVariant: false, ignoreCreateWebpage: true);
-        }
-    }
+        if ($productCategories) {
+            foreach ($productCategories as $productCategory) {
+                $shop = $productCategory->shop;
+                if (isset($modelData['shop_products']) && !array_key_exists($shop->id, $modelData['shop_products'])) {
+                    continue;
+                }
 
-    /**
-     * External shops carry no family mirroring the master one, they hang straight off the master shop.
-     */
-    public function getExternalShops(MasterAsset $masterAsset): Collection
-    {
-        if (!$masterAsset->masterShop) {
-            return new Collection();
-        }
+                $shopProductData = isset($modelData['shop_products'][$shop->id]) ? $modelData['shop_products'][$shop->id] : [];
 
-        return $masterAsset->masterShop->shops()
-            ->where('type', ShopTypeEnum::EXTERNAL)
-            ->where('engine', ShopEngineEnum::FAIRE)
-            ->get();
-    }
+                $createInShop = Arr::get($shopProductData, 'create_in_shop');
 
-    /**
-     * @throws \Throwable
-     */
-    public function storeInShop(
-        MasterAsset $masterAsset,
-        array $modelData,
-        Shop $shop,
-        ?ProductCategory $productCategory,
-        bool $generateVariant,
-        bool $ignoreCreateWebpage
-    ): void {
-        if (isset($modelData['shop_products']) && !array_key_exists($shop->id, $modelData['shop_products'])) {
-            return;
-        }
+                if ($createInShop == 'Yes') {
+                    $price = $masterAsset->getPriceFromCurrency($shop->currency) ;
+                    $rrp   = $masterAsset->getRrpFromCurrency($shop->currency);
 
-        $shopProductData = $modelData['shop_products'][$shop->id] ?? [];
+                    $tradeUnits = [];
+                    foreach ($masterAsset->tradeUnits as $tradeUnit) {
+                        $tradeUnits[$tradeUnit->id] = [
+                            'id'       => $tradeUnit->id,
+                            'quantity' => $tradeUnit->pivot->quantity,
+                        ];
+                    }
 
-        if (Arr::get($shopProductData, 'create_in_shop') != 'Yes') {
-            return;
-        }
+                    $isMain = $masterAsset->is_main;
 
-        $price = $masterAsset->getPriceFromCurrency($shop->currency);
-        $rrp   = $masterAsset->getRrpFromCurrency($shop->currency);
+                    $data   = [
+                        'code'              => $masterAsset->code,
+                        'name'              => $masterAsset->name,
+                        'description'       => $masterAsset->description,
+                        'description_title' => $masterAsset->description_title,
+                        'description_extra' => $masterAsset->description_extra,
+                        'price'             => $price,
+                        'rrp'               => $rrp,
+                        'unit'              => $masterAsset->unit,
+                        'units'             => $masterAsset->units,
+                        'trade_units'       => $tradeUnits,
+                        'master_product_id' => $masterAsset->id,
+                        'state'             => ProductStateEnum::ACTIVE,
+                        'status'            => ProductStatusEnum::FOR_SALE,
+                        'is_main'           => $isMain,
+                        'is_for_sale'       => data_get($modelData, 'is_for_sale', $masterAsset->is_for_sale),
+                        'is_minion_variant' => !$isMain,
+                    ];
 
-        $tradeUnits = [];
-        foreach ($masterAsset->tradeUnits as $tradeUnit) {
-            $tradeUnits[$tradeUnit->id] = [
-                'id'       => $tradeUnit->id,
-                'quantity' => $tradeUnit->pivot->quantity,
-            ];
-        }
+                    if (count($tradeUnits) > 1) {
+                        data_set($data, 'gross_weight', $masterAsset->gross_weight);
+                        data_set($data, 'marketing_weight', $masterAsset->marketing_weight);
+                    }
 
-        $isMain = $masterAsset->is_main;
+                    $product = Product::where('shop_id', $shop->id)
+                        ->whereRaw("lower(code) = lower(?)", [$masterAsset->code])
+                        ->first();
+                    if ($product) {
+                        data_set($data, 'family_id', $productCategory->id);
+                        data_set($data, 'trade_units', $tradeUnits);
 
-        $data = [
-            'code'              => $masterAsset->code,
-            'name'              => $masterAsset->name,
-            'description'       => $masterAsset->description,
-            'description_title' => $masterAsset->description_title,
-            'description_extra' => $masterAsset->description_extra,
-            'price'             => $price,
-            'rrp'               => $rrp,
-            'unit'              => $masterAsset->unit,
-            'units'             => $masterAsset->units,
-            'trade_units'       => $tradeUnits,
-            'master_product_id' => $masterAsset->id,
-            'state'             => ProductStateEnum::ACTIVE,
-            'status'            => ProductStatusEnum::FOR_SALE,
-            'is_main'           => $isMain,
-            'is_for_sale'       => data_get($modelData, 'is_for_sale', $masterAsset->is_for_sale),
-            'is_minion_variant' => !$isMain,
-        ];
+                        $this->updateFoundProduct($product, $data, ($isMain && !$ignoreCreateWebpage));
 
-        if (count($tradeUnits) > 1) {
-            data_set($data, 'gross_weight', $masterAsset->gross_weight);
-            data_set($data, 'marketing_weight', $masterAsset->marketing_weight);
-        }
+                        continue;
+                    }
+                    $product = StoreProduct::run($productCategory, $data);
+                    $product->refresh();
+                    CloneProductImagesFromTradeUnits::run($product);
+                    $product->refresh();
 
-        $product = Product::where('shop_id', $shop->id)
-            ->whereRaw("lower(code) = lower(?)", [$masterAsset->code])
-            ->first();
-        if ($product) {
-            if ($productCategory) {
-                data_set($data, 'family_id', $productCategory->id);
+                    if ($isMain && !$ignoreCreateWebpage) {
+                        $webpage = StoreProductWebpage::run($product);
+                        PublishWebpage::make()->action($webpage, [
+                            'comment' => 'first publish'
+                        ]);
+                    }
+
+                    if ($generateVariant) {
+                        $product = $this->setVariantData($product, $masterAsset);
+                    }
+
+
+                    TranslateModel::dispatch(
+                        model: $product,
+                        translationData: Arr::only($data, [
+                            'unit',
+                            'name',
+                            'description',
+                            'description_title',
+                            'description_extra'
+                        ]),
+                        overwrite: true
+                    );
+                }
             }
-            data_set($data, 'trade_units', $tradeUnits);
-
-            $this->updateFoundProduct($product, $data, ($isMain && !$ignoreCreateWebpage));
-
-            return;
         }
-
-        $product = StoreProduct::run($productCategory ?? $shop, $data);
-        $product->refresh();
-        CloneProductImagesFromTradeUnits::run($product);
-        $product->refresh();
-
-        if ($isMain && !$ignoreCreateWebpage) {
-            $webpage = StoreProductWebpage::run($product);
-            PublishWebpage::make()->action($webpage, [
-                'comment' => 'first publish'
-            ]);
-        }
-
-        if ($generateVariant) {
-            $product = $this->setVariantData($product, $masterAsset);
-        }
-
-
-        TranslateModel::dispatch(
-            model: $product,
-            translationData: Arr::only($data, [
-                'unit',
-                'name',
-                'description',
-                'description_title',
-                'description_extra'
-            ]),
-            overwrite: true
-        );
     }
 
     /**
@@ -184,7 +148,7 @@ class StoreProductFromMasterProduct extends OrgAction
                 'variant_id' => $variant->id,
             ]);
             $leader = $masterAsset->masterVariant->leaderMasterProduct->products()->where('shop_id', $product->shop->id)->first();
-            if ($leader && $leader->id == $product->id) {
+            if ($leader->id == $product->id) {
                 $product->update([
                     'is_main'           => true,
                     'is_minion_variant' => false,
