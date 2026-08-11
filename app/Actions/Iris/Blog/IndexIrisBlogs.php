28346d2cabb96ca03a2820f166711fcf336dcf28
louis-perez <louisprzn@gmail.com>
2026-08-11 10:56:51 +0800

    Add missing sub_type

--- app/Actions/Iris/Blog/IndexIrisBlogs.php
+++ app/Actions/Iris/Blog/IndexIrisBlogs.php
@@ -24,8 +24,10 @@ class IndexIrisBlogs extends IrisAction
 
     public const SUB_TYPES = [
         WebpageSubTypeEnum::BLOG,
-        WebpageSubTypeEnum::DAVIDS_TRAVEL_BLOG,
-        WebpageSubTypeEnum::TIPS,
+        WebpageSubTypeEnum::DAVID_AW_NEWS,
+        WebpageSubTypeEnum::PRODUCT_GUIDES,
+        WebpageSubTypeEnum::BUSINESS_TIPS,
+        WebpageSubTypeEnum::INSIGHT,
     ];
 
     /**
