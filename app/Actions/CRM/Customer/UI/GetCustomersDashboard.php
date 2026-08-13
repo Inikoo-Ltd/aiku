9b67f18765ebb04b9d91974c9adbe6489ed67ab1
stewicca <stewicalf@gmail.com>
2026-08-13 14:12:19 +0800

    INI-1926 feat(crm): add period filter, newsletter revenue and clickable segments to customer segmentation

--- app/Actions/CRM/Customer/UI/GetCustomersDashboard.php
+++ app/Actions/CRM/Customer/UI/GetCustomersDashboard.php
@@ -9,16 +9,24 @@
 
 namespace App\Actions\CRM\Customer\UI;
 
+use App\Actions\Helpers\Dashboard\DashboardIntervalFilters;
+use App\Actions\Traits\Dashboards\WithDashboardIntervalOption;
+use App\Actions\Traits\Dashboards\WithPerformanceDateResolution;
 use App\Enums\CRM\Customer\CustomerStateEnum;
+use App\Enums\DateIntervals\DateIntervalEnum;
 use App\Enums\UI\CRM\CustomersTabsEnum;
 use App\Models\Catalogue\Shop;
+use Illuminate\Support\Arr;
+use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsObject;
 
 class GetCustomersDashboard
 {
     use AsObject;
+    use WithDashboardIntervalOption;
+    use WithPerformanceDateResolution;
 
-    public function handle(Shop $parent): array
+    public function handle(Shop $parent, ?ActionRequest $request = null): array
     {
         $stats = [];
 
@@ -44,9 +52,27 @@ class GetCustomersDashboard
             ];
         }
 
+        $userSettings = $request?->user()?->settings ?? [];
+        $interval     = $this->resolveInterval($request, $userSettings);
+        [$from, $to]  = $this->resolvePerformanceDates($interval, $userSettings);
+
         return array_merge(
-            ['prospectStats' => $stats],
-            GetCustomerRfmComparison::run($parent->id) ?? []
+            [
+                'prospectStats' => $stats,
+                'intervals'     => [
+                    'options'        => $this->dashboardIntervalOption(),
+                    'value'          => $interval->value,
+                    'range_interval' => DashboardIntervalFilters::run($interval, $userSettings)
+                ]
+            ],
+            GetCustomerRfmComparison::run($parent, $from, $to)
         );
+    }
+
+    protected function resolveInterval(?ActionRequest $request, array $userSettings): DateIntervalEnum
+    {
+        $savedInterval = DateIntervalEnum::tryFrom(Arr::get($userSettings, 'selected_interval', 'all')) ?? DateIntervalEnum::ALL;
+
+        return DateIntervalEnum::tryFrom((string) $request?->query('interval')) ?? $savedInterval;
     }
 }
