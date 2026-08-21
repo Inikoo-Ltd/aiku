2e421bf7c85e62905377d29d607b3bce10f66cd8
stewicca <stewicalf@gmail.com>
2026-08-21 16:08:29 +0800

    chore(comms): disable scheduled abandoned cart reminder bulk runs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -588,14 +588,14 @@ class Kernel extends ConsoleKernel
                 scheduledAt: now()->format('H:i')
             );
 
-            $this->logSchedule(
-                $schedule->job(RunAbandonedCartReminderEmailBulkRuns::makeJob())->dailyAt('15:00')->timezone('UTC')->withoutOverlapping()->onOneServer()->sentryMonitor(
-                    monitorSlug: 'RunAbandonedCartReminderEmailBulkRuns',
-                ),
-                name: 'RunAbandonedCartReminderEmailBulkRuns',
-                type: 'job',
-                scheduledAt: now()->format('H:i')
-            );
+            // $this->logSchedule(
+            //     $schedule->job(RunAbandonedCartReminderEmailBulkRuns::makeJob())->dailyAt('15:00')->timezone('UTC')->withoutOverlapping()->onOneServer()->sentryMonitor(
+            //         monitorSlug: 'RunAbandonedCartReminderEmailBulkRuns',
+            //     ),
+            //     name: 'RunAbandonedCartReminderEmailBulkRuns',
+            //     type: 'job',
+            //     scheduledAt: now()->format('H:i')
+            // );
 
             $this->logSchedule(
                 $schedule->job(RunCheckoutAbandonmentScan::makeJob())->hourly()->timezone('UTC')->withoutOverlapping()->onOneServer()->sentryMonitor(
