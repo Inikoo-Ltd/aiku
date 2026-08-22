25312294b075658306e6133a8f70856da01fbeed
Raul Perusquia <raul@inikoo.com>
2026-08-23 00:52:55 +0200

    ⬜ style(chat): plain white page side in theme swatches

--- resources/js/Components/Forms/Fields/ChatTheme.vue
+++ resources/js/Components/Forms/Fields/ChatTheme.vue
@@ -24,11 +24,7 @@ const onClickTheme = (key: string) => {
                 @click="() => onClickTheme(key as string)"
                 class="relative h-20 aspect-[16/9] w-fit flex ring-1 ring-gray-300 hover:ring-2 hover:ring-gray-500 shadow rounded overflow-hidden cursor-pointer bg-white"
             >
-                <div class="w-1/3 h-full bg-white border-r border-gray-200 flex flex-col gap-1 justify-center px-1.5">
-                    <div class="h-0.5 w-6 rounded bg-gray-300" />
-                    <div class="h-0.5 w-5 rounded bg-gray-200" />
-                    <div class="h-0.5 w-4 rounded bg-gray-200" />
-                </div>
+                <div class="w-1/3 h-full bg-white border-r border-gray-200" />
                 <div class="flex-1 h-full flex flex-col justify-center gap-1 px-2" :style="{backgroundColor: theme.bg}">
                     <div class="h-0.5 w-8 rounded" :style="{backgroundColor: theme.text}" />
                     <div class="h-0.5 w-6 rounded" :style="{backgroundColor: theme.muted}" />
