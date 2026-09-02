bc247405a9c87d9ef59b8f7d02672807cfe63f75
Raul Perusquia <raul@inikoo.com>
2026-09-02 15:48:19 +0100

    💄 production positions ordered by rank: floor supervisor, mix preparer, foreman, operative

--- resources/js/Components/Forms/Fields/EmployeePosition.vue
+++ resources/js/Components/Forms/Fields/EmployeePosition.vue
@@ -468,18 +468,18 @@ const optionsJob = reactive<optionsJob>({
                 number_employees: props.options.positions.data.find(position => position.slug == "prod-p")?.number_employees || 0
             },
             {
-                slug: "prod-c",
-                grade: "clerk",
-                label: trans("Operative"),
-                optionsType: ["productions"],
-                number_employees: props.options.positions.data.find(position => position.slug == "prod-c")?.number_employees || 0
-            },
-            {
                 slug: "prod-d",
                 grade: "clerk",
                 label: trans("Foreman"),
                 optionsType: ["productions"],
                 number_employees: props.options.positions.data.find(position => position.slug == "prod-d")?.number_employees || 0
+            },
+            {
+                slug: "prod-c",
+                grade: "clerk",
+                label: trans("Operative"),
+                optionsType: ["productions"],
+                number_employees: props.options.positions.data.find(position => position.slug == "prod-c")?.number_employees || 0
             }
         ],
         isHide: productionsLength < 1
