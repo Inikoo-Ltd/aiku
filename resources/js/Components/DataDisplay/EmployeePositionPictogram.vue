45eed4ec0b0df5eb563c233ed18e577fca72b12c
Raul Perusquia <raul@inikoo.com>
2026-09-02 15:48:29 +0100

    💄 employee position pictogram: add foreman

--- resources/js/Components/DataDisplay/EmployeePositionPictogram.vue
+++ resources/js/Components/DataDisplay/EmployeePositionPictogram.vue
@@ -379,6 +379,12 @@ const optionsJob = reactive<optionsJob>({
                 number_employees: props.options.positions.data.find(position => position.slug == "prod-p")?.number_employees || 0
             },
             {
+                slug: "prod-d",
+                grade: "clerk",
+                label: trans("Foreman"),
+                number_employees: props.options.positions.data.find(position => position.slug == 'prod-d')?.number_employees || 0,
+            },
+            {
                 slug: "prod-c",
                 grade: "clerk",
                 label: trans("Operative"),
