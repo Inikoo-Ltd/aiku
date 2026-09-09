882a55103a7a75a979c1e0acfb28cdfe45d4696a
Rifqi <rifqitaufiqurrohman1@gmail.com>
2026-09-09 12:21:02 +0800

    fix: refactor product offer handling

--- app/Actions/Discounts/OfferCampaign/UI/OfferCampaignProductOffersTrait.php
+++ app/Actions/Discounts/OfferCampaign/UI/OfferCampaignProductOffersTrait.php
@@ -48,8 +48,15 @@ trait OfferCampaignProductOffersTrait
                     ])
                 ],
                 'shop_data' => [
+                    'id'            => $offerCampaign->shop_id,
                     'slug'          => $offerCampaign->shop->slug,
                     'currency_code' => $offerCampaign->shop->currency->code,
+                    'organisation'  => $offerCampaign->shop->organisation->slug,
+                    'offercampaign' => $offerCampaign->slug,
+                    'default_dates' => [
+                        'start' => now()->toDateString(),
+                        'end'   => now()->addDays(7)->toDateString(),
+                    ],
                 ],
                 OfferCampaignTabsEnum::OVERVIEW->value => $this->tab == OfferCampaignTabsEnum::OVERVIEW->value ?
                     fn () => GetOfferCampaignOverview::run($offerCampaign)
